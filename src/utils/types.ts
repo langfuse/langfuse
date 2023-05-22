de646ecbbe52fc9d3cf4fa1b0aaa9a57dd869a63
Marc Klingen <git@marcklingen.com>
2023-05-22 11:39:07 +0200

    Add typing of observations

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,7 +6,19 @@ export type NestedObservation = Observation & {
 
 // TODO: Marc add types for span and event
 
+export type TypedObservation = Event | Span | LlmCall;
+
+export type Event = Observation & {
+  type: "EVENT";
+};
+
+export type Span = Observation & {
+  type: "SPAN";
+  endTime: Date; // not null
+};
+
 export type LlmCall = Observation & {
+  type: "LLMCALL";
   attributes: {
     prompt?: string;
     completion?: string;
