414d333886d8f7ccd6812c32eb447b67017f0ffa
Marc Klingen <git@marcklingen.com>
2023-05-26 15:00:53 +0200

    Show as tab on /traces

--- src/pages/traces.tsx
+++ src/pages/traces.tsx
@@ -9,6 +9,17 @@ import {
 } from "@mui/x-data-grid";
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../components/ui/tabs";
+import { type RouterOutput } from "../utils/types";
+import { Button } from "../components/ui/button";
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+import ObservationDisplay from "../components/observationDisplay";
 
 interface TraceRowData {
   id: string;
@@ -76,12 +87,37 @@ export default function Traces() {
   return (
     <>
       <Header title="Traces" live />
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        slots={{ toolbar: GridToolbar }}
-        loading={traces.isLoading}
-      />
+      <Tabs defaultValue="table">
+        <TabsList>
+          <TabsTrigger value="table">Table</TabsTrigger>
+          <TabsTrigger value="sidebyside">Side-by-side</TabsTrigger>
+        </TabsList>
+        <TabsContent value="table">
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            slots={{ toolbar: GridToolbar }}
+            loading={traces.isLoading}
+            autoHeight
+          />
+        </TabsContent>
+        <TabsContent value="sidebyside">
+          <div className="relative flex max-w-full flex-row gap-2 overflow-x-scroll pb-3">
+            {traces.data?.map((trace) => (
+              <Single key={trace.id} trace={trace} />
+            ))}
+            {traces.data?.map((trace) => (
+              <Single key={trace.id} trace={trace} />
+            ))}
+            {traces.data?.map((trace) => (
+              <Single key={trace.id} trace={trace} />
+            ))}
+            {traces.data?.map((trace) => (
+              <Single key={trace.id} trace={trace} />
+            ))}
+          </div>
+        </TabsContent>
+      </Tabs>
     </>
   );
 }
@@ -89,3 +125,27 @@ export default function Traces() {
 function lastCharacters(str: string, n: number) {
   return str.substring(str.length - n);
 }
+
+const Single = (props: { trace: RouterOutput["traces"]["all"][number] }) => {
+  const { trace } = props;
+
+  if (trace.nestedObservation)
+    return (
+      <div className="w-[550px] flex-none rounded-md border px-3">
+        <div className="mt-4 font-bold">Trace</div>
+        <Button variant="ghost" size="sm" asChild>
+          <Link href={`/traces/${trace.id}`}>
+            {trace.id}
+            <ArrowUpRight className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
+        <div className="mt-4 text-sm font-bold">Timestamp</div>
+        <div>{trace.timestamp.toLocaleString()}</div>
+        <div className="mt-4 text-sm font-bold">Name</div>
+        <div>{trace.name}</div>
+        <div className="mt-4 text-sm font-bold">Observations:</div>
+        <ObservationDisplay key={trace.id} obs={trace.nestedObservation} />
+      </div>
+    );
+  else return null;
+};
