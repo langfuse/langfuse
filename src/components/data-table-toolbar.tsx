f6ca7b0bab450b62329f155f5b7bdab51001a81e
Maximilian Deichmann <m.deichmann@tum.de>
2023-05-30 12:45:01 +0200

    New data tables (#5)

---
+++ src/components/data-table-toolbar.tsx
@@ -0,0 +1,60 @@
+import { type ColumnDef } from "@tanstack/react-table";
+import { X } from "lucide-react";
+
+import { Button } from "@/src/components/ui/button";
+import { DataTableFacetedFilter } from "./data-table-faceted-filter";
+import { type TraceFilterInput, type TraceRowOptions } from "../pages/traces";
+
+interface DataTableToolbarProps<TData, TValue> {
+  columnDefs: ColumnDef<TData, TValue>[];
+  options: TraceRowOptions[];
+  queryOptions: TraceFilterInput;
+  updateQueryOptions: (options: TraceFilterInput) => void;
+}
+
+export function DataTableToolbar<TData, TValue>({
+  columnDefs,
+  options,
+  queryOptions,
+  updateQueryOptions,
+}: DataTableToolbarProps<TData, TValue>) {
+  const isFiltered = queryOptions.name !== null;
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-1 items-center space-x-2">
+        {columnDefs.map((column) => {
+          const columnOptions = options.find(
+            (o) =>
+              o.columnId.toLowerCase() === column.meta?.label?.toLowerCase()
+          );
+          return column.enableColumnFilter && columnOptions ? (
+            <DataTableFacetedFilter
+              columnDef={column}
+              title={column.meta?.label}
+              queryOptions={queryOptions}
+              options={columnOptions}
+            />
+          ) : undefined;
+        })}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() =>
+              updateQueryOptions({
+                attribute: {},
+                name: null,
+                id: null,
+                status: null,
+              })
+            }
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <X className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
