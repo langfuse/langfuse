2f09549e13a78763802768bc2c51f923545997a1
Marc Klingen <git@marcklingen.com>
2023-07-10 17:19:57 +0300

    prettier

--- src/env.mjs
+++ src/env.mjs
@@ -16,9 +16,12 @@ export const env = createEnv({
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => (process.env.VERCEL_URL && process.env.VERCEL_URL !== "") ? process.env.VERCEL_URL : str,
+      (str) =>
+        process.env.VERCEL_URL && process.env.VERCEL_URL !== ""
+          ? process.env.VERCEL_URL
+          : str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string().min(1) : z.string().url(),
+      process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   },
