cb993cad4b04d70491eb085fa495d2ba59c737ec
Marc Klingen <git@marcklingen.com>
2023-07-18 17:00:15 +0200

    push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,33 +9,78 @@ import { api } from "@/src/utils/api";
 import "@/src/styles/globals.css";
 import Layout from "@/src/components/layouts/layout";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+// Check that PostHog is client-side (used to handle Next.js SSR) and that env vars are set
+if (
+  typeof window !== "undefined" &&
+  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+  process.env.NEXT_PUBLIC_POSTHOG_HOST
+) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <SessionProvider session={session} refetchOnWindowFocus={true}>
-      <Layout>
-        <Component {...pageProps} />
-        <SentryUserManager />
-      </Layout>
-    </SessionProvider>
+    <PostHogProvider client={posthog}>
+      <SessionProvider session={session} refetchOnWindowFocus={true}>
+        <Layout>
+          <Component {...pageProps} />
+          <UserTracking />
+        </Layout>
+      </SessionProvider>
+    </PostHogProvider>
   );
 };
 
 export default api.withTRPC(MyApp);
 
-function SentryUserManager() {
+function UserTracking() {
   const session = useSession();
 
   useEffect(() => {
     if (session.status === "authenticated" && session.data) {
+      // PostHog
+      posthog.identify(session.data.user?.id ?? undefined, {
+        email: session.data.user?.email ?? undefined,
+        name: session.data.user?.name ?? undefined,
+        featureFlags: session.data.user?.featureFlags ?? undefined,
+        projects: session.data.user?.projects ?? undefined,
+      });
+      // Sentry
       setUser({
         email: session.data.user?.email ?? undefined,
         id: session.data.user?.id ?? undefined,
       });
     } else {
+      // PostHog
+      posthog.reset();
+      // Sentry
       setUser(null);
     }
   }, [session]);
