0bc90dfe6d9549c1d16125f1b7335250300350da
Marc Klingen <git@marcklingen.com>
2023-07-24 21:07:04 +0200

    GET traces/{id}

---
+++ src/pages/api/public/traces/[traceId].ts
@@ -0,0 +1,75 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { z } from "zod";
+import { cors, runMiddleware } from "@/src/features/publicApi/server/cors";
+import { prisma } from "@/src/server/db";
+import { verifyAuthHeaderAndReturnScope } from "@/src/features/publicApi/server/apiAuth";
+
+const GetTraceSchema = z.object({
+  traceId: z.string(),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await runMiddleware(req, res, cors);
+
+  if (req.method !== "GET") {
+    console.error(req.method, req.body, req.query);
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  // CHECK AUTH
+  const authCheck = await verifyAuthHeaderAndReturnScope(
+    req.headers.authorization
+  );
+  if (!authCheck.validKey)
+    return res.status(401).json({
+      success: false,
+      message: authCheck.error,
+    });
+  // END CHECK AUTH
+
+  try {
+    console.log("Trying to get trace:", req.body, req.query);
+
+    const { traceId } = GetTraceSchema.parse(req.query);
+
+    // CHECK ACCESS SCOPE
+    if (authCheck.scope.accessLevel !== "all") {
+      return res.status(401).json({
+        success: false,
+        message:
+          "Access denied - need to use basic auth with secret key to GET traces",
+      });
+    }
+    // END CHECK ACCESS SCOPE
+
+    const trace = await prisma.trace.findFirst({
+      where: {
+        id: traceId,
+        projectId: authCheck.scope.projectId,
+      },
+      include: {
+        observations: true,
+        scores: true,
+      },
+    });
+    if (!trace) {
+      return res.status(404).json({
+        success: false,
+        message: "Trace not found or no access with credentials",
+      });
+    }
+    return res.status(200).json(trace);
+  } catch (error: unknown) {
+    console.error(error);
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+    res.status(400).json({
+      success: false,
+      message: "Invalid request data",
+      error: errorMessage,
+    });
+  }
+}
