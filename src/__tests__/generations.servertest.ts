b33b35fa3d50619bf2087679764238e15c75b77a
Max Deichmann <m.deichmann@tum.de>
2023-08-10 19:18:15 +0200

    adding more tests

--- src/__tests__/generations.servertest.ts
+++ src/__tests__/generations.servertest.ts
@@ -77,6 +77,71 @@ describe("/api/public/generations API Endpoint", () => {
     expect(dbGeneration?.version).toBe("2.0.0");
   });
 
+  it("should create generation after trace based on externalId", async () => {
+    await pruneDatabase();
+
+    const traceId = uuidv4();
+
+    await makeAPICall("POST", "/api/public/traces", {
+      externalId: traceId,
+      name: "trace-name",
+      userId: "user-1",
+      projectId: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+      metadata: { key: "value" },
+      release: "1.0.0",
+      version: "2.0.0",
+    });
+
+    const dbTrace = await prisma.trace.findMany({
+      where: {
+        externalId: traceId,
+      },
+    });
+
+    expect(dbTrace.length).toBeGreaterThan(0);
+    expect(dbTrace[0]?.externalId).toBe(traceId);
+    expect(dbTrace[0]?.id).not.toBe(traceId);
+
+    const generationId = uuidv4();
+    const createGeneration = await makeAPICall(
+      "POST",
+      "/api/public/generations",
+      {
+        id: generationId,
+        traceIdType: "EXTERNAL",
+        traceId: traceId,
+        name: "generation-name",
+        startTime: "2021-01-01T00:00:00.000Z",
+        endTime: "2021-01-01T00:00:00.000Z",
+        model: "model-name",
+        modelParameters: { key: "value" },
+        prompt: { key: "value" },
+        metadata: { key: "value" },
+        version: "2.0.0",
+      }
+    );
+
+    expect(createGeneration.status).toBe(200);
+    const dbGeneration = await prisma.observation.findUnique({
+      where: {
+        id: generationId,
+      },
+    });
+
+    expect(dbGeneration?.id).toBe(generationId);
+    expect(dbGeneration?.traceId).toBe(dbTrace[0]?.id);
+    expect(dbGeneration?.name).toBe("generation-name");
+    expect(dbGeneration?.startTime).toEqual(
+      new Date("2021-01-01T00:00:00.000Z")
+    );
+    expect(dbGeneration?.endTime).toEqual(new Date("2021-01-01T00:00:00.000Z"));
+    expect(dbGeneration?.model).toBe("model-name");
+    expect(dbGeneration?.modelParameters).toEqual({ key: "value" });
+    expect(dbGeneration?.input).toEqual({ key: "value" });
+    expect(dbGeneration?.metadata).toEqual({ key: "value" });
+    expect(dbGeneration?.version).toBe("2.0.0");
+  });
+
   it("should create trace when creating generation without existing trace", async () => {
     const generationName = uuidv4();
 
@@ -105,6 +170,59 @@ describe("/api/public/generations API Endpoint", () => {
 
     expect(dbTrace.length).toBe(1);
     expect(dbTrace[0]?.name).toBe(generationName);
+
+    expect(createGeneration.status).toBe(200);
+    const dbGeneration = await prisma.observation.findUnique({
+      where: {
+        id: generationId,
+      },
+    });
+
+    expect(dbGeneration?.id).toBe(generationId);
+    expect(dbGeneration?.traceId).toBe(dbTrace[0]?.id);
+    expect(dbGeneration?.name).toBe(generationName);
+    expect(dbGeneration?.startTime).toEqual(
+      new Date("2021-01-01T00:00:00.000Z")
+    );
+    expect(dbGeneration?.endTime).toEqual(new Date("2021-01-01T00:00:00.000Z"));
+    expect(dbGeneration?.model).toBe("model-name");
+    expect(dbGeneration?.modelParameters).toEqual({ key: "value" });
+    expect(dbGeneration?.input).toEqual({ key: "value" });
+    expect(dbGeneration?.metadata).toEqual({ key: "value" });
+    expect(dbGeneration?.version).toBe("2.0.0");
+  });
+
+  it("should create trace when creating generation without existing trace with externalId", async () => {
+    const generationName = uuidv4();
+
+    const generationId = uuidv4();
+    const externalTraceId = uuidv4();
+    const createGeneration = await makeAPICall(
+      "POST",
+      "/api/public/generations",
+      {
+        id: generationId,
+        traceIdType: "EXTERNAL",
+        traceId: externalTraceId,
+        name: generationName,
+        startTime: "2021-01-01T00:00:00.000Z",
+        endTime: "2021-01-01T00:00:00.000Z",
+        model: "model-name",
+        modelParameters: { key: "value" },
+        prompt: { key: "value" },
+        metadata: { key: "value" },
+        version: "2.0.0",
+      }
+    );
+
+    const dbTrace = await prisma.trace.findMany({
+      where: {
+        externalId: externalTraceId,
+      },
+    });
+
+    expect(dbTrace.length).toBe(1);
+    expect(dbTrace[0]?.name).toBeNull();
 
     expect(createGeneration.status).toBe(200);
     const dbGeneration = await prisma.observation.findUnique({
