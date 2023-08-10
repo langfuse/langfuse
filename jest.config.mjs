bc3966fa1c545323deace5573213852a1fdac147
Maximilian Deichmann <m.deichmann@tum.de>
2023-08-10 21:33:12 +0300

    add tests (#76)

---
+++ jest.config.mjs
@@ -0,0 +1,31 @@
+// jest.config.mjs
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+const clientTestConfig = {
+  displayName: "client",
+  testMatch: ["/**/*.clienttest.[jt]s?(x)"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+const serverTestConfig = {
+  displayName: "server",
+  testMatch: ["/**/*.servertest.[jt]s?(x)"],
+  testEnvironment: "jest-environment-node",
+};
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  // Add more setup options before each test is run
+  projects: [
+    await createJestConfig(clientTestConfig)(),
+    await createJestConfig(serverTestConfig)(),
+  ],
+};
+
+export default config;
