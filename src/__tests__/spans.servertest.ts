08384ad87d933a698e1547a3ea325d9cffb0bac2
Max Deichmann <m.deichmann@tum.de>
2023-08-10 22:55:15 +0200

    more tests

--- src/__tests__/spans.servertest.ts
+++ src/__tests__/spans.servertest.ts
@@ -67,6 +67,59 @@ describe("/api/public/spans API Endpoint", () => {
     expect(dbSpan?.version).toBe("2.0.0");
   });
 
+  it("should create span before trace", async () => {
+    await pruneDatabase();
+
+    const traceId = uuidv4();
+    const spanId = uuidv4();
+
+    const createSpan = await makeAPICall("POST", "/api/public/spans", {
+      id: spanId,
+      traceId: traceId,
+      name: "span-name",
+      startTime: "2021-01-01T00:00:00.000Z",
+      endTime: "2021-01-01T00:00:00.000Z",
+      input: { input: "value" },
+      metadata: { meta: "value" },
+      version: "2.0.0",
+    });
+
+    expect(createSpan.status).toBe(200);
+    const dbSpan = await prisma.observation.findUnique({
+      where: {
+        id: spanId,
+      },
+    });
+
+    expect(dbSpan?.id).toBe(spanId);
+    expect(dbSpan?.traceId).toBe(traceId);
+    expect(dbSpan?.name).toBe("span-name");
+    expect(dbSpan?.startTime).toEqual(new Date("2021-01-01T00:00:00.000Z"));
+    expect(dbSpan?.endTime).toEqual(new Date("2021-01-01T00:00:00.000Z"));
+    expect(dbSpan?.input).toEqual({ input: "value" });
+    expect(dbSpan?.metadata).toEqual({ meta: "value" });
+    expect(dbSpan?.version).toBe("2.0.0");
+
+    await makeAPICall("POST", "/api/public/traces", {
+      id: traceId,
+      name: "trace-name",
+      userId: "user-1",
+      projectId: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+      metadata: { key: "value" },
+      release: "1.0.0",
+      version: "2.0.0",
+    });
+
+    const dbTrace = await prisma.trace.findMany({
+      where: {
+        id: traceId,
+      },
+    });
+
+    expect(dbTrace.length).toBeGreaterThan(0);
+    expect(dbTrace[0]?.id).toBe(traceId);
+  });
+
   it("should create span after trace based on externalId", async () => {
     await pruneDatabase();
 
