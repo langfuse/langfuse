e9ec39e97557cd06037118250b87b6a50fa2101f
Marc Klingen <git@marcklingen.com>
2023-08-15 01:08:42 +0200

    Assign demo project to new users (#84)

--- src/env.mjs
+++ src/env.mjs
@@ -34,6 +34,7 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
     NEXT_PUBLIC_HOSTNAME: z.string().optional(),
+    NEXT_PUBLIC_DEMO_PROJECT_ID: z.string().optional(),
   },
 
   /**
@@ -41,6 +42,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    NEXT_PUBLIC_DEMO_PROJECT_ID: process.env.NEXT_PUBLIC_DEMO_PROJECT_ID,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
