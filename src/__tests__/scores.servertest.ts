725b1382642c4e71eb18bb5eef7574f8df6d89ed
Max Deichmann <m.deichmann@tum.de>
2023-08-25 16:38:01 +0200

    add int test

--- src/__tests__/scores.servertest.ts
+++ src/__tests__/scores.servertest.ts
@@ -60,6 +60,52 @@ describe("/api/public/scores API Endpoint", () => {
     expect(dbScore?.observationId).toBeNull();
   });
 
+  it("should create score for a trace with int", async () => {
+    await pruneDatabase();
+
+    const traceId = uuidv4();
+
+    await makeAPICall("POST", "/api/public/traces", {
+      id: traceId,
+      name: "trace-name",
+      userId: "user-1",
+      projectId: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+      metadata: { key: "value" },
+      release: "1.0.0",
+      version: "2.0.0",
+    });
+
+    const dbTrace = await prisma.trace.findMany({
+      where: {
+        id: traceId,
+      },
+    });
+
+    expect(dbTrace.length).toBeGreaterThan(0);
+    expect(dbTrace[0]?.id).toBe(traceId);
+
+    const scoreId = uuidv4();
+    const createScore = await makeAPICall("POST", "/api/public/scores", {
+      id: scoreId,
+      name: "score-name",
+      value: 100,
+      traceId: traceId,
+    });
+
+    expect(createScore.status).toBe(200);
+    const dbScore = await prisma.score.findUnique({
+      where: {
+        id: scoreId,
+      },
+    });
+
+    expect(dbScore?.id).toBe(scoreId);
+    expect(dbScore?.traceId).toBe(traceId);
+    expect(dbScore?.name).toBe("score-name");
+    expect(dbScore?.value).toBe(100);
+    expect(dbScore?.observationId).toBeNull();
+  });
+
   it("should create score for a generation", async () => {
     await pruneDatabase();
 
