c65411c682348167e90cd60b9bd1c2e5ba1bb8b3
Marc Klingen <git@marcklingen.com>
2023-09-11 20:08:15 +0200

    Check formatting in CI (#135)

--- cron.js
+++ cron.js
@@ -1,11 +1,16 @@
-var CronJob = require('cron').CronJob;
+var CronJob = require("cron").CronJob;
 
 var job = new CronJob(
-  '*/5 * * * *', // Every 5 minutes
+  "*/5 * * * *", // Every 5 minutes
   function () {
-    fetch("http://host.docker.internal:3000/api/cron/telemetry").catch(err => console.log(err));
+    fetch("http://host.docker.internal:3000/api/cron/telemetry").catch((err) =>
+      console.log(err),
+    );
   },
 );
 
-if (process.env.TELEMETRY_ENABLED === undefined || process.env.TELEMETRY_ENABLED === "true")
-  job.start()
+if (
+  process.env.TELEMETRY_ENABLED === undefined ||
+  process.env.TELEMETRY_ENABLED === "true"
+)
+  job.start();
