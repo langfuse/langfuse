5cb48c5d4b70518a38e7804599ddc371d922d4fd
Marc Klingen <git@marcklingen.com>
2023-09-12 19:30:58 +0200

    Fix cron

--- cron.js
+++ cron.js
@@ -1,34 +1,38 @@
 var CronJob = require("cron").CronJob;
 var os = require("os");
 
-// get internal ip address
-var networkInterfaces = os.networkInterfaces();
-console.log("networkInterfaces", networkInterfaces);
-var ip = undefined;
+try {
+  // get internal ip address
+  var networkInterfaces = os.networkInterfaces();
+  console.log("CRON: fetched network interfaces", networkInterfaces);
+  var ip = undefined;
 
-if ("eth0" in networkInterfaces) {
-  var ipaddresses = networkInterfaces["eth0"]
-    .filter((networkInterface) => networkInterface.family === "IPv4")
-    .map((networkInterface) => networkInterface.address);
-  if (ipaddresses.length > 0) {
-    ip = ipaddresses[0];
+  if ("eth0" in networkInterfaces) {
+    var ipaddresses = networkInterfaces["eth0"]
+      .filter((networkInterface) => networkInterface.family === "IPv4")
+      .map((networkInterface) => networkInterface.address);
+    if (ipaddresses.length > 0) {
+      ip = ipaddresses[0];
+    }
   }
-}
-console.log("ip", ip);
+  console.log("CRON: IP", ip);
 
-if (ip) {
-  var job = new CronJob("*/5 * * * * *", function () {
-    console.log("Running cron job");
-    fetch(`http://${ip}:3000/api/cron/telemetry`).catch((err) =>
-      console.log(err),
-    );
-  });
+  if (ip) {
+    var job = new CronJob("*/5 * * * *", function () {
+      console.log("CRON: Sending telemetry");
+      fetch(`http://${ip}:3000/api/cron/telemetry`).catch((err) =>
+        console.log(err),
+      );
+    });
 
-  if (
-    process.env.TELEMETRY_ENABLED === undefined ||
-    process.env.TELEMETRY_ENABLED === "true"
-  )
-    job.start();
-} else {
-  console.log("No ip address found, not starting cron job");
+    if (
+      process.env.TELEMETRY_ENABLED === undefined ||
+      process.env.TELEMETRY_ENABLED === "true"
+    )
+      job.start();
+  } else {
+    console.log("CRON: No ip address found, not starting cron job");
+  }
+} catch (err) {
+  console.log("CRON: Error", err);
 }
