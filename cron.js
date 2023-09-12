7a1a385a50616cf5d3f5b3fb8cb1b3b45ab46e3b
Marc Klingen <git@marcklingen.com>
2023-09-12 18:36:12 +0200

    push

--- cron.js
+++ cron.js
@@ -1,20 +1,34 @@
 var CronJob = require("cron").CronJob;
+var os = require("os");
 
-const os = require('os');
-const networkInterfaces = os.networkInterfaces();
-console.log(networkInterfaces);
+// get internal ip address
+var networkInterfaces = os.networkInterfaces();
+console.log("networkInterfaces", networkInterfaces);
+var ip = undefined;
 
-// var job = new CronJob(
-//   "*/5 * * * * *", // Every 5 minutes
-//   function () {
-//     fetch("http://host.docker.internal:3000/api/cron/telemetry").catch((err) =>
-//       console.log(err),
-//     );
-//   },
-// );
+if ("eth0" in networkInterfaces) {
+  var ipaddresses = networkInterfaces["eth0"]
+    .filter((networkInterface) => networkInterface.family === "IPv4")
+    .map((networkInterface) => networkInterface.address);
+  if (ipaddresses.length > 0) {
+    ip = ipaddresses[0];
+  }
+}
+console.log("ip", ip);
 
-// if (
-//   process.env.TELEMETRY_ENABLED === undefined ||
-//   process.env.TELEMETRY_ENABLED === "true"
-// )
-//   job.start();
+if (ip) {
+  var job = new CronJob("*/5 * * * * *", function () {
+    console.log("Running cron job");
+    fetch(`http://${ip}:3000/api/cron/telemetry`).catch((err) =>
+      console.log(err),
+    );
+  });
+
+  if (
+    process.env.TELEMETRY_ENABLED === undefined ||
+    process.env.TELEMETRY_ENABLED === "true"
+  )
+    job.start();
+} else {
+  console.log("No ip address found, not starting cron job");
+}
