f86b025ce0db25f5e35d7d2d4d61d034b97a793c
Marc Klingen <git@marcklingen.com>
2023-09-12 17:13:24 +0200

    test

--- cron.js
+++ cron.js
@@ -1,16 +1,20 @@
 var CronJob = require("cron").CronJob;
 
-var job = new CronJob(
-  "*/5 * * * *", // Every 5 minutes
-  function () {
-    fetch("http://host.docker.internal:3000/api/cron/telemetry").catch((err) =>
-      console.log(err),
-    );
-  },
-);
+const os = require('os');
+const networkInterfaces = os.networkInterfaces();
+console.log(networkInterfaces);
 
-if (
-  process.env.TELEMETRY_ENABLED === undefined ||
-  process.env.TELEMETRY_ENABLED === "true"
-)
-  job.start();
+// var job = new CronJob(
+//   "*/5 * * * * *", // Every 5 minutes
+//   function () {
+//     fetch("http://host.docker.internal:3000/api/cron/telemetry").catch((err) =>
+//       console.log(err),
+//     );
+//   },
+// );
+
+// if (
+//   process.env.TELEMETRY_ENABLED === undefined ||
+//   process.env.TELEMETRY_ENABLED === "true"
+// )
+//   job.start();
