c1ba2fd6a35eb24e34c5d6a3a83cac92ee195903
Marc Klingen <git@marcklingen.com>
2023-09-12 16:44:24 +0200

    remove hotfix

--- cron.js
+++ cron.js
@@ -1,16 +1,16 @@
 var CronJob = require("cron").CronJob;
 
-// var job = new CronJob(
-//   "*/5 * * * *", // Every 5 minutes
-//   function () {
-//     fetch("http://host.docker.internal:3000/api/cron/telemetry").catch((err) =>
-//       console.log(err),
-//     );
-//   },
-// );
+var job = new CronJob(
+  "*/5 * * * *", // Every 5 minutes
+  function () {
+    fetch("http://host.docker.internal:3000/api/cron/telemetry").catch((err) =>
+      console.log(err),
+    );
+  },
+);
 
-// if (
-//   process.env.TELEMETRY_ENABLED === undefined ||
-//   process.env.TELEMETRY_ENABLED === "true"
-// )
-//   job.start();
+if (
+  process.env.TELEMETRY_ENABLED === undefined ||
+  process.env.TELEMETRY_ENABLED === "true"
+)
+  job.start();
