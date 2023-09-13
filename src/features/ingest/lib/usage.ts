a0d3beccdf38b0e06af802e89496ef1a40bbf799
Maximilian Deichmann <m.deichmann@tum.de>
2023-09-14 00:38:00 +0200

    Accurate token calculation (#139)

--- src/features/ingest/lib/usage.ts
+++ src/features/ingest/lib/usage.ts
@@ -1,14 +1,105 @@
-import { type TiktokenEncoding, get_encoding } from "tiktoken";
+import {
+  type TiktokenEncoding,
+  get_encoding,
+  encoding_for_model,
+  type TiktokenModel,
+  type Tiktoken,
+} from "tiktoken";
 import { countTokens } from "@anthropic-ai/tokenizer";
 import { type Pricing } from "@prisma/client";
 import { Decimal } from "decimal.js";
 
 export function tokenCount(p: {
   model: string;
+  text: unknown;
+}): number | undefined {
+  if (isString(p.text)) {
+    return stringTokenCount({ model: p.model, text: p.text });
+  } else if (isChatMessageArray(p.text)) {
+    console.log("isChatMessageArray");
+    if (!isValidModel(p.model)) {
+      console.log(`Unknown model ${p.model} for chat model`);
+      return undefined;
+    }
+    return numTokensFromMessages({
+      model: p.model,
+      messages: p.text,
+    });
+  } else {
+    console.log(
+      "It is neither a string nor a ChatMessage array, converting into string",
+    );
+    return stringTokenCount({ model: p.model, text: JSON.stringify(p.text) });
+  }
+}
+
+type ChatMessage = {
+  role: string;
+  name?: string;
+  content: string;
+};
+
+type TokenCalculationParams = {
+  messages: ChatMessage[];
+  model: TiktokenModel;
+};
+
+function numTokensFromMessages(params: TokenCalculationParams) {
+  let encoding: Tiktoken;
+  try {
+    encoding = encoding_for_model(params.model);
+  } catch (KeyError) {
+    console.log("Warning: model not found. Using cl100k_base encoding.");
+    encoding = get_encoding("cl100k_base");
+  }
+  let tokens_per_message = 0;
+  let tokens_per_name = 0;
+
+  if (
+    [
+      "gpt-3.5-turbo-0613",
+      "gpt-3.5-turbo-16k-0613",
+      "gpt-4-0314",
+      "gpt-4-32k-0314",
+      "gpt-4-0613",
+      "gpt-4-32k-0613",
+    ].includes(params.model)
+  ) {
+    tokens_per_message = 3;
+    tokens_per_name = 1;
+  } else if (params.model === "gpt-3.5-turbo-0301") {
+    tokens_per_message = 4; // every message follows <|start|>{role/name}\n{content}<|end|>\n
+    tokens_per_name = -1; // if there's a name, the role is omitted
+  } else if (params.model.includes("gpt-3.5-turbo-0301")) {
+    return numTokensFromMessages({ ...params, model: "gpt-3.5-turbo-0613" });
+  } else if (params.model.includes("gpt-4")) {
+    return numTokensFromMessages({ ...params, model: "gpt-4-0314" });
+  } else {
+    console.error(`Not implemented for model ${params.model}`);
+    throw new Error(`Not implemented for model ${params.model}`);
+  }
+  let num_tokens = 0;
+  params.messages.forEach((message) => {
+    num_tokens += tokens_per_message;
+
+    Object.keys(message).forEach((key) => {
+      const value = message[key as keyof typeof message];
+      if (value) {
+        num_tokens += encoding.encode(value).length;
+      }
+      if (key === "name") {
+        num_tokens += tokens_per_name;
+      }
+    });
+  });
+  num_tokens += 3; // every reply is primed with <| start |> assistant <| message |>
+  return num_tokens;
+}
+
+export function stringTokenCount(p: {
+  model: string;
   text: string;
 }): number | undefined {
-  if (!p.model || !p.text) return undefined;
-
   if (p.model.toLowerCase().startsWith("gpt")) {
     return getTokens("cl100k_base", p.text);
   }
@@ -28,6 +119,75 @@ const getTokens = (name: TiktokenEncoding, text: string) => {
   encoding.free();
   return tokens.length;
 };
+
+function isString(value: unknown): value is string {
+  return typeof value === "string";
+}
+
+function isValidModel(model: string): model is TiktokenModel {
+  return (
+    [
+      "text-davinci-003",
+      "text-davinci-002",
+      "text-davinci-001",
+      "text-curie-001",
+      "text-babbage-001",
+      "text-ada-001",
+      "davinci",
+      "curie",
+      "babbage",
+      "ada",
+      "code-davinci-002",
+      "code-davinci-001",
+      "code-cushman-002",
+      "code-cushman-001",
+      "davinci-codex",
+      "cushman-codex",
+      "text-davinci-edit-001",
+      "code-davinci-edit-001",
+      "text-embedding-ada-002",
+      "text-similarity-davinci-001",
+      "text-similarity-curie-001",
+      "text-similarity-babbage-001",
+      "text-similarity-ada-001",
+      "text-search-davinci-doc-001",
+      "text-search-curie-doc-001",
+      "text-search-babbage-doc-001",
+      "text-search-ada-doc-001",
+      "code-search-babbage-code-001",
+      "code-search-ada-code-001",
+      "gpt2",
+      "gpt-4",
+      "gpt-4-0314",
+      "gpt-4-0613",
+      "gpt-4-32k",
+      "gpt-4-32k-0314",
+      "gpt-4-32k-0613",
+      "gpt-3.5-turbo",
+      "gpt-3.5-turbo-0301",
+      "gpt-3.5-turbo-0613",
+      "gpt-3.5-turbo-16k",
+      "gpt-3.5-turbo-16k-0613",
+    ].find((m) => m === model) !== undefined
+  );
+}
+
+function isChatMessageArray(value: unknown): value is ChatMessage[] {
+  if (!Array.isArray(value)) {
+    return false;
+  }
+
+  return value.every(
+    (item: unknown) =>
+      typeof item === "object" &&
+      item !== null &&
+      "role" in item &&
+      typeof item.role === "string" &&
+      "content" in item &&
+      typeof item.content === "string" &&
+      (!("name" in item) || typeof item.name === "string"),
+  );
+}
 
 export function calculateTokenCost(
   pricingList: Pricing[],
