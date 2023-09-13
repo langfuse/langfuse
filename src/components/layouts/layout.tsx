5d786569573831edaa222add951c301fa5325490
Marc Klingen <git@marcklingen.com>
2023-09-13 15:39:52 +0200

    meta

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -108,6 +108,23 @@ export default function Layout(props: PropsWithChildren) {
         <title>
           {currentPathName ? `${currentPathName} | Langfuse` : "Langfuse"}
         </title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
