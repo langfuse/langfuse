5a12a9943b3315ab436c0041b255514e77b4dff3
Marc Klingen <git@marcklingen.com>
2023-09-25 14:43:11 -0700

    fix lint

--- src/components/ui/code.tsx
+++ src/components/ui/code.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Button } from "@/src/components/ui/button";
 import { Check, ChevronsDownUp, ChevronsUpDown, Copy } from "lucide-react";
 import { cn } from "@/src/utils/tailwind";
-// rename jsonview import to tmp
 import { default as React18JsonView } from "react18-json-view";
 
 export function JSONView(props: {
@@ -10,8 +9,6 @@ export function JSONView(props: {
   title?: string;
   className?: string;
 }) {
-  const text = parseJsonInput(props.json);
-
   return (
     <div className={cn("max-w-full rounded-md border ", props.className)}>
       {props.title ? (
@@ -26,64 +23,64 @@ export function JSONView(props: {
   );
 }
 
-const parseJsonInput = (jsonIn: unknown): string => {
-  if (typeof jsonIn === "string") return jsonIn;
+// const parseJsonInput = (jsonIn: unknown): string => {
+//   if (typeof jsonIn === "string") return jsonIn;
 
-  try {
-    if (jsonIn && typeof jsonIn === "object") {
-      // For completions of generations, display the generation as text
-      // { completion: "<completion>" } ->  "<completion>"
-      if (
-        Object.keys(jsonIn).length === 1 &&
-        "completion" in jsonIn &&
-        typeof jsonIn.completion === "string"
-      ) {
-        return jsonIn.completion;
-      }
+//   try {
+//     if (jsonIn && typeof jsonIn === "object") {
+//       // For completions of generations, display the generation as text
+//       // { completion: "<completion>" } ->  "<completion>"
+//       if (
+//         Object.keys(jsonIn).length === 1 &&
+//         "completion" in jsonIn &&
+//         typeof jsonIn.completion === "string"
+//       ) {
+//         return jsonIn.completion;
+//       }
 
-      // For OpenAI ChatCompletion Prompts, concat the messages
-      // [ { "role": "<role>", "content": "<content>" } ] -> "<role>\n<content>\n\n"
-      if (
-        Array.isArray(jsonIn) &&
-        jsonIn.length > 0 &&
-        typeof jsonIn[0] === "object" &&
-        "role" in jsonIn[0] &&
-        "content" in jsonIn[0]
-      ) {
-        return (jsonIn as { role: string; content: string }[])
-          .map(
-            (message) => `${message.role.toUpperCase()}\n\n${message.content}`,
-          )
-          .join("\n\n------\n\n");
-      }
+//       // For OpenAI ChatCompletion Prompts, concat the messages
+//       // [ { "role": "<role>", "content": "<content>" } ] -> "<role>\n<content>\n\n"
+//       if (
+//         Array.isArray(jsonIn) &&
+//         jsonIn.length > 0 &&
+//         typeof jsonIn[0] === "object" &&
+//         "role" in jsonIn[0] &&
+//         "content" in jsonIn[0]
+//       ) {
+//         return (jsonIn as { role: string; content: string }[])
+//           .map(
+//             (message) => `${message.role.toUpperCase()}\n\n${message.content}`,
+//           )
+//           .join("\n\n------\n\n");
+//       }
 
-      // If it is an array with a single string, return the string
-      // [ "<string>" ] -> "<string>"
-      if (
-        Array.isArray(jsonIn) &&
-        jsonIn.length === 1 &&
-        typeof jsonIn[0] === "string"
-      ) {
-        return jsonIn[0];
-      }
+//       // If it is an array with a single string, return the string
+//       // [ "<string>" ] -> "<string>"
+//       if (
+//         Array.isArray(jsonIn) &&
+//         jsonIn.length === 1 &&
+//         typeof jsonIn[0] === "string"
+//       ) {
+//         return jsonIn[0];
+//       }
 
-      // If it is an Object with a single key, listed in the list of keys, return the value if it is a string
-      // { "<key>": "<string>" } -> "<string>"
-      const keys = ["input", "output", "text", "prompt"];
-      if (
-        Object.keys(jsonIn).length === 1 &&
-        keys.includes(Object.keys(jsonIn)[0] as string) &&
-        typeof Object.values(jsonIn)[0] === "string"
-      ) {
-        return Object.values(jsonIn)[0] as string;
-      }
-    }
-  } catch (e) {
-    console.error("Error while trying to parse the string", e, jsonIn);
-  }
+//       // If it is an Object with a single key, listed in the list of keys, return the value if it is a string
+//       // { "<key>": "<string>" } -> "<string>"
+//       const keys = ["input", "output", "text", "prompt"];
+//       if (
+//         Object.keys(jsonIn).length === 1 &&
+//         keys.includes(Object.keys(jsonIn)[0] as string) &&
+//         typeof Object.values(jsonIn)[0] === "string"
+//       ) {
+//         return Object.values(jsonIn)[0] as string;
+//       }
+//     }
+//   } catch (e) {
+//     console.error("Error while trying to parse the string", e, jsonIn);
+//   }
 
-  return JSON.stringify(jsonIn, null, 2);
-};
+//   return JSON.stringify(jsonIn, null, 2);
+// };
 
 export function CodeView(props: {
   content: string | undefined | null;
