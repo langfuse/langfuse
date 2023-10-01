3382fc4123acac6b4a01a816d2e1ae7e2e9b91fa
Marc Klingen <git@marcklingen.com>
2023-10-01 15:19:33 -0700

    add stringoptions type

--- src/server/api/interfaces/filters.ts
+++ src/server/api/interfaces/filters.ts
@@ -2,24 +2,31 @@ import { z } from "zod";
 
 export const timeFilter = z.object({
   column: z.string(),
-  operator: z.enum(["=", ">", "<", ">=", "<=", "in", "like"]),
+  operator: z.enum(["=", ">", "<", ">=", "<="]),
   value: z.date(),
   type: z.literal("datetime"),
 });
 export const stringFilter = z.object({
   column: z.string(),
-  operator: z.enum(["=", ">", "<", ">=", "<=", "in", "like"]),
+  operator: z.enum(["="]),
   value: z.string(),
   type: z.literal("string"),
 });
 export const numberFilter = z.object({
   column: z.string(),
-  operator: z.enum(["=", ">", "<", ">=", "<=", "in", "like"]),
+  operator: z.enum(["=", ">", "<", ">=", "<="]),
   value: z.number(),
   type: z.literal("number"),
+});
+export const stringOptionsFilter = z.object({
+  column: z.string(),
+  operator: z.enum(["any of", "none of"]),
+  value: z.array(z.string()),
+  type: z.literal("stringOptions"),
 });
 export const singleFilter = z.discriminatedUnion("type", [
   timeFilter,
   stringFilter,
   numberFilter,
+  stringOptionsFilter,
 ]);
--- src/server/api/interfaces/tableDefinition.ts
+++ src/server/api/interfaces/tableDefinition.ts
@@ -1,8 +1,15 @@
-export type ColumnDefinition = {
-  name: string;
-  type: "number" | "string" | "datetime";
-  internal: string;
-};
+export type ColumnDefinition =
+  | {
+      name: string;
+      type: "number" | "string" | "datetime";
+      internal: string;
+    }
+  | {
+      name: string;
+      type: "stringOptions";
+      options: string[];
+      internal: string;
+    };
 
 export type TableDefinitions = {
   [tableName: string]: {
