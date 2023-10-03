f98fdbface33d1ceef136e49f2110011416c9fea
Max Deichmann <m.deichmann@tum.de>
2023-10-02 21:47:20 -0700

    remove old dashboard

---
+++ src/features/dashboard/components/TokenChart.tsx
@@ -0,0 +1,81 @@
+import Header from "@/src/components/layouts/header";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/src/components/ui/card";
+import { api } from "@/src/utils/api";
+import { BaseTimeSeriesChart } from "@/src/features/dashboard/components/BaseTimeSeriesChart";
+import { type DateTimeAggregationOption } from "@/src/features/dashboard/lib/timeseries-aggregation";
+import { useState } from "react";
+import { type FilterState } from "@/src/features/filters/types";
+import { Loader } from "lucide-react";
+
+export const TokenChart = ({
+  projectId,
+  globalFilterState,
+}: {
+  projectId: string;
+  globalFilterState: FilterState;
+}) => {
+  console.log("globalFilterState.get(observations)", globalFilterState);
+  const data = api.dashboard.chart.useQuery({
+    projectId,
+    from: "observations",
+    select: [
+      { column: "completionTokens", agg: "SUM" },
+      { column: "promptTokens", agg: "SUM" },
+      { column: "totalTokens", agg: "SUM" },
+    ],
+    filter: globalFilterState ?? [],
+    groupBy: [{ type: "datetime", column: "startTime", temporalUnit: "day" }],
+    orderBy: [],
+  });
+
+  // TODO: set agg from UI
+  const [agg] = useState<DateTimeAggregationOption>("7 days");
+
+  const transformedData = data.data
+    ? data.data.map((item) => {
+        return {
+          ts: (item.startTime as Date).getTime(),
+          values: [
+            {
+              label: "Completion Tokens",
+              value: (item.sumCompletionTokens ?? 0) as number,
+            },
+            {
+              label: "Prompt Tokens",
+              value: (item.sumPromptTokens ?? 0) as number,
+            },
+            {
+              label: "Total Tokens",
+              value: (item.sumTotalTokens ?? 0) as number,
+            },
+          ],
+        };
+      })
+    : [];
+
+  return (
+    <div className="md:container">
+      <Header title="Analytics" />
+      <Card>
+        <CardHeader className="relative">
+          <CardTitle>Generations</CardTitle>
+          <CardDescription>Count</CardDescription>
+          {data.isLoading ? (
+            <div className="absolute right-5 top-5 ">
+              <Loader className="h-5 w-5 animate-spin" />
+            </div>
+          ) : null}
+        </CardHeader>
+        <CardContent>
+          <BaseTimeSeriesChart agg={agg} data={transformedData ?? []} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
--- src/pages/project/[projectId]/index.tsx
+++ src/pages/project/[projectId]/index.tsx
@@ -5,17 +5,45 @@ import {
   dateTimeAggregationOptions,
   type DateTimeAggregationOption,
 } from "@/src/features/dashboard/lib/timeseries-aggregation";
-import {
-  ChartScores,
-  ChartTraces,
-  ChartUsage,
-} from "@/src/features/dashboard/components/charts";
+import { ChartScores } from "@/src/features/dashboard/components/charts";
 import { useRouter } from "next/router";
+import { TokenChart } from "@/src/features/dashboard/components/TokenChart";
 
 export default function Start() {
   const [agg, setAgg] = useState<DateTimeAggregationOption>("7 days");
   const router = useRouter();
   const projectId = router.query.projectId as string;
+
+  const convertAggToDateTime = (agg: DateTimeAggregationOption) => {
+    const [num, unit] = agg.split(" ");
+    console.log(num, unit);
+    if (!num || !unit) throw new Error("Invalid agg");
+    const now = new Date();
+    switch (unit) {
+      case "minutes":
+        return new Date(now.getTime() - parseInt(num) * 60 * 1000);
+      case "hours":
+      case "hour":
+        return new Date(now.getTime() - parseInt(num) * 60 * 60 * 1000);
+      case "days":
+        return new Date(now.getTime() - parseInt(num) * 24 * 60 * 60 * 1000);
+      case "weeks":
+        return new Date(
+          now.getTime() - parseInt(num) * 7 * 24 * 60 * 60 * 1000,
+        );
+      case "months":
+      case "month":
+        return new Date(
+          now.getTime() - parseInt(num) * 30 * 24 * 60 * 60 * 1000,
+        );
+      case "year":
+      case "years":
+        return new Date(
+          now.getTime() - parseInt(num) * 365 * 24 * 60 * 60 * 1000,
+        );
+    }
+    throw new Error("Invalid agg");
+  };
 
   return (
     <>
@@ -33,14 +61,28 @@ export default function Start() {
           ))}
         </TabsList>
       </Tabs>
-      <div className="grid gap-4 xl:grid-cols-2">
-        <ChartTraces agg={agg} projectId={projectId} />
-        {/* <ChartGenerations agg={agg} projectId={projectId} /> */}
-        <ChartUsage agg={agg} projectId={projectId} />
-        <div className="col-span-full">
+      {/* <div className="grid gap-4 xl:grid-cols-2"> */}
+      <TokenChart
+        projectId={projectId}
+        globalFilterState={[
+          {
+            column: "startTime",
+            operator: ">",
+            type: "datetime",
+            value: convertAggToDateTime(agg),
+          },
+          {
+            column: "startTime",
+            operator: "<",
+            type: "datetime",
+            value: new Date(),
+          },
+        ]}
+      />
+      {/* <div className="col-span-full">
           <ChartScores agg={agg} projectId={projectId} />
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </>
   );
 }
