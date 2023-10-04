a682bb0029efec5fabbcce68a58be451aa1d19ef
Max Deichmann <m.deichmann@tum.de>
2023-10-04 14:17:29 -0700

    startup message

---
+++ src/instrumentation.ts
@@ -0,0 +1,14 @@
+export function register() {
+  console.log(`
+  __                ___             
+  |  |    __ ___ ___|  _|_ _ ___ ___ 
+  |  |__ |. |   | . |  _| | |_ -| -_|
+  |_____|___|_|_|_  |_| |___|___|___|
+                |___|                
+       
+  
+    Here are our docs to get you up and running fast: https://langfuse.com/docs
+    Join our discord to discuss feature ideas and get help: https://langfuse.com/discord
+  
+  `);
+}
