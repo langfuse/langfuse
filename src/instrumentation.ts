9d52dda9dccf9759f3ae58b7f149507887fe4fe8
Max Deichmann <m.deichmann@tum.de>
2023-10-04 14:22:16 -0700

    change text

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,16 @@
 export function register() {
   console.log(`
-  __                ___             
-  |  |    __ ___ ___|  _|_ _ ___ ___ 
-  |  |__ |. |   | . |  _| | |_ -| -_|
-  |_____|___|_|_|_  |_| |___|___|___|
-                |___|                
+
+    __                ___             
+    |  |    __ ___ ___|  _|_ _ ___ ___ 
+    |  |__ |. |   | . |  _| | |_ -| -_|
+    |_____|___|_|_|_  |_| |___|___|___|
+                  |___|                
        
   
-    Here are our docs to get you up and running fast: https://langfuse.com/docs
-    Join our discord to discuss feature ideas and get help: https://langfuse.com/discord
+    Technical docs: https://langfuse.com/docs
+    Community Discord: https://langfuse.com/discord
+    Contact maintainers: support@langfuse.com
   
   `);
 }
