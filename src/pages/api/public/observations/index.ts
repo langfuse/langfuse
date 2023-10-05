9496d30fd35e81293506bbcb83abbc775968503d
Max Deichmann <m.deichmann@tum.de>
2023-10-04 22:49:21 -0700

    move and fix api endpoint

---
+++ src/pages/api/public/observations/index.ts
@@ -0,0 +1,132 @@
+import { verifyAuthHeaderAndReturnScope } from "@/src/features/public-api/server/apiAuth";
+import { runMiddleware } from "@/src/features/public-api/server/cors";
+import { prisma } from "@/src/server/db";
+import { paginationZod } from "@/src/utils/zod";
+import { Prisma, type PrismaClient, type Observation } from "@prisma/client";
+import cors from "cors";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { z } from "zod";
+
+const ObservationsGetSchema = z.object({
+  ...paginationZod,
+  name: z.string().nullish(),
+  userId: z.string().nullish(),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  await runMiddleware(req, res, cors);
+
+  if (req.method !== "GET") {
+    console.error(req.method, req.body, req.query);
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  // CHECK AUTH
+  const authCheck = await verifyAuthHeaderAndReturnScope(
+    req.headers.authorization,
+  );
+  if (!authCheck.validKey)
+    return res.status(401).json({
+      success: false,
+      message: authCheck.error,
+    });
+  // END CHECK AUTH
+
+  try {
+    console.log(
+      "trying to get observations, project ",
+      authCheck.scope.projectId,
+      ", body:",
+      JSON.stringify(req.query, null, 2),
+    );
+
+    if (authCheck.scope.accessLevel !== "all") {
+      return res.status(401).json({
+        success: false,
+        message:
+          "Access denied - need to use basic auth with secret key to GET generations",
+      });
+    }
+
+    const searchParams = ObservationsGetSchema.parse(req.query);
+
+    const [observations, totalObservations] = await getObservation(
+      prisma,
+      authCheck.scope.projectId,
+      searchParams,
+    );
+    return res.status(200).json({
+      data: observations,
+      meta: {
+        page: searchParams.page,
+        limit: searchParams.limit,
+        totalItems: totalObservations,
+        totalPages: Math.ceil(totalObservations / searchParams.limit),
+      },
+    });
+  } catch (error: unknown) {
+    console.error(error);
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+    res.status(400).json({
+      success: false,
+      message: "Invalid request data",
+      error: errorMessage,
+    });
+  }
+}
+
+const getObservation = async (
+  prisma: PrismaClient,
+  authenticatedProjectId: string,
+  query: z.infer<typeof ObservationsGetSchema>,
+) => {
+  const userIdCondition = query.userId
+    ? Prisma.sql`AND traces."user_id" = ${query.userId}`
+    : Prisma.empty;
+
+  const nameCondition = query.name
+    ? Prisma.sql`AND o."name" = ${query.name}`
+    : Prisma.empty;
+
+  const [observations, count] = await Promise.all([
+    prisma.$queryRaw<Observation[]>`
+      SELECT 
+        o.*,
+        o."trace_id" AS "traceId",
+        o."project_id" AS "projectId",
+        o."start_time" AS "startTime",
+        o."end_time" AS "endTime",
+        o."parent_observation_id" AS "parentObservationId",
+        o."status_message" AS "statusMessage",
+        o."prompt_tokens" AS "promptTokens",
+        o."completion_tokens" AS "completionTokens",
+        o."completion_start_time" AS "completionStartTime"
+      FROM observations o LEFT JOIN traces ON o."trace_id" = traces."id"
+      WHERE o."project_id" = ${authenticatedProjectId}
+      ${nameCondition}
+      ${userIdCondition}
+      ORDER by o."start_time" DESC
+      OFFSET ${(query.page - 1) * query.limit}
+      LIMIT ${query.limit}
+    `,
+    prisma.$queryRaw<{ count: bigint }[]>`
+      SELECT COUNT(*) FROM observations o LEFT JOIN traces ON o."trace_id" = traces."id"
+      WHERE o."project_id" = ${authenticatedProjectId}
+      AND type = 'GENERATION'
+      ${nameCondition}
+      ${userIdCondition}
+  `,
+  ]);
+
+  if (!count || count.length !== 1) {
+    throw new Error(
+      `Unexpected number of results for count query: ${JSON.stringify(count)}`,
+    );
+  } else {
+    return [observations, Number(count[0]?.count)] as const;
+  }
+};
