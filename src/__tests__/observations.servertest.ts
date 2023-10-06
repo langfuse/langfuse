42a96d42f6febe47dc0bfeb317e51e848bf4e79c
Maximilian Deichmann <m.deichmann@tum.de>
2023-10-06 14:25:18 -0700

    filter by observation type (#317)

--- src/__tests__/observations.servertest.ts
+++ src/__tests__/observations.servertest.ts
@@ -9,7 +9,7 @@ describe("/api/public/generations API Endpoint", () => {
   beforeEach(async () => await pruneDatabase());
   afterEach(async () => await pruneDatabase());
 
-  it("should create generation after trace", async () => {
+  it("should fetch all observations", async () => {
     await pruneDatabase();
 
     const traceId = uuidv4();
@@ -61,6 +61,79 @@ describe("/api/public/generations API Endpoint", () => {
     expect(fetchedObservations.body.data[0]?.traceId).toBe(traceId);
     expect(fetchedObservations.body.data[0]?.input).toEqual({ key: "input" });
     expect(fetchedObservations.body.data[0]?.output).toEqual({ key: "output" });
+  });
+  it("should fetch all observations, filtered by generations", async () => {
+    await pruneDatabase();
+
+    const traceId = uuidv4();
+
+    await prisma.trace.create({
+      data: {
+        id: traceId,
+        name: "trace-name",
+        userId: "user-1",
+        projectId: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+        metadata: { key: "value" },
+        release: "1.0.0",
+        version: "2.0.0",
+      },
+    });
+
+    await prisma.observation.create({
+      data: {
+        id: uuidv4(),
+        traceId: traceId,
+        name: "generation-name",
+        startTime: new Date("2021-01-01T00:00:00.000Z"),
+        endTime: new Date("2021-01-01T00:00:00.000Z"),
+        model: "model-name",
+        modelParameters: { key: "value" },
+        input: { key: "input" },
+        output: { key: "output" },
+        version: "2.0.0",
+        type: "GENERATION",
+        project: {
+          connect: { id: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a" },
+        },
+      },
+    });
+
+    await prisma.observation.create({
+      data: {
+        id: uuidv4(),
+        traceId: traceId,
+        name: "generation-name",
+        startTime: new Date("2021-01-01T00:00:00.000Z"),
+        endTime: new Date("2021-01-01T00:00:00.000Z"),
+        model: "model-name",
+        modelParameters: { key: "value" },
+        input: { key: "input" },
+        output: { key: "output" },
+        version: "2.0.0",
+        type: "SPAN",
+        project: {
+          connect: { id: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a" },
+        },
+      },
+    });
+
+    const fetchedObservations = await makeAPICall(
+      "GET",
+      "/api/public/observations?type=GENERATION",
+      undefined,
+    );
+
+    expect(fetchedObservations.status).toBe(200);
+
+    if (!isObservationList(fetchedObservations.body)) {
+      throw new Error("Expected body to be an array of observations");
+    }
+
+    expect(fetchedObservations.body.data.length).toBe(1);
+    expect(fetchedObservations.body.data[0]?.traceId).toBe(traceId);
+    expect(fetchedObservations.body.data[0]?.input).toEqual({ key: "input" });
+    expect(fetchedObservations.body.data[0]?.output).toEqual({ key: "output" });
+    expect(fetchedObservations.body.data[0]?.type).toEqual("GENERATION");
   });
 });
 
