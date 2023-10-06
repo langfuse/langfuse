d155399c89073e2b6179432c2bcfcd9b99bf6780
Max Deichmann <m.deichmann@tum.de>
2023-10-06 13:14:10 -0700

    test

--- src/__tests__/observations.servertest.ts
+++ src/__tests__/observations.servertest.ts
@@ -3,6 +3,7 @@
 import { prisma } from "@/src/server/db";
 import { makeAPICall, pruneDatabase } from "@/src/__tests__/test-utils";
 import { v4 as uuidv4 } from "uuid";
+import { type Observation } from "@prisma/client";
 
 describe("/api/public/generations API Endpoint", () => {
   beforeEach(async () => await pruneDatabase());
@@ -34,9 +35,13 @@ describe("/api/public/generations API Endpoint", () => {
         endTime: new Date("2021-01-01T00:00:00.000Z"),
         model: "model-name",
         modelParameters: { key: "value" },
-        input: { key: "value" },
-        metadata: { key: "value" },
+        input: { key: "input" },
+        metadata: { key: "output" },
         version: "2.0.0",
+        type: "GENERATION",
+        project: {
+          connect: { id: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a" },
+        },
       },
     });
 
@@ -48,7 +53,32 @@ describe("/api/public/generations API Endpoint", () => {
 
     expect(fetchedObservations.status).toBe(200);
 
-    expect(fetchedObservations.body?.length).toBe(1);
-    expect(fetchedObservations.body[0]?.trace_id).toBe(traceId);
+    if (!isObservationList(fetchedObservations.body)) {
+      throw new Error("Expected body to be an array of observations");
+    }
+
+    expect(fetchedObservations.body.length).toBe(1);
+    expect(fetchedObservations.body[0]?.traceId).toBe(traceId);
+    expect(fetchedObservations.body[0]?.input).toBe({ key: "input" });
+    expect(fetchedObservations.body[0]?.output).toBe({ key: "output" });
   });
 });
+
+const isObservationList = (val: unknown): val is Observation[] =>
+  Array.isArray(val) &&
+  val.every(
+    (element) =>
+      typeof element === "object" &&
+      element !== null &&
+      "id" in element &&
+      "traceId" in element &&
+      "name" in element &&
+      "startTime" in element &&
+      "endTime" in element &&
+      "model" in element &&
+      "modelParameters" in element &&
+      "input" in element &&
+      "output" in element &&
+      "metadata" in element &&
+      "version" in element,
+  );
