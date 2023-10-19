4c569fe4b09dae25e7a8f24603708317a0c6d71f
Max Deichmann <m.deichmann@tum.de>
2023-10-19 16:48:50 +0200

    Api auth (#382)

---
+++ src/__tests__/api-auth.servertest.ts
@@ -0,0 +1,115 @@
+import {
+  getDisplaySecretKey,
+  hashSecretKey,
+} from "@/src/features/public-api/lib/apiKeys";
+import { verifyAuthHeaderAndReturnScope } from "@/src/features/public-api/server/apiAuth";
+import { prisma } from "@/src/server/db";
+
+describe("Validate api calls", () => {
+  beforeEach(async () => {
+    await prisma.score.deleteMany();
+    await prisma.observation.deleteMany();
+    await prisma.trace.deleteMany();
+    await prisma.apiKey.deleteMany();
+  });
+
+  it("should create new api key", async () => {
+    await createAPIKey();
+    const auth = await verifyAuthHeaderAndReturnScope(
+      "Basic cGstbGYtMTIzNDU2Nzg5MDpzay1sZi0xMjM0NTY3ODkw",
+    );
+    expect(auth.validKey).toBe(true);
+
+    const apiKey = await prisma.apiKey.findUnique({
+      where: { publicKey: "pk-lf-1234567890" },
+    });
+    expect(apiKey).not.toBeNull();
+    expect(apiKey?.fastHashedSecretKey).not.toBeNull();
+  });
+
+  it("should create new api key and succeed with new key", async () => {
+    await createAPIKey();
+    const auth = await verifyAuthHeaderAndReturnScope(
+      "Basic cGstbGYtMTIzNDU2Nzg5MDpzay1sZi0xMjM0NTY3ODkw",
+    );
+    expect(auth.validKey).toBe(true);
+
+    const apiKey = await prisma.apiKey.findUnique({
+      where: { publicKey: "pk-lf-1234567890" },
+    });
+    expect(apiKey).not.toBeNull();
+    expect(apiKey?.fastHashedSecretKey).not.toBeNull();
+
+    const auth2 = await verifyAuthHeaderAndReturnScope(
+      "Basic cGstbGYtMTIzNDU2Nzg5MDpzay1sZi0xMjM0NTY3ODkw",
+    );
+    expect(auth2.validKey).toBe(true);
+  });
+
+  it("should fail on wrong api key with new key", async () => {
+    await createAPIKey();
+    const auth = await verifyAuthHeaderAndReturnScope(
+      "Basic cGstbGYtMTIzNDU2Nzg5MDpzay1sZi0xMjM0NTY3ODkw",
+    );
+    expect(auth.validKey).toBe(true);
+
+    const apiKey = await prisma.apiKey.findUnique({
+      where: { publicKey: "pk-lf-1234567890" },
+    });
+    expect(apiKey).not.toBeNull();
+    expect(apiKey?.fastHashedSecretKey).not.toBeNull();
+
+    const wrongAuth = await verifyAuthHeaderAndReturnScope(
+      "Basic cGstbGYtMTIzNDU2Nzg5MDpzay1sZi0xMjM0NTY3ODkx",
+    );
+    expect(wrongAuth.validKey).toBe(false);
+  });
+
+  it("should fail on wrong api key without new key", async () => {
+    await createAPIKey();
+    const initialApiKey = await prisma.apiKey.findUnique({
+      where: { publicKey: "pk-lf-1234567890" },
+    });
+    expect(initialApiKey).not.toBeNull();
+    expect(initialApiKey?.fastHashedSecretKey).toBeNull();
+
+    const auth = await verifyAuthHeaderAndReturnScope(
+      "Basic cGstbGYtMTIzNDU2Nzg5MDpzay1sZi0xMjM0NTY3ODkx",
+    );
+    expect(auth.validKey).toBe(false);
+
+    const apiKey = await prisma.apiKey.findUnique({
+      where: { publicKey: "pk-lf-1234567890" },
+    });
+    expect(apiKey).not.toBeNull();
+    expect(apiKey?.fastHashedSecretKey).toBeNull();
+  });
+
+  const createAPIKey = async () => {
+    const seedApiKey = {
+      id: "seed-api-key",
+      secret: "sk-lf-1234567890",
+      public: "pk-lf-1234567890",
+      note: "seeded key",
+    };
+    await prisma.apiKey.create({
+      data: {
+        note: seedApiKey.note,
+        id: seedApiKey.id,
+        publicKey: seedApiKey.public,
+        hashedSecretKey: await hashSecretKey(seedApiKey.secret),
+        displaySecretKey: getDisplaySecretKey(seedApiKey.secret),
+        project: {
+          connect: {
+            id: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+          },
+        },
+      },
+    });
+  };
+
+  afterAll(async () => {
+    await prisma.apiKey.deleteMany();
+    await createAPIKey();
+  });
+});
--- src/env.mjs
+++ src/env.mjs
@@ -29,6 +29,10 @@ export const env = createEnv({
     LANGFUSE_NEW_USER_SIGNUP_WEBHOOK: z.string().url().optional(),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     LANGFUSE_ENABLE_EXPERIMENTAL_FEATURES: z.enum(["true", "false"]).optional(),
+    SALT: z.string({
+      required_error:
+        "A strong Salt is required to encrypt API keys securely. See: https://langfuse.com/docs/deployment/self-host#deploy-the-container",
+    }),
   },
 
   /**
@@ -63,5 +67,6 @@ export const env = createEnv({
     LANGFUSE_TEAM_SLACK_WEBHOOK: process.env.LANGFUSE_TEAM_SLACK_WEBHOOK,
     LANGFUSE_NEW_USER_SIGNUP_WEBHOOK:
       process.env.LANGFUSE_NEW_USER_SIGNUP_WEBHOOK,
+    SALT: process.env.SALT,
   },
 });
