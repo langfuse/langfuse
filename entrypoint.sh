f292af46f6a6ebb0e7e46a98bc64d99d298ef82d
Marc Klingen <git@marcklingen.com>
2023-10-31 18:26:29 +0100

    Improve docker: exit if db migrations fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,13 @@ export DIRECT_URL=$DATABASE_URL
 
 # Apply migrations
 prisma migrate deploy
+status=$?
+
+# If migration fails (returns non-zero exit status), exit script with that status
+if [ $status -ne 0 ]; then
+    echo "Applying database migrations failed. Exiting..."
+    exit $status
+fi
 
 # Start background cron job
 node cron.js &
