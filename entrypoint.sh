4c05cd62c8fa67e24473031c96953d937acf97b2
Marc Klingen <git@marcklingen.com>
2023-10-31 14:49:55 +0100

    Fix DIRECT_URL env for migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Set DIRECT_URL to the value of DATABASE_URL, required for migrations
+export DIRECT_URL=$DATABASE_URL
+
+# Apply migrations
 prisma migrate deploy
+
+# Start background cron job
 node cron.js &
+
+# Start server
 node server.js
