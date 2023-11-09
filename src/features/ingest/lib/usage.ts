191f5066888179268cff7789365585be6117e731
Max Deichmann <m.deichmann@tum.de>
2023-11-09 17:52:21 +0100

    add test

--- src/features/ingest/lib/usage.ts
+++ src/features/ingest/lib/usage.ts
@@ -183,7 +183,7 @@ function isOpenAiModel(model: string): model is TiktokenModel {
       "gpt-4-32k-0314",
       "gpt-4-32k-0613",
       "gpt-4-1106-preview",
-      "gpt-4-1106-vision-preview",
+      "gpt-4-vision-preview",
       "gpt-3.5",
       "gpt-3.5-turbo",
       "gpt-3.5-turbo-0301",
