b4ede4f620b74d8482f51a1649f5d3f4600b9f18
Max Deichmann <m.deichmann@tum.de>
2023-11-09 17:55:56 +0100

    Add gpt4 to token calculations (#459)
    * add
    
    * add test

--- src/features/ingest/lib/usage.ts
+++ src/features/ingest/lib/usage.ts
@@ -182,6 +182,8 @@ function isOpenAiModel(model: string): model is TiktokenModel {
       "gpt-4-32k",
       "gpt-4-32k-0314",
       "gpt-4-32k-0613",
+      "gpt-4-1106-preview",
+      "gpt-4-vision-preview",
       "gpt-3.5",
       "gpt-3.5-turbo",
       "gpt-3.5-turbo-0301",
