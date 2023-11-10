bb1dfaa6b236e769572073d2b7c921e9bc650314
Marc Klingen <git@marcklingen.com>
2023-11-10 01:20:03 +0100

    observation created_at

---
+++ prisma/migrations/20231110001631_observation_created_at/migration.sql
@@ -0,0 +1,14 @@
+-- Overall goal
+-- ALTER TABLE "observations" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- Crete column
+ALTER TABLE "observations" ADD COLUMN "created_at" TIMESTAMP(3);
+
+-- Backfill column, set to start_time
+UPDATE "observations" SET "created_at" = "start_time";
+
+-- Set default value
+ALTER TABLE "observations" ALTER COLUMN "created_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- Add not null constraint
+ALTER TABLE "observations" ALTER COLUMN "created_at" SET NOT NULL;
