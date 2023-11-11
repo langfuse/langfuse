a53b82a7fc1af305c707bcfc2ee7f71096aa7f01
Max Deichmann <m.deichmann@tum.de>
2023-11-11 10:15:04 +0100

    add test

--- src/__tests__/ingestion.servertest.ts
+++ src/__tests__/ingestion.servertest.ts
@@ -308,4 +308,50 @@ describe("/api/public/ingestion API Endpoint", () => {
     expect(observation?.promptTokens).toEqual(5);
     expect(observation?.completionTokens).toEqual(11);
   });
+
+  it("null does not override set values", async () => {
+    const traceId = v4();
+
+    const responseOne = await makeAPICall("POST", "/api/public/ingestion", {
+      batch: [
+        {
+          id: v4(),
+          type: "trace",
+          body: {
+            id: traceId,
+            name: "trace-name",
+            userId: "user-1",
+            projectId: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+            metadata: { key: "value" },
+            release: "1.0.0",
+            version: "2.0.0",
+          },
+        },
+        {
+          id: v4(),
+          type: "trace",
+          body: {
+            id: traceId,
+            name: "trace-name",
+            userId: "user-1",
+            projectId: "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a",
+            metadata: { key: "value" },
+            release: null,
+            version: null,
+          },
+        },
+      ],
+    });
+    expect(responseOne.status).toBe(201);
+
+    const dbTrace = await prisma.trace.findMany({
+      where: {
+        name: "trace-name",
+      },
+    });
+
+    expect(dbTrace.length).toEqual(1);
+    expect(dbTrace[0]?.release).toBe("1.0.0");
+    expect(dbTrace[0]?.version).toBe("2.0.0");
+  });
 });
