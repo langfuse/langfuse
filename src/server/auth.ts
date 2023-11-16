3da9a7cfdccb09b99f6c27b7c9cd1c94446a87b1
Robert Sturla <robertsturla@outlook.com>
2023-11-16 19:30:48 +0000

    Add safety block in the server to prevent any malicious attempts to use local credentials

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -53,9 +53,19 @@ const providers: Provider[] = [
     async authorize(credentials, _req) {
       if (!credentials) throw new Error("No credentials");
 
+      const internalCredentialsDisabled = env.NEXT_PUBLIC_INTERNAL_CREDENTIALS_DISABLED;
       const blockedDomains =
         env.AUTH_DOMAINS_WITH_SSO_ENFORCEMENT?.split(",") ?? [];
       const domain = credentials.email.split("@")[1]?.toLowerCase();
+
+      // Should never reach here under normal circumstances, but block attempts to sign in
+      // with internal credentials in the server
+      if (domain && internalCredentialsDisabled) {
+        throw new Error(
+          "Sign in with email and password is disabled for this instance. Please use SSO.",
+        );
+      }
+
       if (domain && blockedDomains.includes(domain)) {
         throw new Error(
           "Sign in with email and password is disabled for this domain. Please use SSO.",
