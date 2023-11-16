cae28bb202d807157419fde31d7f9c1664ad8d53
Marc Klingen <git@marcklingen.com>
2023-11-16 23:36:51 +0100

    feat(ui): add more string filters Types: contains, starts with, ends with

--- src/server/api/interfaces/filters.ts
+++ src/server/api/interfaces/filters.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const filterOperators = {
   datetime: [">", "<", ">=", "<="],
-  string: ["="],
+  string: ["=", "contains", "starts with", "ends with"],
   stringOptions: ["any of", "none of"],
   number: ["=", ">", "<", ">=", "<="],
   stringObject: ["="],
