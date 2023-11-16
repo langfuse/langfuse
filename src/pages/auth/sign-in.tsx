19e9913cc8ebbe844edf798780b354c30e740b06
Robert Sturla <robertsturla@outlook.com>
2023-11-16 22:53:15 +0000

    Revert useless formatting change

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -100,8 +100,7 @@ export default function SignIn(props: PageProps) {
 
         <div className="mt-14 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="divide-y bg-white p-6 py-6 shadow sm:rounded-lg sm:px-12">
-            {props.authProviders.credentials
-             ? (
+            {props.authProviders.credentials ? (
               <Form {...credentialsForm}>
                 <form
                   className="space-y-6 py-6"
