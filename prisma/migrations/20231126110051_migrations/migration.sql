af633d689279d72177491d1346747c305d02a3f9
Marc Klingen <git@marcklingen.com>
2023-11-28 00:43:58 +0100

    merge migrations

--- prisma/migrations/20231126110051_migrations/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "traces" ADD COLUMN     "bookmark" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20231126215430_bookmarked/migration.sql
+++ prisma/migrations/20231126215430_bookmarked/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
 
-  - You are about to drop the column `bookmark` on the `traces` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "traces" DROP COLUMN "bookmark",
-ADD COLUMN     "bookmarked" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "traces" ADD COLUMN "bookmarked" BOOLEAN NOT NULL DEFAULT false;
