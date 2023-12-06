fa1a51041e2c69b40481c8f46823a96b9de73c79
Max Deichmann <m.deichmann@tum.de>
2023-12-06 20:47:54 +0100

    remove logs

--- src/pages/api/public/generations.ts
+++ src/pages/api/public/generations.ts
@@ -58,12 +58,6 @@ export default async function handler(
         body: convertToObservation(GenerationsCreateSchema.parse(req.body)),
       };
 
-      console.log(
-        "event",
-        event,
-        GenerationsCreateSchema.parse(req.body).usage,
-      );
-
       const result = await handleBatch(
         ingestionBatch.parse([event]),
         req,
