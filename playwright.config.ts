194f6da1f3b8074d335e5de927fd7ddcea9eb0c5
Max Deichmann <m.deichmann@tum.de>
2023-12-18 17:46:49 +0100

    add

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   use: {
@@ -12,4 +12,37 @@ export default defineConfig({
     stdout: "ignore",
     stderr: "pipe",
   },
+  projects: [
+    /* Test against desktop browsers */
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    /* Test against mobile viewports. */
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
+    /* Test against branded browsers. */
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" }, // or 'chrome-beta'
+    },
+    {
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" }, // or 'msedge-dev'
+    },
+  ],
 });
