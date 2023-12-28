e6e6cdab6f95c28a80d9d347ed38b056d9235bb8
Max Deichmann <m.deichmann@tum.de>
2023-12-28 23:42:31 +0100

    add prompt api

--- src/features/prompts/server/prompt-router.ts
+++ src/features/prompts/server/prompt-router.ts
@@ -5,6 +5,14 @@ import {
   protectedProjectProcedure,
 } from "@/src/server/api/trpc";
 import { throwIfNoAccess } from "@/src/features/rbac/utils/checkAccess";
+import { type PrismaClient } from "@prisma/client";
+
+export const CreatePrompt = z.object({
+  projectId: z.string(),
+  name: z.string(),
+  isActive: z.boolean(),
+  prompt: z.string(),
+});
 
 export const promptRouter = createTRPCRouter({
   all: protectedProjectProcedure
@@ -19,25 +27,11 @@ export const promptRouter = createTRPCRouter({
           projectId: input.projectId,
         },
         orderBy: [{ name: "asc" }, { version: "desc" }],
-        include: {
-          user: {
-            select: {
-              email: true,
-            },
-          },
-        },
       });
     }),
 
   create: protectedProjectProcedure
-    .input(
-      z.object({
-        projectId: z.string(),
-        name: z.string(),
-        isActive: z.boolean(),
-        prompt: z.string(),
-      }),
-    )
+    .input(CreatePrompt)
     .mutation(async ({ input, ctx }) => {
       try {
         throwIfNoAccess({
@@ -46,55 +40,80 @@ export const promptRouter = createTRPCRouter({
           scope: "prompts:CUD",
         });
 
-        const latestPrompt = await ctx.prisma.prompt.findFirst({
-          where: {
-            projectId: input.projectId,
-            name: input.name,
-          },
-          orderBy: [{ version: "desc" }],
-          take: 1,
+        return await createPrompt({
+          projectId: input.projectId,
+          name: input.name,
+          prompt: input.prompt,
+          isActive: input.isActive,
+          createdBy: ctx.session.user.id,
+          prisma: ctx.prisma,
         });
-
-        const latestActivePrompt = await ctx.prisma.prompt.findFirst({
-          where: {
-            projectId: input.projectId,
-            name: input.name,
-            isActive: true,
-          },
-          orderBy: [{ version: "desc" }],
-          take: 1,
-        });
-
-        const create = [
-          ctx.prisma.prompt.create({
-            data: {
-              prompt: input.prompt,
-              name: input.name,
-              version: latestPrompt?.version ? latestPrompt.version + 1 : 1,
-              isActive: input.isActive,
-              project: { connect: { id: input.projectId } },
-              user: { connect: { id: ctx.session.user.id } },
-            },
-          }),
-        ];
-        if (latestActivePrompt)
-          create.push(
-            ctx.prisma.prompt.update({
-              where: {
-                id: latestActivePrompt.id,
-              },
-              data: {
-                isActive: false,
-              },
-            }),
-          );
-
-        const [prompt] = await ctx.prisma.$transaction(create);
-
-        return prompt;
       } catch (e) {
         console.log(e);
         throw e;
       }
     }),
 });
+
+export const createPrompt = async ({
+  projectId,
+  name,
+  prompt,
+  isActive = true,
+  createdBy,
+  prisma,
+}: {
+  projectId: string;
+  name: string;
+  prompt: string;
+  isActive?: boolean;
+  createdBy: string;
+  prisma: PrismaClient;
+}) => {
+  const latestPrompt = await prisma.prompt.findFirst({
+    where: {
+      projectId: projectId,
+      name: name,
+    },
+    orderBy: [{ version: "desc" }],
+    take: 1,
+  });
+
+  const latestActivePrompt = await prisma.prompt.findFirst({
+    where: {
+      projectId: projectId,
+      name: name,
+      isActive: true,
+    },
+    orderBy: [{ version: "desc" }],
+    take: 1,
+  });
+
+  const create = [
+    prisma.prompt.create({
+      data: {
+        prompt: prompt,
+        name: name,
+        version: latestPrompt?.version ? latestPrompt.version + 1 : 1,
+        isActive: isActive,
+        project: { connect: { id: projectId } },
+        createdBy: createdBy,
+      },
+    }),
+  ];
+  if (latestActivePrompt)
+    create.push(
+      prisma.prompt.update({
+        where: {
+          id: latestActivePrompt.id,
+        },
+        data: {
+          isActive: false,
+        },
+      }),
+    );
+
+  const [createdPrompt] = await prisma.$transaction(create);
+
+  return createdPrompt;
+};
