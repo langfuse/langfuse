27f733ad201bb46b763bc7fbda42e7ecd58a3681
Tameem Asim <tmeemu15@gmail.com>
2024-01-02 00:50:39 +0500

    feat: foldable and unfoldable observation tree

--- src/components/trace/ObservationTree.tsx
+++ src/components/trace/ObservationTree.tsx
@@ -2,10 +2,11 @@ import { type NestedObservation } from "@/src/utils/types";
 import { cn } from "@/src/utils/tailwind";
 import { type Trace, type Score } from "@prisma/client";
 import { GroupedScoreBadges } from "@/src/components/grouped-score-badge";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { type ObservationReturnType } from "@/src/server/api/routers/traces";
 import { LevelColors } from "@/src/components/level-colors";
 import { formatInterval } from "@/src/utils/dates";
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export const ObservationTree = (props: {
   observations: ObservationReturnType[];
@@ -15,6 +16,10 @@ export const ObservationTree = (props: {
   setCurrentObservationId: (id: string | undefined) => void;
 }) => {
   const nestedObservations = nestObservations(props.observations);
+
+  const [observationTreeNodeHidden, setObservationTreeNodeHidden] =
+    useState(false);
+
   return (
     <div className="flex flex-col">
       <ObservationTreeTraceNode
@@ -22,14 +27,18 @@ export const ObservationTree = (props: {
         scores={props.scores}
         currentObservationId={props.currentObservationId}
         setCurrentObservationId={props.setCurrentObservationId}
+        childHidden={observationTreeNodeHidden}
+        setChildHidden={setObservationTreeNodeHidden}
       />
-      <ObservationTreeNode
-        observations={nestedObservations}
-        scores={props.scores}
-        indentationLevel={1}
-        currentObservationId={props.currentObservationId}
-        setCurrentObservationId={props.setCurrentObservationId}
-      />
+      {!observationTreeNodeHidden && (
+        <ObservationTreeNode
+          observations={nestedObservations}
+          scores={props.scores}
+          indentationLevel={1}
+          currentObservationId={props.currentObservationId}
+          setCurrentObservationId={props.setCurrentObservationId}
+        />
+      )}
     </div>
   );
 };
@@ -38,6 +47,8 @@ const ObservationTreeTraceNode = (props: {
   scores: Score[];
   currentObservationId: string | undefined;
   setCurrentObservationId: (id: string | undefined) => void;
+  childHidden: boolean;
+  setChildHidden: (value: boolean) => void;
 }) => (
   <div
     className={cn(
@@ -50,6 +61,17 @@ const ObservationTreeTraceNode = (props: {
     onClick={() => props.setCurrentObservationId(undefined)}
   >
     <div className="flex gap-2">
+      {props.childHidden ? (
+        <PlusIcon
+          className="border border-black"
+          onClick={() => props.setChildHidden(false)}
+        />
+      ) : (
+        <MinusIcon
+          className="border border-black"
+          onClick={() => props.setChildHidden(true)}
+        />
+      )}
       <span className={cn("rounded-sm bg-gray-200 p-1 text-xs")}>TRACE</span>
       <span>{props.trace.name}</span>
     </div>
@@ -76,111 +98,138 @@ const ObservationTreeNode = (props: {
   indentationLevel: number;
   currentObservationId: string | undefined;
   setCurrentObservationId: (id: string | undefined) => void;
-}) => (
-  <>
-    {props.observations
-      .sort((a, b) => a.startTime.getTime() - b.startTime.getTime())
-      .map((observation) => (
-        <Fragment key={observation.id}>
-          <div className="flex w-full">
-            {props.indentationLevel === 1 && (
-              <div className="mx-2 border-r border-yellow-300 lg:mr-4" />
-            )}
-            <div className="flex w-full flex-col">
-              <div
-                className={cn(
-                  "group my-1 flex flex-1 cursor-pointer flex-col gap-1 rounded-sm p-2 ",
-                  props.currentObservationId === observation.id
-                    ? "border-2 border-gray-600"
-                    : "hover:scale-105",
-                  observation.type === "SPAN"
-                    ? "bg-pink-300"
-                    : observation.type === "GENERATION"
-                      ? "bg-blue-300"
-                      : "bg-green-300",
-                )}
-                onClick={() => props.setCurrentObservationId(observation.id)}
-              >
-                <div className="flex gap-2">
-                  <span
-                    className={cn(
-                      "self-start rounded-sm bg-gray-200 p-1 text-xs",
-                    )}
-                  >
-                    {observation.type}
-                  </span>
-                  <span className="line-clamp-1">{observation.name}</span>
-                </div>
-                <div className="flex gap-2">
-                  {observation.endTime ? (
-                    <span className="text-xs text-gray-500">
-                      {formatInterval(
-                        (observation.endTime.getTime() -
-                          observation.startTime.getTime()) /
-                          1000,
-                      )}
-                    </span>
-                  ) : null}
-                  {observation.promptTokens ||
-                  observation.completionTokens ||
-                  observation.totalTokens ? (
-                    <span className="text-xs text-gray-500">
-                      {observation.promptTokens} →{" "}
-                      {observation.completionTokens} (∑{" "}
-                      {observation.totalTokens})
-                    </span>
-                  ) : null}
-                </div>
-                {observation.level !== "DEFAULT" ? (
-                  <div className="flex">
-                    <span
-                      className={cn(
-                        "rounded-sm p-0.5 text-xs",
-                        LevelColors[observation.level].bg,
-                        LevelColors[observation.level].text,
-                      )}
-                    >
-                      {observation.level}
-                    </span>
-                  </div>
-                ) : null}
-                {props.scores.find(
-                  (s) => s.observationId === observation.id,
-                ) ? (
-                  <div className="flex flex-wrap gap-1">
-                    <GroupedScoreBadges
-                      scores={props.scores.filter(
-                        (s) => s.observationId === observation.id,
-                      )}
-                    />
-                  </div>
-                ) : null}
-              </div>
-              <div className="flex">
+}) => {
+  const [childHidden, setChildHidden] = useState(true);
+
+  return (
+    <>
+      {props.observations
+        .sort((a, b) => a.startTime.getTime() - b.startTime.getTime())
+        .map((observation) => (
+          <Fragment key={observation.id}>
+            <div className="flex w-full">
+              {props.indentationLevel === 1 && (
+                <div className="mx-2 border-r border-yellow-300 lg:mr-4" />
+              )}
+              <div className="flex w-full flex-col">
                 <div
                   className={cn(
-                    "mx-2 border-r lg:mr-4",
+                    "group my-1 flex flex-1 cursor-pointer flex-col gap-1 rounded-sm p-2 ",
+                    props.currentObservationId === observation.id
+                      ? "border-2 border-gray-600"
+                      : "hover:scale-105",
                     observation.type === "SPAN"
-                      ? "border-pink-300"
-                      : observation.type == "GENERATION"
-                        ? "border-blue-300"
-                        : "border-green-300",
+                      ? "bg-pink-300"
+                      : observation.type === "GENERATION"
+                        ? "bg-blue-300"
+                        : "bg-green-300",
                   )}
-                />
-                <ObservationTreeNode
-                  observations={observation.children}
-                  scores={props.scores}
-                  indentationLevel={props.indentationLevel + 1}
-                  currentObservationId={props.currentObservationId}
-                  setCurrentObservationId={props.setCurrentObservationId}
-                />
+                  onClick={() => props.setCurrentObservationId(observation.id)}
+                >
+                  <div className="flex gap-2">
+                    {childHidden
+                      ? observation.children.length > 0 && (
+                          <PlusIcon
+                            className="border border-black"
+                            onClick={(event) => {
+                              event.stopPropagation();
+                              setChildHidden(false);
+                            }}
+                          />
+                        )
+                      : observation.children.length > 0 && (
+                          <MinusIcon
+                            className="border border-black"
+                            onClick={(event) => {
+                              event.stopPropagation();
+                              setChildHidden(true);
+                            }}
+                          />
+                        )}
+                    <span
+                      className={cn(
+                        "self-start rounded-sm bg-gray-200 p-1 text-xs",
+                      )}
+                    >
+                      {observation.type}
+                    </span>
+                    <span className="line-clamp-1">{observation.name}</span>
+                  </div>
+                  <div className="flex gap-2">
+                    {observation.endTime ? (
+                      <span className="text-xs text-gray-500">
+                        {formatInterval(
+                          (observation.endTime.getTime() -
+                            observation.startTime.getTime()) /
+                            1000,
+                        )}
+                      </span>
+                    ) : null}
+                    {observation.promptTokens ||
+                    observation.completionTokens ||
+                    observation.totalTokens ? (
+                      <span className="text-xs text-gray-500">
+                        {observation.promptTokens} →{" "}
+                        {observation.completionTokens} (∑{" "}
+                        {observation.totalTokens})
+                      </span>
+                    ) : null}
+                  </div>
+                  {observation.level !== "DEFAULT" ? (
+                    <div className="flex">
+                      <span
+                        className={cn(
+                          "rounded-sm p-0.5 text-xs",
+                          LevelColors[observation.level].bg,
+                          LevelColors[observation.level].text,
+                        )}
+                      >
+                        {observation.level}
+                      </span>
+                    </div>
+                  ) : null}
+                  {props.scores.find(
+                    (s) => s.observationId === observation.id,
+                  ) ? (
+                    <div className="flex flex-wrap gap-1">
+                      <GroupedScoreBadges
+                        scores={props.scores.filter(
+                          (s) => s.observationId === observation.id,
+                        )}
+                      />
+                    </div>
+                  ) : null}
+                </div>
+                <div className="flex">
+                  {!childHidden && (
+                    <>
+                      <div
+                        className={cn(
+                          "mx-2 border-r lg:mr-4",
+                          observation.type === "SPAN"
+                            ? "border-pink-300"
+                            : observation.type == "GENERATION"
+                              ? "border-blue-300"
+                              : "border-green-300",
+                        )}
+                      />
+                      <ObservationTreeNode
+                        observations={observation.children}
+                        scores={props.scores}
+                        indentationLevel={props.indentationLevel + 1}
+                        currentObservationId={props.currentObservationId}
+                        setCurrentObservationId={props.setCurrentObservationId}
+                      />
+                    </>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </Fragment>
-      ))}
-  </>
-);
+          </Fragment>
+        ))}
+    </>
+  );
+};
 export function nestObservations(
   list: ObservationReturnType[],
 ): NestedObservation[] {
