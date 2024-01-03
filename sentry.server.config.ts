f244e16aa24997483ac4cdf3b7ffeb4fdcb56fc0
Max Deichmann <m.deichmann@tum.de>
2024-01-03 14:37:41 +0100

    refactor: reduce sentry sample rate (#798)
    sample

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -12,9 +12,9 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN)
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 0.8,
+    tracesSampleRate: 0.5,
 
-    profilesSampleRate: 0.8, // Profiling sample rate is relative to tracesSampleRate
+    profilesSampleRate: 0.5, // Profiling sample rate is relative to tracesSampleRate
     integrations: [
       // Add profiling integration to list of integrations
       new ProfilingIntegration(),
