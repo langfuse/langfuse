a108a1508e6e70c2a4537b2fa4698072d02601a9
Max Deichmann <m.deichmann@tum.de>
2024-01-04 22:09:04 +0100

    fix

--- prisma/migrations/20240104210051_add_model_indices/migration.sql
+++ prisma/migrations/20240104210051_add_model_indices/migration.sql
@@ -1,5 +1,3 @@
 -- CreateIndex
 CREATE INDEX CONCURRENTLY "observations_model_idx" ON "observations"("model");
 
--- CreateIndex
-CREATE INDEX CONCURRENTLY "pricings_model_name_idx" ON "pricings"("model_name");
