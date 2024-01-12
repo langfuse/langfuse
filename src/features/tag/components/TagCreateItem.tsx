99da59824500ebc6db6b4228230f35ae4bdc9c67
Richard <richard.kruemmel@code.berlin>
2024-01-12 20:42:38 +0100

    Refactor TagCreateItem component

--- src/features/tag/components/TagCreateItem.tsx
+++ src/features/tag/components/TagCreateItem.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { cn } from "@/src/utils/tailwind";
 import { CommandItem } from "cmdk";
 
-export const TagItemCreate = ({
-  inputValue,
-  options,
-  onSelect,
-  onMouseDown,
-}: {
+type TagItemCreateProps = {
   inputValue: string;
   options: string[];
   onSelect: () => void;
-  onMouseDown: (event: React.MouseEvent<HTMLElement>) => void;
-}) => {
+};
+
+const TagItemCreate = ({
+  inputValue,
+  options,
+  onSelect,
+}: TagItemCreateProps) => {
   const hasNoOption = !options
     .map((value) => value.toLowerCase())
     .includes(inputValue.toLowerCase());
@@ -27,10 +27,11 @@ export const TagItemCreate = ({
       value={inputValue}
       className="flex cursor-pointer items-center rounded-sm p-2 px-1 py-2 text-muted-foreground hover:bg-secondary/80"
       onSelect={onSelect}
-      onMouseDown={onMouseDown}
     >
       <div className={cn("mr-2 h-4 w-4")} />
       Create new tag: &quot;{inputValue}&quot;
     </CommandItem>
   );
 };
+
+export default TagItemCreate;
