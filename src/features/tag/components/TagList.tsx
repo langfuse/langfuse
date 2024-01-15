e0690321a52a983d1cee018cddd897e845d8e869
Richard <richard.kruemmel@code.berlin>
2024-01-15 13:01:51 +0100

    Refactor TagList component

--- src/features/tag/components/TagList.tsx
+++ src/features/tag/components/TagList.tsx
@@ -1,4 +1,3 @@
-import { forwardRef } from "react";
 import { Button } from "@/src/components/ui/button";
 import { TagButton } from "@/src/features/tag/components/TagButton";
 
@@ -7,28 +6,20 @@ type TagListProps = {
   isLoading: boolean;
 };
 
-export const TagList = forwardRef<HTMLDivElement, TagListProps>(
-  ({ selectedTags, isLoading, ...props }: TagListProps, ref) => {
-    return (
-      <div ref={ref} className="flex flex-wrap gap-x-2 gap-y-1">
-        {selectedTags.length > 0 ? (
-          selectedTags.map((tag) => (
-            <TagButton {...props} key={tag} tag={tag} loading={isLoading} />
-          ))
-        ) : (
-          <Button
-            variant="outline"
-            size="xs"
-            className="text-xs font-bold opacity-0 hover:bg-white hover:opacity-100"
-          >
-            Add tag
-          </Button>
-        )}
-      </div>
-    );
-  },
-);
-
-TagList.displayName = "TagList";
+const TagList = ({ selectedTags, isLoading }: TagListProps) => {
+  return selectedTags.length > 0 ? (
+    selectedTags.map((tag) => (
+      <TagButton key={tag} tag={tag} loading={isLoading} />
+    ))
+  ) : (
+    <Button
+      variant="outline"
+      size="xs"
+      className="text-xs font-bold opacity-0 hover:bg-white hover:opacity-100"
+    >
+      Add tag
+    </Button>
+  );
+};
 
 export default TagList;
