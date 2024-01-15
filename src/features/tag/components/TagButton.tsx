bddb02a132de663730680b04545d444774a157d7
Richard <richard.kruemmel@code.berlin>
2024-01-15 13:01:38 +0100

    Refactor TagButton component

--- src/features/tag/components/TagButton.tsx
+++ src/features/tag/components/TagButton.tsx
@@ -2,14 +2,13 @@ import { Button } from "@/src/components/ui/button";
 import React from "react";
 
 export const TagButton: React.FC<{ tag: string; loading: boolean }> =
-  React.memo(({ tag, loading, ...props }) => (
+  React.memo(({ tag, loading }) => (
     <Button
       key={tag}
       variant="secondary"
       size="xs"
       className="text-xs font-semibold hover:bg-white"
       loading={loading}
-      {...props}
     >
       {tag}
     </Button>
