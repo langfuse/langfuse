04f0844a312ba05c9597c9665c988075b409cdb4
Max Deichmann <m.deichmann@tum.de>
2024-01-16 16:27:56 +0100

    upgrade decimal on export

--- src/server/api/routers/generations.ts
+++ src/server/api/routers/generations.ts
@@ -278,7 +278,7 @@ export const generationsRouter = createTRPCRouter({
                   generation.model ?? "",
                   generation.startTime.toISOString(),
                   generation.endTime?.toISOString() ?? "",
-                  generation.cost ? usdFormatter(generation.cost, 2, 4) : "",
+                  generation.cost ? usdFormatter(generation.cost, 2, 8) : "",
                   JSON.stringify(generation.input),
                   JSON.stringify(generation.output),
                   JSON.stringify(generation.metadata),
