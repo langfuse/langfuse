c91e608a195ce1421015f1b228c9e4e44c290c88
Max Deichmann <m.deichmann@tum.de>
2024-01-18 18:57:29 +0100

    fix all tests

--- prisma/migrations/20240118204937_add_observations_view/migration.sql
+++ prisma/migrations/20240118204937_add_observations_view/migration.sql
@@ -8,19 +8,19 @@ SELECT
     m.total_price,
     m.tokenizer_config AS "tokenizer_config",
     CASE 
-        WHEN o.input_cost IS NOT NULL OR o.output_cost IS NOT NULL OR o.total_cost IS NOT NULL THEN
+        WHEN o.input_cost IS NULL AND o.output_cost IS NULL AND o.total_cost IS NULL THEN
             (o.prompt_tokens::decimal / 1000) * m.input_price
         ELSE
             o.input_cost
     END AS "calculated_input_cost",
     CASE 
-        WHEN o.input_cost IS NOT NULL OR o.output_cost IS NOT NULL OR o.total_cost IS NOT NULL THEN
+        WHEN o.input_cost IS NULL AND o.output_cost IS NULL AND o.total_cost IS NULL THEN
             (o.completion_tokens::decimal / 1000) * m.output_price
         ELSE
             o.output_cost
     END AS "calculated_output_cost",
     CASE 
-        WHEN o.input_cost IS NOT NULL OR o.output_cost IS NOT NULL OR o.total_cost IS NOT NULL THEN
+        WHEN o.input_cost IS NULL AND o.output_cost IS NULL AND o.total_cost IS NULL THEN
             CASE 
                 WHEN m.total_price IS NOT NULL AND o.total_tokens IS NOT NULL THEN
                     m.total_price * o.total_tokens
