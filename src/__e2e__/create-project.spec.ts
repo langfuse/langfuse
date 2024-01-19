2dff05c40c0c25ca60388af9942d1b55db2b44f7
gitstart-langfuse <gitstart-langfuse@users.noreply.github.com>
2024-01-19 13:21:10 +0000

    update test to fix failing CI test

--- src/__e2e__/create-project.spec.ts
+++ src/__e2e__/create-project.spec.ts
@@ -18,7 +18,7 @@ test("should see new projects dialog open after clicking new project btn", async
 });
 
 test("Create a project with provided name", async ({ page }) => {
-  test.setTimeout(120000);
+  test.setTimeout(60000);
 
   await page.goto("/auth/sign-in");
   await page.fill('input[name="email"]', "demo@langfuse.com");
@@ -37,7 +37,7 @@ test("Create a project with provided name", async ({ page }) => {
   await page.waitForTimeout(2000);
   expect(page.url()).toContain("/project/");
   await page.waitForTimeout(2000);
-  expect(await page.getByTestId("project-title-span-2").textContent()).toBe(
+  expect(await page.getByTestId("project-name").textContent()).toContain(
     "my e2e demo project",
   );
 });
