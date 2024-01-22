36dbdbc7797debdda4df85c886669ffcbe09e9a3
RichardKruemmel <richard.kruemmel@code.berlin>
2024-01-22 18:14:06 +0100

    Refactor useColumnSizing function

--- src/features/column-sizing/hooks/useColumnSizing.ts
+++ src/features/column-sizing/hooks/useColumnSizing.ts
@@ -1,12 +1,7 @@
 import { useState, useEffect } from "react";
 import { type ColumnSizingState } from "@tanstack/react-table";
-import { type LangfuseColumnDef } from "@/src/components/table/types";
 
-function useColumnSizing<TData>(
-  localStorageKey: string,
-  // We currently do not examine passed columns, but we might in the future.
-  _columns: LangfuseColumnDef<TData>[],
-) {
+function useColumnSizing(localStorageKey: string) {
   const [columnSizing, setColumnSizing] = useState<ColumnSizingState>(() => {
     try {
       const savedSizing = localStorage.getItem(localStorageKey);
