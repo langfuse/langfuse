db10b647e88602f6923969bfd22b820366da9eda
Marc Klingen <git@marcklingen.com>
2024-01-22 22:05:48 +0100

    perf: native db upserts for TraceProcessor and ObservationProcessor (#920)

--- src/server/api/services/EventProcessor.ts
+++ src/server/api/services/EventProcessor.ts
@@ -57,10 +57,13 @@ export class ObservationProcessor implements EventProcessor {
     this.event = event;
   }
 
-  async convertToObservation(apiScope: ApiAccessScope): Promise<{
+  async convertToObservation(
+    apiScope: ApiAccessScope,
+    existingObservation: Observation | null,
+  ): Promise<{
     id: string;
-    create: Prisma.ObservationCreateInput;
-    update: Prisma.ObservationUpdateInput;
+    create: Prisma.ObservationUncheckedCreateInput;
+    update: Prisma.ObservationUncheckedUpdateInput;
   }> {
     const { body } = this.event;
 
@@ -84,12 +87,6 @@ export class ObservationProcessor implements EventProcessor {
     }
 
     const { id, traceId, name, startTime, metadata } = body;
-
-    const existingObservation = id
-      ? await prisma.observation.findUnique({
-          where: { id, projectId: apiScope.projectId },
-        })
-      : null;
 
     if (
       this.event.type === eventTypes.OBSERVATION_UPDATE &&
@@ -124,19 +121,25 @@ export class ObservationProcessor implements EventProcessor {
       metadata ?? undefined,
     );
 
-    const prompts =
+    const prompt =
       "promptName" in this.event.body &&
       typeof this.event.body.promptName === "string" &&
       "promptVersion" in this.event.body &&
       typeof this.event.body.promptVersion === "number"
-        ? await prisma.prompt.findMany({
+        ? await prisma.prompt.findUnique({
             where: {
-              projectId: apiScope.projectId,
-              name: this.event.body.promptName,
-              version: this.event.body.promptVersion,
+              projectId_name_version: {
+                projectId: apiScope.projectId,
+                name: this.event.body.promptName,
+                version: this.event.body.promptVersion,
+              },
             },
           })
         : undefined;
+
+    // Only null if promptName and promptVersion are set but prompt is not found
+    if (prompt === null)
+      console.warn("Prompt not found for observation", this.event.body);
 
     const observationId = id ?? v4();
     return {
@@ -174,10 +177,8 @@ export class ObservationProcessor implements EventProcessor {
         statusMessage: body.statusMessage ?? undefined,
         parentObservationId: body.parentObservationId ?? undefined,
         version: body.version ?? undefined,
-        project: { connect: { id: apiScope.projectId } },
-        ...(prompts && prompts.length === 1
-          ? { prompt: { connect: { id: prompts[0]?.id } } }
-          : undefined),
+        projectId: apiScope.projectId,
+        promptId: prompt ? prompt.id : undefined,
       },
       update: {
         name,
@@ -209,9 +210,7 @@ export class ObservationProcessor implements EventProcessor {
         statusMessage: body.statusMessage ?? undefined,
         parentObservationId: body.parentObservationId ?? undefined,
         version: body.version ?? undefined,
-        ...(prompts && prompts.length === 1
-          ? { prompt: { connect: { id: prompts[0]?.id } } }
-          : undefined),
+        promptId: prompt ? prompt.id : undefined,
       },
     };
   }
@@ -250,14 +249,28 @@ export class ObservationProcessor implements EventProcessor {
     if (apiScope.accessLevel !== "all")
       throw new AuthenticationError("Access denied for observation creation");
 
-    const obs = await this.convertToObservation(apiScope);
+    const existingObservation = this.event.body.id
+      ? await prisma.observation.findFirst({
+          where: { id: this.event.body.id },
+        })
+      : null;
 
+    if (
+      existingObservation &&
+      existingObservation.projectId !== apiScope.projectId
+    ) {
+      throw new AuthenticationError(
+        `Access denied for observation creation ${existingObservation.projectId} `,
+      );
+    }
+
+    const obs = await this.convertToObservation(apiScope, existingObservation);
+
+    // Do not use nested upserts or multiple where conditions as this should be a single native database upsert
+    // https://www.prisma.io/docs/orm/reference/prisma-client-reference#database-upserts
     return await prisma.observation.upsert({
       where: {
-        id_projectId: {
-          id: obs.id,
-          projectId: apiScope.projectId,
-        },
+        id: obs.id,
       },
       create: obs.create,
       update: obs.update,
@@ -290,15 +303,11 @@ export class TraceProcessor implements EventProcessor {
       body,
     );
 
-    const existingTrace = await prisma.trace.findUnique({
+    const existingTrace = await prisma.trace.findFirst({
       where: {
         id: internalId,
       },
     });
-
-    // access rights note:
-    // if trace exists, check if project id matches
-    // if trace does not exist, insert the trace with the projectId from scope
 
     if (existingTrace && existingTrace.projectId !== apiScope.projectId) {
       throw new AuthenticationError(
@@ -313,6 +322,24 @@ export class TraceProcessor implements EventProcessor {
       body.metadata ?? undefined,
     );
 
+    if (body.sessionId) {
+      await prisma.traceSession.upsert({
+        where: {
+          id_projectId: {
+            id: body.sessionId,
+            projectId: apiScope.projectId,
+          },
+        },
+        create: {
+          id: body.sessionId,
+          projectId: apiScope.projectId,
+        },
+        update: {},
+      });
+    }
+
+    // Do not use nested upserts or multiple where conditions as this should be a single native database upsert
+    // https://www.prisma.io/docs/orm/reference/prisma-client-reference#database-upserts
     const upsertedTrace = await prisma.trace.upsert({
       where: {
         id: internalId,
@@ -326,16 +353,9 @@ export class TraceProcessor implements EventProcessor {
         metadata: mergedMetadata ?? body.metadata ?? undefined,
         release: body.release ?? undefined,
         version: body.version ?? undefined,
-        session: body.sessionId
-          ? {
-              connectOrCreate: {
-                where: { id: body.sessionId, projectId: apiScope.projectId },
-                create: { id: body.sessionId, projectId: apiScope.projectId },
-              },
-            }
-          : undefined,
+        sessionId: body.sessionId ?? undefined,
         public: body.public ?? undefined,
-        project: { connect: { id: apiScope.projectId } },
+        projectId: apiScope.projectId,
         tags: body.tags ?? undefined,
       },
       update: {
@@ -346,14 +366,7 @@ export class TraceProcessor implements EventProcessor {
         metadata: mergedMetadata ?? body.metadata ?? undefined,
         release: body.release ?? undefined,
         version: body.version ?? undefined,
-        session: body.sessionId
-          ? {
-              connectOrCreate: {
-                where: { id: body.sessionId, projectId: apiScope.projectId },
-                create: { id: body.sessionId, projectId: apiScope.projectId },
-              },
-            }
-          : undefined,
+        sessionId: body.sessionId ?? undefined,
         public: body.public ?? undefined,
         tags: body.tags ?? undefined,
       },
