e72a344d05cc34e70b4d4f9487ebac67b9a5d8a8
Marc Klingen <git@marcklingen.com>
2024-01-23 20:34:13 +0100

    minor fixes

--- src/pages/project/[projectId]/models.tsx
+++ src/pages/project/[projectId]/models.tsx
@@ -3,7 +3,7 @@ import Header from "@/src/components/layouts/header";
 import { useRouter } from "next/router";
 import ModelTable from "@/src/components/table/use-cases/models";
 
-export default function ScoresPage() {
+export default function ModelsPage() {
   const router = useRouter();
   const projectId = router.query.projectId as string;
 
