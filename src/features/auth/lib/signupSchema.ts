d0bd8e9e2d4d559d0d5df39642872bd21b36e56c
RichardKruemmel <richard.kruemmel@code.berlin>
2024-01-23 15:10:37 +0100

    Refactor signupSchema to exclude HTML

--- src/features/auth/lib/signupSchema.ts
+++ src/features/auth/lib/signupSchema.ts
@@ -1,9 +1,14 @@
 import * as z from "zod";
 
+export const noHtmlRegex = /<[^>]*>/;
+
 export const signupSchema = z.object({
-  name: z.string().min(1, {
-    message: "Name is required",
-  }),
+  name: z
+    .string()
+    .min(1, { message: "Name is required" })
+    .refine((value) => !noHtmlRegex.test(value), {
+      message: "Input should not contain HTML",
+    }),
   email: z.string().email(),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters long",
