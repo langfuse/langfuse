291f36ec4bd27084ed8cbc340193b1b26487c0db
Marc Klingen <git@marcklingen.com>
2024-01-27 18:52:31 +0100

    feat(ui): custom model prices and tokenization (#954)

--- src/pages/project/[projectId]/models.tsx
+++ src/pages/project/[projectId]/models.tsx
@@ -2,10 +2,15 @@ import Header from "@/src/components/layouts/header";
 
 import { useRouter } from "next/router";
 import ModelTable from "@/src/components/table/use-cases/models";
+import { Button } from "@/src/components/ui/button";
+import { useHasAccess } from "@/src/features/rbac/utils/checkAccess";
+import { Lock } from "lucide-react";
+import Link from "next/link";
 
 export default function ModelsPage() {
   const router = useRouter();
   const projectId = router.query.projectId as string;
+  const hasWriteAccess = useHasAccess({ projectId, scope: "models:CUD" });
 
   return (
     <div>
@@ -16,6 +21,14 @@ export default function ModelsPage() {
             "A model represents a LLM model. It is used to calculate tokens and cost.",
           href: "https://langfuse.com/docs/token-usage",
         }}
+        actionButtons={
+          <Button disabled={!hasWriteAccess} asChild>
+            <Link href={`/project/${projectId}/models/new`}>
+              {!hasWriteAccess && <Lock size={16} className="mr-2" />}
+              Add model definition{" "}
+            </Link>
+          </Button>
+        }
       />
       <ModelTable projectId={projectId} />
     </div>
