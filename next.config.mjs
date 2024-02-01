c6f12200dc851536798f98d90db6603eb34c378c
Marc Klingen <git@marcklingen.com>
2024-02-01 12:38:54 +0100

    feat(security): add default Content-Security-Policy Fixes #1018

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ await import("./src/env.mjs");
 import { withSentryConfig } from "@sentry/nextjs";
 import { env } from "./src/env.mjs";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;`
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -30,6 +43,15 @@ const nextConfig = {
           {
             key: "x-frame-options",
             value: "SAMEORIGIN",
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
           },
         ],
       },
