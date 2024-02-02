108afa3fbf450b89f1eb012a58d66d0d2e712b18
RichardKruemmel <richard.kruemmel@code.berlin>
2024-02-02 18:12:51 +0100

    Add NOTIFICATIONS constants

---
+++ src/constants/NOTIFICATIONS.ts
@@ -0,0 +1,17 @@
+export const NOTIFICATIONS = [
+  {
+    id: 1,
+    releaseDate: "01.02.2024",
+    message: "Langfuse 2.1 just released :rocket: check it out",
+  },
+  {
+    id: 2,
+    releaseDate: "02.02.2024",
+    message: "Langfuse 2.2 just released 🚀 check it out",
+  },
+  {
+    id: 3,
+    releaseDate: "03.02.2024",
+    message: "Langfuse 2.3 just released 🚀 check it out",
+  },
+];
