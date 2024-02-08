7bab9e3151cf3ba5b9d9670b5ea330035a8c0cb6
RichardKruemmel <richard.kruemmel@code.berlin>
2024-02-08 15:15:13 +0100

    Implement Custom Notification UI

--- src/features/notifications/checkNotifications.tsx
+++ src/features/notifications/checkNotifications.tsx
@@ -5,6 +5,7 @@ import useLocalStorage from "@/src/components/useLocalStorage";
 
 export interface Notification {
   id: number;
+  // MM.DD.YYYY
   releaseDate: string;
   message: string | JSX.Element;
   description?: JSX.Element | string;
@@ -13,7 +14,7 @@ export interface Notification {
 export const NOTIFICATIONS: Notification[] = [
   {
     id: 1,
-    releaseDate: "01.02.2024",
+    releaseDate: "03/02/2024",
     message: "Langfuse 2.0 just released 🚀 check it out",
     description: (
       <Link href={"https://www.langfuse.com/changelog"}>
@@ -23,18 +24,27 @@ export const NOTIFICATIONS: Notification[] = [
   },
   {
     id: 2,
-    releaseDate: "05.02.2024",
-    message: "Langfuse 2.1 just released 🚀 check it out",
+    releaseDate: "05/02/2024",
+    message:
+      "Langfuse 2.1 just released 🚀 check the very long announcement notification out",
     description: (
-      <Link href={"https://www.langfuse.com/changelog"}>
-        Click to check out the new features and improvements
+      <Link
+        href={"https://www.langfuse.com/changelog"}
+        className="my-2 rounded-md border border-gray-800 px-2 py-1 text-gray-800"
+      >
+        View changes
       </Link>
     ),
   },
   {
     id: 3,
-    releaseDate: "06.02.2024",
-    message: "Langfuse 2.2 just released 🚀 check it out",
+    releaseDate: "06/02/2024",
+    message: "We got nominated for the Kitty Awards 🏆",
+    description: (
+      <Link href={"https://www.langfuse.com/changelog"}>
+        Vote for us and get a chance to win a lifetime subscription
+      </Link>
+    ),
   },
 ];
 
@@ -55,21 +65,23 @@ export const useCheckNotification = (
         if (new Date(n.releaseDate) >= new Date() && n.id > lastSeenId) {
           toast.custom(
             (t) => (
-              <div className="relative box-border flex justify-between rounded-lg border p-4">
-                <div className="min-w-0 flex-1">
-                  <p
-                    className={`m-0 text-sm font-medium leading-none text-gray-800 ${n.description ? "mb-2" : ""}`}
+              <div className="flex justify-between">
+                <div className="flex min-w-[300px] flex-1 flex-col justify-center">
+                  <div
+                    className={
+                      "m-0 text-sm font-medium leading-tight text-gray-800"
+                    }
                   >
                     {n.message}
-                  </p>
+                  </div>
                   {n.description && (
-                    <p className="m-0 text-sm leading-none text-gray-800">
+                    <div className="mt-2 flex-1 text-sm leading-tight text-gray-800">
                       {n.description}
-                    </p>
+                    </div>
                   )}
                 </div>
                 <button
-                  className="flex h-6 w-6 cursor-pointer items-center justify-center border-none bg-transparent p-0 text-gray-800 transition-colors duration-200"
+                  className={`flex h-6 w-6 cursor-pointer justify-end border-none bg-transparent p-0 text-gray-800 transition-colors duration-200 ${n.description ? "items-start" : "items-center"}`}
                   onClick={() => {
                     setLastSeenId(n.id);
                     toast.dismiss(t);
@@ -90,6 +102,11 @@ export const useCheckNotification = (
             {
               id: n.id.toString(),
               duration: 99999,
+              style: {
+                padding: "1rem",
+                border: "1px solid #e2e8f0",
+                borderRadius: "0.5rem",
+              },
             },
           );
         }
