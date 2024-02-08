4e0c3d800bced6ec8202f3d552be34b738567cae
Marc Klingen <git@marcklingen.com>
2024-02-08 20:27:19 +0100

    edit notification and let notifications expire after 30d

--- src/features/notifications/checkNotifications.tsx
+++ src/features/notifications/checkNotifications.tsx
@@ -5,32 +5,33 @@ import useLocalStorage from "@/src/components/useLocalStorage";
 import Notification, {
   type TNotification,
 } from "@/src/features/notifications/Notification";
+import { Button } from "@/src/components/ui/button";
 
 export const NOTIFICATIONS: TNotification[] = [
   {
     id: 1,
-    releaseDate: new Date("2024-01-01"),
-    message: "🚢 Custom models and pricings",
+    releaseDate: new Date("2024-01-29"),
+    message: "New: Custom model prices",
     description: (
-      <Link
-        href={"https://langfuse.com/changelog/2024-01-29-custom-model-prices"}
-      >
+      <div>
         <p>
-          Click to check out revamped models and pricings:
-          <br />
-          <br />
+          Langfuse now supports any LLM model for usage cost tracking. The
+          highlights:
         </p>
-        <ul className="ms-4 list-outside list-disc">
-          <li>Ingest your calculated cost via the SKDs</li>
-          <li>Define custom models with own names and prices</li>
+        <ul className="ms-4 mt-2 list-outside list-disc">
+          <li>Create custom model definitions to define your own pricing.</li>
+          <li>Ingest cost per generation via the SDK/API.</li>
           <li>
-            Choose among a variety of usage units like{" "}
-            <span className="inline">Tokens</span>,{" "}
-            <span className="inline">Seconds</span> or{" "}
-            <span className="inline">Characters</span>
+            Choose among a variety of usage units like Tokens, Seconds or
+            Characters
           </li>
         </ul>
-      </Link>
+        <Button size="sm" variant="secondary" className="mt-3">
+          <Link href="https://langfuse.com/changelog/2024-01-29-custom-model-prices">
+            Changelog post
+          </Link>
+        </Button>
+      </div>
     ),
   },
 ];
@@ -47,31 +48,41 @@ export const useCheckNotification = (
     if (!authenticated) {
       return;
     }
+
     const timeoutId = setTimeout(() => {
-      notification.reverse().forEach((n) => {
-        if (n.id > lastSeenId) {
-          toast.custom(
-            (t) => (
-              <Notification
-                notification={n}
-                setLastSeenId={setLastSeenId}
-                dismissToast={toast.dismiss}
-                toast={t}
-              />
-            ),
-            {
-              // needed to upsert toasts in case it is rendered multiple times
-              id: n.id.toString(),
-              duration: 99999,
-              style: {
-                padding: "1rem",
-                border: "1px solid #e2e8f0",
-                borderRadius: "0.5rem",
+      notification
+        .reverse()
+        .filter(
+          (n) =>
+            // only show notifications that are less than 30 days old
+            (new Date().getTime() - n.releaseDate.getTime()) /
+              (1000 * 60 * 60 * 24) <=
+            30,
+        )
+        .forEach((n) => {
+          if (n.id > lastSeenId) {
+            toast.custom(
+              (t) => (
+                <Notification
+                  notification={n}
+                  setLastSeenId={setLastSeenId}
+                  dismissToast={toast.dismiss}
+                  toast={t}
+                />
+              ),
+              {
+                // needed to upsert toasts in case it is rendered multiple times
+                id: n.id.toString(),
+                duration: 600_000, // 10 minutes
+                style: {
+                  padding: "1rem",
+                  border: "1px solid #e2e8f0",
+                  borderRadius: "0.5rem",
+                },
               },
-            },
-          );
-        }
-      });
+            );
+          }
+        });
     }, 1500);
     return () => clearTimeout(timeoutId);
   }, [lastSeenId, notification, setLastSeenId, authenticated]);
