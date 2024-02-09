920e58bd605b03bb0da9be203cae42f6cc9a69fe
Marc Klingen <git@marcklingen.com>
2024-02-09 16:59:07 +0100

    wip

--- src/features/public-api/components/CreateApiKeyButton.tsx
+++ src/features/public-api/components/CreateApiKeyButton.tsx
@@ -74,28 +74,13 @@ export function CreateApiKeyButton(props: { projectId: string }) {
       </DialogTrigger>
       <DialogContent
         onPointerDownOutside={(e) => e.preventDefault()}
-        className="max-w-xl"
+        className="max-w-full md:max-w-xl"
       >
-        {hostname ? (
-          <>
-            <DialogTitle>Hostname</DialogTitle>
-            <div className="mb-6">
-              <div className="my-2">
-                When connecting to Langfuse, use this hostname / baseurl.
-              </div>
-              <CodeView content={hostname} />
-            </div>
-          </>
-        ) : null}
         <DialogTitle>API Keys</DialogTitle>
         <div className="mb-2">
           <div className="text-md font-semibold">Secret Key</div>
           <div className="my-2">
-            Please save this secret key.{" "}
-            <span className="font-semibold">
-              You will not be able to view it again
-            </span>
-            . If you lose it, you will need to generate a new one.
+            This key can only be viewed once. You can always generate a new key.
           </div>
           <CodeView content={generatedKeys?.secretKey ?? "Loading ..."} />
         </div>
@@ -103,51 +88,155 @@ export function CreateApiKeyButton(props: { projectId: string }) {
           <div className="text-md mb-2 font-semibold">Public Key</div>
           <CodeView content={generatedKeys?.publicKey ?? "Loading ..."} />
         </div>
+        {hostname ? (
+          <>
+            <div>
+              <div className="text-md mb-2 font-semibold">Host</div>
+              <CodeView content={hostname} />
+            </div>
+          </>
+        ) : null}
         <div className="mb-2">
           <div className="text-md my-2 font-semibold">Usage</div>
-          <Tabs defaultValue="javaScript">
+          <Tabs defaultValue="python">
             <TabsList>
-              <TabsTrigger value="javaScript">JS/TS</TabsTrigger>
               <TabsTrigger value="python">Python</TabsTrigger>
+              <TabsTrigger value="js">JS/TS</TabsTrigger>
+              <TabsTrigger value="openai">OpenAI</TabsTrigger>
+              <TabsTrigger value="langchain">Langchain</TabsTrigger>
+              <TabsTrigger value="langchain-js">Langchain JS</TabsTrigger>
+              <TabsTrigger value="other">Other</TabsTrigger>
             </TabsList>
-            <TabsContent value="javaScript">
+            <TabsContent value="python">
+              <CodeView
+                content="pip install langfuse"
+                className="mb-2 bg-blue-50"
+              />
               <CodeView
                 className="bg-blue-50"
                 content={
                   generatedKeys?.publicKey && generatedKeys.secretKey
-                    ? `import { Langfuse } from "langfuse";\n\nconst langfuse = new Langfuse({\n  secretKey: "${generatedKeys.secretKey}",\n  publicKey: "${generatedKeys.publicKey}",\n  ${hostname ? `baseUrl: "${hostname}"` : '//baseUrl: defaults to "https://cloud.langfuse.com"'}\n});`
+                    ? `from langfuse import Langfuse\n\nlangfuse = Langfuse(\n  secret_key="${generatedKeys.secretKey}",\n  public_key="${generatedKeys.publicKey}",\n  host="${hostname}"\n)`
                     : "Loading ..."
                 }
               />
               <p className="mt-3 text-xs text-gray-600">
                 See{" "}
                 <a
-                  href="https://langfuse.com/docs/sdk/typescript"
+                  href="https://langfuse.com/docs/get-started"
                   className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  documentation
+                  Quickstart
                 </a>{" "}
-                for details.
-              </p>
-            </TabsContent>
-            <TabsContent value="python">
-              <CodeView
-                className="bg-blue-50"
-                content={
-                  generatedKeys?.publicKey && generatedKeys.secretKey
-                    ? `from langfuse import Langfuse\n\nlangfuse = Langfuse(\n  secret_key="${generatedKeys.secretKey}",\n  public_key="${generatedKeys.publicKey}",\n  ${hostname ? `host="${hostname}"` : 'host="https://cloud.langfuse.com"'}\n)`
-                    : "Loading ..."
-                }
-              />
-              <p className="mt-3 text-xs text-gray-600">
-                See{" "}
+                and{" "}
                 <a
                   href="https://langfuse.com/docs/sdk/python"
                   className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  documentation
+                  Python docs
                 </a>{" "}
-                for details.
+                for more details.
+              </p>
+            </TabsContent>
+            <TabsContent value="js">
+              <CodeView
+                content="npm install langfuse"
+                className="mb-2 bg-blue-50"
+              />
+              <CodeView
+                className="bg-blue-50"
+                content={
+                  generatedKeys?.publicKey && generatedKeys.secretKey
+                    ? `import { Langfuse } from "langfuse";\n\nconst langfuse = new Langfuse({\n  secretKey: "${generatedKeys.secretKey}",\n  publicKey: "${generatedKeys.publicKey}",\n  baseUrl: "${hostname}"\n});`
+                    : "Loading ..."
+                }
+              />
+              <p className="mt-3 text-xs text-gray-600">
+                See{" "}
+                <a
+                  href="https://langfuse.com/docs/get-started"
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Quickstart
+                </a>{" "}
+                and{" "}
+                <a
+                  href="https://langfuse.com/docs/sdk/typescript"
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  JS/TS docs
+                </a>{" "}
+                for more details.
+              </p>
+            </TabsContent>
+            <TabsContent value="openai">
+              <p className="mt-2 text-xs text-gray-600">
+                The integration is a drop-in replacement for the OpenAI Python
+                SDK. By changing the import, Langfuse will capture all LLM calls
+                and send them to Langfuse asynchronously.
+              </p>
+              <CodeView
+                content="pip install langfuse"
+                className="my-2 bg-blue-50"
+              />
+              <CodeView
+                title=".env"
+                content={
+                  generatedKeys?.publicKey && generatedKeys.secretKey
+                    ? `LANGFUSE_SECRET_KEY=${generatedKeys.secretKey};\nLANGFUSE_PUBLIC_KEY=${generatedKeys.publicKey};\nLANGFUSE_HOST="${hostname}";`
+                    : "Loading ..."
+                }
+                className="my-2 bg-blue-50"
+              />
+              <CodeView
+                content={`#remove: import openai\n\nfrom langfuse.openai import openai`}
+                className="my-2 bg-blue-50"
+              />
+              <p className="mt-2 text-xs text-gray-600">
+                Use the OpenAI SDK as you would normally. See the{" "}
+                <a
+                  href="https://langfuse.com/docs/integrations/openai"
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  OpenAI Integration docs
+                </a>{" "}
+                for more details.
+              </p>
+            </TabsContent>
+            <TabsContent value="langchain"></TabsContent>
+            <TabsContent value="langchain-js"></TabsContent>
+            <TabsContent value="other">
+              <p className="mt-2 text-xs text-gray-600">
+                Use the{" "}
+                <a
+                  href="https://api.reference.langfuse.com/"
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  API
+                </a>{" "}
+                or one of the{" "}
+                <a
+                  href="https://langfuse.com/docs/integrations"
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  native integrations
+                </a>{" "}
+                (e.g. LiteLLM, Flowise, and Langflow) to integrate with
+                Langfuse.
               </p>
             </TabsContent>
           </Tabs>
