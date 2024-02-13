6221be155de7b430758511e7a87e03cdf783b59a
Marc Klingen <git@marcklingen.com>
2024-02-13 11:35:54 +0100

    feat(security): add audit logging (#1142)

---
+++ src/features/audit-logs/auditLog.ts
@@ -0,0 +1,57 @@
+import { prisma as _prisma } from "@/src/server/db";
+import { type MembershipRole } from "@prisma/client";
+
+export type AuditableResource =
+  | "membership"
+  | "membershipInvitation"
+  | "datasetItem"
+  | "dataset"
+  | "trace"
+  | "project"
+  | "observation"
+  | "score"
+  | "model"
+  | "prompt"
+  | "session"
+  | "apiKey";
+
+type AuditLog = {
+  resourceType: AuditableResource;
+  resourceId: string;
+  action: string;
+  before?: unknown;
+  after?: unknown;
+} & (
+  | {
+      projectId: string;
+      userId: string;
+      userProjectRole: MembershipRole;
+    }
+  | {
+      session: {
+        user: {
+          id: string;
+        };
+        projectRole: MembershipRole;
+        projectId: string;
+      };
+    }
+);
+
+export async function auditLog(log: AuditLog, prisma?: typeof _prisma) {
+  await (prisma ?? _prisma).auditLog.create({
+    data: {
+      projectId: "projectId" in log ? log.projectId : log.session.projectId,
+      userId: "userId" in log ? log.userId : log.session.user.id,
+      userProjectRole:
+        "userProjectRole" in log
+          ? log.userProjectRole
+          : log.session.projectRole,
+      resourceType: log.resourceType,
+      resourceId: log.resourceId,
+      action: log.action,
+      before: log.before ? JSON.stringify(log.before) : undefined,
+      after: log.after ? JSON.stringify(log.after) : undefined,
+    },
+  });
+}
