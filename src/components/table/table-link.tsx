95a7e93b46418645938ccdd345abd16b14d26294
RichardKruemmel <richard.kruemmel@code.berlin>
2024-02-14 11:44:46 +0100

    Add hover effect and truncate long values in TableLink component

--- src/components/table/table-link.tsx
+++ src/components/table/table-link.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useState } from "react";
+import { cn } from "@/src/utils/tailwind";
 
 export type TableLinkProps = {
   path: string;
@@ -11,15 +13,39 @@ export default function TableLink({
   value,
   truncateAt = 7,
 }: TableLinkProps) {
+  const [isHovered, setIsHovered] = useState(false);
+  const truncatedValue =
+    value.length - truncateAt > 0
+      ? `...${value.substring(value.length - truncateAt)}`
+      : value;
+  const isTruncated = value !== truncatedValue;
   return (
-    <Link
-      className="inline-block rounded bg-indigo-50 px-2 py-1 text-xs font-semibold text-blue-600 shadow-sm hover:bg-indigo-100"
-      href={path}
-      title={value}
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="relative"
     >
-      {value.length > truncateAt
-        ? `...${value.substring(value.length - truncateAt)}`
-        : value}
-    </Link>
+      <Link
+        className={cn(
+          isHovered && isTruncated ? "opacity-0" : "opacity-100",
+          isHovered && !isTruncated ? "bg-indigo-100" : "bg-indigo-50",
+          "absolute inline-block rounded  px-2 py-1 text-xs font-semibold text-blue-600 shadow-sm transition-opacity duration-300",
+        )}
+        href={path}
+        title={value}
+      >
+        {truncatedValue}
+      </Link>
+      <Link
+        className={cn(
+          isHovered && isTruncated ? "opacity-100" : "opacity-0",
+          "inline-block rounded bg-indigo-100 px-2 py-1 text-xs font-semibold text-blue-600 shadow-sm transition-opacity duration-300",
+        )}
+        href={path}
+        title={value}
+      >
+        {value}
+      </Link>
+    </div>
   );
 }
