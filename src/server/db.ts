71e0dcdcc59909c56acc551d625f39d0a75df6d1
Max Deichmann <m.deichmann@tum.de>
2024-02-15 14:37:44 +0100

    push

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,42 +1,17 @@
-import { type Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 import { env } from "@/src/env.mjs";
 import {
+  DummyDriver,
   Kysely,
   PostgresAdapter,
   PostgresIntrospector,
   PostgresQueryCompiler,
 } from "kysely";
-import { type DB } from "@/prisma/generated/types";
-import kyselyExtension from "prisma-extension-kysely";
-import {
-  type DynamicClientExtensionThis,
-  type InternalArgs,
-} from "@prisma/client/runtime/library";
+import { type DB as Database } from "@/prisma/generated/types";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma:
-    | (PrismaClient<Prisma.PrismaClientOptions, never> & {
-        client: {
-          $kysely: () => Kysely<DB>;
-        };
-      })
-    | DynamicClientExtensionThis<
-        Prisma.TypeMap<
-          InternalArgs & {
-            client: {
-              $kysely: () => Kysely<DB>;
-            };
-          }
-        >,
-        Prisma.TypeMapCb,
-        {
-          client: {
-            $kysely: () => Kysely<DB>;
-          };
-        }
-      >
-    | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
@@ -44,23 +19,15 @@ export const prisma =
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  }).$extends(
-    kyselyExtension({
-      kysely: (driver) =>
-        new Kysely<DB>({
-          dialect: {
-            // This is where the magic happens!
-            createDriver: () => driver,
-            // Don't forget to customize these to match your database!
-            createAdapter: () => new PostgresAdapter(),
-            createIntrospector: (db) => new PostgresIntrospector(db),
-            createQueryCompiler: () => new PostgresQueryCompiler(),
-          },
-          plugins: [
-            // Add your favorite plugins here!
-          ],
-        }),
-    }),
-  );
+  });
+
+export const DB = new Kysely<Database>({
+  dialect: {
+    createAdapter: () => new PostgresAdapter(),
+    createDriver: () => new DummyDriver(),
+    createIntrospector: (db) => new PostgresIntrospector(db),
+    createQueryCompiler: () => new PostgresQueryCompiler(),
+  },
+});
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
