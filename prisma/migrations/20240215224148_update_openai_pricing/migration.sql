ce1ac745bd4df0bafebd81d963dfe4cc9ad9aa61
Max Deichmann <m.deichmann@tum.de>
2024-02-15 22:17:03 +0100

    push

--- prisma/migrations/20240215224148_update_openai_pricing/migration.sql
+++ prisma/migrations/20240215224148_update_openai_pricing/migration.sql
@@ -14,6 +14,4 @@ INSERT INTO models (
   tokenizer_config
 )
 VALUES
-  -- according to email, gpt-3.5-turbo and gpt-3.5-turbo-16k will point to 0125 models as of 2024-02-16
-  -- gpt-3.5-turbo-0125 now supports 16k token length. 16k model will point to regular 3.5 turbo model according to mail.
-  ('clruwnahl00060al74fcfehas', NULL, 'gpt-4-turbo-preview', '(?i)^(gpt-4-turbo-preview)$', '2023-11-06', 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4" }'),
+  ('clsnq07bn000008l4e46v1ll8', NULL, 'gpt-4-turbo-preview', '(?i)^(gpt-4-turbo-preview)$', '2023-11-06', 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4" }')
