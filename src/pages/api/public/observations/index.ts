6e86d6ebab3c0f06bfaa3da34fc856c47308cd25
Marc Klingen <git@marcklingen.com>
2024-02-15 22:10:19 +0100

    fix

--- src/pages/api/public/observations/index.ts
+++ src/pages/api/public/observations/index.ts
@@ -139,8 +139,8 @@ const getObservation = async (
         o."calculated_input_cost" as "calculatedInputCost",
         o."calculated_output_cost" as "calculatedOutputCost",
         o."calculated_total_cost" as "calculatedTotalCost",
-        o."latency",
-      FROM observations_view o LEFT JOIN traces ON o."trace_id" = traces."id"
+        o."latency"
+      FROM observations_view o LEFT JOIN traces ON o."trace_id" = traces."id" AND traces."project_id" = o."project_id"
       WHERE o."project_id" = ${authenticatedProjectId}
       ${nameCondition}
       ${userIdCondition}
