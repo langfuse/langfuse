fb1644733b3a4461892dc2bfa6073ef46d5614b1
Marc Klingen <git@marcklingen.com>
2024-02-16 16:02:21 +0100

    improve perf/security of api route

--- src/pages/api/public/scores/[scoreId].ts
+++ src/pages/api/public/scores/[scoreId].ts
@@ -1,11 +1,10 @@
 import { prisma } from "@/src/server/db";
-import { Prisma, type Score } from "@prisma/client";
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { z } from "zod";
 import { cors, runMiddleware } from "@/src/features/public-api/server/cors";
 import { verifyAuthHeaderAndReturnScope } from "@/src/features/public-api/server/apiAuth";
 
-const ScoresGetSchema = z.object({
+const ScoreDeleteSchema = z.object({
   scoreId: z.string(),
 });
 
@@ -30,44 +29,40 @@ export default async function handler(
       if (authCheck.scope.accessLevel !== "all") {
         return res.status(401).json({
           message:
-            "Access denied - need to use basic auth with secret key to GET scores",
+            "Access denied - need to use basic auth with secret key to DELETE scores",
         });
       }
 
-      const { scoreId } = ScoresGetSchema.parse(req.query); // uses query and not body
+      const { scoreId } = ScoreDeleteSchema.parse(req.query); // uses query and not body
 
-      // check if user is authorized to delete scores in this project
-      const scores = await prisma.$queryRaw<
-        Array<Score & { trace: { userId: string } }>
-      >(Prisma.sql`
-          SELECT
-            s.id,
-            s.timestamp,
-            s.name,
-            s.value,
-            s.comment,
-            s.trace_id as "traceId",
-            s.observation_id as "observationId",
-            json_build_object('userId', t.user_id) as "trace"
-          FROM "scores" AS s
-          JOIN "traces" AS t ON t.id = s.trace_id
-          WHERE t.project_id = ${authCheck.scope.projectId}
-          AND s.id = ${scoreId}
-          `);
+      const score = await prisma.score.findUnique({
+        select: {
+          id: true,
+        },
+        where: {
+          id: scoreId,
+          trace: {
+            projectId: authCheck.scope.projectId,
+          },
+        },
+      });
 
-      if (scores.length == 0) {
+      if (!score) {
         return res.status(404).json({
           message: "Score not found within authorized project",
         });
       }
 
-      const deletedScore = await prisma.score.delete({
+      await prisma.score.delete({
         where: {
           id: scoreId,
+          trace: {
+            projectId: authCheck.scope.projectId,
+          },
         },
       });
 
-      return res.status(200).json({ ...deletedScore });
+      return res.status(200).json({ message: "Score deleted successfully" });
     } catch (error: unknown) {
       console.error(error);
       const errorMessage =
