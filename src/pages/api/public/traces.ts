1e812f595b9befc8228380753f123906ae3939a4
Max Deichmann <m.deichmann@tum.de>
2024-02-20 16:34:28 +0100

    fix

--- src/pages/api/public/traces.ts
+++ src/pages/api/public/traces.ts
@@ -44,7 +44,6 @@ export default async function handler(
   // CHECK AUTH
   const authCheck = await verifyAuthHeaderAndReturnScope(
     req.headers.authorization,
-    res,
   );
   if (!authCheck.validKey)
     return res.status(401).json({
