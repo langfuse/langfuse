70da5693c7067868154c34131be24bf7ead182a9
Max Deichmann <m.deichmann@tum.de>
2024-02-22 18:06:55 +0100

    fix

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -13,7 +13,7 @@ fastify.register(consumer);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 8080 });
+    await fastify.listen({ port: 8000 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
