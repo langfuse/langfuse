abe1bc73681bd42c74abd19ed609218caf58c903
Max Deichmann <m.deichmann@tum.de>
2024-02-22 09:57:39 +0100

    add worker and redis

---
+++ worker/src/index.ts
@@ -0,0 +1,29 @@
+import Fastify from "fastify";
+
+import { getLogger } from "./logger";
+import redis from "@fastify/redis";
+
+const fastify = Fastify({
+  logger: getLogger("development") ?? true, // defaults to true if no entry matches in the map
+});
+
+fastify.register(redis, { host: "127.0.0.1" });
+fastify.register(require("./redis-consumer"));
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8080 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
+
+fastify.get("/", async (request, reply) => {
+  console.log("Hello world!");
+  // const { redis } = fastify;
+  // redis.set("mykey", "value");
+  // console.log(await redis.get("mykey"));
+  return { hello: "world" };
+});
