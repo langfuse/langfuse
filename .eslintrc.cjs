b449b1b351c4a441084a9acf502989f7d8f4bf71
Max Deichmann <m.deichmann@tum.de>
2024-02-22 13:27:05 +0100

    move eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+      rules: {
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-confusing-void-expression": "off",
+      },
+    },
+  ],
+  extends: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  ignorePatterns: ["generated/**/*"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
+
+module.exports = config;
--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,40 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
+const baseConfig = require('../.eslintrc.cjs');
+
+// Check if baseConfig.extends is an array before spreading it
+const extendsArray = Array.isArray(baseConfig.extends) ? baseConfig.extends : [];
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/strict-type-checked",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-      rules: {
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-confusing-void-expression": "off",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals"],
-  ignorePatterns: ["generated/**/*"],
+  ...baseConfig,
+  extends: [...extendsArray, "next/core-web-vitals"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    ...baseConfig.rules,
     "react/jsx-key": [
       "error",
       {
@@ -42,6 +17,7 @@ const config = {
       },
     ],
   },
+  plugins: ["@typescript-eslint"],
 };
 
 module.exports = config;
