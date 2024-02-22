b603dbb516d06a2dbdd02eeb6363d41e93a42404
Max Deichmann <m.deichmann@tum.de>
2024-02-22 14:39:54 +0100

    push

--- worker/src/database.ts
+++ worker/src/database.ts
@@ -1,11 +1,11 @@
-import { Kysely, PostgresDialect } from "kysely";
-import { DB } from "../../web/prisma/generated/types";
-import { Pool } from "pg";
+// import { Kysely, PostgresDialect } from "kysely";
+// import { DB } from "../../web/prisma/generated/types";
+// import { Pool } from "pg";
 
-export const db = new Kysely<DB>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      connectionString: "postgres://postgres:password@localhost:5432/postgres",
-    }),
-  }),
-});
+// export const db = new Kysely<DB>({
+//   dialect: new PostgresDialect({
+//     pool: new Pool({
+//       connectionString: "postgres://postgres:password@localhost:5432/postgres",
+//     }),
+//   }),
+// });
