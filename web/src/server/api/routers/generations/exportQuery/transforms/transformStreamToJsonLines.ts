fafb3ae505ac3bbcaa3cbf61b746eab9be1f3f2f
Marc Klingen <git@marcklingen.com>
2024-02-23 14:54:31 +0100

    fix(export): messages key in jsonl output format (#1236)

--- web/src/server/api/routers/generations/exportQuery/transforms/transformStreamToJsonLines.ts
+++ web/src/server/api/routers/generations/exportQuery/transforms/transformStreamToJsonLines.ts
@@ -29,17 +29,19 @@ export function transformStreamToJsonLines(): Transform {
       const parsedOutput = outputSchema.safeParse(row.output);
 
       if (parsedInput.success && parsedOutput.success) {
-        const output = JSON.stringify([
-          ...parsedInput.data,
-          {
-            role: "assistant",
-            content:
-              typeof parsedOutput.data === "object" &&
-              "completion" in parsedOutput.data
-                ? JSON.stringify(parsedOutput.data.completion)
-                : JSON.stringify(parsedOutput.data),
-          },
-        ]);
+        const output = JSON.stringify({
+          messages: [
+            ...parsedInput.data,
+            {
+              role: "assistant",
+              content:
+                typeof parsedOutput.data === "object" &&
+                "completion" in parsedOutput.data
+                  ? JSON.stringify(parsedOutput.data.completion)
+                  : JSON.stringify(parsedOutput.data),
+            },
+          ],
+        });
         this.push(output + "\n");
       }
 
