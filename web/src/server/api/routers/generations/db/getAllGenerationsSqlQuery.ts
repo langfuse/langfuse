6fe8ffdddfd0c6cba50fb3798e91f2fa57f9b44a
RichardKruemmel <richard.kruemmel@code.berlin>
2024-02-26 18:27:38 +0100

    Add scores column to getAllGenerationsSqlQuery

--- web/src/server/api/routers/generations/db/getAllGenerationsSqlQuery.ts
+++ web/src/server/api/routers/generations/db/getAllGenerationsSqlQuery.ts
@@ -117,7 +117,8 @@ export function getAllGenerationsSqlQuery({
         o."latency",
         o.prompt_id as "promptId",
         p.name as "promptName",
-        p.version as "promptVersion"
+        p.version as "promptVersion",
+        s_avg.scores_avg as "scores"
       FROM observations_view o
       JOIN traces t ON t.id = o.trace_id AND t.project_id = o.project_id
       LEFT JOIN scores_avg AS s_avg ON s_avg.trace_id = t.id and s_avg.observation_id = o.id
