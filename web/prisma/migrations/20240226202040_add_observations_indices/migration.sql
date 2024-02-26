123dc0b0257ac7ed64721ebc9d6582a682ea3dbd
Max Deichmann <m.deichmann@tum.de>
2024-02-26 21:33:19 +0100

    push

--- web/prisma/migrations/20240226202040_add_observations_indices/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX CONCURRENTLY "observations_trace_id_project_id_type_start_time_idx" ON "observations"("trace_id", "project_id", "type", "start_time");
-
--- CreateIndex
-CREATE INDEX CONCURRENTLY "observations_trace_id_project_id_start_time_idx" ON "observations"("trace_id", "project_id", "start_time");
