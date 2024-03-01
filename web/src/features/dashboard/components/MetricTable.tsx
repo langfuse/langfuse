673ed7ea7c358e7511e80cee5644d51add0258de
Max Deichmann <m.deichmann@tum.de>
2024-03-01 18:42:52 +0100

    feat: increase scale of dashboard USD numbers (#1301)

--- web/src/features/dashboard/components/MetricTable.tsx
+++ web/src/features/dashboard/components/MetricTable.tsx
@@ -4,8 +4,9 @@ import { DashboardCard } from "@/src/features/dashboard/components/cards/Dashboa
 import { DashboardTable } from "@/src/features/dashboard/components/cards/DashboardTable";
 import { type FilterState } from "@/src/features/filters/types";
 import { api } from "@/src/utils/api";
-import { compactNumberFormatter, usdFormatter } from "@/src/utils/numbers";
+import { compactNumberFormatter } from "@/src/utils/numbers";
 import { TotalMetric } from "./TotalMetric";
+import { totalCostDashboardFormatted } from "@/src/features/dashboard/lib/dashboard-utils";
 
 export const MetricTable = ({
   className,
@@ -69,7 +70,9 @@ export const MetricTable = ({
           </RightAlignedCell>,
           <RightAlignedCell key={`${i}-cost`}>
             {item.sumCalculatedTotalCost
-              ? usdFormatter(item.sumCalculatedTotalCost as number, 2, 2)
+              ? totalCostDashboardFormatted(
+                  item.sumCalculatedTotalCost as number,
+                )
               : "$0"}
           </RightAlignedCell>,
         ])
@@ -91,7 +94,7 @@ export const MetricTable = ({
         collapse={{ collapsed: 5, expanded: 20 }}
       >
         <TotalMetric
-          metric={totalTokenCost ? usdFormatter(totalTokenCost, 2, 2) : "$0"}
+          metric={totalCostDashboardFormatted(totalTokenCost)}
           description="Total cost"
         >
           <DocPopup
