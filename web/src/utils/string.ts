c47cd2a154f8155fa882f8ac3dcf63e664ea60a7
Max Deichmann <m.deichmann@tum.de>
2024-03-04 20:40:19 +0100

    feat: add tooltip to latency tables (#1313)

--- web/src/utils/string.ts
+++ web/src/utils/string.ts
@@ -2,7 +2,7 @@ export function lastCharacters(str: string, n: number) {
   return str.substring(str.length - n);
 }
 
-export function truncate(str: string, n: number = 10) {
+export function truncate(str: string, n: number = 16) {
   // '...' suffix if the string is longer than n
   if (str.length > n) {
     return str.substring(0, n) + "...";
