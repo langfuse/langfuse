ffb436fccd7f9fe0c25456998962ea65a4a394ff
Max Deichmann <m.deichmann@tum.de>
2024-03-04 20:54:24 +0100

    fix

--- web/prisma/migrations/20240304123642_traces_view/migration.sql
+++ web/prisma/migrations/20240304123642_traces_view/migration.sql
@@ -1,7 +1,6 @@
 CREATE VIEW trace_metrics_view AS
 SELECT
     t.id,
-    t.project_id,
     EXTRACT(EPOCH FROM COALESCE(MAX(o.end_time), MAX(o.start_time))) - EXTRACT(EPOCH FROM MIN(o.start_time))::double precision AS duration
 FROM
     traces t
