42a23a575aba5749b50e9d6819c277d503c737f2
Max Deichmann <m.deichmann@tum.de>
2024-03-06 13:29:34 +0100

    push

--- web/src/pages/project/[projectId]/users.tsx
+++ web/src/pages/project/[projectId]/users.tsx
@@ -52,53 +52,6 @@ export default function UsersPage() {
     userIds: users.data?.users.map((u) => u.userId) ?? [],
   });
 
-  function joinUserCoreAndMetrics<
-    Core extends { id: string },
-    Metric extends { id: string },
-  >(
-    userCoreData?: Core[],
-    userMetricsData?: Metric[],
-  ): {
-    status: "loading" | "error" | "success";
-    rows: (Core & Partial<Metric>)[] | undefined;
-  } {
-    if (!userCoreData) {
-      return { status: "error", rows: undefined };
-    }
-
-    const userCoreDataProcessed = userCoreData;
-
-    if (!userMetricsData) {
-      // create an object with all the keys of the UserMetrics type with undefined value
-
-      return {
-        status: "success",
-        rows: userCoreDataProcessed.map((u) => ({
-          ...u,
-          ...({} as Partial<Metric>),
-        })),
-      };
-    }
-
-    const metricsById = userMetricsData.reduce<Record<string, Metric>>(
-      (acc, metric) => {
-        acc[metric.id] = metric;
-        return acc;
-      },
-      {},
-    );
-
-    const joinedData = userCoreDataProcessed.map((userCore) => {
-      const metrics = metricsById[userCore.id];
-      return {
-        ...userCore,
-        ...(metrics ?? ({} as Partial<Metric>)),
-      };
-    });
-
-    return { status: "success", rows: joinedData };
-  }
-
   type UserCoreOutput = RouterOutput["users"]["all"]["users"][number];
   type UserMetricsOutput = RouterOutput["users"]["metrics"][number];
 
@@ -292,4 +245,51 @@ export default function UsersPage() {
       />
     </div>
   );
+}
+
+function joinUserCoreAndMetrics<
+  Core extends { id: string },
+  Metric extends { id: string },
+>(
+  userCoreData?: Core[],
+  userMetricsData?: Metric[],
+): {
+  status: "loading" | "error" | "success";
+  rows: (Core & Partial<Metric>)[] | undefined;
+} {
+  if (!userCoreData) {
+    return { status: "error", rows: undefined };
+  }
+
+  const userCoreDataProcessed = userCoreData;
+
+  if (!userMetricsData) {
+    // create an object with all the keys of the UserMetrics type with undefined value
+
+    return {
+      status: "success",
+      rows: userCoreDataProcessed.map((u) => ({
+        ...u,
+        ...({} as Partial<Metric>),
+      })),
+    };
+  }
+
+  const metricsById = userMetricsData.reduce<Record<string, Metric>>(
+    (acc, metric) => {
+      acc[metric.id] = metric;
+      return acc;
+    },
+    {},
+  );
+
+  const joinedData = userCoreDataProcessed.map((userCore) => {
+    const metrics = metricsById[userCore.id];
+    return {
+      ...userCore,
+      ...(metrics ?? ({} as Partial<Metric>)),
+    };
+  });
+
+  return { status: "success", rows: joinedData };
 }
