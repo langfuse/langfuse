a09758fd1ea4063843a6cf63795c1077368d679c
Max Deichmann <m.deichmann@tum.de>
2024-03-07 09:21:29 +0100

    fix

--- web/prisma/migrations/20240307090110_claude_model_three/migration.sql
+++ web/prisma/migrations/20240307090110_claude_model_three/migration.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO models (
   id,
   project_id,
@@ -15,8 +13,6 @@ INSERT INTO models (
 )
 VALUES
   -- https://docs.anthropic.com/claude/docs/models-overview, https://docs.anthropic.com/claude/docs/quickstart-guide
-  ('cltgy0iuw000008le3vod1hhy', NULL, 'claude-3-opus-20240229', '(?i)^(claude-3-opus-20240229)$', NULL, 0,000015, 0,000075, NULL, 'TOKENS', 'claude', NULL),
-  ('cltgy0pp6000108le56se7bl3', NULL, 'claude-3-sonnet-20240229', '(?i)^(claude-3-sonnet-20240229)$', NULL, 0.000003, 0,000015, NULL, 'TOKENS', 'claude', NULL)
-
-
+  ('cltgy0iuw000008le3vod1hhy', NULL, 'claude-3-opus-20240229', '(?i)^(claude-3-opus-20240229)$', NULL, 0.000015, 0.000075, NULL, 'TOKENS', 'claude', NULL),
+  ('cltgy0pp6000108le56se7bl3', NULL, 'claude-3-sonnet-20240229', '(?i)^(claude-3-sonnet-20240229)$', NULL, 0.000003, 0.000015, NULL, 'TOKENS', 'claude', NULL)
 
