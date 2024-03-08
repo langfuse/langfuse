afcd020a0524c36db93a005f92f05d880c38df16
Max Deichmann <m.deichmann@tum.de>
2024-03-08 16:21:26 +0100

    push

--- worker/src/eval-service.ts
+++ worker/src/eval-service.ts
@@ -1,3 +1,19 @@
-export type EvalProps = {};
+import { z } from "zod";
+import { EvalBody } from "./redis-consumer";
+import { db } from "./database";
 
-export const evaluate = ({}) => {};
+export const evaluate = async ({
+  data,
+}: {
+  data: z.infer<typeof EvalBody>;
+}) => {
+  const trace = await db
+    .selectFrom("traces")
+    .selectAll()
+    .where("id", "=", data.traceId)
+    .where("project_id", "=", data.projectId)
+    .where("project_id", "=", data.projectId)
+    .executeTakeFirst();
+
+  console.log(trace);
+};
