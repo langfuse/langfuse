0d5777ed7c35825fb606b1cb08c52acab672a1ef
Max Deichmann <m.deichmann@tum.de>
2024-03-13 00:03:30 +0100

    push

---
+++ web/.next/server/vendor-chunks/react18-json-view.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/react18-json-view";
+exports.ids = ["vendor-chunks/react18-json-view"];
+exports.modules = {
+
+/***/ "../node_modules/react18-json-view/src/style.css":
+/*!*******************************************************!*\
+  !*** ../node_modules/react18-json-view/src/style.css ***!
+  \*******************************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
