ff4b32f436f7719a5fb000f5e861c85ad9b588cc
RichardKruemmel <richard.kruemmel@code.berlin>
2024-03-19 12:30:06 +0100

    Refactor filter state handling and add utility function

--- web/src/features/filters/hooks/useFilterState.ts
+++ web/src/features/filters/hooks/useFilterState.ts
@@ -1,7 +1,9 @@
-import { type UIFilterState } from "@/src/features/filters/types";
-import { GENERATIONS_NAME_ID_MAP } from "@/src/server/api/definitions/observationsTable";
-import { TRACES_NAME_ID_MAP } from "@/src/server/api/definitions/tracesTable";
-import { singleFilterWithUrlParam } from "@/src/server/api/interfaces/filters";
+import { type FilterState, type TableName } from "@/src/features/filters/types";
+import { GENERATIONS_ID_NAME_MAP } from "@/src/server/api/definitions/observationsTable";
+import { SCORES_ID_NAME_MAP } from "@/src/server/api/definitions/scoresTable";
+import { SESSIONS_ID_NAME_MAP } from "@/src/server/api/definitions/sessionsView";
+import { TRACES_ID_NAME_MAP } from "@/src/server/api/definitions/tracesTable";
+import { singleFilter } from "@/src/server/api/interfaces/filters";
 import { useState } from "react";
 import {
   useQueryParam,
@@ -15,11 +17,18 @@ const DEBUG_QUERY_STATE = false;
 // encode/decode filter state
 // The decode has to return null or undefined so that withDefault will use the default value.
 // An empty array will be interpreted as existing state and hence the default value will not be used.
-const CommaArrayParam = (table: string) => ({
-  encode: (value: UIFilterState) =>
+const CommaArrayParam = (table: TableName) => ({
+  encode: (value: FilterState) =>
     encodeDelimitedArray(
       value.map((f) => {
-        const stringified = `${f.urlName};${f.type};${
+        const map = getIdNameMap(table);
+        const reversed_map = Object.fromEntries(
+          Object.entries(map).map(([key, value]) => [value, key]),
+        );
+        const columnName =
+          f.column in reversed_map ? reversed_map[f.column] : f.column;
+
+        const stringified = `${columnName};${f.type};${
           f.type === "numberObject" || f.type === "stringObject" ? f.key : ""
         };${f.operator};${encodeURIComponent(
           f.type === "datetime"
@@ -40,28 +49,10 @@ const CommaArrayParam = (table: string) => ({
   decode: (arrayStr: string | (string | null)[] | null | undefined) =>
     (decodeDelimitedArray(arrayStr, ",")
       ?.map((f) => {
+        const map = getIdNameMap(table);
         if (!f) return null;
         const [column, type, key, operator, value] = f.split(";");
-        let map: { [key: string]: string };
-        switch (table) {
-          case "generation":
-            map = GENERATIONS_NAME_ID_MAP;
-            break;
-          case "trace":
-            map = TRACES_NAME_ID_MAP;
-            break;
-          case "session":
-            map = {};
-            break;
-          case "score":
-            map = {};
-            break;
-          default:
-            map = {};
-            break;
-        }
         const columnName = column && column in map ? map[column] : column;
-
         // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         if (DEBUG_QUERY_STATE)
           console.log("values", [column, type, key, operator, value]);
@@ -82,24 +73,23 @@ const CommaArrayParam = (table: string) => ({
                       : decodedValue;
         // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         if (DEBUG_QUERY_STATE) console.log("parsedValue", parsedValue);
-        const parsed = singleFilterWithUrlParam.safeParse({
+        const parsed = singleFilter.safeParse({
           column: columnName,
           key: key !== "" ? key : undefined,
           operator,
-          urlName: column,
           value: parsedValue,
           type,
         });
         if (!parsed.success) return null;
         return parsed.data;
       })
-      .filter((v) => v !== null) as UIFilterState | undefined) ?? undefined,
+      .filter((v) => v !== null) as FilterState | undefined) ?? undefined,
 });
 
 // manage state with hook
 export const useQueryFilterState = (
-  initialState: UIFilterState = [],
-  table: string,
+  initialState: FilterState = [],
+  table: TableName,
 ) => {
   const [filterState, setFilterState] = useQueryParam(
     "filter",
@@ -109,7 +99,25 @@ export const useQueryFilterState = (
   return [filterState, setFilterState] as const;
 };
 
-export const useMemoryFilterState = (initialState: UIFilterState = []) => {
+export const useMemoryFilterState = (initialState: FilterState = []) => {
   const [filterState, setFilterState] = useState(initialState);
   return [filterState, setFilterState] as const;
 };
+
+// Utility function to get the ID-Name map based on the table name
+function getIdNameMap(table: TableName): { [key: string]: string } {
+  switch (table) {
+    case "generations":
+      return GENERATIONS_ID_NAME_MAP;
+    case "traces":
+      return TRACES_ID_NAME_MAP;
+    case "sessions":
+      return SESSIONS_ID_NAME_MAP;
+    case "scores":
+      return SCORES_ID_NAME_MAP;
+    case "dashboard":
+      return { traceName: "Trace Name" };
+    default:
+      return {};
+  }
+}
