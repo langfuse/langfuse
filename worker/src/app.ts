e3f405fc475deeb0c8fcdf06c2e0d7d50b7c933a
Max Deichmann <m.deichmann@tum.de>
2024-03-21 14:45:33 +0100

    remove ioredis for now

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -4,15 +4,8 @@ import cors from "cors";
 import * as middlewares from "./middlewares";
 import api from "./api";
 import MessageResponse from "./interfaces/MessageResponse";
-import Redis from "ioredis";
 
 require("dotenv").config();
-
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
-  password: process.env.REDIS_AUTH,
-});
 
 const app = express();
 
