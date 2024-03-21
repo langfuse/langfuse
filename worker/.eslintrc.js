4f9adbbbf686b2f7dc6e666e697d672d328abc93
Max Deichmann <m.deichmann@tum.de>
2024-03-21 14:39:43 +0100

    push

--- worker/.eslintrc.js
+++ worker/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/server.js"],
+  extends: ["@repo/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
