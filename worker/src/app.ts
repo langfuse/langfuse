e5032000c4e206a570ab77fc394c15febca097af
Max Deichmann <m.deichmann@tum.de>
2024-03-21 11:16:54 +0100

    docs and start command

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -9,7 +9,7 @@ import Redis from "ioredis";
 require("dotenv").config();
 
 const redis = new Redis({
-  host: process.env.REDIS_URL,
+  host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
   password: process.env.REDIS_AUTH,
 });
