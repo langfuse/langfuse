abae42063defa34cd0b7654eb047eb46c49b63af
Max Deichmann <m.deichmann@tum.de>
2024-03-23 18:34:11 +0100

    push

--- web/src/features/telemetry/index.ts
+++ web/src/features/telemetry/index.ts
@@ -1,5 +1,5 @@
 import { VERSION } from "@/src/constants";
-import { prisma } from "@langfuse/shared/src/db";
+import { Prisma, prisma } from "@langfuse/shared/src/db";
 import { PostHog } from "posthog-node";
 import { v4 as uuidv4 } from "uuid";
 
