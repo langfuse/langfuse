8255ef121be6591549b1a369701f536ab2b48dd6
Max Deichmann <m.deichmann@tum.de>
2024-03-24 10:31:11 +0100

    push

--- worker/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, type Options } from "tsup";
-
-export default defineConfig((options: Options) => ({
-  entryPoints: ["src/index.ts"],
-  clean: true,
-  format: ["cjs"],
-  ...options,
-}));
