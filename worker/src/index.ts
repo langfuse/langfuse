63aaa2eb132a518d6fa00be54a99bb85e185c85f
Max Deichmann <m.deichmann@tum.de>
2024-03-24 17:53:20 +0100

    feat(worker): add pino logger (#1478)

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import { env } from "./env";
+import logger from "./logger";
 
 app.listen(env.PORT, () => {
-  console.log(`Listening: http://localhost:${env.PORT}`);
+  logger.info(`Listening: http://localhost:${env.PORT}`);
 });
