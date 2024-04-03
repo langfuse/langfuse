20030fde71513a9521f6753a84b9bf71c990a514
Max Deichmann <m.deichmann@tum.de>
2024-04-03 06:45:49 +0200

    refactor: reduce sentry sampling (#1564)

--- web/sentry.edge.config.ts
+++ web/sentry.edge.config.ts
@@ -7,7 +7,7 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN)
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 0.3,
+    tracesSampleRate: 0.2,
 
     // ...
 
