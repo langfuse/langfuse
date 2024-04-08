89a01b2ff2cc54cffe3ea63e57739721a35fa43b
RichardKruemmel <richard.kruemmel@code.berlin>
2024-04-08 18:29:54 +0200

    Add promptsTableCols import to useFilterState.ts

--- web/src/features/filters/hooks/useFilterState.ts
+++ web/src/features/filters/hooks/useFilterState.ts
@@ -14,6 +14,7 @@ import {
   decodeDelimitedArray,
   withDefault,
 } from "use-query-params";
+import { promptsTableCols } from "@/src/server/api/definitions/promptsTable";
 
 const DEBUG_QUERY_STATE = false;
 
