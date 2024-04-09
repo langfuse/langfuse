6d2680bbecdcb201cbc38b0482e63908f6682b70
Marc Klingen <git@marcklingen.com>
2024-04-09 19:52:44 +0200

    feat(api): add newObjectsWithinLastMinute to health api

--- web/src/pages/api/public/health.ts
+++ web/src/pages/api/public/health.ts
@@ -10,15 +10,56 @@ export default async function handler(
 ) {
   await runMiddleware(req, res, cors);
   await telemetry();
+  let tracesWithinLastMinute = false;
+  let observationsWithinLastMinute = false;
   try {
     await prisma.$queryRaw`SELECT 1;`;
+
+    const now = Date.now();
+
+    // Check if there are any traces or observations within the last minute
+    if (
+      await prisma.trace.findFirst({
+        where: {
+          timestamp: {
+            gte: new Date(now - 60000),
+            lte: new Date(now),
+          },
+        },
+        select: {
+          id: true,
+        },
+      })
+    ) {
+      tracesWithinLastMinute = true;
+    }
+    if (
+      await prisma.observation.findFirst({
+        where: {
+          startTime: {
+            gte: new Date(now - 60000),
+            lte: new Date(now),
+          },
+        },
+        select: {
+          id: true,
+        },
+      })
+    ) {
+      observationsWithinLastMinute = true;
+    }
   } catch (e) {
     return res.status(503).json({
       status: "Database not available",
       version: VERSION.replace("v", ""),
     });
   }
-  return res
-    .status(200)
-    .json({ status: "OK", version: VERSION.replace("v", "") });
+  return res.status(200).json({
+    status: "OK",
+    version: VERSION.replace("v", ""),
+    newObjectsWithinLastMinute: {
+      traces: tracesWithinLastMinute,
+      observations: observationsWithinLastMinute,
+    },
+  });
 }
