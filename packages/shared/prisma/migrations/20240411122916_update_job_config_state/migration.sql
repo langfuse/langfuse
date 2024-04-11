46d0ddf58a42bc30bf47f0e7fede6a39adfd84d4
Max Deichmann <m.deichmann@tum.de>
2024-04-11 15:41:55 +0200

    push

--- packages/shared/prisma/migrations/20240411122916_update_job_config_state/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `state` on the `job_configurations` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "job_configurations" DROP COLUMN "state",
-ADD COLUMN     "status" "JobConfigState" NOT NULL DEFAULT 'ACTIVE';
--- packages/shared/prisma/migrations/20240411190828_add_job_config_state/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "JobConfigState" AS ENUM ('ACTIVE', 'INACTIVE');
-
--- AlterTable
-ALTER TABLE "job_configurations" ADD COLUMN     "state" "JobConfigState" NOT NULL DEFAULT 'ACTIVE';
