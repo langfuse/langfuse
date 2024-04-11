db84d60d6486cc0328e8ee02ec2cb848713e1a51
Max Deichmann <m.deichmann@tum.de>
2024-04-11 20:38:01 +0200

    feat: graceful worker shutdown (#1680)
    push

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,7 +1,29 @@
 import app from "./app";
 import { env } from "./env";
 import logger from "./logger";
+import { evalJobCreator, evalJobExecutor } from "./redis/consumer";
 
-app.listen(env.PORT, () => {
+const server = app.listen(env.PORT, () => {
   logger.info(`Listening: http://localhost:${env.PORT}`);
 });
+
+// Function to handle shutdown logic
+function onShutdown() {
+  logger.info("Shutting down application...");
+
+  // Stop accepting new connections
+  server.close();
+
+  // Perform necessary cleanup tasks here
+  // For example, close database connections, stop job executors, etc.
+  evalJobCreator
+    ?.close()
+    .then(() => logger.info("Eval Job Creator has been closed."));
+  evalJobExecutor
+    ?.close()
+    .then(() => logger.info("Eval Job Executor has been closed."));
+}
+
+// Capture shutdown signals
+process.on("SIGTERM", onShutdown);
+process.on("SIGINT", onShutdown);
