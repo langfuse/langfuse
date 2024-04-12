4c681a6aeb5305d254367cfd1750b3b891eb2d79
Marc Klingen <git@marcklingen.com>
2024-04-12 10:53:25 +0200

    chore: edit settings

--- web/src/pages/project/[projectId]/settings.tsx
+++ web/src/pages/project/[projectId]/settings.tsx
@@ -15,6 +15,10 @@ import { env } from "@/src/env.mjs";
 import { Card } from "@tremor/react";
 import { Button } from "@/src/components/ui/button";
 import Link from "next/link";
+import {
+  sendUserChatMessage,
+  showAgentChatMessage,
+} from "@/src/features/support-chat/chat";
 
 export default function SettingsPage() {
   const router = useRouter();
@@ -137,12 +141,16 @@ const Beta = () => {
           Langfuse Events/Metrics available in your Posthog Dashboards.
         </p>
         <div className="flex items-center gap-2">
-          <Button asChild variant="secondary">
-            <Link
-              href={`mailto:early-access@langfuse.com?subject=I%20am%20interested%20in%20the%20Langfuse%3C%3EPosthog%20Integration&body=%23%23%20Please%20fill%20in%20the%20following%20details%20to%20help%20us%20set%20up%20the%20integration%20for%20you.%20These%20settings%20will%20be%20available%20within%20Langfuse%20once%20this%20integration%20is%20generally%20available.%0A%0ALangfuse%20Project%3A%20${encodeURIComponent(window.location.href.replace("/settings", ""))}%0APostHog%20Host%20(EU%2C%20US%20or%20self-hosted)%3A%0APostHog%20Key%20(starting%20with%20%22phc_%22)%3A%0A%0A%0A%23%23%20Do%20you%20prefer%20to%20set%20this%20up%20via%20a%20short%20call%20or%20async%20via%20email%3F%0A%0A%0A%0A%23%23%20Any%20Questions%3F%0A%0A%0A%0A%23%23%20Links%20to%20docs%0A%0Ahttps%3A%2F%2Flangfuse.com%2Fdocs%2Fanalytics%2Fposthog%0Ahttps%3A%2F%2Fposthog.com%2Fdocs%2Fproduct-analytics%2Fllms%0A`}
-            >
-              Request Access (Email)
-            </Link>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              sendUserChatMessage(
+                "I am interested to join the PostHog Integration Beta",
+              );
+              showAgentChatMessage("We'll be in touch to get you set up!");
+            }}
+          >
+            Get Access
           </Button>
           <Button asChild variant="ghost">
             <Link href="https://langfuse.com/docs/analytics/posthog">
