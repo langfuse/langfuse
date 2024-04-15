32e5cc975403b41d49bc0b5f8aec83674236f16c
Max Deichmann <m.deichmann@tum.de>
2024-04-15 17:12:12 +0200

    push

--- packages/shared/prisma/migrations/20240415164142_update_models copy/migration.sql
+++ packages/shared/prisma/migrations/20240415164142_update_models copy/migration.sql
@@ -24,7 +24,8 @@ VALUES
   
   -- add new model name gpt-4-turbo-preview pointing to gpt-4-0125-preview
   ('clv12vkly000008jre4cohbx1', NULL, 'gpt-4-turbo-preview', '(?i)^(gpt-4-turbo-preview)$',  NULL, 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4-turbo-preview" }'),
-  -- add new model name gpt-4-turbo currentlu pointing to gpt-4-turbo-2024-04-09
+  -- add new model name gpt-4-turbo currently pointing to gpt-4-turbo-2024-04-09
   ('clrkvq6iq000008ju6c16gynt', NULL, 'gpt-4-turbo', '(?i)^(gpt-4-turbo)$',  NULL, 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4-turbo" }'),
+  -- apparently azure supports gpt-4-preview
   ('clrkvq6iq000008ju6c16gynt', NULL, ' gpt-4-preview', '(?i)^(gpt-4-turbo-preview)$',  NULL, 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4-turbo-preview" }')
   
