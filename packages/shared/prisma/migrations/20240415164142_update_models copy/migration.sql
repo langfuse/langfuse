6625e7a0093c0504f0b4cbebd532d301f412f461
Max Deichmann <m.deichmann@tum.de>
2024-04-15 17:14:21 +0200

    push

--- packages/shared/prisma/migrations/20240415164142_update_models copy/migration.sql
+++ packages/shared/prisma/migrations/20240415164142_update_models copy/migration.sql
@@ -16,7 +16,7 @@ INSERT INTO models (
   tokenizer_config
 )
 VALUES
-  -- update gpt-4-1106-preview to gpt-4-1106-preview also in the naming
+  -- update gpt-4-1106-preview naming replacing gpt-4-turbo
   ('clrkvq6iq000008ju6c16gynt', NULL, 'gpt-4-1106-preview', '(?i)^(gpt-4-1106-preview)$', NULL, 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4-1106-preview" }'),
   -- updating tokenizer model to gpt-4-turbo-2024-04-09
   ('cluv2t5k3000508ih5kve9zag', NULL, 'gpt-4-turbo-2024-04-09', '(?i)^(gpt-4-turbo-2024-04-09)$', NULL, 0.00001, 0.00003, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4-turbo-2024-04-09" }'),
