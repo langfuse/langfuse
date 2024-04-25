e11d02a86122dc08b9c3a78c9e29e1a06224515f
Max Deichmann <m.deichmann@tum.de>
2024-04-25 20:55:38 +0200

    perf: reduce sentry sampling rate (#1861)

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -30,9 +30,9 @@ if (isSentryEnabled) {
       nodeProfilingIntegration(),
     ],
     // Performance Monitoring
-    tracesSampleRate: 0.5, //  Capture 100% of the transactions
+    tracesSampleRate: 0.2, //  Capture 100% of the transactions
     // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 0.5,
+    profilesSampleRate: 0.2,
   });
 
   // The request handler must be the first middleware on the app
