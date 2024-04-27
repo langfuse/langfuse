a80e89c79fc71802c5bb390bf5546df09f3a7529
Max Deichmann <m.deichmann@tum.de>
2024-04-27 13:54:23 +0200

    push

--- worker/src/__tests__/redis-consumer.test.ts
+++ worker/src/__tests__/redis-consumer.test.ts
@@ -5,6 +5,7 @@ import { randomUUID } from "crypto";
 import { z } from "zod";
 import logger from "../logger";
 import { evalJobCreator } from "../redis/consumer";
+import { log } from "console";
 
 describe("handle redis events", () => {
   test("handle redis job succeeding", async () => {
@@ -39,6 +40,55 @@ describe("handle redis events", () => {
       async () => {
         const jobState = await evalQueue?.getJobState(job!.id!);
         expect(jobState).toEqual("completed");
+      },
+      {
+        timeout: 10_000,
+      }
+    );
+  }, 10_000);
+
+  test("handle redis job failing", async () => {
+    vi.mock("../eval-service", () => ({
+      createEvalJobs: async ({
+        data,
+      }: {
+        data: z.infer<typeof TraceUpsertEvent>;
+      }) => {
+        logger.error("Failed to create eval jobs");
+        throw new Error("Failed to create eval jobs");
+      },
+    }));
+
+    // this activates the consumer
+    evalJobCreator?.on("completed", (job, err) => {
+      logger.info(`Eval Job with id ${job?.id} completed`);
+    });
+
+    expect(evalQueue).toBeDefined();
+
+    const job = await evalQueue?.add(
+      QueueJobs.TraceUpsert,
+      {
+        id: randomUUID(),
+        timestamp: new Date(),
+        payload: {
+          projectId: "project-id",
+          traceId: "trace-id",
+        },
+        name: QueueJobs.TraceUpsert as const,
+      },
+      {
+        attempts: 2,
+      }
+    );
+
+    await vi.waitFor(
+      async () => {
+        const jobState = await evalQueue?.getJobState(job!.id!);
+        logger.info(`Job state: ${jobState}`);
+        expect(jobState).toEqual("failed");
+        const j = await evalQueue?.getJob(job!.id!);
+        expect(j?.attemptsMade).toEqual(2);
       },
       {
         timeout: 10_000,
