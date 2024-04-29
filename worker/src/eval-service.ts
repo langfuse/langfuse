3959298d92ce19c3f96a82bef0da82035c3e2c9c
Max Deichmann <m.deichmann@tum.de>
2024-04-29 15:28:41 +0200

    push

--- worker/src/eval-service.ts
+++ worker/src/eval-service.ts
@@ -265,9 +265,6 @@ export const evaluate = async ({
     .executeTakeFirst();
 
   if (!apiKey) {
-    logger.error(
-      `API key for provider ${provider} and project ${event.projectId} not found. Failing job id ${job.id}`
-    );
     // this will fail the eval execution if a user deletes the API key.
     throw new LangfuseNotFoundError(
       `API key for provider ${provider} and project ${event.projectId} not found.`
