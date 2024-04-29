aa589422e12f35af0708f6ec64e989c5cb2430e1
RichardKruemmel <richard.kruemmel@code.berlin>
2024-04-29 19:03:19 +0200

    Add posthog tracking to DataTableRowHeightSwitch component

--- web/src/components/table/data-table-row-height-switch.tsx
+++ web/src/components/table/data-table-row-height-switch.tsx
@@ -1,5 +1,7 @@
 import { Tabs, TabsList, TabsTrigger } from "@/src/components/ui/tabs";
 import useLocalStorage from "@/src/components/useLocalStorage";
+import useTableNameFromURL from "@/src/hooks/useTableNameFromURL";
+import { usePostHog } from "posthog-js/react";
 import {
   MdDensityLarge,
   MdDensityMedium,
@@ -35,11 +37,16 @@ export const DataTableRowHeightSwitch = ({
 }: {
   rowHeight: RowHeight;
   setRowHeight: (e: RowHeight) => void;
-}) => (
+}) => {
+  const posthog = usePostHog();
+  const tableName = useTableNameFromURL();
+  return (
   <Tabs
     //defaultValue={height}
     value={rowHeight}
-    onValueChange={(e) => setRowHeight(e as any)}
+    onValueChange={(e) => {
+      posthog.capture("table:row_height_switch_select", { "table": tableName, "size": e });
+      setRowHeight(e as any)}}
     key="height"
   >
     <TabsList className="gap-1 border bg-transparent px-2">
@@ -55,5 +62,5 @@ export const DataTableRowHeightSwitch = ({
         </TabsTrigger>
       ))}
     </TabsList>
-  </Tabs>
-);
+  </Tabs>);
+};
