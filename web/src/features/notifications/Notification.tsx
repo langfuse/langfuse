d1cffe5d4dfb8a0bdda4764bfde4c543858d62fb
RichardKruemmel <richard.kruemmel@code.berlin>
2024-05-02 16:33:20 +0200

    Add posthog tracking to dismiss notification button

--- web/src/features/notifications/Notification.tsx
+++ web/src/features/notifications/Notification.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { usePostHog } from "posthog-js/react";
 
 export interface TNotification {
   id: number;
@@ -19,29 +20,35 @@ const Notification: React.FC<NotificationProps> = ({
   setLastSeenId,
   dismissToast,
   toast,
-}) => (
-  <div className="flex justify-between">
-    <div className="flex min-w-[300px] flex-1 flex-col justify-center">
-      <div className="m-0 text-sm font-medium leading-tight text-gray-800">
-        {notification.message}
-      </div>
-      {notification.description && (
-        <div className="mt-2 flex-1 text-sm leading-tight text-gray-800">
-          {notification.description}
+}) => {
+  const posthog = usePostHog();
+  return (
+    <div className="flex justify-between">
+      <div className="flex min-w-[300px] flex-1 flex-col justify-center">
+        <div className="m-0 text-sm font-medium leading-tight text-gray-800">
+          {notification.message}
         </div>
-      )}
+        {notification.description && (
+          <div className="mt-2 flex-1 text-sm leading-tight text-gray-800">
+            {notification.description}
+          </div>
+        )}
+      </div>
+      <button
+        className="flex h-6 w-6 cursor-pointer items-start justify-end border-none bg-transparent p-0 text-gray-800 transition-colors duration-200"
+        onClick={() => {
+          posthog.capture("notification:dismiss_notification", {
+            notificationId: notification.id,
+          });
+          setLastSeenId(notification.id);
+          dismissToast(toast);
+        }}
+        aria-label="Close"
+      >
+        <X size={14} />
+      </button>
     </div>
-    <button
-      className="flex h-6 w-6 cursor-pointer items-start justify-end border-none bg-transparent p-0 text-gray-800 transition-colors duration-200"
-      onClick={() => {
-        setLastSeenId(notification.id);
-        dismissToast(toast);
-      }}
-      aria-label="Close"
-    >
-      <X size={14} />
-    </button>
-  </div>
-);
+  );
+};
 
 export default Notification;
