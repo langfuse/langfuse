a64ae3bd7b310e8dcff31df0009f9be3da9f0267
Marc Klingen <git@marcklingen.com>
2024-05-03 15:06:36 +0200

    CREATE INDEX CONCURRENTLY

--- packages/shared/prisma/migrations/20240503125742_traces_add_createdat_updatedat/migration.sql
+++ packages/shared/prisma/migrations/20240503125742_traces_add_createdat_updatedat/migration.sql
@@ -1,9 +1,3 @@
 -- AlterTable
 ALTER TABLE "traces" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
--- CreateIndex
-CREATE INDEX "traces_updated_at_idx" ON "traces"("updated_at");
-
--- CreateIndex
-CREATE INDEX "traces_created_at_idx" ON "traces"("created_at");
