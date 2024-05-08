6800eafc28f868214aa45257d03a1f64d0280803
RichardKruemmel <richard.kruemmel@code.berlin>
2024-05-08 15:58:41 +0200

    Update level colors in LevelColors component

--- web/src/components/level-colors.tsx
+++ web/src/components/level-colors.tsx
@@ -1,6 +1,6 @@
 export const LevelColors = {
   DEFAULT: { text: "", bg: "" },
-  DEBUG: { text: "text-gray-500", bg: "bg-gray-50" },
+  DEBUG: { text: "text-muted-foreground", bg: "bg-primary-foreground" },
   WARNING: { text: "text-yellow-800", bg: "bg-yellow-50" },
   ERROR: { text: "text-red-800", bg: "bg-red-50" },
 };
