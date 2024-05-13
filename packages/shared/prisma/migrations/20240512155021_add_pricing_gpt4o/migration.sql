a96e7dbdb420e8504b3139d22b450e851bb736ff
Marc Klingen <git@marcklingen.com>
2024-05-14 00:29:11 +0200

    add tokenizer config for gpt4o

--- packages/shared/prisma/migrations/20240512155021_add_pricing_gpt4o/migration.sql
+++ packages/shared/prisma/migrations/20240512155021_add_pricing_gpt4o/migration.sql
@@ -13,10 +13,8 @@ INSERT INTO models (
 )
 VALUES
   -- gpt-4o
-  -- tokenizer_config to be added when js-tiktoken is upgraded, gpt-4o is not supported yet
-  ('b9854a5c92dc496b997d99d20', NULL, 'gpt-4o', '(?i)^(gpt-4o)$', NULL, 0.000005, 0.000015, NULL, 'TOKENS', 'openai', NULL),
+  ('b9854a5c92dc496b997d99d20', NULL, 'gpt-4o', '(?i)^(gpt-4o)$', NULL, 0.000005, 0.000015, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4o" }'),
   
   -- gpt-4o-2024-05-13
-  -- tokenizer_config to be added when js-tiktoken is upgraded, gpt-4o is not supported yet
-  ('b9854a5c92dc496b997d99d21', NULL, 'gpt-4o-2024-05-13', '(?i)^(gpt-4o-2024-05-13)$', NULL, 0.000005, 0.000015, NULL, 'TOKENS', 'openai', NULL)
+  ('b9854a5c92dc496b997d99d21', NULL, 'gpt-4o-2024-05-13', '(?i)^(gpt-4o-2024-05-13)$', NULL, 0.000005, 0.000015, NULL, 'TOKENS', 'openai', '{ "tokensPerMessage": 3, "tokensPerName": 1, "tokenizerModel": "gpt-4o-2024-05-13" }')
   
