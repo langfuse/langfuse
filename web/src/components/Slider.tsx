0874dc7b952e86058eaaeb62c17e18211a96107c
RichardKruemmel <richard.kruemmel@code.berlin>
2024-05-15 18:57:35 +0200

    Update Slider component styles

--- web/src/components/Slider.tsx
+++ web/src/components/Slider.tsx
@@ -14,7 +14,7 @@ export const Slider = (props: {
     onChange={props.onChecked}
     className={cn(
       props.isChecked ? "bg-background" : "bg-input",
-      "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2",
+      "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-offset-2",
     )}
   >
     <span className="sr-only">Use setting</span>
