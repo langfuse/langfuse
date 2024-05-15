dbf8b33f41380e87e4f0e6524ba0835d5e11d3e3
Marc Klingen <git@marcklingen.com>
2024-05-15 15:00:40 +0200

    concurrent unique index creation

--- packages/shared/prisma/migrations/20240513082204_scores_unique_id_and_projectid_instead_of_id_and_traceid_index/migration.sql
+++ packages/shared/prisma/migrations/20240513082204_scores_unique_id_and_projectid_instead_of_id_and_traceid_index/migration.sql
@@ -4,5 +4,5 @@
   - A unique constraint covering the columns `[id,project_id]` on the table `scores` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropIndex
-DROP INDEX "scores_id_trace_id_key";
+-- CreateIndex
+CREATE UNIQUE INDEX CONCURRENTLY "scores_id_project_id_key" ON "scores"("id", "project_id");
