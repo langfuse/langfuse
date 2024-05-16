403054adeb1fc6c46c373deb7b81358b46c9ce03
Marc Klingen <git@marcklingen.com>
2024-05-16 16:12:42 +0200

    use dvh for fullHeight pages to work in mobile browsers

--- web/src/components/layouts/full-screen-page.tsx
+++ web/src/components/layouts/full-screen-page.tsx
@@ -2,7 +2,7 @@ export const FullScreenPage: React.FC<React.PropsWithChildren<{}>> = ({
   children,
 }) => {
   return (
-    <div className="flex h-[calc(100vh-6rem)] flex-col overflow-hidden lg:h-[calc(100vh-2rem)]">
+    <div className="flex h-[calc(100dvh-6rem)] flex-col overflow-hidden lg:h-[calc(100dvh-2rem)]">
       {children}
     </div>
   );
