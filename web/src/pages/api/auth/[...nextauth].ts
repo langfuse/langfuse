7e2690c786db4a200841786d5d5be82c8369f648
Max Deichmann <m.deichmann@tum.de>
2024-05-17 16:01:33 +0200

    fix: set cache control explicitly

--- web/src/pages/api/auth/[...nextauth].ts
+++ web/src/pages/api/auth/[...nextauth].ts
@@ -5,5 +5,9 @@ import NextAuth from "next-auth";
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   // Do whatever you want here, before the request is passed down to `NextAuth`
   const authOptions = await getAuthOptions();
+  // https://github.com/nextauthjs/next-auth/issues/2408#issuecomment-1382629234
+  // for api routes, we need to call the headers in the api route itself
+  // disable caching for anything auth related
+  res.setHeader("Cache-Control", "no-store, max-age=0");
   return await NextAuth(req, res, authOptions);
 }
