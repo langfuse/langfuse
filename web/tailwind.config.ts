0b1aa06e9e82d6d75fcf7fb7db1f1c735e5c7543
Marc Klingen <git@marcklingen.com>
2024-05-22 00:58:52 +0200

    use typing

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
