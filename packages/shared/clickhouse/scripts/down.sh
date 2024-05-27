56e66c195119f0cea0535cd61eafe5d7068b6ff8
Max Deichmann <m.deichmann@tum.de>
2024-05-27 11:18:10 +0200

    migrations

---
+++ packages/shared/clickhouse/scripts/down.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load environment variables
+source ../../.env
+
+# Construct the database URL
+DATABASE_URL="clickhouse://${CLICKHOUSE_MIGRATION_URL}?username=${CLICKHOUSE_USER}&password=${CLICKHOUSE_PASSWORD}&database=langfuse&x-multi-statement=true"
+
+
+echo $DATABASE_URL
+# Execute the drop command
+migrate -source file://clickhouse/migrations -database "$DATABASE_URL" down
