7ec7e1c93abc2b83975189f427cc44bf68a0e778
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-05-28 20:26:24 +0200

    feat: annotation button with saving scores on touch

--- web/src/features/manual-scoring/components/AnnotateButton.tsx
+++ web/src/features/manual-scoring/components/AnnotateButton.tsx
@@ -1,7 +1,7 @@
 import { useHasAccess } from "@/src/features/rbac/utils/checkAccess";
 import React, { useState } from "react";
 import { Button } from "@/src/components/ui/button";
-import { LockIcon, X } from "lucide-react";
+import { LockIcon, TrashIcon, X } from "lucide-react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -13,10 +13,8 @@ import {
 import Link from "next/link";
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
   DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
@@ -37,15 +35,27 @@ import {
 } from "@/src/components/ui/popover";
 import { api } from "@/src/utils/api";
 import { type CheckedState } from "@radix-ui/react-checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/src/components/ui/select";
+import { cn } from "@/src/utils/tailwind";
+
+type ConfigCategory = {
+  label: string;
+  value: string;
+};
 
 const score = z.object({
-  id: z.string().optional(),
+  scoreId: z.string().optional(),
   name: z.string(),
-  value: z.coerce.number(),
+  value: z.number(),
   stringValue: z.string().optional(),
   dataType: z.nativeEnum(ScoreDataType),
   configId: z.string().optional(),
-  isDeleted: z.boolean(),
 });
 
 const formSchema = z.object({
@@ -65,7 +75,6 @@ export function AnnotateButton({
 }) {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
-  // replace current default values with trpc call to fetch data
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -79,13 +88,12 @@ export function AnnotateButton({
               : s.observationId === null),
         )
         .map((s) => ({
-          id: s.id,
+          scoreId: s.id,
           name: s.name,
           value: s.value,
-          stringValue: s.stringValue ?? undefined,
           dataType: s.dataType,
+          stringValue: s.stringValue ?? undefined,
           configId: s.configId ?? undefined,
-          isDeleted: false,
         })),
     },
   });
@@ -95,7 +103,7 @@ export function AnnotateButton({
     scope: "scores:CUD",
   });
 
-  const { fields, append, update, remove } = useFieldArray({
+  const { fields, append, remove, update } = useFieldArray({
     control: form.control,
     name: "scoreData",
   });
@@ -109,23 +117,67 @@ export function AnnotateButton({
     },
   );
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    // capture("eval_config:new_form_submit");
+  const mutDeleteScore = api.scores.deleteAnnotationScore.useMutation({
+    onMutate: async () => {
+      // setIsLoading(true);
+      // Snapshot the previous value
+      // return { prev };
+    },
+    onError: (err, _newTags, context) => {
+      // setIsLoading(false);
+      // Rollback to the previous value if mutation fails
+      // we should also set form error here
+    },
+    onSettled: async (data, error) => {
+      if (!data || error) return; // handle error
+
+      const { id } = data;
+      const updatedScoreIndex = fields.findIndex(
+        (field) => field.scoreId === id,
+      );
+      remove(updatedScoreIndex);
+    },
+  });
+
+  const mutScores = api.scores.annotate.useMutation({
+    onMutate: async () => {
+      // setIsLoading(true);
+      // Snapshot the previous value
+      // return { prev };
+    },
+    onError: (err, _newTags, context) => {
+      // setIsLoading(false);
+      // Rollback to the previous value if mutation fails
+      // we should also set form error here
+    },
+    onSettled: (data, error) => {
+      if (!data || error) return; // handle error
+
+      const { id, value, stringValue, name, dataType, configId } = data;
+      const updatedScoreIndex = fields.findIndex(
+        (field) => field.configId === configId,
+      );
+
+      update(updatedScoreIndex, {
+        scoreId: id,
+        value,
+        stringValue: stringValue ?? undefined,
+        name,
+        dataType,
+        configId: configId ?? undefined,
+      });
+    },
+  });
+
+  function getConfigCategories(score: any): ConfigCategory[] {
+    return (
+      (configs.data?.configs.find((config) => config.id === score.configId)
+        ?.categories as ConfigCategory[]) ?? []
+    );
   }
 
   function handleOnCheckedChange(config: ScoreConfig, value: CheckedState) {
-    const isOriginalScore = form.formState.defaultValues?.scoreData?.some(
-      (score) => !!score && score.configId === config.id,
-    );
     const index = fields.findIndex((field) => field.configId === config.id);
-
-    if (isOriginalScore) {
-      update(index, {
-        ...fields[index],
-        isDeleted: !value,
-      });
-      return;
-    }
 
     value
       ? append({
@@ -133,7 +185,6 @@ export function AnnotateButton({
           value: 0,
           dataType: config.dataType,
           configId: config.id,
-          isDeleted: false,
         })
       : remove(index);
   }
@@ -162,12 +213,12 @@ export function AnnotateButton({
                       Add score
                     </Button>
                   </PopoverTrigger>
-                  <PopoverContent className="max-h-96 overflow-y-auto">
+                  <PopoverContent>
                     <div className="flex flex-col space-y-4">
                       <div className="flex items-center justify-between">
                         <>
                           <Link
-                            className="sticky top-0 z-50 inline-block
+                            className="inline-block
        rounded bg-primary-accent/10 px-2 py-1 text-sm font-semibold text-accent-dark-blue shadow-sm hover:bg-accent-light-blue/45"
                             href={`/project/${projectId}/settings`}
                           >
@@ -191,9 +242,7 @@ export function AnnotateButton({
                         >
                           <Checkbox
                             checked={fields.some(
-                              (field) =>
-                                field.configId === config.id &&
-                                !field.isDeleted,
+                              (field) => field.configId === config.id,
                             )}
                             onCheckedChange={(value) =>
                               handleOnCheckedChange(config, value)
@@ -212,11 +261,7 @@ export function AnnotateButton({
             </DrawerDescription>
           </DrawerHeader>
           <Form {...form}>
-            <form
-              // eslint-disable-next-line @typescript-eslint/no-misused-promises
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="flex flex-col gap-4"
-            >
+            <form className="flex flex-col gap-4">
               <div className="grid grid-flow-row gap-2 px-4">
                 <FormField
                   control={form.control}
@@ -226,42 +271,101 @@ export function AnnotateButton({
                       <FormControl>
                         Here will some variable mapping be added.
                       </FormControl>
-                      {fields.map((score, index) =>
-                        score.isDeleted ? null : (
-                          <div
-                            key={`${score.id}-langfuseObject`}
-                            className="grid grid-cols-[auto,1fr,auto] items-center gap-2 text-left"
+                      {fields.map((score, index) => (
+                        <div
+                          key={score.id}
+                          className="grid grid-cols-[auto,1fr,2fr,auto] items-center gap-2 text-left"
+                        >
+                          <div className="h-4 w-4 shrink-0 rounded-sm bg-primary-accent" />
+                          <span className="text-sm">{score.name}</span>
+                          <FormField
+                            control={form.control}
+                            name={`scoreData.${index}.value`}
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormControl>
+                                  {score.dataType === ScoreDataType.NUMERIC ? (
+                                    <Input
+                                      {...field}
+                                      onBlur={async () => {
+                                        if (!!field.value)
+                                          await mutScores.mutateAsync({
+                                            projectId,
+                                            traceId,
+                                            ...score,
+                                            observationId,
+                                            value: Number(field.value),
+                                            id: score.scoreId,
+                                          });
+                                      }}
+                                    />
+                                  ) : (
+                                    <Select
+                                      defaultValue={score.stringValue}
+                                      onValueChange={async (value) => {
+                                        if (!!value)
+                                          await mutScores.mutateAsync({
+                                            projectId,
+                                            traceId,
+                                            ...score,
+                                            observationId,
+                                            value: Number(field.value),
+                                            id: score.scoreId,
+                                            stringValue: value,
+                                          });
+                                      }}
+                                    >
+                                      <SelectTrigger>
+                                        <SelectValue placeholder="Select category" />
+                                      </SelectTrigger>
+                                      <SelectContent>
+                                        {getConfigCategories(score).map(
+                                          (category) => (
+                                            <SelectItem
+                                              key={category.value}
+                                              value={category.label}
+                                            >
+                                              {category.label}
+                                            </SelectItem>
+                                          ),
+                                        )}
+                                      </SelectContent>
+                                    </Select>
+                                  )}
+                                </FormControl>
+                              </FormItem>
+                            )}
+                          />
+                          <Button
+                            variant="outline"
+                            size="icon"
+                            type="button"
+                            onClick={async () => {
+                              // capture("scores:delete_form_open", {
+                              //   source: "annotation",
+                              // });
+                              if (score.scoreId)
+                                await mutDeleteScore.mutateAsync({
+                                  id: score.scoreId,
+                                  projectId,
+                                });
+                            }}
                           >
-                            <div className="h-4 w-4 shrink-0 rounded-sm bg-primary-accent" />
-                            <span className="text-sm">{score.name}</span>
-                            <FormField
-                              control={form.control}
-                              name={`scoreData.${index}.value`}
-                              render={({ field }) => (
-                                <FormItem>
-                                  <FormControl>
-                                    <Input {...field} />
-                                  </FormControl>
-                                </FormItem>
+                            <TrashIcon
+                              className={cn(
+                                "h-4 w-4 text-muted-gray",
+                                score.scoreId ? "text-gray" : "text-muted-gray",
                               )}
                             />
-                          </div>
-                        ),
-                      )}
+                          </Button>
+                        </div>
+                      ))}
                     </>
                   )}
                 />
               </div>
             </form>
           </Form>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose asChild>
-              <Button variant="outline" onClick={() => setIsPopoverOpen(false)}>
-                Cancel
-              </Button>
-            </DrawerClose>
-          </DrawerFooter>
         </div>
       </DrawerContent>
     </Drawer>
