b14e782b9ef5f07d1fe4c496462ba4682b36e679
Max Deichmann <m.deichmann@tum.de>
2024-05-28 09:55:53 +0200

    push

--- packages/shared/clickhouse/scripts/down.sh
+++ packages/shared/clickhouse/scripts/down.sh
@@ -4,7 +4,8 @@
 source ../../.env
 
 # Construct the database URL
-DATABASE_URL="clickhouse://${CLICKHOUSE_MIGRATION_URL}?username=${CLICKHOUSE_USER}&password=${CLICKHOUSE_PASSWORD}&database=langfuse&x-multi-statement=true"
+# DATABASE_URL="clickhouse://${CLICKHOUSE_MIGRATION_URL}?username=${CLICKHOUSE_USER}&password=${CLICKHOUSE_PASSWORD}&database=langfuse&x-multi-statement=true"
+DATABASE_URL="clickhouse://${CLICKHOUSE_MIGRATION_URL}?username=${CLICKHOUSE_USER}&password=${CLICKHOUSE_PASSWORD}&database=langfuse&x-multi-statement=true&secure=true&skip_verify=true&x-migrations-table-engine=MergeTree"
 
 
 # Execute the drop command
--- packages/shared/clickhouse/scripts/up.sh
+++ packages/shared/clickhouse/scripts/up.sh
@@ -4,7 +4,6 @@
 source ../../.env
 
 # Construct the database URL
-DATABASE_URL="clickhouse://${CLICKHOUSE_MIGRATION_URL}?username=${CLICKHOUSE_USER}&password=${CLICKHOUSE_PASSWORD}&database=langfuse&x-multi-statement=true"
-
+DATABASE_URL="clickhouse://${CLICKHOUSE_MIGRATION_URL}?username=${CLICKHOUSE_USER}&password=${CLICKHOUSE_PASSWORD}&database=langfuse&x-multi-statement=true&secure=true&skip_verify=true&x-migrations-table-engine=MergeTree"
 # Execute the drop command
 migrate -source file://clickhouse/migrations -database "$DATABASE_URL" up
