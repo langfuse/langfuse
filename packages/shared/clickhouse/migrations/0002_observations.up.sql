a3946d67d1fbee8ba80dc9cbde7252a9034212bb
Max Deichmann <m.deichmann@tum.de>
2024-06-04 10:57:34 +0200

    push

--- packages/shared/clickhouse/migrations/0002_observations.up.sql
+++ packages/shared/clickhouse/migrations/0002_observations.up.sql
@@ -1,18 +1,18 @@
 CREATE TABLE observations (
     `id` String,
     `trace_id` String,
-    `project_id` String,
-    `type` String,
+    `project_id` UUID,
+    `type` LowCardinality(String),
     `parent_observation_id` Nullable(String),
     `start_time` DateTime64(6),
     `end_time` Nullable(DateTime64(6)),
     `name` String,
-    metadata Map(String, String) CODEC(ZSTD(1)),
-    `level` String,
+    metadata Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+    `level` LowCardinality(String),
     `status_message` Nullable(String),
     `version` Nullable(String),
-    `input` Nullable(String) CODEC(ZSTD(1)),
-    `output` Nullable(String) CODEC(ZSTD(1)),
+    `input` Nullable(String) CODEC(ZSTD(3)),
+    `output` Nullable(String) CODEC(ZSTD(3)),
     `model` Nullable(String),
     `internal_model` Nullable(String),
     `model_parameters` Nullable(String),
@@ -26,16 +26,16 @@ CREATE TABLE observations (
     `completion_start_time` Nullable(DateTime64(6)),
     `prompt_id` Nullable(String),
     created_at DateTime64(6) DEFAULT now(),
-    INDEX idx_id id TYPE bloom_filter(0.001) GRANULARITY 1,
-    INDEX idx_trace_id trace_id TYPE bloom_filter(0.001) GRANULARITY 1,
-    INDEX idx_project_id trace_id TYPE bloom_filter(0.001) GRANULARITY 1,
-    INDEX idx_res_metadata_key mapKeys(metadata) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_res_metadata_value mapValues(metadata) TYPE bloom_filter(0.01) GRANULARITY 1
+    INDEX idx_id id TYPE bloom_filter() GRANULARITY 1,
+    INDEX idx_trace_id trace_id TYPE bloom_filter() GRANULARITY 1,
+    INDEX idx_project_id project_id TYPE bloom_filter() GRANULARITY 1,
+    INDEX idx_res_metadata_key mapKeys(metadata) TYPE bloom_filter() GRANULARITY 1,
+    INDEX idx_res_metadata_value mapValues(metadata) TYPE bloom_filter() GRANULARITY 1
 ) ENGINE = ReplacingMergeTree Partition by toYYYYMM(start_time)
 ORDER BY (
         project_id,
         `type`,
-        toUnixTimestamp(start_time),
         trace_id,
+        toUnixTimestamp(start_time),
         id
     );
