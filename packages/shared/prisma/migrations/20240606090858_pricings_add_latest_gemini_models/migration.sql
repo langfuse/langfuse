77b7162a254c10f178b0cfd044aea99ee2be5e8c
Marc Klingen <git@marcklingen.com>
2024-06-06 11:14:43 +0200

    feat: add token/character count support for gemini-1.5-flash and gemini-1.5-pro

---
+++ packages/shared/prisma/migrations/20240606090858_pricings_add_latest_gemini_models/migration.sql
@@ -0,0 +1,5 @@
+-- does not include pricing yet, will be added as soon as it is calculated at ingestion time
+
+INSERT INTO "public"."models" ("id", "model_name", "match_pattern", "unit") VALUES ('clx30djsn0000w9mzebiv41we', 'gemini-1.5-flash', '(?i)^(gemini-1.5-flash)(@[a-zA-Z0-9]+)?$', 'CHARACTERS');
+
+INSERT INTO "public"."models" ("id", "model_name", "match_pattern", "unit") VALUES ('clx30hkrx0000w9mz7lqi0ial', 'gemini-1.5-pro', '(?i)^(gemini-1.5-pro)(@[a-zA-Z0-9]+)?$', 'CHARACTERS');
