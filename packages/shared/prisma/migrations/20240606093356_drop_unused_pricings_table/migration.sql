4266b46dd94206d8d53a823539524429458f98c0
Marc Klingen <git@marcklingen.com>
2024-06-06 11:36:08 +0200

    chore(db): drop legacy `pricings` table, replaced by `models` (#2256)

---
+++ packages/shared/prisma/migrations/20240606093356_drop_unused_pricings_table/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `pricings` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "pricings";
+
+-- DropEnum
+DROP TYPE "PricingUnit";
+
+-- DropEnum
+DROP TYPE "TokenType";
