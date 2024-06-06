ab7c361a80b602f355e4c3818f60f6c2e78c2fb4
Marc Klingen <git@marcklingen.com>
2024-06-06 02:48:23 +0200

    feat(ui): add upgrade popover to version number (#2251)

--- web/src/components/LangfuseLogo.tsx
+++ web/src/components/LangfuseLogo.tsx
@@ -3,6 +3,83 @@ import { AlertTriangle, Check } from "lucide-react";
 import { VERSION } from "@/src/constants";
 import { env } from "@/src/env.mjs";
 import { cn } from "@/src/utils/tailwind";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/src/components/ui/popover";
+import { ArrowUp } from "lucide-react";
+import { api } from "@/src/utils/api";
+import { Button } from "@/src/components/ui/button";
+import Link from "next/link";
+
+const VersionLabel = ({ className }: { className?: string }) => {
+  const checkUpdate = api.public.checkUpdate.useQuery(undefined, {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    enabled: !env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION, // do not check for updates on Langfuse Cloud
+    onError: (error) => console.error("checkUpdate error", error), // do not render default error message
+  });
+
+  const hasUpdate =
+    !env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION &&
+    checkUpdate.data &&
+    checkUpdate.data.updateType;
+
+  const color =
+    checkUpdate.data?.updateType === "major"
+      ? "text-dark-red"
+      : checkUpdate.data?.updateType === "minor"
+        ? "text-dark-yellow"
+        : undefined;
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="ghost" size="xs" className={className}>
+          {VERSION}
+          {hasUpdate && <ArrowUp className={`ml-1 h-3 w-3 ${color}`} />}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="max-w-[260px]">
+        {hasUpdate ? (
+          <div className="mb-4 text-center">
+            New {checkUpdate.data?.updateType} version:{" "}
+            {checkUpdate.data?.latestRelease}
+          </div>
+        ) : !env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION ? (
+          <div className="mb-4 text-center">This is the latest release</div>
+        ) : null}
+        <div className="flex flex-col gap-2">
+          <Button size="sm" variant="secondary" asChild>
+            <Link
+              href="https://github.com/langfuse/langfuse/releases"
+              target="_blank"
+            >
+              GitHub Releases
+            </Link>
+          </Button>
+          <Button size="sm" variant="secondary" asChild>
+            <Link href="https://langfuse.com/changelog" target="_blank">
+              Changelog
+            </Link>
+          </Button>
+          {hasUpdate && (
+            <Button size="sm">
+              <Link
+                href="https://langfuse.com/docs/deployment/self-host#update"
+                target="_blank"
+              >
+                Update
+              </Link>
+            </Button>
+          )}
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
 
 export const LangfuseIcon = ({
   size = 32,
@@ -65,22 +142,12 @@ export const LangfuseLogo = ({
       <span
         className={cn(
           "ml-2 font-mono font-semibold",
-          size === "sm" ? "text-sm" : "text-xl",
+          size === "sm" ? "text-sm" : "text-lg",
         )}
       >
         Langfuse
       </span>
-      {version && (
-        <a
-          href="https://github.com/langfuse/langfuse/releases"
-          target="_blank"
-          rel="noopener"
-          title="View releases on GitHub"
-          className="ml-2 text-xs text-muted-foreground"
-        >
-          {VERSION}
-        </a>
-      )}
+      {version && <VersionLabel className="ml-2" />}
     </div>
   </div>
 );
