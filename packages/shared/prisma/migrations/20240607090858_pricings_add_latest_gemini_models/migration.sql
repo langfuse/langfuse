46cd01c06866f12281f3f504b79c4263a7c0adb7
Marc Klingen <git@marcklingen.com>
2024-06-07 17:39:05 +0200

    add more comments

--- packages/shared/prisma/migrations/20240607090858_pricings_add_latest_gemini_models/migration.sql
+++ packages/shared/prisma/migrations/20240607090858_pricings_add_latest_gemini_models/migration.sql
@@ -1,5 +1,6 @@
 -- does not include pricing yet, will be added as soon as it is calculated at ingestion time
 
+-- remove model if added by faulty migration, context: https://github.com/langfuse/langfuse/issues/2266
 DELETE FROM models WHERE id in ('clx30djsn0000w9mzebiv41we', 'clx30hkrx0000w9mz7lqi0ial');
 
 INSERT INTO "models" ("id", "model_name", "match_pattern", "unit") VALUES ('clx30djsn0000w9mzebiv41we', 'gemini-1.5-flash', '(?i)^(gemini-1.5-flash)(@[a-zA-Z0-9]+)?$', 'CHARACTERS');
--- packages/shared/src/scripts/manipulate-migrations.ts
+++ packages/shared/src/scripts/manipulate-migrations.ts
@@ -1,12 +1,13 @@
 // Description: Deletes a migration which added a model to the models table. Accidentially, we specified the schema in this file.
 // Self-hosters running on different schemas than public ran into errors with this migration.
 // As this migration does not introduce any model changes, we can just remove it.
+// Issue: https://github.com/langfuse/langfuse/issues/2266
 
 // test cases to consider:
-// - user applied faulty migration on public, everything worked -> removal of migrtion file and row, will be added back.
+// - user applied faulty migration on public, everything worked -> removal of migration file and row, will be added back.
 // - user did not apply faulty migration on public -> no impact, row not applied yet in db.
-// - user applied faulty migration on non public, ran into error -> we remove migration file and db entry before migration. He will get the updated migration.
-// - user did not apply faulty migration on non public, about to run into error -> we removed faulty migration. He will get the updated migration.
+// - user applied faulty migration on non public, ran into error -> we remove migration file and db entry before migration, will get the updated migration which patches the error in models no matter how the migration was manually fixed.
+// - user did not apply faulty migration on non public, about to run into error -> we removed faulty migration, will only get updated migration.
 
 import "dotenv/config";
 
@@ -22,14 +23,14 @@ main().catch((err) => {
 });
 
 export async function manipulateMigrations() {
-  const tabelExists: { table_schema: string; table_name: string }[] =
+  const tableExists: { table_schema: string; table_name: string }[] =
     await prisma.$queryRaw<{ table_schema: string; table_name: string }[]>`
       SELECT * FROM information_schema.tables 
       WHERE    table_name   = '_prisma_migrations';`;
 
-  console.log(tabelExists);
+  console.log(tableExists);
 
-  if (tabelExists.length > 0) {
+  if (tableExists.length > 0) {
     // update checksum
     await prisma.$executeRaw`
     DELETE FROM _prisma_migrations WHERE migration_name in ('20240606090858_pricings_add_latest_gemini_models', '20240530212419_model_price_anthropic_via_google_vertex');`;
