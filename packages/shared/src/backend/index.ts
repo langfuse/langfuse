688023c4ec112f214080352897e9800f7cf44948
Max Deichmann <m.deichmann@tum.de>
2024-06-09 14:04:28 +0200

    push

--- packages/shared/src/backend/index.ts
+++ packages/shared/src/backend/index.ts
@@ -2,5 +2,4 @@ export * from "./clickhouse";
 export * from "./definitions";
 export * from "./ingestion/types";
 export * from "./ingestion/model-match";
-export * from "./ingestion/usage";
 export * from "./ingestion/types";
