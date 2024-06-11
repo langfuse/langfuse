a7681970b05d2bc9f2eec1703c6d5f6febaa8221
Max Deichmann <m.deichmann@tum.de>
2024-06-11 20:05:52 +0200

    fix infra

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -18,7 +18,7 @@ if [ -z "$DIRECT_URL" ]; then
     export DIRECT_URL=$DATABASE_URL
 fi
 # Run cleanup script before running migrations
-pnpm --filter=shared db:migrations:fix-history
+pnpm --filter=shared run db:migrations:fix-history
 
 # Apply migrations
 prisma migrate deploy --schema=./packages/shared/prisma/schema.prisma
