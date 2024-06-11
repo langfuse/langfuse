8c0897b2a45d0a46877a4ad0ab26897bfa71f308
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-06-11 19:01:54 +0200

    push

--- packages/shared/scripts/fix-migration-history.sh
+++ packages/shared/scripts/fix-migration-history.sh
@@ -21,34 +21,30 @@ fi
 # - postgresql://postgres:postgres@localhost:5432/postgres
 # - postgresql://postgres:postgres@localhost:5432/postgres?foo=bar
 
-
 # Function to transform DATABASE_URL
 transform_database_url() {
     local conn_str="$1"
     local schema
     local transformed_url
 
-    echo $conn_str
     # search conn str for 'schema=' and grab the word after it. Pattern: sed 's/pattern/replacement/flags'
-    schema=$(echo "$conn_str" | sed 's/.*?schema=\([^&]*\).*/\1/p')
-
-    echo "schema: $schema"    
+    # -n option in sed suppresses the automatic printing of pattern space
+    schema=$(echo "$conn_str" | sed -n 's/.*schema=\([^&]*\).*/\1/p')
+  
     if [ -n "$schema" ]; then
         # Schema found, replace schema with search_path
-        transformed_url=$(echo "$conn_str" | sed 's/?schema=/?search_path=/')
+        transformed_url=$(echo "$conn_str" | sed 's/schema=/search_path=/')
     else
         # Schema not found, keep the original connection string
         transformed_url="$conn_str"
     fi
 
-    # Print the transformed URL
+    # Return transformed url
     echo "$transformed_url"
 }
 
 # Call the function with the DATABASE_URL
 transformed_url=$(transform_database_url "$DATABASE_URL")
-
-echo "Cleaning up migration history for database: $transformed_url"
 
 # Connect to the database and execute the query
 psql "${transformed_url}" -f "./scripts/cleanup.sql"
