2a5db3c89d2e041b9d3e14f4b3fda156f45aa287
Max Deichmann <m.deichmann@tum.de>
2024-06-11 16:08:21 +0200

    push

--- packages/shared/scripts/fix-migration-history.sh
+++ packages/shared/scripts/fix-migration-history.sh
@@ -11,8 +11,50 @@ then
     sudo apt-get install postgresql-client -y
 fi
 
+# echo "$DATABASE_URL"
+
+# potential inputs in DATABASE_URL
+# - postgresql://postgres:postgres@localhost:5432/postgres?schema=something
+# - postgresql://postgres:postgres@localhost:5432/postgres
+# - postgresql://postgres:postgres@localhost:5432/postgres?foo=bar
+
+# expected outputs
+# - postgresql://postgres:postgres@localhost:5432/postgres?search_path=something
+# - postgresql://postgres:postgres@localhost:5432/postgres
+# - postgresql://postgres:postgres@localhost:5432/postgres?foo=bar
+
+
+# Function to transform DATABASE_URL
+transform_database_url() {
+    local conn_str="$1"
+    local schema
+    local transformed_url
+
+    echo $conn_str
+    # Extract schema using regex
+    schema=$(echo "$conn_str" | grep -oP "(?<=schema=)[^&]+")
+
+    
+    if [ -n "$schema" ]; then
+        # Schema found, replace schema with search_path
+        transformed_url=$(echo "$conn_str" | sed "s/schema=$schema/search_path=$schema/")
+    else
+        # Schema not found, keep the original connection string
+        transformed_url="$conn_str"
+    fi
+
+    # Print the transformed URL
+    echo "$transformed_url"
+}
+
+# Call the function with the DATABASE_URL
+transformed_url=$(transform_database_url "$DATABASE_URL")
+
+echo "Cleaning up migration history for database: $transformed_url"
 
 # Connect to the database and execute the query
-psql "${DATABASE_URL}" -f "./scripts/cleanup.sql"
+psql "${transformed_url}" -f "./scripts/cleanup.sql"
+
+
 
 echo "Migration history cleaned up successfully"
