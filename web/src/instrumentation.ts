49a8522826d2b8c0392884346a38d5ab657e6466
Max Deichmann <m.deichmann@tum.de>
2024-06-17 14:46:08 +0200

    push

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,75 +1,9 @@
-import { jsonSchema } from "@langfuse/shared";
-import * as Sentry from "@sentry/nextjs";
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { nodeProfilingIntegration } = await import("@sentry/profiling-node");
-
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-      integrations: [nodeProfilingIntegration(), Sentry.prismaIntegration()],
-
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
-      tracesSampler: (samplingContext) => {
-        if (
-          samplingContext.request &&
-          samplingContext.request.url &&
-          samplingContext.request.url.includes("api/trpc")
-        ) {
-          return 0.1;
-        }
-        if (
-          samplingContext.request &&
-          samplingContext.request.url &&
-          samplingContext.request.url.includes("api/auth")
-        ) {
-          return 0.1;
-        }
-        return 0.01;
-      },
-
-      profilesSampleRate: 0.1,
-
-      // filter out passwords from the signup request body
-      // transaction events are sentry transactions which include logs and spans.
-      beforeSendTransaction(transaction) {
-        if (
-          transaction.request &&
-          typeof transaction.request.data === "string" &&
-          transaction.request.url &&
-          transaction.request.url.includes("api/auth/signup")
-        ) {
-          const jsonBody = jsonSchema.safeParse(transaction.request.data);
-
-          if (
-            jsonBody.success &&
-            typeof jsonBody.data === "object" &&
-            "data" in jsonBody.data
-          ) {
-            delete jsonBody.data.password;
-            transaction.request.data = JSON.stringify(jsonBody.data);
-            transaction.request.data = JSON.stringify(jsonBody);
-          } else {
-            console.log("Signup: Non Json Request body");
-          }
-
-          return transaction;
-        }
-
-        return transaction;
-      },
-    });
+    await import("./sentry.server.config");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    const { nodeProfilingIntegration } = await import("@sentry/profiling-node");
-
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-      integrations: [nodeProfilingIntegration()],
-      tracesSampleRate: 0.1,
-    });
+    await import("./sentry.edge.config");
   }
 }
