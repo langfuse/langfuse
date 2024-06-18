90ad7a68c91fef16a675fda3df797ad3fbb89819
Max Deichmann <m.deichmann@tum.de>
2024-06-18 16:32:35 +0200

    feat: configure default lookback for dashboards and tables (#2364)

--- web/src/pages/project/[projectId]/users.tsx
+++ web/src/pages/project/[projectId]/users.tsx
@@ -17,9 +17,9 @@ import { compactNumberFormatter, usdFormatter } from "@/src/utils/numbers";
 import { type RouterInput, type RouterOutput } from "@/src/utils/types";
 import { type Score } from "@langfuse/shared";
 import { utcDateOffsetByDays } from "@/src/utils/dates";
-import { useSession } from "next-auth/react";
 import { usersTableCols } from "@/src/server/api/definitions/usersTable";
 import { joinTableCoreAndMetrics } from "@/src/components/table/utils/joinTableCoreAndMetrics";
+import { useLookBackDays } from "@/src/hooks/useLookBackDays";
 
 export type ScoreFilterInput = Omit<RouterInput["users"]["all"], "projectId">;
 
@@ -32,7 +32,6 @@ type RowData = {
 
 export default function UsersPage() {
   const router = useRouter();
-  const session = useSession();
   const projectId = router.query.projectId as string;
 
   const [userFilterState, setUserFilterState] = useQueryFilterState(
@@ -41,9 +40,7 @@ export default function UsersPage() {
         column: "timestamp",
         type: "datetime",
         operator: ">",
-        value: utcDateOffsetByDays(
-          session.data?.environment.defaultTableDateTimeOffset ?? -7,
-        ),
+        value: utcDateOffsetByDays(-useLookBackDays(projectId)),
       },
     ],
     "users",
