4e27b6f0f1d1ac192f2c698f3d6090ef1f6ff88d
marliessophie <74332854+marliessophie@users.noreply.github.com>
2024-06-24 10:13:20 +0200

    feat(api): add GET /score-configs/[configId] and GET /score-configs (#2361)
    * feat(api): add GET /score-configs/[configId]
    
    * feat(api): add GET /score-configs
    
    * push
    
    * test: servertest
    
    * fern
    
    * push
    
    * push
    
    * push
    
    * push
    
    * push
    
    * docs: address feedback
    
    * tests: address feedback to test for full objects
    
    * fix: address feedback and properly verify config is of correct type
    
    * push
    
    * push
    
    * push
    
    * push

--- web/src/features/manual-scoring/lib/helpers.ts
+++ web/src/features/manual-scoring/lib/helpers.ts
@@ -1,4 +1,8 @@
-import { ScoreDataType } from "@langfuse/shared";
+import {
+  type ScoreConfig,
+  ScoreDataType,
+  type CastedConfig,
+} from "@langfuse/shared";
 
 export const isNumericDataType = (dataType: ScoreDataType) =>
   dataType === ScoreDataType.NUMERIC;
@@ -13,3 +17,17 @@ export const isPresent = <T>(value: T): value is NonNullable<T> =>
   value !== null && value !== undefined && value !== "";
 
 export const isScoreUnsaved = (scoreId?: string): boolean => !scoreId;
+
+export const isCastedConfig = (config: ScoreConfig): config is CastedConfig => {
+  return (
+    config.categories === null ||
+    (Array.isArray(config.categories) &&
+      config.categories.every(
+        (category) =>
+          category !== null &&
+          typeof category === "object" &&
+          "label" in category &&
+          "value" in category,
+      ))
+  );
+};
