1245aab4a02c4ec09bfa3b303c97b9a6fa4478ff
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-06-27 16:31:57 +0200

    add max row size and verification step

--- scripts/backfill_observations_calculated_cost.sql
+++ scripts/backfill_observations_calculated_cost.sql
@@ -11,10 +11,12 @@ BEGIN
 END $$;
 
 -- Step 2: Create the batch update function
-CREATE OR REPLACE FUNCTION update_calculated_costs(batch_size INT) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION update_calculated_costs(batch_size INT, max_rows_to_process INT DEFAULT NULL, sleep_between FLOAT DEFAULT 0.1) RETURNS VOID AS $$
 DECLARE
     last_id TEXT := '';
-    rows_processed INT := 0;
+    has_processed_rows INT := 0;
+    total_rows_processed INT := 0;
+
 BEGIN
     LOOP
         WITH batch AS (
@@ -70,16 +72,90 @@ BEGIN
         ORDER BY id DESC
         LIMIT 1;
 
-        -- Get the number of rows processed in this batch
-        GET DIAGNOSTICS rows_processed = ROW_COUNT;
+        -- Get whether any rows have been processed in this batch. (previous row count is 1 if yes, 0 if no)
+        GET DIAGNOSTICS has_processed_rows = ROW_COUNT;
 
-        -- Exit the loop if no more rows were updated
-        EXIT WHEN rows_processed = 0;
+        -- Increment the total number of rows processed
+        total_rows_processed := total_rows_processed + batch_size;
+        RAISE NOTICE 'Total rows processed after increment: % rows', total_rows_processed;
+
+        -- Exit the loop if the maximum number of rows to process has been reached or no more rows were updated
+        EXIT WHEN max_rows_to_process IS NOT NULL AND total_rows_processed >= max_rows_to_process;
+        EXIT WHEN has_processed_rows = 0;
 
         -- Small sleep to reduce lock contention (optional)
-        PERFORM pg_sleep(0.1);
+        PERFORM pg_sleep(sleep_between);
+
     END LOOP;
 END $$ LANGUAGE plpgsql;
 
 -- Step 3: Execute the function with the desired batch size
-SELECT update_calculated_costs(10000);
+SELECT update_calculated_costs(10000, 1000000, 0.1);
+
+-- Step 4: Drop the temporary column and function
+ALTER TABLE observations DROP COLUMN tmp_has_calculated_cost;
+
+DROP FUNCTION update_calculated_costs;
+
+-- Step 5: Verify the results. No rows should be returned by the following query
+WITH cost_diff AS (
+	SELECT
+		o.id,
+		o.project_id,
+		o.trace_id,
+		o.start_time,
+		o.model AS "model",
+		ov.internal_model AS "ov_internal_model",
+		o.internal_model AS "o_internal_model",
+		o.input,
+		o.output,
+		o.unit,
+		ov.prompt_tokens,
+		ov.completion_tokens,
+		ov.total_tokens,
+		o.input_cost,
+		o.output_cost,
+		o.total_cost,
+		ov.model_id AS "ov_model_id",
+		o.internal_model_id AS "o_model_id",
+		ov.calculated_input_cost AS "ov_input",
+		o.calculated_input_cost AS "o_input",
+		ov.calculated_input_cost - o.calculated_input_cost AS input_diff,
+		ov.calculated_output_cost AS "ov_output",
+		o.calculated_output_cost AS "o_output",
+		ov.calculated_output_cost - o.calculated_output_cost AS output_diff,
+		ov.calculated_total_cost AS "ov_total",
+		o.calculated_total_cost AS "o_total",
+		CASE WHEN ABS(ov.calculated_total_cost - o.calculated_total_cost) < 0.00000001
+			OR((ov.calculated_total_cost IS NULL
+				OR ov.calculated_total_cost = 0)
+			AND o.calculated_total_cost IS NULL) THEN
+			'OK'
+		ELSE
+			'NOK'
+		END AS total_diff,
+		CASE WHEN ov.model_id = o.internal_model_id
+			OR(ov.model_id IS NULL
+				AND o.internal_model_id IS NULL) THEN
+			'OK'
+		ELSE
+			'NOK'
+		END AS model_match
+	FROM
+		observations_view ov
+	LEFT JOIN observations o ON ov.id = o.id
+	WHERE
+		AND o.created_at > '2024-06-25T16:50:00'::TIMESTAMP WITH time zone at time zone 'UTC'
+ORDER BY
+	total_diff ASC
+)
+SELECT
+	*
+FROM
+	cost_diff c
+WHERE
+	total_diff = 'NOK'
+	OR model_match = 'NOK'
+ORDER BY
+	total_diff ASC,
+	c.start_time DESC;
