16ca484694deb69e58e42bd0f884daf05ba83081
Marc Klingen <git@marcklingen.com>
2024-06-30 19:26:45 +0200

    fix totalPages type

--- packages/shared/src/utils/zod.ts
+++ packages/shared/src/utils/zod.ts
@@ -66,7 +66,7 @@ export const paginationMetaResponseZod = z.object({
   page: z.number().int().positive(),
   limit: z.number().int().positive(),
   totalItems: z.number().int().nonnegative(),
-  totalPages: z.number().int().positive(),
+  totalPages: z.number().int().nonnegative(),
 });
 
 export const noHtmlRegex = /<[^>]*>/;
