fb6bbdc67ab5f49ad13a82c13c02850d08fe51d3
Max Deichmann <m.deichmann@tum.de>
2024-07-02 16:17:48 +0200

    push

--- web/src/nr-instrumentation.ts
+++ web/src/nr-instrumentation.ts
@@ -1,9 +1,20 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import {
+  Resource,
+  envDetector,
+  hostDetector,
+  osDetector,
+  processDetector,
+} from "@opentelemetry/resources";
+import {
+  awsEc2Detector,
+  awsEksDetector,
+} from "@opentelemetry/resource-detector-aws";
+import { containerDetector } from "@opentelemetry/resource-detector-container";
 
 const sdk = new NodeSDK({
   resource: new Resource({
@@ -11,12 +22,22 @@ const sdk = new NodeSDK({
   }),
   spanProcessor: new SimpleSpanProcessor(
     new OTLPTraceExporter({
-      url: "https://otlp.nr-data.net",
+      url: process.env.OTLP_ENDPOINT || "https://otlp.nr-data.net",
       headers: {
         "api-key": process.env.NEW_RELIC_API_KEY,
       },
     }),
   ),
+  resourceDetectors: [
+    containerDetector,
+    envDetector,
+    hostDetector,
+    osDetector,
+    processDetector,
+    awsEksDetector,
+    awsEc2Detector,
+  ],
   instrumentations: [getNodeAutoInstrumentations()],
 });
+
 sdk.start();
