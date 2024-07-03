adb41116c56a88cc1d51d7336eed5f4a70c906f0
Max Deichmann <m.deichmann@tum.de>
2024-07-03 23:01:43 +0200

    push

--- web/src/nr-instrumentation.ts
+++ web/src/nr-instrumentation.ts
@@ -1,44 +1,35 @@
-import { registerOTel } from "@vercel/otel";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import { PrismaInstrumentation } from "@prisma/instrumentation";
 
-const { TracerProvider } = (await import("dd-trace")).default.init({
-  logInjection: true,
-  startupLogs: true,
-  runtimeMetrics: true,
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "web",
+  }),
+  spanProcessors: [
+    new SimpleSpanProcessor(
+      new OTLPTraceExporter({
+        url: process.env.OTLP_ENDPOINT || "https://otlp.nr-data.net",
+      }),
+    ),
+  ],
+  // resourceDetectors: [
+  //   containerDetector,
+  //   envDetector,
+  //   hostDetector,
+  //   osDetector,
+  //   processDetector,
+  //   awsEksDetector,
+  //   awsEc2Detector,
+  // ],
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new PrismaInstrumentation(),
+  ],
 });
 
-const provider = new TracerProvider();
-
-registerOTel({
-  instrumentations: [new PrismaInstrumentation()],
-});
-
-provider.register();
-
-// const sdk = new NodeSDK({
-//   resource: new Resource({
-//     [SemanticResourceAttributes.SERVICE_NAME]: "web",
-//   }),
-//   spanProcessors: [
-//     new SimpleSpanProcessor(
-//       new OTLPTraceExporter({
-//         url: process.env.OTLP_ENDPOINT || "https://otlp.nr-data.net",
-//         // headers: {
-//         //   "api-key": process.env.NEW_RELIC_API_KEY,
-//         // },
-//       }),
-//     ),
-//   ],
-//   resourceDetectors: [
-//     containerDetector,
-//     envDetector,
-//     hostDetector,
-//     osDetector,
-//     processDetector,
-//     awsEksDetector,
-//     awsEc2Detector,
-//   ],
-//   instrumentations: [getNodeAutoInstrumentations()],
-// });
-
-// sdk.start();
+sdk.start();
