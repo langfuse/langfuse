c075555c75b03f35f6a6a646961ba8c7db82c63b
Max Deichmann <m.deichmann@tum.de>
2024-07-04 15:39:46 +0200

    push

--- worker/src/instrumentation.ts
+++ worker/src/instrumentation.ts
@@ -66,16 +66,17 @@ export function initializeOtel(serviceName: string, version?: string) {
   }
 }
 
-type CallbackAsyncFn<T> = (span?: Sentry.Span) => Promise<T>;
+type CallbackAsyncFn<T> = () => Promise<T>; //span?: Sentry.Span
 
 export async function instrumentAsync<T>(
   ctx: { name: string },
   callback: CallbackAsyncFn<T>
 ): Promise<T> {
   if (env.SENTRY_DSN) {
-    return Sentry.startSpan(ctx, async (span) => {
-      return callback(span);
-    });
+    // return Sentry.startSpan(ctx, async (span) => {
+    //   return callback(span);
+    // });
+    return callback();
   } else {
     return callback();
   }
