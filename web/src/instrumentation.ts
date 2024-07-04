3f7012b9075532491d5b3026b802081ab8904ba4
Max Deichmann <m.deichmann@tum.de>
2024-07-04 15:53:30 +0200

    push

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -3,7 +3,7 @@ import { registerOTel } from "@vercel/otel";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./nr-instrumentation");
+    // await import("./nr-instrumentation");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
