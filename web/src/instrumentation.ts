e3367c200bf3afe9adaecf4d94742d80570bcd63
Max Deichmann <m.deichmann@tum.de>
2024-07-05 15:55:28 +0200

    feat: add newrelic instrumentation (#2545)

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,6 +1,7 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
+    await import("./new-relic-instrumentation");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
