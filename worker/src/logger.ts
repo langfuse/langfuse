d2cee0a5119f246e37dedd7cfba304ace72c337e
Max Deichmann <m.deichmann@tum.de>
2024-07-05 11:54:59 +0200

    push

--- worker/src/logger.ts
+++ worker/src/logger.ts
@@ -1,51 +1,7 @@
 import pino from "pino";
 import { env } from "./env";
 
-// Map Pino levels to Google Cloud Logging severity levels
-// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
-const levelToSeverity = {
-  trace: "DEBUG",
-  debug: "DEBUG",
-  info: "INFO",
-  warn: "WARNING",
-  error: "ERROR",
-  fatal: "CRITICAL",
-};
-
-export const getLogger = (env: "development" | "production" | "test") => {
-  if (env === "production") {
-    return pino({
-      base: { serviceContext: { service: "langfuse-worker" } },
-      formatters: {
-        level(label) {
-          const pinoLevel = label as pino.Level;
-          // `@type` property tells Error Reporting to track even if there is no `stack_trace`
-          const typeProp =
-            label === "error" || label === "fatal"
-              ? {
-                  "@type":
-                    "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent",
-                }
-              : {};
-          return {
-            level: pinoLevel,
-            severity: levelToSeverity[pinoLevel],
-            ...typeProp,
-          };
-        },
-        log(object: object & { err?: Error }) {
-          const stackTrace = object.err?.stack;
-          const stackProp = stackTrace ? { stack_trace: stackTrace } : {};
-          return {
-            ...object,
-            ...stackProp,
-          };
-        },
-      },
-      messageKey: "message",
-      timestamp: () => `,"eventTime":${Date.now() / 1000.0}`,
-    });
-  }
+export const getLogger = () => {
   return pino({
     transport: {
       target: "pino-pretty",
@@ -57,6 +13,6 @@ export const getLogger = (env: "development" | "production" | "test") => {
   });
 };
 
-const logger = getLogger(env.NODE_ENV);
+const logger = getLogger();
 
 export default logger;
