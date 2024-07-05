bb48689c48b5589107e7fb191bd404b96c53cca3
Max Deichmann <m.deichmann@tum.de>
2024-07-05 19:02:29 +0200

    push

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,6 +1,5 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./sentry.server.config");
     await import("./new-relic-instrumentation");
   }
 
