e4a82c96517d59f1629f420dc5ac63138e737ee2
Max Deichmann <m.deichmann@tum.de>
2024-07-07 18:10:58 +0200

    fix: remove newrelic again (#2559)

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,10 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
-    // do not run new relic instrumentation in vercel production environments
-    if (process.env.NEW_RELIC_API_KEY && process.env.OTLP_ENDPOINT) {
-      await import("./new-relic-instrumentation");
-    }
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
