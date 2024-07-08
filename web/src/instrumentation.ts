6bbdb4325e07da9faf94b78bc7d49ee144f1e6b7
Max Deichmann <m.deichmann@tum.de>
2024-07-08 21:45:19 +0200

    push

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -4,8 +4,7 @@ import { registerOTel } from "@vercel/otel";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { TracerProvider } = (await import("dd-trace")).default.init({
-      logInjection: true,
-      startupLogs: true,
+      runtimeMetrics: true,
       profiling: true,
     });
 
