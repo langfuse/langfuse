095cd1ddf1525e3da04e0122c75c32f5dd23bc2a
Max Deichmann <m.deichmann@tum.de>
2024-07-08 21:44:03 +0200

    something

--- worker/src/instrumentation.ts
+++ worker/src/instrumentation.ts
@@ -1,48 +1,7 @@
 require("dd-trace").init({
   profiling: true,
+  runtimeMetrics: true,
 });
-
-import logger from "./logger";
-
-export function initializeOtel(serviceName: string, version?: string) {
-  try {
-    // const tracer = require("dd-trace").init();
-    // const { TracerProvider } = tracer;
-
-    // const provider = new TracerProvider();
-    // provider.register();
-
-    // opentelemetry.api.trace.setGlobalTracerProvider(provider);
-
-    // const sdk = new opentelemetry.NodeSDK({
-    //   // Optional - if omitted, the tracing SDK will be initialized from environment variables
-
-    //   // Optional - you can use the metapackage or load each instrumentation individually
-    //   instrumentations: [
-    //     new PrismaInstrumentation(),
-    //     getNodeAutoInstrumentations(),
-    //     new IORedisInstrumentation(),
-    //     new ExpressInstrumentation(),
-    //   ],
-
-    //   resourceDetectors: [
-    //     containerDetector,
-    //     envDetector,
-    //     hostDetector,
-    //     osDetector,
-    //     processDetector,
-    //     awsEksDetector,
-    //     awsEc2Detector,
-    //   ],
-    // });
-
-    logger.info("OpenTelemetry setup complete");
-    // return sdk;
-  } catch (e) {
-    logger.error("Error setting up OpenTelemetry", e);
-    throw e;
-  }
-}
 
 type CallbackAsyncFn<T> = () => Promise<T>; //span?: Sentry.Span
 
