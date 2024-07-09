62289563849c620e06b29ccd1a0e1828803fb046
Max Deichmann <m.deichmann@tum.de>
2024-07-09 23:11:57 +0200

    fix: add sentry back to server (#2593)
    something

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    // await import("./sentry.server.config");
+    await import("./sentry.server.config");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
