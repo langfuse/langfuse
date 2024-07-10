4cfad4de5c9568878f69143144609dcedfd3fa98
Max Deichmann <m.deichmann@tum.de>
2024-07-10 18:33:20 +0200

    perf: add dd tracing (#2609)

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,6 +2,7 @@ import "./instrumentation";
 import app from "./app";
 import { env } from "./env";
 import logger from "./logger";
+
 import { evalJobCreator, evalJobExecutor } from "./queues/evalQueue";
 import { batchExportJobExecutor } from "./queues/batchExportQueue";
 
