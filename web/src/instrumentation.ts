8cceaf4448041c4284dd9e696675f892c67bfcc4
Max Deichmann <m.deichmann@tum.de>
2024-07-10 17:19:22 +0200

    push

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,21 +1,13 @@
-import { PrismaInstrumentation } from "@prisma/instrumentation";
-import { registerOTel } from "@vercel/otel";
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { TracerProvider } = (await import("dd-trace")).default.init({
-      runtimeMetrics: true,
-      profiling: true,
-    });
-
-    const provider = new TracerProvider();
-
-    registerOTel({
-      // instrumentations: [new PrismaInstrumentation()],
-    });
-    provider.register();
+    if (!process.env.VERCEL) {
+      await import("./dd-instrumentation");
+    } else {
+      await import("./sentry.server.config");
+    }
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
   }
 }
