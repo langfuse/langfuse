e19bb7281b1a1bcf9c0668c9f826469566a879be
Max Deichmann <m.deichmann@tum.de>
2024-07-10 14:26:02 +0200

    push

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -20,7 +20,6 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
 app.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
     message: "Langfuse Worker API 🚀",
