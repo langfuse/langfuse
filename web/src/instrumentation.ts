c99b12ea9cc2b87bd47e2610031c83da88becb3d
Max Deichmann <m.deichmann@tum.de>
2024-07-11 09:40:29 +0200

    something

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -2,10 +2,9 @@
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    // if (!process.env.VERCEL) {
-    //   await import("./dd-instrumentation");
-    // }
-    await import("./sentry.server.config");
+    if (!process.env.VERCEL) {
+      await import("./dd-instrumentation");
+    }
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
