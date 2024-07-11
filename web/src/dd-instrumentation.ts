04ecf2576a6e95f6a88fe14d7fdc4d37cef161a7
Max Deichmann <m.deichmann@tum.de>
2024-07-11 10:50:38 +0200

    something

--- web/src/dd-instrumentation.ts
+++ web/src/dd-instrumentation.ts
@@ -3,6 +3,8 @@ import { registerOTel } from "@vercel/otel";
 const { TracerProvider } = (await import("dd-trace")).default.init({
   runtimeMetrics: true,
   profiling: true,
+  startupLogs: true,
+  logInjection: true,
 });
 
 const provider = new TracerProvider();
