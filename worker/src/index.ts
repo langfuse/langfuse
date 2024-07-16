01c5f1198bb15fa57810fb96a2aca18cc3f7ea98
Max Deichmann <m.deichmann@tum.de>
2024-07-16 23:12:04 +0200

    something

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -11,7 +11,7 @@ const server = app.listen(env.PORT, () => {
 });
 
 // Function to handle shutdown logic
-function onShutdown() {
+async function onShutdown() {
   logger.info("Shutting down application...");
 
   // Stop accepting new connections
@@ -19,15 +19,12 @@ function onShutdown() {
 
   // Perform necessary cleanup tasks here
   // For example, close database connections, stop job executors, etc.
-  evalJobCreator
-    ?.close()
-    .then(() => logger.info("Eval Job Creator has been closed."));
-  evalJobExecutor
-    ?.close()
-    .then(() => logger.info("Eval Job Executor has been closed."));
-  batchExportJobExecutor
-    ?.close()
-    .then(() => logger.info("Batch Export Executor has been closed."));
+  await evalJobCreator?.close();
+  logger.info("Eval Job Creator has been closed.");
+  await evalJobExecutor?.close();
+  logger.info("Eval Job Executor has been closed.");
+  await batchExportJobExecutor?.close();
+  logger.info("Batch Export Executor has been closed.");
 }
 
 // Capture shutdown signals
