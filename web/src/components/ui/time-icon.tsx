0ae0060424acc92788d2a7379ef659620723deff
RichardKruemmel <richard.kruemmel@code.berlin>
2024-07-18 20:07:49 +0200

    refactor(ui): Update TimeIcon component to handle both Date and Period types

--- web/src/components/ui/time-icon.tsx
+++ web/src/components/ui/time-icon.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { Clock, Moon, Sun } from "lucide-react";
+import { type Period } from "@/src/components/ui/time-picker-utils";
 
-export const TimeIcon: React.FC<{ date: Date | undefined }> = ({ date }) => {
-  if (date) {
+export const TimeIcon: React.FC<{ date: Date | Period }> = ({ date }) => {
+  if (typeof date === "string") {
+    if (date === "AM") {
+      return <Sun className="size-5" />;
+    }
+    if (date === "PM") {
+      return <Moon className="size-5" />;
+    }
+  } else {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
