31e5892e747fa127755aea5f673c9e02e4514e66
RichardKruemmel <richard.kruemmel@code.berlin>
2024-07-19 18:42:54 +0200

    chore: Update LatencyChart component to use new date range utils

--- web/src/features/dashboard/components/LatencyChart.tsx
+++ web/src/features/dashboard/components/LatencyChart.tsx
@@ -1,8 +1,4 @@
 import { api } from "@/src/utils/api";
-import {
-  dateTimeAggregationSettings,
-  type DateTimeAggregationOption,
-} from "@/src/features/dashboard/lib/timeseries-aggregation";
 import { type FilterState } from "@langfuse/shared";
 import {
   getAllModels,
@@ -15,6 +11,8 @@ import { BaseTimeSeriesChart } from "@/src/features/dashboard/components/BaseTim
 import { TabComponent } from "@/src/features/dashboard/components/TabsComponent";
 import { latencyFormatter } from "@/src/utils/numbers";
 import { NoData } from "@/src/features/dashboard/components/NoData";
+import { type DashboardDateRangeAggregationOption } from "@/src/utils/date-range-utils";
+import { dashboardDateRangeAggregationSettings } from "@/src/features/dashboard/lib/timeseries-aggregation";
 
 export const GenerationLatencyChart = ({
   className,
@@ -25,7 +23,7 @@ export const GenerationLatencyChart = ({
   className?: string;
   projectId: string;
   globalFilterState: FilterState;
-  agg: DateTimeAggregationOption;
+  agg: DashboardDateRangeAggregationOption;
 }) => {
   const latencies = api.dashboard.chart.useQuery(
     {
@@ -52,7 +50,7 @@ export const GenerationLatencyChart = ({
         {
           type: "datetime",
           column: "startTime",
-          temporalUnit: dateTimeAggregationSettings[agg].date_trunc,
+          temporalUnit: dashboardDateRangeAggregationSettings[agg].date_trunc,
         },
         { type: "string", column: "model" },
       ],
