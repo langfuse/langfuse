a3b203e32cd40898fc9a2cc08d7be0d950359d5d
RichardKruemmel <richard.kruemmel@code.berlin>
2024-07-19 18:42:36 +0200

    chore: Update DateRangeDropdown component to use new date range utils

--- web/src/components/DateRangeDropdown.tsx
+++ web/src/components/DateRangeDropdown.tsx
@@ -7,37 +7,55 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/src/components/ui/select";
-import {
-  dateTimeAggregationOptions,
-  dateTimeAggregationSettings,
-} from "@/src/features/dashboard/lib/timeseries-aggregation";
-import { isValidOption } from "@/src/utils/types";
-import {
-  DEFAULT_DATE_RANGE_SELECTION,
-  type AvailableDateRangeSelections,
-} from "@/src/components/date-picker";
 import { type DashboardDateRange } from "@/src/pages/project/[projectId]";
-import { type AvailableTableDateRangeSelections } from "@/src/components/useDateRange";
+import { tableDateRangeAggregationSettings } from "@/src/components/useDateRange";
+import { dashboardDateRangeAggregationSettings } from "@/src/features/dashboard/lib/timeseries-aggregation";
+import {
+  type AllDateRangeAggregationOption,
+  DEFAULT_DATE_RANGE_SELECTION,
+  isValidOption,
+  type DashboardDateRangeAggregationOption,
+  type TableDateRangeAggregationOption,
+} from "@/src/utils/date-range-utils";
 
 type DateRangeDropdownProps = {
-  selectedOption:
-    | AvailableDateRangeSelections
-    | AvailableTableDateRangeSelections;
+  type: "dashboard" | "table";
+  selectedOption: AllDateRangeAggregationOption;
   setDateRangeAndOption: (
-    option: AvailableDateRangeSelections | AvailableTableDateRangeSelections,
+    option: AllDateRangeAggregationOption,
     date?: DashboardDateRange,
   ) => void;
 };
 
 const DateRangeDropdown: React.FC<DateRangeDropdownProps> = ({
+  type,
   selectedOption,
   setDateRangeAndOption,
 }) => {
-  const onDropDownSelection = (value: string) => {
+  const onDropDownSelection = (
+    value:
+      | DashboardDateRangeAggregationOption
+      | TableDateRangeAggregationOption
+      | typeof DEFAULT_DATE_RANGE_SELECTION,
+  ) => {
     if (isValidOption(value)) {
-      const setting = dateTimeAggregationSettings[value];
-      const fromDate = addMinutes(new Date(), -1 * setting.minutes);
-
+      let fromDate: Date;
+      if (value in dashboardDateRangeAggregationSettings) {
+        const setting =
+          dashboardDateRangeAggregationSettings[
+            value as DashboardDateRangeAggregationOption
+          ];
+        fromDate = addMinutes(new Date(), -setting.minutes);
+      } else if (value in tableDateRangeAggregationSettings) {
+        const setting =
+          tableDateRangeAggregationSettings[
+            value as TableDateRangeAggregationOption
+          ];
+        fromDate = addMinutes(new Date(), -setting.minutes);
+      } else {
+        setDateRangeAndOption(DEFAULT_DATE_RANGE_SELECTION, undefined);
+        return;
+      }
       setDateRangeAndOption(value, {
         from: fromDate,
         to: new Date(),
@@ -47,19 +65,31 @@ const DateRangeDropdown: React.FC<DateRangeDropdownProps> = ({
     }
   };
 
+  const dashboardOptions = [
+    DEFAULT_DATE_RANGE_SELECTION,
+    ...Object.keys(dashboardDateRangeAggregationSettings),
+  ] as (
+    | DashboardDateRangeAggregationOption
+    | typeof DEFAULT_DATE_RANGE_SELECTION
+  )[];
+
+  const tableOptions = [
+    DEFAULT_DATE_RANGE_SELECTION,
+    ...Object.keys(tableDateRangeAggregationSettings),
+  ] as (
+    | TableDateRangeAggregationOption
+    | typeof DEFAULT_DATE_RANGE_SELECTION
+  )[];
+
+  const currentOptions = type === "dashboard" ? dashboardOptions : tableOptions;
+
   return (
-    <Select value={selectedOption} onValueChange={onDropDownSelection}>
+    <Select value={String(selectedOption)} onValueChange={onDropDownSelection}>
       <SelectTrigger className="w-[120px] hover:bg-accent hover:text-accent-foreground focus:ring-0 focus:ring-offset-0">
         <SelectValue placeholder="Select" />
       </SelectTrigger>
       <SelectContent position="popper" defaultValue={60}>
-        <SelectItem
-          key={DEFAULT_DATE_RANGE_SELECTION}
-          value={DEFAULT_DATE_RANGE_SELECTION}
-        >
-          {DEFAULT_DATE_RANGE_SELECTION}
-        </SelectItem>
-        {dateTimeAggregationOptions.toReversed().map((item) => (
+        {currentOptions.reverse().map((item) => (
           <SelectItem key={item} value={item}>
             {item}
           </SelectItem>
