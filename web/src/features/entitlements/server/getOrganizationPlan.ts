7ff645bb4891908da797050e347f2bf0865b1197
Marc Klingen <git@marcklingen.com>
2024-07-22 18:41:15 +0200

    entitlement documentation

--- web/src/features/entitlements/server/getOrganizationPlan.ts
+++ web/src/features/entitlements/server/getOrganizationPlan.ts
@@ -2,6 +2,9 @@ import { env } from "@/src/env.mjs";
 import { type Plan } from "@/src/features/entitlements/constants/plans";
 import { type CloudConfigSchema } from "@/src/features/organizations/utils/cloudConfigSchema";
 
+/**
+ * Get the plan of the organization based on the cloud configuration. Used to add this plan to the organization object in JWT via NextAuth.
+ */
 export function getOrganizationPlan(cloudConfig?: CloudConfigSchema): Plan {
   if (process.env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
     if (cloudConfig) {
