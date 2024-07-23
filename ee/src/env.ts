c27c32f844c9acbbdffe1c5fa100f7d296d96af0
Marc Klingen <git@marcklingen.com>
2024-07-23 19:22:09 +0200

    refactor(ee): move multi-tenant sso to /web/src/ee (#2722)
    * refactor(ee): move multi-tenant sso to /web/src/ee
    
    * fix lint

--- ee/src/env.ts
+++ ee/src/env.ts
@@ -3,7 +3,6 @@ import { env as sharedEnv } from "@langfuse/shared";
 
 const EnvSchema = z.object({
   NEXT_PUBLIC_LANGFUSE_CLOUD_REGION: z.string().optional(),
-  ADMIN_API_KEY: z.string().optional(),
 });
 
 export const env = { ...sharedEnv, ...EnvSchema.parse(process.env) };
