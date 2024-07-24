97af9b2b828fa95e5143a857cf24b7a495653fae
Max Deichmann <m.deichmann@tum.de>
2024-07-24 18:18:19 +0200

    types

--- web/src/utils/shutdown.ts
+++ web/src/utils/shutdown.ts
@@ -31,7 +31,19 @@ export const shutdown = async (signal: PrexitSignal) => {
     setSigtermReceived();
 
     return await new Promise<void>((resolve) => {
-      setTimeout(() => {
+      setTimeout(async () => {
+        if (typeof window === "undefined") {
+          const { redis } = await import("@langfuse/shared/src/server");
+          console.log(`Redis status ${redis?.status}`);
+          if (!redis) {
+            return;
+          }
+          if (redis.status === "end") {
+            console.log("Redis connection already closed");
+            return;
+          }
+          redis?.disconnect();
+        }
         console.log("Shutdown complete");
         resolve();
       }, TIMEOUT);
