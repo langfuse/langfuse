b61093119f8dce53fb3373457ca0ed7151b1e5e6
Max Deichmann <m.deichmann@tum.de>
2024-07-25 18:52:00 +0200

    refactor: revert sending trace events from web via redis (#2744)

--- web/src/pages/api/public/ingestion.ts
+++ web/src/pages/api/public/ingestion.ts
@@ -40,10 +40,6 @@ import {
   ForbiddenError,
   UnauthorizedError,
 } from "@langfuse/shared";
-import {
-  convertTraceUpsertEventsToRedisEvents,
-  traceUpsertQueue,
-} from "@langfuse/shared/src/server";
 
 import { isSigtermReceived } from "@/src/utils/shutdown";
 
@@ -528,20 +524,6 @@ export const sendToWorkerIfEnvironmentConfigured = async (
 
   try {
     if (
-      (env.REDIS_CONNECTION_STRING || env.REDIS_HOST) &&
-      env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION
-    ) {
-      console.log("Sending events to worker via redis", traceEvents);
-      const jobs = convertTraceUpsertEventsToRedisEvents(traceEvents);
-      await traceUpsertQueue?.addBulk(jobs);
-
-      if (traceUpsertQueue) {
-        console.log(
-          `Added ${jobs.length} trace upsert jobs to the queue`,
-          jobs,
-        );
-      }
-    } else if (
       env.LANGFUSE_WORKER_HOST &&
       env.LANGFUSE_WORKER_PASSWORD &&
       env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION
