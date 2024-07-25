aea029e1bcdd96d6737b46f13e004298b1b74d74
Max Deichmann <m.deichmann@tum.de>
2024-07-25 18:41:55 +0200

    test fix

--- web/src/__tests__/teardown.ts
+++ web/src/__tests__/teardown.ts
@@ -1,15 +1,13 @@
 export default async function teardown() {
-  if (window === undefined) {
-    const { redis } = await import("@langfuse/shared/src/server");
-    console.log(`Redis status ${redis?.status}`);
-    if (!redis) {
-      return;
-    }
-    if (redis.status === "end") {
-      console.log("Redis connection already closed");
-      return;
-    }
-    redis?.disconnect();
-    console.log("Teardown complete");
+  const { redis } = await import("@langfuse/shared/src/server");
+  console.log(`Redis status ${redis?.status}`);
+  if (!redis) {
+    return;
   }
+  if (redis.status === "end") {
+    console.log("Redis connection already closed");
+    return;
+  }
+  redis?.disconnect();
+  console.log("Teardown complete");
 }
