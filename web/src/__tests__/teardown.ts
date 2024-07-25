5de1973438d4929224a5ff8606d6cfea9e59fc13
Max Deichmann <m.deichmann@tum.de>
2024-07-25 17:51:03 +0200

    refactor: revert sending trace events from web via redis

--- web/src/__tests__/teardown.ts
+++ web/src/__tests__/teardown.ts
@@ -1,13 +1,15 @@
 export default async function teardown() {
-  const { redis } = await import("@langfuse/shared/src/server");
-  console.log(`Redis status ${redis?.status}`);
-  if (!redis) {
-    return;
+  if (window === undefined) {
+    const { redis } = await import("@langfuse/shared/src/server");
+    console.log(`Redis status ${redis?.status}`);
+    if (!redis) {
+      return;
+    }
+    if (redis.status === "end") {
+      console.log("Redis connection already closed");
+      return;
+    }
+    redis?.disconnect();
+    console.log("Teardown complete");
   }
-  if (redis.status === "end") {
-    console.log("Redis connection already closed");
-    return;
-  }
-  redis?.disconnect();
-  console.log("Teardown complete");
 }
