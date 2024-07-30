e84b83aaf0e1ccf29d4e86e76e8bb1fd452f57e1
RichardKruemmel <richard.kruemmel@code.berlin>
2024-07-30 17:03:36 +0200

    chore: Refactor useDebounce hook to handle dynamic timeout values

--- web/src/hooks/useDebounce.tsx
+++ web/src/hooks/useDebounce.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useMemo, useRef } from "react";
 
-function debounce(func: (...args: any[]) => void, timeout = 250) {
-  let timer: NodeJS.Timeout;
+function debounce(func: (...args: any[]) => void, timeout: number = 250) {
+  let timer: NodeJS.Timeout | null = null;
   return (...args: any[]) => {
-    clearTimeout(timer);
+    if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
       func(...args);
     }, timeout);
@@ -11,12 +11,12 @@ function debounce(func: (...args: any[]) => void, timeout = 250) {
 }
 export function useDebounce(
   callback: (...args: any[]) => void,
-  delay?: number,
+  delay: number = 250,
 ) {
   const callbackRef = useRef(callback);
   useEffect(() => {
     callbackRef.current = callback;
-  });
+  }, [callback]);
   return useMemo(
     () => debounce((...args: any[]) => callbackRef.current(...args), delay),
     [delay],
