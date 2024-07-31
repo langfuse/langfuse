6c00d0136145169f7029eac3425bdb74d89c67fa
Max Deichmann <m.deichmann@tum.de>
2024-08-01 00:50:17 +0200

    inject logs

--- worker/src/instrumentation.ts
+++ worker/src/instrumentation.ts
@@ -1,6 +1,3 @@
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
-import { env } from "./env";
-import * as Sentry from "@sentry/node";
 import tracer from "dd-trace";
 import ot from "@opentelemetry/api";
 
@@ -10,28 +7,15 @@ const { TracerProvider } = tracer.init({
 });
 
 const provider = new TracerProvider();
+
 provider.register();
 
+// registerInstrumentations({
+//   tracerProvider: provider,
+//   instrumentations: [new PrismaInstrumentation()],
+// });
+
 export const otelTracer = ot.trace.getTracer("worker");
-
-Sentry.init({
-  dsn: String(env.SENTRY_DSN),
-  integrations: [
-    Sentry.httpIntegration(),
-    Sentry.expressIntegration(),
-    nodeProfilingIntegration(),
-    Sentry.redisIntegration(),
-    Sentry.prismaIntegration(),
-  ],
-
-  // Add Tracing by setting tracesSampleRate
-  // We recommend adjusting this value in production
-  tracesSampleRate: env.LANGFUSE_TRACING_SAMPLE_RATE,
-
-  // Set sampling rate for profiling
-  // This is relative to tracesSampleRate
-  profilesSampleRate: 0.1,
-});
 
 type CallbackAsyncFn<T> = () => Promise<T>;
 
