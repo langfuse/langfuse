6068319ca006fab1ddda16173353197872a97068
Max Deichmann <m.deichmann@tum.de>
2024-08-01 00:51:06 +0200

    inject logs

--- web/src/datadog.server.config.ts
+++ web/src/datadog.server.config.ts
@@ -1,11 +1,12 @@
 import { env } from "@/src/env.mjs";
 
 if (!process.env.VERCEL && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
-  const tracer = (await import("dd-trace")).default.init({
+  const { TracerProvider } = (await import("dd-trace")).default.init({
     runtimeMetrics: true,
     profiling: false,
-    logInjection: true,
   });
 
-  tracer.use("next");
+  const provider = new TracerProvider();
+
+  provider.register();
 }
