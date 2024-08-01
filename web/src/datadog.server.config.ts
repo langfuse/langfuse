2a5eb067223e94d5af7b9f39056cd79a0d0f49f7
Max Deichmann <m.deichmann@tum.de>
2024-08-02 00:59:06 +0200

    deploy

--- web/src/datadog.server.config.ts
+++ web/src/datadog.server.config.ts
@@ -3,6 +3,7 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { IORedisInstrumentation } from "@opentelemetry/instrumentation-ioredis";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { PrismaInstrumentation } from "@prisma/instrumentation";
+import { registerOTel } from "@vercel/otel";
 
 if (!process.env.VERCEL && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
   const { TracerProvider } = (await import("dd-trace")).default.init({
@@ -13,6 +14,16 @@ if (!process.env.VERCEL && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
   const provider = new TracerProvider();
 
   provider.register();
+
+  // registerOTel({
+  //   serviceName: "next-app",
+  //   instrumentations: [
+  //     new IORedisInstrumentation(),
+  //     new HttpInstrumentation(),
+  //     new PrismaInstrumentation(),
+  //   ],
+  //   pr
+  // });
 
   registerInstrumentations({
     instrumentations: [
