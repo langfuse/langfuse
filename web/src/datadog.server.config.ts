d3f47a30b091aa4a2067084925a7e25e0d1c65e3
Max Deichmann <m.deichmann@tum.de>
2024-08-02 16:39:34 +0200

    something

--- web/src/datadog.server.config.ts
+++ web/src/datadog.server.config.ts
@@ -12,34 +12,29 @@ import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
 import { context } from "@opentelemetry/api";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { registerOTel } from "@vercel/otel";
+import tracer from "dd-trace";
 
 if (!process.env.VERCEL && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
   // const contextManager = new AsyncHooksContextManager().enable();
 
   // context.setGlobalContextManager(contextManager);
 
-  const { TracerProvider } = (await import("dd-trace")).default.init({
-    runtimeMetrics: true,
+  const { TracerProvider } = tracer.init({
     profiling: false,
+    runtimeMetrics: true,
   });
 
   const provider = new TracerProvider();
 
-  // const provider = new NodeTracerProvider({
-  //   forceFlushTimeoutMillis: 15_000,
-  //   resource: new Resource({
-  //     [SEMRESATTRS_SERVICE_NAME.SERVICE_NAME]: "some-service",
-  //   }),
-  // });
-  // provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+  provider.register();
 
-  registerOTel({
+  registerInstrumentations({
     instrumentations: [
-      getNodeAutoInstrumentations(),
-      new HttpInstrumentation(),
-      new PrismaInstrumentation(),
       new IORedisInstrumentation(),
+      new HttpInstrumentation(),
+      // new PrismaInstrumentation(),
     ],
   });
+
   provider.register();
 }
