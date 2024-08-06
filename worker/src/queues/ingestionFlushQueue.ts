dc913f44f64a93c4314e27f1bbcf73a0bfe6a7c9
Max Deichmann <m.deichmann@tum.de>
2024-08-06 16:00:27 +0200

    fixes

--- worker/src/queues/ingestionFlushQueue.ts
+++ worker/src/queues/ingestionFlushQueue.ts
@@ -9,6 +9,7 @@ import { env } from "../env";
 import logger from "../logger";
 import { ClickhouseWriter } from "../services/ClickhouseWriter";
 import { IngestionService } from "../services/IngestionService";
+import { instrumentAsync } from "../instrumentation";
 
 export type IngestionFlushQueue = Queue<null>;
 
@@ -28,62 +29,71 @@ export const flushIngestionQueueExecutor = redis
   ? new Worker(
       QueueName.IngestionFlushQueue,
       async (job) => {
-        if (job.name === QueueJobs.FlushIngestionEntity) {
-          const projectEntityId = job.id;
-          if (!projectEntityId) {
-            throw new Error("ProjectEntity ID not provided");
+        return instrumentAsync(
+          { name: "flush-ingestion-consumer" },
+          async () => {
+            if (job.name === QueueJobs.FlushIngestionEntity) {
+              const projectEntityId = job.id;
+              if (!projectEntityId) {
+                throw new Error("ProjectEntity ID not provided");
+              }
+
+              // Log wait time
+              const waitTime = Date.now() - job.timestamp;
+              logger.debug(
+                `Received flush request after ${waitTime} ms for ${projectEntityId}`
+              );
+              Sentry.metrics.distribution(
+                "ingestion_flush_wait_time",
+                waitTime,
+                {
+                  unit: "milliseconds",
+                }
+              );
+
+              // Check dependencies
+              if (!redis) throw new Error("Redis not available");
+              if (!prisma) throw new Error("Prisma not available");
+              if (!ingestionFlushQueue)
+                throw new Error("Ingestion flush queue not available");
+
+              // Flush ingestion buffer
+              const processingStartTime = Date.now();
+
+              await new IngestionService(
+                redis,
+                prisma,
+                ingestionFlushQueue,
+                ClickhouseWriter.getInstance(),
+                clickhouseClient,
+                env.LANGFUSE_INGESTION_BUFFER_TTL_SECONDS
+              ).flush(projectEntityId);
+
+              // Log processing time
+              const processingTime = Date.now() - processingStartTime;
+              logger.debug(
+                `Prepared and scheduled CH-write in ${processingTime} ms for ${projectEntityId}`
+              );
+              Sentry.metrics.distribution(
+                "ingestion_flush_processing_time",
+                processingTime,
+                { unit: "milliseconds" }
+              );
+
+              // Log queue size
+              await ingestionFlushQueue
+                .count()
+                .then((count) => {
+                  logger.debug(`Ingestion flush queue length: ${count}`);
+                  Sentry.metrics.gauge("ingestion_flush_queue_length", count, {
+                    unit: "records",
+                  });
+                  return count;
+                })
+                .catch();
+            }
           }
-
-          // Log wait time
-          const waitTime = Date.now() - job.timestamp;
-          logger.debug(
-            `Received flush request after ${waitTime} ms for ${projectEntityId}`
-          );
-          Sentry.metrics.distribution("ingestion_flush_wait_time", waitTime, {
-            unit: "milliseconds",
-          });
-
-          // Check dependencies
-          if (!redis) throw new Error("Redis not available");
-          if (!prisma) throw new Error("Prisma not available");
-          if (!ingestionFlushQueue)
-            throw new Error("Ingestion flush queue not available");
-
-          // Flush ingestion buffer
-          const processingStartTime = Date.now();
-
-          await new IngestionService(
-            redis,
-            prisma,
-            ingestionFlushQueue,
-            ClickhouseWriter.getInstance(),
-            clickhouseClient,
-            env.LANGFUSE_INGESTION_BUFFER_TTL_SECONDS
-          ).flush(projectEntityId);
-
-          // Log processing time
-          const processingTime = Date.now() - processingStartTime;
-          logger.debug(
-            `Prepared and scheduled CH-write in ${processingTime} ms for ${projectEntityId}`
-          );
-          Sentry.metrics.distribution(
-            "ingestion_flush_processing_time",
-            processingTime,
-            { unit: "milliseconds" }
-          );
-
-          // Log queue size
-          await ingestionFlushQueue
-            .count()
-            .then((count) => {
-              logger.debug(`Ingestion flush queue length: ${count}`);
-              Sentry.metrics.gauge("ingestion_flush_queue_length", count, {
-                unit: "records",
-              });
-              return count;
-            })
-            .catch();
-        }
+        );
       },
       {
         connection: redis,
