46316cb4ae1262ac082040d42de5c06508e90bf2
Max Deichmann <m.deichmann@tum.de>
2024-08-12 14:39:01 +0200

    fix

--- worker/src/logger.ts
+++ worker/src/logger.ts
@@ -1,31 +1,33 @@
 import pino from "pino";
 import { env } from "./env";
 
-const transport = env.LANGFUSE_WORKER_BETTERSTACK_TOKEN
-  ? pino.transport({
-      target: "@logtail/pino",
-      options: { sourceToken: env.LANGFUSE_WORKER_BETTERSTACK_TOKEN },
-    })
-  : undefined;
-console.log("transport", env.LANGFUSE_WORKER_BETTERSTACK_TOKEN);
+const getBetterstackLogginTransport = (minLevel: string) => {
+  return env.LANGFUSE_WORKER_BETTERSTACK_TOKEN
+    ? pino.transport({
+        target: "@logtail/pino",
+        options: { sourceToken: env.LANGFUSE_WORKER_BETTERSTACK_TOKEN },
+        level: minLevel,
+      })
+    : undefined;
+};
+
 export const getLogger = (
   env: "development" | "production" | "test",
   minLevel = "info"
 ) => {
   if (env === "production") {
-    return pino({
-      base: { serviceContext: { service: "langfuse-worker" } },
-      formatters: {
-        level(label) {
-          const pinoLevel = label as pino.Level;
-          // `@type` property tells Error Reporting to track even if there is no `stack_trace`
-          const typeProp =
-            label === "error" || label === "fatal"
-              ? {
-                  "@type":
-                    "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent",
-                }
-  return pino(transport);
+    return pino(getBetterstackLogginTransport(minLevel));
+  }
+  return pino({
+    level: minLevel,
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  });
 };
 const logger = getLogger(env.NODE_ENV, env.LANGFUSE_LOG_LEVEL);
 export default logger;
