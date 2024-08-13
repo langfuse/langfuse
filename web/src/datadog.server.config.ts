94f368ca0630d0ae58421e9ae0e7ee4051d1adf7
Max Deichmann <m.deichmann@tum.de>
2024-08-13 23:42:25 +0200

    git push

--- web/src/datadog.server.config.ts
+++ web/src/datadog.server.config.ts
@@ -3,9 +3,13 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { IORedisInstrumentation } from "@opentelemetry/instrumentation-ioredis";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { PrismaInstrumentation } from "@prisma/instrumentation";
-import dd from "dd-trace";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
+import {
+  SimpleSpanProcessor,
+  ConsoleSpanExporter,
+} from "@opentelemetry/sdk-trace-base";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import opentelemetry from "@opentelemetry/api";
 
 if (!process.env.VERCEL && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
@@ -13,39 +17,9 @@ if (!process.env.VERCEL && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
 
   opentelemetry.context.setGlobalContextManager(contextManager);
 
-  const tracer = dd.init({
-    profiling: false,
-    runtimeMetrics: true,
-  });
+  const provider = new NodeTracerProvider();
 
-  const { TracerProvider } = tracer;
-
-  const provider = new TracerProvider();
-
-  // tracer.use("http", {
-  //   hooks: {
-  //     request(span, req) {
-  //       if (span && req) {
-  //         const urlString = "path" in req ? req.path : req.url;
-
-  //         if (urlString) {
-  //           const url = new URL(urlString, "http://localhost");
-  //           const path = url.pathname + url.search;
-  //           const resourceGroup = (() => {
-  //             return url.pathname;
-  //           })();
-  //           const method = req.method;
-
-  //           span.setTag(
-  //             "resource.name",
-  //             method ? `${method} ${resourceGroup}` : resourceGroup,
-  //           );
-  //           span.setTag("http.route", method ? `${method} ${path}` : path);
-  //         }
-  //       }
-  //     },
-  //   },
-  // });
+  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
   registerInstrumentations({
     instrumentations: [
