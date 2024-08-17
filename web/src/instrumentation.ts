84e047053afc55614a6327ea585cfc558a16a982
Max Deichmann <m.deichmann@tum.de>
2024-08-17 16:28:49 +0200

    fixes

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,9 +1,7 @@
 // See: https://vercel.com/docs/observability/otel-overview
 
 export async function register() {
-  console.log(`NEXT_RUNTIME ${process.env.NEXT_RUNTIME}`);
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    console.log("Initializing datadog tracing");
     await import("./datadog.server.config");
   }
 }
