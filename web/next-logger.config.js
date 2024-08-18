9984eeb26a9948c229a7dd75f48be8ce65616e8c
Max Deichmann <m.deichmann@tum.de>
2024-08-18 21:51:07 +0200

    fixes

--- web/next-logger.config.js
+++ web/next-logger.config.js
@@ -2,29 +2,33 @@ const pino = require("pino");
 const pretty = require("pino-pretty");
 
 const logger = (defaultConfig) => {
-  return process.env.NODE_ENV !== "development"
-    ? pino(
-        pino.transport({
-          ...defaultConfig,
-          target: "@logtail/pino", //sends logs to betterstack
-          options: {
-            sourceToken: process.env.LANGFUSE_WEB_BETTERSTACK_TOKEN,
-          },
-        }),
-      )
-    : pino(
-        {
-          ...defaultConfig,
-          browser: {
-            asObject: true,
-          },
+  if (process.env.NODE_ENV !== "development") {
+    console.log("Using betterstack logger");
+    return pino(
+      pino.transport({
+        ...defaultConfig,
+        target: "@logtail/pino", //sends logs to betterstack
+        options: {
+          sourceToken: process.env.LANGFUSE_WEB_BETTERSTACK_TOKEN,
         },
-        pretty({
-          levelFirst: false,
-          colorize: true,
-          ignore: "",
-        }),
-      );
+      }),
+    );
+  } else {
+    console.log("Using pino pretty logger");
+    return pino(
+      {
+        ...defaultConfig,
+        browser: {
+          asObject: true,
+        },
+      },
+      pretty({
+        levelFirst: false,
+        colorize: true,
+        ignore: "",
+      }),
+    );
+  }
 };
 
 module.exports = {
