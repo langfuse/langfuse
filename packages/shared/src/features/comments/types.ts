bf2bab2eb56b01561945709c2f799a72598cb3b3
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-08-19 16:02:59 +0200

    push

--- packages/shared/src/features/comments/types.ts
+++ packages/shared/src/features/comments/types.ts
@@ -6,11 +6,11 @@ const OBSERVATION: CommentObjectType = "OBSERVATION";
 const SESSION: CommentObjectType = "SESSION";
 const PROMPT: CommentObjectType = "PROMPT";
 
-const COMMENT_OBJECT_TYPE = [TRACE, OBSERVATION, SESSION, PROMPT] as const;
+const COMMENT_OBJECT_TYPES = [TRACE, OBSERVATION, SESSION, PROMPT] as const;
 
 export const CreateCommentData = z.object({
   projectId: z.string(),
   content: z.string().trim().min(1).max(500),
   objectId: z.string(),
-  objectType: z.enum(COMMENT_OBJECT_TYPE),
+  objectType: z.enum(COMMENT_OBJECT_TYPES),
 });
