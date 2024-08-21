83d5f9f494950570fc8d55ac3508abcaa733c8bc
Max Deichmann <m.deichmann@tum.de>
2024-08-21 19:41:59 +0200

    push

--- web/src/sentry.client.config.ts
+++ web/src/sentry.client.config.ts
@@ -1,33 +1,27 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://44c5a8d22f44a2d2a7d6c780c6377a5c@o4505408450789376.ingest.us.sentry.io/4505408525565952",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Replay may only be enabled for the client-side
-  integrations: [
-    Sentry.replayIntegration(),
-    Sentry.replayIntegration({
-      // Additional SDK configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: process.env.NEXT_LANGFUSE_TRACING_SAMPLE_RATE
+    ? Number(process.env.NEXT_LANGFUSE_TRACING_SAMPLE_RATE)
+    : 0.1,
 
   // Capture Replay for 10% of all sessions,
   // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 1,
+  replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 1.0,
   debug: true,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  // ...
-
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  beforeSend(event, _hint) {
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  },
 });
