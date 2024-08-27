d6430eada8ad30215b73b1d867307d6d894f43f4
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-08-27 14:54:13 +0200

    chore(ui): style trpc error toast to differentiate between errors and warnings

--- web/src/features/notifications/showErrorToast.tsx
+++ web/src/features/notifications/showErrorToast.tsx
@@ -1,17 +1,35 @@
 import { toast } from "sonner";
 import { ErrorNotification } from "@/src/features/notifications/ErrorNotification";
 
+const isServerError = (httpStatus: number): boolean => {
+  return httpStatus >= 500 && httpStatus < 600;
+};
+
+const toastErrorStyleProps = {
+  border: "1px solid hsl(var(--destructive))",
+  backgroundColor: "hsl(var(--destructive))",
+};
+
+const toastWarningStyleProps = {
+  border: "1px solid hsl(var(--light-yellow))",
+  backgroundColor: "hsl(var(--light-yellow))",
+};
+
 export const showErrorToast = (
   error: string,
   description: string,
+  httpStatus: number,
   cause?: string,
   path?: string,
 ) => {
+  const isError = isServerError(httpStatus);
+
   toast.custom(
     (t) => (
       <ErrorNotification
         error={error}
         description={description}
+        type={isError ? "ERROR" : "WARNING"}
         cause={cause}
         path={path}
         dismissToast={toast.dismiss}
@@ -22,9 +40,8 @@ export const showErrorToast = (
       duration: Infinity,
       style: {
         padding: "1rem",
-        border: "1px solid hsl(var(--destructive))",
         borderRadius: "0.5rem",
-        backgroundColor: "hsl(var(--destructive))",
+        ...(isError ? toastErrorStyleProps : toastWarningStyleProps),
       },
     },
   );
