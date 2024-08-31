81db9134d794f9b4bc330212118cfd4177b00d6d
Max Deichmann <m.deichmann@tum.de>
2024-08-31 17:56:20 +0200

    send sentry source maps

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -188,5 +188,5 @@ export default withSentryConfig(nextConfig, {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  automaticVercelMonitors: false,
 });
