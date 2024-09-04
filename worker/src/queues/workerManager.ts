10bbf759b01cb0ec910166cc9e8ada06b3cca001
Steffen Schmitz <steffen@langfuse.com>
2024-09-04 10:22:59 +0200

    fix: reconnect workers to redis on errors (#3211)

---
+++ worker/src/queues/workerManager.ts
@@ -0,0 +1,63 @@
+import logger from "../logger";
+
+import { Job, Processor, Worker, WorkerOptions } from "bullmq";
+import { createNewRedisInstance } from "@langfuse/shared/src/server";
+
+export class WorkerManager {
+  private static workers: { [key: string]: Worker } = {};
+
+  public static async closeWorkers(): Promise<void> {
+    await Promise.all(
+      Object.values(WorkerManager.workers).map((worker) => worker.close()),
+    );
+    logger.info("All workers have been closed.");
+  }
+
+  public static register(
+    queueName: string,
+    processor: Processor,
+    additionalOptions: Partial<WorkerOptions> = {},
+  ): void {
+    if (WorkerManager.workers[queueName]) {
+      logger.info(`Worker ${queueName} is already registered`);
+      return;
+    }
+
+    // Create redis connection for queue worker
+    const redisInstance = createNewRedisInstance({
+      retryStrategy: (times: number) => {
+        // https://docs.bullmq.io/guide/going-to-production#retrystrategy
+        // Retries forever. Waits at least 1s and at most 20s between retries.
+        logger.debug(`Connection to redis lost. Retry attempt: ${times}`);
+        return Math.max(Math.min(Math.exp(times), 20000), 1000);
+      },
+      reconnectOnError: (err: Error) => {
+        logger.warn(`Failed to connect to redis: ${err}. Reconnecting...`);
+        return true;
+      },
+    });
+    if (!redisInstance) {
+      logger.error("Failed to initialize redis connection");
+      return;
+    }
+
+    // Register worker
+    const worker = new Worker(queueName, processor, {
+      connection: redisInstance,
+      ...additionalOptions,
+    });
+    WorkerManager.workers[queueName] = worker;
+    logger.info(`${queueName} executor started: ${worker.isRunning()}`);
+
+    // Add error handling
+    worker.on("failed", (job: Job | undefined, err: Error) => {
+      logger.error(
+        err,
+        `Queue Job ${job?.name} with id ${job?.id} in ${queueName} failed with error ${err}`,
+      );
+    });
+    worker.on("error", (failedReason: Error) => {
+      logger.error(`Queue worker ${queueName} failed: ${failedReason}`);
+    });
+  }
+}
--- worker/src/utils/shutdown.ts
+++ worker/src/utils/shutdown.ts
@@ -2,16 +2,11 @@ import logger from "../logger";
 
 import { redis } from "@langfuse/shared/src/server";
 
-import { evalJobCreator, evalJobExecutor } from "../queues/evalQueue";
-import { batchExportJobExecutor } from "../queues/batchExportQueue";
-import { ingestionQueueExecutor } from "../queues/ingestionFlushQueueExecutor";
-import { repeatQueueExecutor } from "../queues/repeatQueue";
 import { ClickhouseWriter } from "../services/ClickhouseWriter";
 import { setSigtermReceived } from "../features/health";
 import { server } from "../index";
-import { legacyIngestionExecutor } from "../queues/legacyIngestionQueue";
-import { cloudUsageMeteringJobExecutor } from "../queues/cloudUsageMeteringQueue";
 import { freeAllTokenizers } from "../features/tokenisation/usage";
+import { WorkerManager } from "../queues/workerManager";
 
 export const onShutdown: NodeJS.SignalsListener = async (signal) => {
   logger.info(`Received ${signal}, closing server...`);
@@ -22,18 +17,7 @@ export const onShutdown: NodeJS.SignalsListener = async (signal) => {
   logger.info("Server has been closed.");
 
   // Shutdown workers (https://docs.bullmq.io/guide/going-to-production#gracefully-shut-down-workers)
-  const workers = [
-    evalJobCreator,
-    evalJobExecutor,
-    batchExportJobExecutor,
-    ingestionQueueExecutor,
-    repeatQueueExecutor,
-    legacyIngestionExecutor,
-    cloudUsageMeteringJobExecutor,
-  ];
-
-  await Promise.all(workers.map(async (worker) => await worker?.close()));
-  logger.info("All workers have been closed.");
+  await WorkerManager.closeWorkers();
 
   // Flush all pending writes to Clickhouse AFTER closing ingestion queue worker that is writing to it
   await ClickhouseWriter.getInstance().shutdown();
