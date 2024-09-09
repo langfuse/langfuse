1bde812d37e6b638cbd950a0f45ae32c20090f9c
steffen911 <steffen@langfuse.com>
2024-09-09 13:21:55 +0200

    chore: downgrade winston instrumentation and fix worker import

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,8 +1,7 @@
-import { logger } from "@langfuse/shared/src/server";
-
 import "./instrumentation"; // instrumenting the application
 import app from "./app";
 import { env } from "./env";
+import { logger } from "@langfuse/shared/src/server";
 
 export const server = app.listen(env.PORT, () => {
   logger.info(`Listening: http://localhost:${env.PORT}`);
