aa3c11d9c1bcecdfc675045cec3f1a6993a3d3fc
steffen911 <steffen@langfuse.com>
2024-09-12 10:10:40 +0200

    chore: remove auto instrumentation and add bullmq

--- worker/src/instrumentation.ts
+++ worker/src/instrumentation.ts
@@ -1,15 +1,13 @@
 import tracer from "dd-trace";
+import opentelemetry from "@opentelemetry/api";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { IORedisInstrumentation } from "@opentelemetry/instrumentation-ioredis";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
-import { PrismaInstrumentation } from "@prisma/instrumentation";
-import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
-import opentelemetry from "@opentelemetry/api";
-import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
 import { WinstonInstrumentation } from "@opentelemetry/instrumentation-winston";
-// import { BullMQInstrumentation } from "@appsignal/opentelemetry-instrumentation-bullmq";
+import { PrismaInstrumentation } from "@prisma/instrumentation";
+import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
+import { BullMQInstrumentation } from "@appsignal/opentelemetry-instrumentation-bullmq";
 
 const contextManager = new AsyncHooksContextManager().enable();
 
@@ -27,10 +25,8 @@ registerInstrumentations({
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
     new PrismaInstrumentation(),
+    new BullMQInstrumentation(),
     new WinstonInstrumentation({ disableLogSending: true }),
-    getNodeAutoInstrumentations(),
-    new UndiciInstrumentation(),
-    // new BullMQInstrumentation(),
   ],
 });
 
