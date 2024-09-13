b8429bdc989482668b9961723a84b5b15c8b0ac6
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-09-13 14:38:20 +0200

    style(evals_configs): fix table wrappers and apply sticky headings

--- web/src/components/layouts/full-screen-page.tsx
+++ web/src/components/layouts/full-screen-page.tsx
@@ -4,12 +4,13 @@ export const FullScreenPage: React.FC<
   React.PropsWithChildren<{
     mobile?: boolean;
     className?: string;
+    customHeight?: string;
   }>
-> = ({ children, mobile = true, className }) => {
+> = ({ children, mobile = true, className, customHeight = "1.5rem" }) => {
   return (
     <div
       className={cn(
-        "flex flex-col overflow-hidden lg:h-[calc(100dvh-1.5rem)]",
+        `flex flex-col overflow-hidden lg:h-[calc(100dvh-${customHeight})] h-[calc(100dvh-${customHeight})]`,
         mobile && "h-[calc(100dvh-6rem)]",
         className,
       )}
