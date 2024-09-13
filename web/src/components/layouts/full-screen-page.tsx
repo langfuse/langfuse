bc3f39272b594a1be684fa721390cc9055ca207c
marliessophie <74332854+marliessophie@users.noreply.github.com>
2024-09-13 15:27:22 +0200

    style(evals): apply sticky headers to eval logs and eval config tables  (#3347)

--- web/src/components/layouts/full-screen-page.tsx
+++ web/src/components/layouts/full-screen-page.tsx
@@ -4,13 +4,14 @@ export const FullScreenPage: React.FC<
   React.PropsWithChildren<{
     mobile?: boolean;
     className?: string;
+    customHeight?: string;
   }>
-> = ({ children, mobile = true, className }) => {
+> = ({ children, mobile = true, className, customHeight = "1.5rem" }) => {
   return (
     <div
       className={cn(
-        "flex flex-col overflow-hidden lg:h-[calc(100dvh-1.5rem)]",
-        mobile && "h-[calc(100dvh-6rem)]",
+        `flex flex-col overflow-hidden lg:h-[calc(100dvh-${customHeight})]`,
+        mobile ? "h-[calc(100dvh-6rem)]" : `h-[calc(100dvh-${customHeight})]`,
         className,
       )}
     >
