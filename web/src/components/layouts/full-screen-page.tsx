5af0008f07e70563b51e1b713f397e69f3b9a132
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-09-13 19:52:59 +0200

    style(tables): ensure footer is fixed on the bottom

--- web/src/components/layouts/full-screen-page.tsx
+++ web/src/components/layouts/full-screen-page.tsx
@@ -4,14 +4,21 @@ export const FullScreenPage: React.FC<
   React.PropsWithChildren<{
     mobile?: boolean;
     className?: string;
-    customHeight?: string;
+    lgHeight?: string;
+    mobileHeight?: string;
   }>
-> = ({ children, mobile = true, className, customHeight = "1.5rem" }) => {
+> = ({
+  children,
+  mobile = true,
+  className,
+  lgHeight = "lg:h-[calc(100dvh-1.5rem)]",
+  mobileHeight = "h-[calc(100dvh-6rem)]",
+}) => {
   return (
     <div
       className={cn(
-        `flex flex-col overflow-hidden lg:h-[calc(100dvh-${customHeight})]`,
-        mobile ? "h-[calc(100dvh-6rem)]" : `h-[calc(100dvh-${customHeight})]`,
+        `flex flex-col overflow-hidden ${lgHeight}`,
+        mobile && mobileHeight,
         className,
       )}
     >
