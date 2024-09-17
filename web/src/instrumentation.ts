b5c56a5edc9069c20a17825b333aced6465b2b55
Max Deichmann <m.deichmann@tum.de>
2024-09-17 19:41:03 +0200

    perf

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,10 +1,14 @@
 // See: https://vercel.com/docs/observability/otel-overview
 
 export async function register() {
-  if (
-    process.env.NEXT_RUNTIME === "nodejs" &&
-    process.env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION !== "DEV"
-  ) {
+  // This variable is set in the .env file or environment variables
+  // Value is true if NEXT_PUBLIC_LANGFUSE_RUN_NEXT_INIT_ENABLED is "true" or undefined
+  const isInitLoadingEnabled =
+    process.env.NEXT_PUBLIC_LANGFUSE_RUN_NEXT_INIT_ENABLED !== undefined
+      ? process.env.NEXT_PUBLIC_LANGFUSE_RUN_NEXT_INIT_ENABLED === "true"
+      : true;
+
+  if (process.env.NEXT_RUNTIME === "nodejs" && isInitLoadingEnabled) {
     await import("./datadog.server.config");
     await import("./initialize");
   }
