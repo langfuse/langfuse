2177821e045f01e644ee007622e2d44b68051a46
Max Deichmann <m.deichmann@tum.de>
2024-09-17 18:57:47 +0200

    chore: fix dev server speed

--- web/src/instrumentation.ts
+++ web/src/instrumentation.ts
@@ -1,7 +1,10 @@
 // See: https://vercel.com/docs/observability/otel-overview
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (
+    process.env.NEXT_RUNTIME === "nodejs" &&
+    process.env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION !== "DEV"
+  ) {
     await import("./datadog.server.config");
     await import("./initialize");
   }
