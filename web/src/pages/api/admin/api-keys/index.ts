f2b703e15087590e3d4a2d28c638610b1fb4a8b9
Max Deichmann <m.deichmann@tum.de>
2024-09-24 14:11:37 +0200

    chroe

--- web/src/pages/api/admin/api-keys/index.ts
+++ web/src/pages/api/admin/api-keys/index.ts
@@ -1,9 +1,9 @@
 /**
- * This endpoint is used to add a new SSO configuration to the database.
+ * This endpoint is used to remove API keys from the database and from Redis
  *
  * This is an EE feature and will return a 404 response if EE is not available.
  */
 
-import { deleteApiKeys } from "@/src/ee/features/admin/api-keys/index";
+import deleteApiKeys from "@/src/ee/features/admin/api-keys";
 
 export default deleteApiKeys;
