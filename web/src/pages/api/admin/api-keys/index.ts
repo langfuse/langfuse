e2f19da7548c5a2228b05e7fb33ddcb8b9b6898f
Max Deichmann <m.deichmann@tum.de>
2024-09-24 14:16:22 +0200

    chroe

--- web/src/pages/api/admin/api-keys/index.ts
+++ web/src/pages/api/admin/api-keys/index.ts
@@ -1,9 +1,91 @@
-/**
- * This endpoint is used to remove API keys from the database and from Redis
- *
- * This is an EE feature and will return a 404 response if EE is not available.
- */
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { z } from "zod";
+import { prisma } from "@langfuse/shared/src/db";
+import { logger, redis } from "@langfuse/shared/src/server";
+import { env } from "@/src/env.mjs";
 
-import deleteApiKeys from "@/src/ee/features/admin/api-keys";
+const DeleteApiKeySchema = z.object({
+  projectIds: z.array(z.string()),
+});
 
-export default deleteApiKeys;
+export default async function deleteApiKeys(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    // allow only POST requests
+    if (req.method !== "DELETE") {
+      res.status(405).json({ error: "Method Not Allowed" });
+      return;
+    }
+
+    if (!env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
+      res.status(403).json({ error: "Only accessible on Langfuse cloud" });
+      return;
+    }
+
+    // check if ADMIN_API_KEY is set
+    if (!env.ADMIN_API_KEY) {
+      logger.error("ADMIN_API_KEY is not set");
+      res.status(500).json({ error: "ADMIN_API_KEY is not set" });
+      return;
+    }
+
+    // check bearer token
+    const { authorization } = req.headers;
+    if (!authorization) {
+      res
+        .status(401)
+        .json({ error: "Unauthorized: No authorization header provided" });
+      return;
+    }
+    const [scheme, token] = authorization.split(" ");
+    if (scheme !== "Bearer" || !token || token !== env.ADMIN_API_KEY) {
+      res.status(401).json({ error: "Unauthorized: Invalid token" });
+      return;
+    }
+
+    const body = DeleteApiKeySchema.safeParse(req.body);
+
+    if (!body.success) {
+      res.status(400).json({ error: body.error });
+      return;
+    }
+
+    logger.info(
+      `trying to remove API keys for projects ${body.data.projectIds.join(", ")}`,
+    );
+
+    // delete the API keys in the database first
+    const apiKeysToBeDeleted = await prisma.apiKey.findMany({
+      where: {
+        projectId: {
+          in: body.data.projectIds,
+        },
+      },
+    });
+
+    await prisma.apiKey.deleteMany({
+      where: {
+        projectId: {
+          in: body.data.projectIds,
+        },
+      },
+    });
+
+    // then delete from the cache
+    await new ApiAuthService(prisma, redis).invalidate(
+      apiKeysToBeDeleted,
+      `projects ${body.data.projectIds.join(", ")}`,
+    );
+
+    logger.info(
+      `Removed API keys for projects ${body.data.projectIds.join(", ")}`,
+    );
+
+    res.status(200).json({ message: "API keys deleted" });
+  } catch (e) {
+    logger.error("failed to remove API keys", e);
+    res.status(500).json({ error: e });
+  }
+}
