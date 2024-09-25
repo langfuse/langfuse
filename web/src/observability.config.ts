e9c950932cf7ad9e67cfa04fa3ddf8c669df479d
steffen911 <steffen@langfuse.com>
2024-09-25 06:24:49 +0200

    chore: add logging

--- web/src/observability.config.ts
+++ web/src/observability.config.ts
@@ -32,6 +32,11 @@ if (!process.env.VERCEL && process.env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION) {
       new HttpInstrumentation({
         requireParentforOutgoingSpans: true,
         requestHook: (span, request: any) => {
+          // TODO: Remove
+          console.log("requestHook method", request?.method);
+          console.log("requestHook url", request?.url);
+          console.log("requestHook host", request?.host);
+          console.log("requestHook path", request?.path);
           const { method, url } = request;
           let path = new URL(url, `http://${request.host}`).pathname;
           if (path.startsWith("/_next/static")) {
