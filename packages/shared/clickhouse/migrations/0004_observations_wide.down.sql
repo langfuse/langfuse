7a48e33f896dbe52bfa84a7025a88a2d817837ee
Max Deichmann <m.deichmann@tum.de>
2024-10-07 11:14:56 -0700

    fix down migration

--- packages/shared/clickhouse/migrations/0004_observations_wide.down.sql
+++ packages/shared/clickhouse/migrations/0004_observations_wide.down.sql
@@ -1,3 +1,3 @@
-DROP MATERIALIZED VIEW IF EXISTS mv_observations_to_observations_wide;
-DROP MATERIALIZED VIEW IF EXISTS mv_traces_to_observations_wide;
+DROP TABLE IF EXISTS mv_observations_to_observations_wide;
+DROP TABLE IF EXISTS mv_traces_to_observations_wide;
 DROP TABLE IF EXISTS observations_wide;
