9fc0215d22b2b300c8262c19fa76c4d2466cefad
Steffen Schmitz <steffen@langfuse.com>
2024-10-09 22:30:54 +0200

    chore: upgrade nextjs to 14.2.14 (#3643)
    Co-authored-by: Max Deichmann <m.deichmann@tum.de>

--- ee/src/env.ts
+++ ee/src/env.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
-import { env as sharedEnv } from "@langfuse/shared";
+import { env as sharedEnv, removeEmptyEnvVariables } from "@langfuse/shared";
 
 const EnvSchema = z.object({
   NEXT_PUBLIC_LANGFUSE_CLOUD_REGION: z.string().optional(),
   LANGFUSE_EE_LICENSE_KEY: z.string().optional(),
 });
 
-export const env = { ...sharedEnv, ...EnvSchema.parse(process.env) };
+export const env = {
+  ...sharedEnv,
+  ...EnvSchema.parse(removeEmptyEnvVariables(process.env)),
+};
