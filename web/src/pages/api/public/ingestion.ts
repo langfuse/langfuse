6d6a4961ce4315d7dfc8e2e17b0728cd33c979f2
Max Deichmann <m.deichmann@tum.de>
2024-10-08 17:04:03 -0700

    chore: remove metrics by users datadog

--- web/src/pages/api/public/ingestion.ts
+++ web/src/pages/api/public/ingestion.ts
@@ -123,10 +123,6 @@ export default async function handler(
     recordIncrement(
       "langfuse.ingestion.event",
       parsedSchema.success ? parsedSchema.data.batch.length : 0,
-      {
-        orgId: authCheck.scope.orgId,
-        projectId: authCheck.scope.projectId,
-      },
     );
 
     // add context of api call to the span
