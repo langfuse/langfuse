669e760b760be125967d59e16eecc83a70ad5a44
steffen911 <steffen@langfuse.com>
2024-10-16 16:02:44 -0700

    chore: refactor options into default object

--- packages/shared/src/server/redis/redis.ts
+++ packages/shared/src/server/redis/redis.ts
@@ -2,6 +2,21 @@ import Redis, { RedisOptions } from "ioredis";
 import { env } from "../../env";
 import { logger } from "../logger";
 
+const defaultRedisOptions: Partial<RedisOptions> = {
+  maxRetriesPerRequest: null,
+  enableAutoPipelining: env.REDIS_ENABLE_AUTO_PIPELINING === "true",
+  retryStrategy: (times: number) => {
+    // Retries forever. Waits at least 1s and at most 20s between retries.
+    logger.warn(`Connection to redis lost. Retry attempt: ${times}`);
+    return Math.max(Math.min(Math.exp(times), 20000), 1000);
+  },
+  reconnectOnError: (err) => {
+    // Reconnects on READONLY errors and auto-retries the command.
+    logger.warn(`Redis connection error: ${err.message}`);
+    return err.message.includes("READONLY") ? 2 : false;
+  },
+};
+
 /**
  * Create a new redis client instance. We automatically disable retries per request which means that commands will wait
  * forever until connection is live again, enable auto pipelining, and auto-reconnect on READONLY errors which
@@ -14,16 +29,7 @@ export const createNewRedisInstance = (
 ) => {
   return env.REDIS_CONNECTION_STRING
     ? new Redis(env.REDIS_CONNECTION_STRING, {
-        maxRetriesPerRequest: null,
-        enableAutoPipelining: env.REDIS_ENABLE_AUTO_PIPELINING === "true",
-        retryStrategy: (times: number) => {
-          // https://docs.bullmq.io/guide/going-to-production#retrystrategy
-          // Retries forever. Waits at least 1s and at most 20s between retries.
-          logger.debug(`Connection to redis lost. Retry attempt: ${times}`);
-          return Math.max(Math.min(Math.exp(times), 20000), 1000);
-        },
-        reconnectOnError: (err) =>
-          err.message.includes("READONLY") ? 2 : false,
+        ...defaultRedisOptions,
         ...additionalOptions,
       })
     : env.REDIS_HOST
@@ -31,16 +37,7 @@ export const createNewRedisInstance = (
           host: String(env.REDIS_HOST),
           port: Number(env.REDIS_PORT),
           password: String(env.REDIS_AUTH),
-          maxRetriesPerRequest: null,
-          enableAutoPipelining: env.REDIS_ENABLE_AUTO_PIPELINING === "true",
-          retryStrategy: (times: number) => {
-            // https://docs.bullmq.io/guide/going-to-production#retrystrategy
-            // Retries forever. Waits at least 1s and at most 20s between retries.
-            logger.debug(`Connection to redis lost. Retry attempt: ${times}`);
-            return Math.max(Math.min(Math.exp(times), 20000), 1000);
-          },
-          reconnectOnError: (err) =>
-            err.message.includes("READONLY") ? 2 : false,
+          ...defaultRedisOptions,
           ...additionalOptions,
         })
       : null;
@@ -54,6 +51,7 @@ const createRedisClient = () => {
     return null;
   }
 };
+
 declare global {
   // eslint-disable-next-line no-var
   var redis: undefined | ReturnType<typeof createRedisClient>;
