87104728550e3ece9683af93a516e0d8aee610da
Marc Klingen <git@marcklingen.com>
2024-10-23 00:24:37 +0200

    fix initials

--- web/src/components/nav-user.tsx
+++ web/src/components/nav-user.tsx
@@ -41,6 +41,13 @@ export type UserNavigationProps = {
 export function NavUser({ user, items }: UserNavigationProps) {
   const { isMobile } = useSidebar();
 
+  const initials = user.name
+    .split(" ")
+    .slice(0, 2)
+    .map((word) => word[0])
+    .join("")
+    .toUpperCase();
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -52,7 +59,9 @@ export function NavUser({ user, items }: UserNavigationProps) {
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">
+                  {initials}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name}</span>
@@ -71,7 +80,9 @@ export function NavUser({ user, items }: UserNavigationProps) {
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">
+                    {initials}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{user.name}</span>
