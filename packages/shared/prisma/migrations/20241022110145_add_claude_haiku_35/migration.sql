609fae7cc0c6f745a264a7544a0346d57e210571
Max Deichmann <m.deichmann@tum.de>
2024-10-22 20:26:16 +0200

    feat: add new claude model

---
+++ packages/shared/prisma/migrations/20241022110145_add_claude_haiku_35/migration.sql
@@ -0,0 +1,17 @@
+INSERT INTO models (
+  id,
+  project_id,
+  model_name,
+  match_pattern,
+  start_date,
+  input_price,
+  output_price,
+  total_price,
+  unit,
+  tokenizer_id,
+  tokenizer_config
+)
+VALUES
+  -- https://docs.anthropic.com/en/docs/about-claude/models#model-comparison-table
+  ('cm2krz1uf000208jjg5653iud', NULL, 'claude-3.5-haiku-20241022', '(?i)^(claude-3-5-sonnet-20241022|anthropic\.claude-3-5-sonnet-20241022-v1:0|claude-3-5-sonnet@20241022)$', NULL, 0.000003, 0.000015, NULL, 'TOKENS', 'claude')
+
