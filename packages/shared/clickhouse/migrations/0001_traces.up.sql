5b1704c95ed61bde1d10247e7ff1762bc57cc1ee
Max Deichmann <m.deichmann@tum.de>
2024-10-23 16:26:50 +0200

    perf: improve CH primary keys and order by (#3880)

--- packages/shared/clickhouse/migrations/0001_traces.up.sql
+++ packages/shared/clickhouse/migrations/0001_traces.up.sql
@@ -20,6 +20,10 @@ CREATE TABLE traces (
     INDEX idx_res_metadata_key mapKeys(metadata) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_res_metadata_value mapValues(metadata) TYPE bloom_filter(0.01) GRANULARITY 1
 ) ENGINE = ReplacingMergeTree Partition by toYYYYMM(timestamp)
+PRIMARY KEY (
+        project_id,
+        toDate(timestamp)
+)
 ORDER BY (
         project_id,
         toDate(timestamp),
