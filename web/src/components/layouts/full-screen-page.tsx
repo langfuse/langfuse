b26a5371816f8eccfa92573d4bb95c7f956178d8
marliessophie <74332854+marliessophie@users.noreply.github.com>
2024-10-29 11:34:45 +0100

    refactor: `full-screen-page` to drop magic in height calculation   (#3944)
    * fix: remove magic from full screen page
    
    * fix: wrap prompts table as full page
    
    * style: account for sticky header layout for mobile
    ---------
    
    Co-authored-by: Marc Klingen <git@marcklingen.com>

--- web/src/components/layouts/full-screen-page.tsx
+++ web/src/components/layouts/full-screen-page.tsx
@@ -1,28 +1,9 @@
 import { cn } from "@/src/utils/tailwind";
 
-export const FullScreenPage: React.FC<
-  React.PropsWithChildren<{
-    mobile?: boolean;
-    className?: string;
-    lgHeight?: string;
-    mobileHeight?: string;
-  }>
-> = ({
+export const FullScreenPage: React.FC<React.PropsWithChildren<{}>> = ({
   children,
-  mobile = true,
-  className,
-  lgHeight = "lg:h-[calc(100dvh-1.5rem)]",
-  mobileHeight = "h-[calc(100dvh-6rem)]",
 }) => {
   return (
-    <div
-      className={cn(
-        `flex flex-col overflow-hidden ${lgHeight}`,
-        mobile && mobileHeight,
-        className,
-      )}
-    >
-      {children}
-    </div>
+    <div className={cn("flex h-full flex-col overflow-hidden")}>{children}</div>
   );
 };
