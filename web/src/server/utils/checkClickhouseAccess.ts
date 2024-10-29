63905809a22fa061e3a8728e5b3321f41fced7af
Max Deichmann <m.deichmann@tum.de>
2024-10-29 14:14:23 +0100

    push

--- web/src/server/utils/checkClickhouseAccess.ts
+++ web/src/server/utils/checkClickhouseAccess.ts
@@ -1,5 +1,5 @@
 import { env } from "@/src/env.mjs";
 
 export const isClickhouseEligible = (admin: boolean) => {
-  return admin === true && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION !== undefined;
+  return admin === true && env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION;
 };
