e553f94c3feb420d0980783608d5dbf5925df86a
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-11-04 16:04:16 +0100

    bring back table resizing

--- web/src/components/ui/table.tsx
+++ web/src/components/ui/table.tsx
@@ -74,7 +74,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-10 border-b bg-background px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "relative h-10 border-b bg-background px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className,
     )}
     {...props}
