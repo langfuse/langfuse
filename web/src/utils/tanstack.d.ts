dc2028a8c733766c5eb0b8be6c40c1e3adf4da0a
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-11-04 15:57:41 +0100

    lint

--- web/src/utils/tanstack.d.ts
+++ web/src/utils/tanstack.d.ts
@@ -7,6 +7,7 @@ export declare module "@tanstack/table-core" {
 }
 
 export declare module "@tanstack/react-table" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     metadata?: unknown;
   }
