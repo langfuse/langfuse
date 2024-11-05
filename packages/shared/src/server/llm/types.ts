731cc0b1450c2b016a32ecb628b55d38bb0fcb64
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-11-05 12:00:23 +0100

    update comment

--- packages/shared/src/server/llm/types.ts
+++ packages/shared/src/server/llm/types.ts
@@ -49,7 +49,7 @@ export const ZodModelConfig = z.object({
   top_p: z.coerce.number().optional(),
 });
 
-// NOTE: Update docs page when changing this!
+// NOTE: Update docs page when changing this! https://langfuse.com/docs/playground#openai-playground--anthropic-playground
 export const openAIModels = [
   "gpt-4o",
   "gpt-4o-2024-08-06",
@@ -76,7 +76,7 @@ export const openAIModels = [
 
 export type OpenAIModel = (typeof openAIModels)[number];
 
-// NOTE: Update docs page when changing this!
+// NOTE: Update docs page when changing this! https://langfuse.com/docs/playground#openai-playground--anthropic-playground
 export const anthropicModels = [
   "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-20240620",
