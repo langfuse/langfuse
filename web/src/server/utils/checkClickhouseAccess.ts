3331bf10757682f83a9eb58615cb212e6a74fc8d
Max Deichmann <m.deichmann@tum.de>
2024-11-10 15:39:10 +0100

    fix

--- web/src/server/utils/checkClickhouseAccess.ts
+++ web/src/server/utils/checkClickhouseAccess.ts
@@ -1,5 +1,7 @@
 import { env } from "@/src/env.mjs";
+import { getCurrentSpan } from "@langfuse/shared/src/server";
 import { type User } from "next-auth";
+import type * as opentelemetry from "@opentelemetry/api";
 
 export const isClickhouseEligible = (user?: User | null) => {
   return (
@@ -15,10 +17,17 @@ export const isClickhouseEligible = (user?: User | null) => {
 
 export const measureAndReturnApi = async <T, Y>(
   input: T & { queryClickhouse: boolean },
+  user: User | undefined,
   pgExecution: (input: T) => Promise<Y>,
   clickhouseExecution: (input: T) => Promise<Y>,
 ) => {
-  if (input.queryClickhouse && !isClickhouseEligible()) {
+  const currentSpan = getCurrentSpan();
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (input.queryClickhouse && !isClickhouseEligible(user)) {
     throw new Error("Not eligible to query clickhouse");
   }
 
@@ -30,13 +39,29 @@ export const measureAndReturnApi = async <T, Y>(
     return pgExecution(input);
   }
 
-  const promises = await Promise.all([
-    pgExecution(input),
-    clickhouseExecution(input),
+  const [[pgResult, pgDuration], [chResult, chDuration]] = await Promise.all([
+    executionWrapper(input, pgExecution, currentSpan, "pg"),
+    executionWrapper(input, clickhouseExecution, currentSpan, "ch"),
   ]);
 
+  const durationDifference = Math.abs(pgDuration - chDuration);
+  currentSpan?.setAttribute("execution-time-difference", durationDifference);
+
   if (env.LANGFUSE_RETURN_FROM_CLICKHOUSE === "true") {
-    return promises[1];
+    return chResult;
   }
-  return promises[0];
+  return pgResult;
+};
+
+const executionWrapper = async <T, Y>(
+  input: T & { queryClickhouse: boolean },
+  fn: (input: T) => Promise<Y>,
+  span?: opentelemetry.Span,
+  attributePrefix?: string,
+): Promise<[Y, number]> => {
+  const startTime = Date.now();
+  const res = await fn(input);
+  const duration = Date.now() - startTime;
+  span?.setAttribute(`${attributePrefix}-duration`, duration);
+  return [res, duration];
 };
