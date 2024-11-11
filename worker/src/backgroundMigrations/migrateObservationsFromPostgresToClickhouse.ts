f3bb40e764de4aaad2ff9acdf7bd087b214d6d4b
steffen911 <steffen@langfuse.com>
2024-11-11 20:37:08 +0100

    chore: iterate over observations from a single day

--- worker/src/backgroundMigrations/migrateObservationsFromPostgresToClickhouse.ts
+++ worker/src/backgroundMigrations/migrateObservationsFromPostgresToClickhouse.ts
@@ -17,41 +17,6 @@ export default class MigrateObservationsFromPostgresToClickhouse
   private isAborted = false;
   private isFinished = false;
 
-  private async updateMaxDate(stateSuffix: string, maxDate: Date) {
-    await prisma.$transaction(
-      async (tx) => {
-        // @ts-ignore
-        const migrationState: { state: Record<string, string> } =
-          await tx.backgroundMigration.findUniqueOrThrow({
-            where: { id: backgroundMigrationId },
-            select: { state: true },
-          });
-        migrationState.state[`maxDate${stateSuffix}`] = maxDate.toISOString();
-        await tx.backgroundMigration.update({
-          where: { id: backgroundMigrationId },
-          data: { state: migrationState.state },
-        });
-      },
-      {
-        maxWait: 5000,
-        isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
-      },
-    );
-  }
-
-  private async getMaxDate(stateSuffix: string): Promise<Date | undefined> {
-    // @ts-ignore
-    const migrationState: { state: Record<string, string | undefined> } =
-      await prisma.backgroundMigration.findUniqueOrThrow({
-        where: { id: backgroundMigrationId },
-        select: { state: true },
-      });
-
-    return migrationState.state?.[`maxDate${stateSuffix}`]
-      ? new Date(migrationState.state[`maxDate${stateSuffix}`] as string)
-      : undefined;
-  }
-
   async validate(
     args: Record<string, unknown>,
   ): Promise<{ valid: boolean; invalidReason: string | undefined }> {
@@ -70,14 +35,9 @@ export default class MigrateObservationsFromPostgresToClickhouse
       `Migrating observations from postgres to clickhouse with ${JSON.stringify(args)}`,
     );
 
-    const stateSuffix = (args.stateSuffix as string) ?? "";
-    const initialDate = await this.getMaxDate(stateSuffix);
     const maxRowsToProcess = Number(args.maxRowsToProcess ?? Infinity);
     const batchSize = Number(args.batchSize ?? 5000);
-    const maxDate =
-      initialDate ?? new Date((args.maxDate as string) ?? new Date());
-
-    await this.updateMaxDate(stateSuffix, maxDate);
+    let idIterator = "a369b915-6326-468e-9692-be9244844d62";
 
     let processedRows = 0;
     while (
@@ -87,17 +47,15 @@ export default class MigrateObservationsFromPostgresToClickhouse
     ) {
       const fetchStart = Date.now();
 
-      const maxDate = await this.getMaxDate(stateSuffix);
-      logger.info(`Max date: ${maxDate?.toISOString()}`);
-
       const observations = await prisma.$queryRaw<
         Array<Record<string, any>>
       >(Prisma.sql`
         SELECT o.id, o.trace_id, o.project_id, o.type, o.parent_observation_id, o.start_time, o.end_time, o.name, o.metadata, o.level, o.status_message, o.version, o.input, o.output, o.unit, o.model, o.internal_model_id, o."modelParameters" as model_parameters, o.prompt_tokens, o.completion_tokens, o.total_tokens, o.completion_start_time, o.prompt_id, p.name as prompt_name, p.version as prompt_version, o.input_cost, o.output_cost, o.total_cost, o.calculated_input_cost, o.calculated_output_cost, o.calculated_total_cost, o.created_at, o.updated_at
         FROM observations o
         LEFT JOIN prompts p ON o.prompt_id = p.id
-        WHERE o.created_at <= ${maxDate}
-        ORDER BY o.created_at DESC
+        WHERE o.created_at <= '2023-11-10T01:26:24.732Z'
+        AND o.id >= ${idIterator}
+        ORDER BY id
         LIMIT ${batchSize};
       `);
       if (observations.length === 0) {
@@ -120,19 +78,15 @@ export default class MigrateObservationsFromPostgresToClickhouse
         `Inserted ${observations.length} observations into Clickhouse in ${Date.now() - insertStart}ms`,
       );
 
-      await this.updateMaxDate(
-        stateSuffix,
-        new Date(observations[observations.length - 1].created_at),
-      );
-
       if (observations.length < batchSize) {
         logger.info("No more observations to migrate. Exiting...");
         this.isFinished = true;
       }
 
       processedRows += observations.length;
+      idIterator = observations[observations.length - 1].id;
       logger.info(
-        `Processed batch in ${Date.now() - fetchStart}ms. Oldest record in batch: ${new Date(observations[observations.length - 1].created_at).toISOString()}`,
+        `Processed batch in ${Date.now() - fetchStart}ms. ID iterator: ${idIterator}`,
       );
     }
 
