6e48ca10e886ddc751f9519d8a5ff481a2a43310
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-11-13 10:20:59 +0100

    feat: add validation of dataset items vs prompt variables

---
+++ web/src/ee/features/experiments/server/router.ts
@@ -0,0 +1,137 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProjectProcedure,
+} from "@/src/server/api/trpc";
+import { extractVariables } from "@/src/utils/string";
+import { PromptType } from "@/src/features/prompts/server/utils/validation";
+import { type DatasetItem } from "@langfuse/shared";
+
+const ValidConfigResponse = z.object({
+  isValid: z.literal(true),
+  totalItems: z.number(),
+  includesAll: z.number(),
+  includesSome: z.number(),
+  missing: z.number(),
+});
+
+const InvalidConfigResponse = z.object({
+  isValid: z.literal(false),
+  message: z.string(),
+});
+
+const ConfigResponse = z.discriminatedUnion("isValid", [
+  ValidConfigResponse,
+  InvalidConfigResponse,
+]);
+
+const validateDatasetItems = (
+  datasetItems: DatasetItem[],
+  variables: string[],
+) => {
+  return datasetItems.reduce(
+    (acc, { input }) => {
+      if (!input) {
+        return { ...acc, missing: acc.missing + 1 };
+      }
+
+      const inputKeys = Object.keys(input);
+      const hasAllVariables = variables.every((v) => inputKeys.includes(v));
+      const hasSomeVariables = variables.some((v) => inputKeys.includes(v));
+
+      return {
+        includesAll: acc.includesAll + Number(hasAllVariables),
+        includesSome:
+          acc.includesSome + Number(!hasAllVariables && hasSomeVariables),
+        missing: acc.missing + Number(!hasAllVariables && !hasSomeVariables),
+      };
+    },
+    { includesAll: 0, includesSome: 0, missing: 0 },
+  );
+};
+
+export const experimentsRouter = createTRPCRouter({
+  validateConfig: protectedProjectProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+        datasetId: z.string(),
+        promptId: z.string(),
+      }),
+    )
+    .output(ConfigResponse)
+    .query(async ({ input, ctx }) => {
+      // throwIfNoEntitlement({
+      //   entitlement: "model-based-evaluations",
+      //   projectId: input.projectId,
+      //   sessionUser: ctx.session.user,
+      // });
+      // throwIfNoProjectAccess({
+      //   session: ctx.session,
+      //   projectId: input.projectId,
+      //   scope: "evalJob:read",
+      // });
+      // load prompt to get the variables and schema
+      const prompt = await ctx.prisma.prompt.findFirst({
+        where: {
+          id: input.promptId,
+          projectId: input.projectId,
+        },
+      });
+
+      if (!prompt) {
+        return {
+          isValid: false,
+          message: "Selected prompt not found.",
+        };
+      }
+
+      const extractedVariables = extractVariables(
+        prompt?.type === PromptType.Text
+          ? (prompt.prompt?.toString() ?? "")
+          : JSON.stringify(prompt.prompt),
+      );
+
+      if (!Boolean(extractedVariables.length)) {
+        return {
+          isValid: false,
+          message: "Selected prompt has no variables.",
+        };
+      }
+
+      const datasetItems = await ctx.prisma.datasetItem.findMany({
+        where: {
+          datasetId: input.datasetId,
+          projectId: input.projectId,
+        },
+      });
+
+      if (!Boolean(datasetItems.length)) {
+        return {
+          isValid: false,
+          message: "Selected dataset is empty.",
+        };
+      }
+
+      const { includesAll, includesSome, missing } = validateDatasetItems(
+        datasetItems,
+        extractedVariables,
+      );
+
+      if (missing === datasetItems.length) {
+        return {
+          isValid: false,
+          message: "No dataset item contains all variables.",
+        };
+      }
+
+      return {
+        isValid: true,
+        totalItems: datasetItems.length,
+        includesAll,
+        includesSome,
+        missing,
+      };
+    }),
+});
