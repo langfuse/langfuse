cf3f8fb26eda202ef79e93dfffb0ddac2877c399
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-11-13 12:52:01 +0100

    remove instrumentation from uploadurl

--- web/src/pages/api/public/media/upload-url.ts
+++ web/src/pages/api/public/media/upload-url.ts
@@ -16,7 +16,6 @@ import {
   InvalidRequestError,
 } from "@langfuse/shared";
 import { prisma } from "@langfuse/shared/src/db";
-import { instrumentAsync } from "@langfuse/shared/src/server";
 
 export default withMiddlewares({
   POST: createAuthedAPIRoute({
@@ -24,48 +23,147 @@ export default withMiddlewares({
     bodySchema: GetMediaUploadUrlQuerySchema,
     responseSchema: GetMediaUploadUrlResponseSchema,
     successStatusCode: 201,
-    fn: async ({ body, auth }) =>
-      instrumentAsync({ name: "create-media-upload-url" }, async () => {
-        if (auth.scope.accessLevel !== "all") throw new ForbiddenError();
+    fn: async ({ body, auth }) => {
+      if (auth.scope.accessLevel !== "all") throw new ForbiddenError();
 
-        const { projectId } = auth.scope;
-        const {
-          contentType,
-          contentLength,
-          sha256Hash,
-          traceId,
-          observationId,
-          field,
-        } = body;
+      const { projectId } = auth.scope;
+      const {
+        contentType,
+        contentLength,
+        sha256Hash,
+        traceId,
+        observationId,
+        field,
+      } = body;
 
-        if (contentLength > env.LANGFUSE_S3_MEDIA_MAX_CONTENT_LENGTH)
-          throw new InvalidRequestError(
-            `File size must be less than ${env.LANGFUSE_S3_MEDIA_MAX_CONTENT_LENGTH} bytes`,
+      if (contentLength > env.LANGFUSE_S3_MEDIA_MAX_CONTENT_LENGTH)
+        throw new InvalidRequestError(
+          `File size must be less than ${env.LANGFUSE_S3_MEDIA_MAX_CONTENT_LENGTH} bytes`,
+        );
+
+      const { mediaId, uploadUrl } = await prisma.$transaction(async (tx) => {
+        const existingMedia = await tx.media.findUnique({
+          where: {
+            projectId_sha256Hash: {
+              projectId,
+              sha256Hash,
+            },
+          },
+        });
+
+        if (
+          existingMedia &&
+          existingMedia.uploadHttpStatus === 200 &&
+          existingMedia.contentType === contentType
+        ) {
+          if (observationId) {
+            await tx.observationMedia.upsert({
+              where: {
+                projectId_traceId_observationId_mediaId_field: {
+                  projectId,
+                  traceId,
+                  observationId,
+                  mediaId: existingMedia.id,
+                  field,
+                },
+              },
+              update: {},
+              create: {
+                projectId,
+                traceId,
+                observationId,
+                mediaId: existingMedia.id,
+                field,
+              },
+            });
+          } else {
+            await tx.traceMedia.upsert({
+              where: {
+                projectId_traceId_mediaId_field: {
+                  projectId,
+                  traceId,
+                  mediaId: existingMedia.id,
+                  field,
+                },
+              },
+              update: {},
+              create: {
+                projectId,
+                traceId,
+                field,
+                mediaId: existingMedia.id,
+              },
+            });
+          }
+
+          return {
+            mediaId: existingMedia.id,
+            uploadUrl: null,
+          };
+        }
+
+        const mediaId = existingMedia?.id ?? randomUUID();
+
+        if (
+          !(
+            env.LANGFUSE_S3_MEDIA_UPLOAD_ENABLED &&
+            env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET
+          )
+        )
+          throw new InternalServerError(
+            "Media upload to blob storage not enabled or no bucket configured",
           );
 
-        const { mediaId, uploadUrl } = await prisma.$transaction(async (tx) => {
-          const existingMedia = await tx.media.findUnique({
+        const s3Client = getMediaStorageServiceClient(
+          env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET,
+        );
+
+        const bucketPath = getBucketPath({
+          projectId,
+          mediaId,
+          contentType,
+        });
+
+        const uploadUrl = await s3Client.getSignedUploadUrl({
+          path: bucketPath,
+          ttlSeconds: 60 * 60, // 1 hour
+          sha256Hash,
+          contentType,
+          contentLength,
+        });
+
+        await Promise.all([
+          tx.media.upsert({
             where: {
               projectId_sha256Hash: {
                 projectId,
                 sha256Hash,
               },
             },
-          });
-
-          if (
-            existingMedia &&
-            existingMedia.uploadHttpStatus === 200 &&
-            existingMedia.contentType === contentType
-          ) {
-            if (observationId) {
-              await tx.observationMedia.upsert({
+            update: {
+              bucketName: env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET,
+              bucketPath,
+              contentType,
+              contentLength,
+            },
+            create: {
+              id: mediaId,
+              projectId,
+              sha256Hash,
+              bucketPath,
+              bucketName: env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET,
+              contentType,
+              contentLength,
+            },
+          }),
+          observationId
+            ? tx.observationMedia.upsert({
                 where: {
                   projectId_traceId_observationId_mediaId_field: {
                     projectId,
                     traceId,
                     observationId,
-                    mediaId: existingMedia.id,
+                    mediaId,
                     field,
                   },
                 },
@@ -74,17 +172,16 @@ export default withMiddlewares({
                   projectId,
                   traceId,
                   observationId,
-                  mediaId: existingMedia.id,
+                  mediaId,
                   field,
                 },
-              });
-            } else {
-              await tx.traceMedia.upsert({
+              })
+            : tx.traceMedia.upsert({
                 where: {
                   projectId_traceId_mediaId_field: {
                     projectId,
                     traceId,
-                    mediaId: existingMedia.id,
+                    mediaId,
                     field,
                   },
                 },
@@ -93,121 +190,22 @@ export default withMiddlewares({
                   projectId,
                   traceId,
                   field,
-                  mediaId: existingMedia.id,
+                  mediaId,
                 },
-              });
-            }
-
-            return {
-              mediaId: existingMedia.id,
-              uploadUrl: null,
-            };
-          }
-
-          const mediaId = existingMedia?.id ?? randomUUID();
-
-          if (
-            !(
-              env.LANGFUSE_S3_MEDIA_UPLOAD_ENABLED &&
-              env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET
-            )
-          )
-            throw new InternalServerError(
-              "Media upload to blob storage not enabled or no bucket configured",
-            );
-
-          const s3Client = getMediaStorageServiceClient(
-            env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET,
-          );
-
-          const bucketPath = getBucketPath({
-            projectId,
-            mediaId,
-            contentType,
-          });
-
-          const uploadUrl = await s3Client.getSignedUploadUrl({
-            path: bucketPath,
-            ttlSeconds: 60 * 60, // 1 hour
-            sha256Hash,
-            contentType,
-            contentLength,
-          });
-
-          await Promise.all([
-            tx.media.upsert({
-              where: {
-                projectId_sha256Hash: {
-                  projectId,
-                  sha256Hash,
-                },
-              },
-              update: {
-                bucketName: env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET,
-                bucketPath,
-                contentType,
-                contentLength,
-              },
-              create: {
-                id: mediaId,
-                projectId,
-                sha256Hash,
-                bucketPath,
-                bucketName: env.LANGFUSE_S3_MEDIA_UPLOAD_BUCKET,
-                contentType,
-                contentLength,
-              },
-            }),
-            observationId
-              ? tx.observationMedia.upsert({
-                  where: {
-                    projectId_traceId_observationId_mediaId_field: {
-                      projectId,
-                      traceId,
-                      observationId,
-                      mediaId,
-                      field,
-                    },
-                  },
-                  update: {},
-                  create: {
-                    projectId,
-                    traceId,
-                    observationId,
-                    mediaId,
-                    field,
-                  },
-                })
-              : tx.traceMedia.upsert({
-                  where: {
-                    projectId_traceId_mediaId_field: {
-                      projectId,
-                      traceId,
-                      mediaId,
-                      field,
-                    },
-                  },
-                  update: {},
-                  create: {
-                    projectId,
-                    traceId,
-                    field,
-                    mediaId,
-                  },
-                }),
-          ]);
-
-          return {
-            mediaId,
-            uploadUrl,
-          };
-        });
+              }),
+        ]);
 
         return {
-          uploadUrl,
           mediaId,
+          uploadUrl,
         };
-      }),
+      });
+
+      return {
+        uploadUrl,
+        mediaId,
+      };
+    },
   }),
 });
 
