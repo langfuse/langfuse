8e54d9c6003f7fb7c3a8424c5662173e418a437a
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2024-11-15 11:11:28 +0100

    chore: remove fk on dataset item for job execution relation

--- packages/shared/prisma/migrations/20241114175010_job_executions_add_observation_dataset_item_cols/migration.sql
+++ packages/shared/prisma/migrations/20241114175010_job_executions_add_observation_dataset_item_cols/migration.sql
@@ -4,6 +4,3 @@ ALTER TABLE "job_executions" DROP CONSTRAINT "job_executions_job_input_trace_id_
 -- AlterTable
 ALTER TABLE "job_executions" ADD COLUMN     "job_input_dataset_item_id" TEXT,
 ADD COLUMN     "job_input_observation_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "job_executions" ADD CONSTRAINT "job_executions_job_input_dataset_item_id_project_id_fkey" FOREIGN KEY ("job_input_dataset_item_id", "project_id") REFERENCES "dataset_items"("id", "project_id") ON DELETE CASCADE ON UPDATE CASCADE;
