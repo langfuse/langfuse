372af0e343a37ef1496766d79c3d7500eb969f72
Max Deichmann <m.deichmann@tum.de>
2024-11-18 17:09:36 +0100

    feat: fetch sessions by id for public API

--- web/src/pages/api/public/sessions/[sessionId].ts
+++ web/src/pages/api/public/sessions/[sessionId].ts
@@ -6,6 +6,8 @@ import {
 import { withMiddlewares } from "@/src/features/public-api/server/withMiddlewares";
 import { createAuthedAPIRoute } from "@/src/features/public-api/server/createAuthedAPIRoute";
 import { LangfuseNotFoundError } from "@langfuse/shared";
+import { measureAndReturnApi } from "@/src/server/utils/checkClickhouseAccess";
+import { getTracesBySessionId } from "@langfuse/shared/src/server";
 
 export default withMiddlewares({
   GET: createAuthedAPIRoute({
@@ -15,28 +17,65 @@ export default withMiddlewares({
     fn: async ({ query, auth }) => {
       const { sessionId } = query;
 
-      const session = await prisma.traceSession.findUnique({
-        where: {
-          id_projectId: {
-            id: sessionId,
-            projectId: auth.scope.projectId,
-          },
+      return await measureAndReturnApi({
+        input: { projectId: auth.scope.projectId, queryClickhouse: true },
+        operation: "scores.countAll",
+        user: null,
+        pgExecution: async () => {
+          const session = await prisma.traceSession.findUnique({
+            where: {
+              id_projectId: {
+                id: sessionId,
+                projectId: auth.scope.projectId,
+              },
+            },
+            select: {
+              id: true,
+              createdAt: true,
+              projectId: true,
+              traces: true,
+            },
+          });
+
+          if (!session) {
+            throw new LangfuseNotFoundError(
+              "Session not found within authorized project",
+            );
+          }
+
+          return session;
         },
-        select: {
-          id: true,
-          createdAt: true,
-          projectId: true,
-          traces: true,
+        clickhouseExecution: async () => {
+          const session = await prisma.traceSession.findUnique({
+            where: {
+              id_projectId: {
+                id: sessionId,
+                projectId: auth.scope.projectId,
+              },
+            },
+            select: {
+              id: true,
+              createdAt: true,
+              projectId: true,
+            },
+          });
+
+          if (!session) {
+            throw new LangfuseNotFoundError(
+              "Session not found within authorized project",
+            );
+          }
+
+          const traces = await getTracesBySessionId(auth.scope.projectId, [
+            sessionId,
+          ]);
+
+          return {
+            ...session,
+            traces,
+          };
         },
       });
-
-      if (!session) {
-        throw new LangfuseNotFoundError(
-          "Session not found within authorized project",
-        );
-      }
-
-      return session;
     },
   }),
 });
