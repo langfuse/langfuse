41ebc87655ec959168d55bb7e7e09741c0b1e835
Max Deichmann <m.deichmann@tum.de>
2024-11-18 23:01:01 +0100

    fix

--- web/src/server/api/routers/observations.ts
+++ web/src/server/api/routers/observations.ts
@@ -12,41 +12,6 @@ import { getObservationById } from "@langfuse/shared/src/server";
 import type Decimal from "decimal.js";
 import { z } from "zod";
 
-export type SingleObservationReturnType = {
-  name: string | null;
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
-  type: ObservationType;
-  traceId: string | null;
-  projectId: string;
-  input: z.infer<typeof jsonSchema> | null;
-  startTime: Date;
-  endTime: Date | null;
-  metadata: z.infer<typeof jsonSchema> | null;
-  parentObservationId: string | null;
-  level: ObservationLevel;
-  statusMessage: string | null;
-  version: string | null;
-  model: string | null;
-  internalModel: string | null;
-  internalModelId: string | null;
-  modelParameters: z.infer<typeof jsonSchema> | null;
-  output: z.infer<typeof jsonSchema> | null;
-  promptTokens: number;
-  completionTokens: number;
-  totalTokens: number;
-  unit: string | null;
-  inputCost: Decimal | null;
-  outputCost: Decimal | null;
-  totalCost: Decimal | null;
-  calculatedInputCost: Decimal | null;
-  calculatedOutputCost: Decimal | null;
-  calculatedTotalCost: Decimal | null;
-  completionStartTime: Date | null;
-  promptId: string | null;
-};
-
 export const observationsRouter = createTRPCRouter({
   byId: protectedGetTraceProcedure
     .input(
