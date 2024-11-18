bb3850d422040116bb4fb09985346586661792a7
Max Deichmann <m.deichmann@tum.de>
2024-11-18 18:40:05 +0100

    chore: add async test setup

--- web/src/__tests__/async/some.servertest.ts
+++ web/src/__tests__/async/some.servertest.ts
@@ -4,7 +4,7 @@ describe("Sample Test Suite", () => {
   });
 
   it("should add two numbers correctly", () => {
-    const sum = (a, b) => a + b;
+    const sum = (a: number, b: number) => a + b;
     expect(sum(1, 2)).toBe(3);
   });
 });
