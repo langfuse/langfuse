6684c4f9f36d4966c27056d3218c4f695c1d0350
Max Deichmann <m.deichmann@tum.de>
2024-11-19 23:38:08 +0100

    fix

--- web/src/__tests__/async/traces-table.servertest.ts
+++ web/src/__tests__/async/traces-table.servertest.ts
@@ -7,10 +7,18 @@ import {
   createObservation,
   createTrace,
 } from "@/src/__tests__/fixtures/tracing-factory";
-import { getTracesTable } from "@langfuse/shared/src/server";
+import {
+  getTracesTable,
+  type TracesAllReturnType,
+  type ObservationRecordInsertType,
+  type TraceRecordInsertType,
+  type TracesTableReturnType,
+} from "@langfuse/shared/src/server";
+import { type FilterState } from "@langfuse/shared";
+import { useQueryFilterState } from "@/src/features/filters/hooks/useFilterState";
 
 describe("Traces table API test", () => {
-  it("should get a correct trace without observation row for the UI", async () => {
+  it("should get a correct trace without observation", async () => {
     const project_id = v4();
     const trace_id = v4();
 
@@ -45,7 +53,7 @@ describe("Traces table API test", () => {
     expect(tableRows[0].scoresAvg).toEqual([]);
   });
 
-  it("should get a correct trace with observations row for the UI", async () => {
+  it("should get a correct trace with observations", async () => {
     const project_id = v4();
     const trace_id = v4();
 
@@ -92,5 +100,163 @@ describe("Traces table API test", () => {
     expect(tableRows[0].level).toBeDefined();
     expect(tableRows[0].observationCount).toBeGreaterThanOrEqual(0);
     expect(tableRows[0].scoresAvg).toEqual([]);
+  });
+
+  type TestCase = {
+    traceInput: Partial<TraceRecordInsertType>;
+    observationInput: Partial<ObservationRecordInsertType>[];
+    filterstate: FilterState;
+    expected: Partial<TracesTableReturnType>[];
+  };
+
+  [
+    {
+      traceInput: {},
+      observationInput: [
+        { cost_details: { total: 100 } },
+        { cost_details: { total: 200 } },
+      ],
+      filterstate: [
+        {
+          column: "totalCost",
+          operator: ">" as const,
+          value: 100000,
+          type: "number" as const,
+        },
+      ],
+      expected: [],
+    },
+    {
+      traceInput: {},
+      observationInput: [
+        { cost_details: { total: 100 } },
+        { cost_details: { total: 200 } },
+      ],
+      filterstate: [
+        {
+          column: "totalCost",
+          operator: "<" as const,
+          value: 100000,
+          type: "number" as const,
+        },
+      ],
+      expected: [
+        {
+          cost_details: { total: 300 },
+        },
+      ],
+    },
+    {
+      traceInput: {},
+      observationInput: [],
+      filterstate: [
+        {
+          column: "id",
+          operator: "=" as const,
+          value: "some-id",
+          type: "string" as const,
+        },
+      ],
+      expected: [],
+    },
+  ].forEach(async (testConfig: TestCase) => {
+    it(`should get a correct trace with filters ${JSON.stringify(testConfig)}`, async () => {
+      const project_id = v4();
+      const trace_id = v4();
+
+      const trace = createTrace({
+        id: trace_id,
+        project_id,
+        ...testConfig.traceInput,
+      });
+      await createTraces([trace]);
+
+      expect(testConfig.observationInput.length).not.toBeGreaterThan(2);
+
+      const obs1 = createObservation({
+        trace_id,
+        project_id,
+        ...(testConfig.observationInput.length > 0
+          ? testConfig.observationInput[0]
+          : {}),
+      });
+      const obs2 = createObservation({
+        trace_id,
+        project_id,
+        ...(testConfig.observationInput.length > 1
+          ? testConfig.observationInput[1]
+          : {}),
+      });
+      await createObservations([obs1, obs2]);
+
+      const tableRows = await getTracesTable(
+        project_id,
+        testConfig.filterstate,
+        undefined,
+        undefined,
+        1,
+        0,
+      );
+
+      expect(tableRows).toHaveLength(testConfig.expected.length);
+      testConfig.expected.forEach((expectedTrace, index) => {
+        if (expectedTrace.id !== undefined) {
+          expect(tableRows[index].id).toEqual(expectedTrace.id);
+        }
+        if (expectedTrace.project_id !== undefined) {
+          expect(tableRows[index].projectId).toEqual(expectedTrace.project_id);
+        }
+        if (expectedTrace.tags !== undefined) {
+          expect(tableRows[index].tags).toEqual(expectedTrace.tags);
+        }
+        if (expectedTrace.name !== undefined) {
+          expect(tableRows[index].name).toEqual(expectedTrace.name);
+        }
+        if (expectedTrace.bookmarked !== undefined) {
+          expect(tableRows[index].bookmarked).toEqual(expectedTrace.bookmarked);
+        }
+        if (expectedTrace.release !== undefined) {
+          expect(tableRows[index].release).toEqual(expectedTrace.release);
+        }
+        if (expectedTrace.version !== undefined) {
+          expect(tableRows[index].version).toEqual(expectedTrace.version);
+        }
+        if (expectedTrace.user_id !== undefined) {
+          expect(tableRows[index].userId).toEqual(expectedTrace.user_id);
+        }
+        if (expectedTrace.session_id !== undefined) {
+          expect(tableRows[index].sessionId).toEqual(expectedTrace.session_id);
+        }
+        if (expectedTrace.public !== undefined) {
+          expect(tableRows[index].public).toEqual(expectedTrace.public);
+        }
+        if (expectedTrace.latency_milliseconds !== undefined) {
+          expect(tableRows[index].latencyMilliseconds).toEqual(
+            expectedTrace.latency_milliseconds,
+          );
+        }
+        if (expectedTrace.usage_details !== undefined) {
+          expect(tableRows[index].usageDetails).toEqual(
+            expectedTrace.usage_details,
+          );
+        }
+        if (expectedTrace.cost_details !== undefined) {
+          expect(tableRows[index].costDetails).toEqual(
+            expectedTrace.cost_details,
+          );
+        }
+        if (expectedTrace.level !== undefined) {
+          expect(tableRows[index].level).toEqual(expectedTrace.level);
+        }
+        if (expectedTrace.observation_count !== undefined) {
+          expect(tableRows[index].observationCount).toEqual(
+            expectedTrace.observation_count,
+          );
+        }
+        if (expectedTrace.scores_avg !== undefined) {
+          expect(tableRows[index].scoresAvg).toEqual(expectedTrace.scores_avg);
+        }
+      });
+    });
   });
 });
