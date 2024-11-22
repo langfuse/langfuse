18abb2ecbf135041dfd3f2b9376b55f662320723
steffen911 <steffen@langfuse.com>
2024-11-22 16:08:58 +0100

    adjust timeouts

--- worker/src/__tests__/evalService.test.ts
+++ worker/src/__tests__/evalService.test.ts
@@ -594,7 +594,7 @@ describe("eval service tests", () => {
       expect(scores[0].trace_id).toBe(traceId);
       expect(scores[0].comment).not.toBeNull();
       expect(scores[0].project_id).toBe("7a88fb47-b4e2-43b8-a06c-a5ce950dc53a");
-    }, 10_000);
+    }, 20_000);
 
     test("fails to eval without llm api key", async () => {
       await pruneDatabase();
@@ -910,7 +910,7 @@ describe("eval service tests", () => {
           },
         );
       });
-    }, 10_000);
+    }, 20_000);
   });
 
   describe("test variable extraction", () => {
