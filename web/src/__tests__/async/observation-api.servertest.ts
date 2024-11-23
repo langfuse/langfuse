f5c598dabf23c1dc832d606b39626d443a6e6a1b
Max Deichmann <m.deichmann@tum.de>
2024-11-24 00:38:02 +0100

    feat

--- web/src/__tests__/async/observation-api.servertest.ts
+++ web/src/__tests__/async/observation-api.servertest.ts
@@ -4,46 +4,45 @@ import { makeZodVerifiedAPICall } from "@/src/__tests__/test-utils";
 import { GetObservationV1Response } from "@/src/features/public-api/types/observations";
 import { v4 } from "uuid";
 import { v4 as uuidv4 } from "uuid";
-import { prisma } from "@langfuse/shared/src/db";
 import { GetObservationsV1Response } from "@/src/features/public-api/types/observations";
 
 const projectId = "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a";
 
-// describe("/api/public/observations API Endpoint", () => {
-//   describe("GET /api/public/observations/:id", () => {
-//     it("should GET an observation", async () => {
-//       const observationId = v4();
-//       const traceId = v4();
+describe("/api/public/observations API Endpoint", () => {
+  describe("GET /api/public/observations/:id", () => {
+    it("should GET an observation", async () => {
+      const observationId = v4();
+      const traceId = v4();
 
-//       const observation = createObservationObject({
-//         id: observationId,
-//         project_id: projectId,
-//         trace_id: traceId,
-//         internal_model_id: "b9854a5c92dc496b997d99d21",
-//         provided_model_name: "gpt-4o-2024-05-13",
-//         input: "input",
-//         output: "output",
-//       });
+      const observation = createObservationObject({
+        id: observationId,
+        project_id: projectId,
+        trace_id: traceId,
+        internal_model_id: "b9854a5c92dc496b997d99d21",
+        provided_model_name: "gpt-4o-2024-05-13",
+        input: "input",
+        output: "output",
+      });
 
-//       await createObservationsInClickhouse([observation]);
+      await createObservationsInClickhouse([observation]);
 
-//       const getEventRes = await makeZodVerifiedAPICall(
-//         GetObservationV1Response,
-//         "GET",
-//         "/api/public/observations/" + observationId,
-//       );
-//       expect(getEventRes.body).toMatchObject({
-//         id: observationId,
-//         traceId: traceId,
-//         type: observation.type,
-//         modelId: observation.internal_model_id,
-//         inputPrice: 0.000005,
-//         input: observation.input,
-//         output: observation.output,
-//       });
-//     });
-//   });
-// });
+      const getEventRes = await makeZodVerifiedAPICall(
+        GetObservationV1Response,
+        "GET",
+        "/api/public/observations/" + observationId,
+      );
+      expect(getEventRes.body).toMatchObject({
+        id: observationId,
+        traceId: traceId,
+        type: observation.type,
+        modelId: observation.internal_model_id,
+        inputPrice: 0.000005,
+        input: observation.input,
+        output: observation.output,
+      });
+    });
+  });
+});
 
 describe("/api/public/observations API Endpoint", () => {
   it("should fetch all observations", async () => {
