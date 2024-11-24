c7415723ed45285013d674f245cad71d9a9bb308
steffen911 <steffen@langfuse.com>
2024-11-24 11:47:42 +0100

    chore: error handling

--- worker/src/queues/traceDelete.ts
+++ worker/src/queues/traceDelete.ts
@@ -3,8 +3,10 @@ import {
   deleteObservationsByTraceIds,
   deleteScoresByTraceIds,
   deleteTraces,
+  logger,
   QueueName,
   TQueueJobTypes,
+  traceException,
 } from "@langfuse/shared/src/server";
 import { prisma } from "@langfuse/shared/src/db";
 import { env } from "../env";
@@ -13,47 +15,65 @@ export const traceDeleteProcessor: Processor = async (
   job: Job<TQueueJobTypes[QueueName.TraceDelete]>,
 ): Promise<void> => {
   const { traceId, projectId } = job.data.payload;
-  await prisma.$transaction([
-    prisma.trace.deleteMany({
-      where: {
-        id: traceId,
-        projectId: projectId,
-      },
-    }),
-    prisma.observation.deleteMany({
-      where: {
-        traceId,
-        projectId: projectId,
-      },
-    }),
-    prisma.score.deleteMany({
-      where: {
-        traceId,
-        projectId: projectId,
-      },
-    }),
-    // given traces and observations live in ClickHouse we cannot enforce a fk relationship and onDelete: setNull
-    prisma.jobExecution.updateMany({
-      where: {
-        jobInputTraceId: traceId,
-        projectId: projectId,
-      },
-      data: {
-        jobInputTraceId: {
-          set: null,
+  try {
+    await prisma.$transaction([
+      prisma.trace.deleteMany({
+        where: {
+          id: traceId,
+          projectId: projectId,
         },
-        jobInputObservationId: {
-          set: null,
+      }),
+      prisma.observation.deleteMany({
+        where: {
+          traceId,
+          projectId: projectId,
         },
-      },
-    }),
-  ]);
+      }),
+      prisma.score.deleteMany({
+        where: {
+          traceId,
+          projectId: projectId,
+        },
+      }),
+      // given traces and observations live in ClickHouse we cannot enforce a fk relationship and onDelete: setNull
+      prisma.jobExecution.updateMany({
+        where: {
+          jobInputTraceId: traceId,
+          projectId: projectId,
+        },
+        data: {
+          jobInputTraceId: {
+            set: null,
+          },
+          jobInputObservationId: {
+            set: null,
+          },
+        },
+      }),
+    ]);
+  } catch (e) {
+    logger.error(
+      `Error deleting trace ${traceId} in project ${projectId} from Postgres`,
+      e,
+    );
+    traceException(e);
+    throw e;
+  }
 
   if (env.CLICKHOUSE_URL) {
-    await Promise.all([
-      deleteTraces(projectId, [traceId]),
-      deleteObservationsByTraceIds(projectId, [traceId]),
-      deleteScoresByTraceIds(projectId, [traceId]),
-    ]);
+    try {
+      await Promise.all([
+        deleteTraces(projectId, [traceId]),
+        deleteObservationsByTraceIds(projectId, [traceId]),
+        deleteScoresByTraceIds(projectId, [traceId]),
+      ]);
+    } catch (e) {
+      logger.error(
+        `Error deleting trace ${traceId} in project ${projectId} from Clickhouse`,
+        e,
+      );
+      traceException(e);
+      throw e;
+    }
   }
 };
