701139582f558db770d88b14b4fdfd3b9895b71a
Marc Klingen <git@marcklingen.com>
2024-12-03 01:22:11 +0100

    [cherry-pick][2.93.x] feat: optionally disable csp headers via `LANGFUSE_CSP_DISABLE=true` (#4532)

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -97,34 +97,39 @@ const nextConfig = {
           ...(env.SENTRY_CSP_REPORT_URI ? [reportToHeader] : []),
         ],
       },
-      {
-        source: "/:path((?!api).*)*",
-        headers: [
+      // CSP header
+      ...(env.LANGFUSE_CSP_DISABLE !== "true"
+        ? [
           {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
+            source: "/:path((?!api).*)*",
+            headers: [
+              {
+                key: "Content-Security-Policy",
+                value: cspHeader.replace(/\n/g, ""),
+              },
+            ],
           },
-        ],
-      },
+        ]
+        : []),
       // Required to check authentication status from langfuse.com
       ...(env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION !== undefined
         ? [
-            {
-              source: "/api/auth/session",
-              headers: [
-                {
-                  key: "Access-Control-Allow-Origin",
-                  value: "https://langfuse.com",
-                },
-                { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-                {
-                  key: "Access-Control-Allow-Headers",
-                  value: "Content-Type, Authorization",
-                },
-              ],
-            },
-          ]
+          {
+            source: "/api/auth/session",
+            headers: [
+              {
+                key: "Access-Control-Allow-Origin",
+                value: "https://langfuse.com",
+              },
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+              {
+                key: "Access-Control-Allow-Headers",
+                value: "Content-Type, Authorization",
+              },
+            ],
+          },
+        ]
         : []),
       // all files in /public/generated are public and can be accessed from any origin, e.g. to render an API reference based on our openapi schema
       {
