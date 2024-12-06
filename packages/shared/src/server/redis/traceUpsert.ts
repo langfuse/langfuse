438281e4000bcc9f9727b2ac3ae14b9d45468a99
Max Deichmann <m.deichmann@tum.de>
2024-12-06 18:03:05 +0100

    fix: increase trace upsert delay

--- packages/shared/src/server/redis/traceUpsert.ts
+++ packages/shared/src/server/redis/traceUpsert.ts
@@ -26,7 +26,7 @@ export class TraceUpsertQueue {
               removeOnComplete: 100, // Important: If not true, new jobs for that ID would be ignored as jobs in the complete set are still considered as part of the queue
               removeOnFail: 100_000,
               attempts: 5,
-              delay: 15_000, // 10 seconds
+              delay: 15_000, // 15 seconds
               backoff: {
                 type: "exponential",
                 delay: 5000,
