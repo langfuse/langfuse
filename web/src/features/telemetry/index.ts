1bc33c7b9239c2c904895cf61412560facff4803
Marc Klingen <git@marcklingen.com>
2025-01-12 09:57:46 +0100

    fix: telemetry error "invalid input syntax for type interval" (#4991)

--- web/src/features/telemetry/index.ts
+++ web/src/features/telemetry/index.ts
@@ -11,7 +11,7 @@ import {
 import { env } from "@/src/env.mjs";
 
 // Interval between jobs in minutes
-const JOB_INTERVAL_MINUTES = Prisma.raw("24 * 60"); // 24 hours
+const JOB_INTERVAL_MINUTES = Prisma.raw("720"); // 12 hours
 
 // Timeout for job in minutes, if job is not finished in this time, it will be retried
 const JOB_TIMEOUT_MINUTES = Prisma.raw("10"); // 10 minutes
