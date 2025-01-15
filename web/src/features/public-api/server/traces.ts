f71d28900a3d6daf93ba50bc826b1d87d46ae63e
Max Deichmann <m.deichmann@tum.de>
2025-01-15 10:25:28 +0100

    perf: use streaming for observation and trace list endpoints (#5038)
    push

--- web/src/features/public-api/server/traces.ts
+++ web/src/features/public-api/server/traces.ts
@@ -6,6 +6,7 @@ import {
   orderByToClickhouseSql,
   type DateTimeFilter,
   parseClickhouseUTCDateTimeFormat,
+  queryClickhouseStream,
 } from "@langfuse/shared/src/server";
 import {
   convertRecordToJsonSchema,
@@ -108,7 +109,7 @@ export const generateTracesForPublicApi = async (
     ${props.limit !== undefined && props.page !== undefined ? `LIMIT {limit: Int32} OFFSET {offset: Int32}` : ""}
   `;
 
-  const result = await queryClickhouse<
+  const asyncGenerator = queryClickhouseStream<
     Trace & {
       observations: string[];
       scores: string[];
@@ -132,6 +133,19 @@ export const generateTracesForPublicApi = async (
         : {}),
     },
   });
+
+  const result: Array<
+    Trace & {
+      observations: string[];
+      scores: string[];
+      totalCost: number;
+      latency: number;
+      htmlPath: string;
+    }
+  > = [];
+  for await (const row of asyncGenerator) {
+    result.push(row);
+  }
 
   return result.map((trace) => ({
     ...trace,
