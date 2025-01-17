8ac4176aafa9f9cfe848ee770742b1f0739c3f5d
marliessophie <74332854+marliessophie@users.noreply.github.com>
2025-01-17 14:51:25 +0100

    feat(datasets): upload dataset via csv import (#4656)
    * fixup: add csv import for datasets
    
    * feat: include DnD logic
    
    * add: fastest-levenshtein dep
    
    * fixup: upload & parse .csv file
    
    * fixup: uploading file
    
    * fix: allow uploading .csv file
    
    * chore: add csv-parse
    
    * eslint
    
    * chore: include project id in temp storage logic
    
    * chore: access checks
    
    * push
    
    * chore: use s3 signed url to upload .csv file temporarily
    
    fixup: env credentials
    
    * deps: fix circular dep
    
    * fixup
    
    * create import bucket for uploads
    
    * push
    
    * push
    
    * push
    
    * remove s3 config for import
    
    * remove server side processing
    
    * fix: metadata display for items
    
    * style: fix css styling and add progress bar
    
    * push
    
    * push
    
    * push
    
    * refactor: extract helper function
    
    * wrap in try catch block
    
    * fix: button logic in case of no access
    
    * analytics: add posthog events

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -128,22 +128,22 @@ const nextConfig = {
       // Required to check authentication status from langfuse.com
       ...(env.NEXT_PUBLIC_LANGFUSE_CLOUD_REGION !== undefined
         ? [
-          {
-            source: "/api/auth/session",
-            headers: [
-              {
-                key: "Access-Control-Allow-Origin",
-                value: "https://langfuse.com",
-              },
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-              {
-                key: "Access-Control-Allow-Headers",
-                value: "Content-Type, Authorization",
-              },
-            ],
-          },
-        ]
+            {
+              source: "/api/auth/session",
+              headers: [
+                {
+                  key: "Access-Control-Allow-Origin",
+                  value: "https://langfuse.com",
+                },
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+                {
+                  key: "Access-Control-Allow-Headers",
+                  value: "Content-Type, Authorization",
+                },
+              ],
+            },
+          ]
         : []),
       // all files in /public/generated are public and can be accessed from any origin, e.g. to render an API reference based on our openapi schema
       {
