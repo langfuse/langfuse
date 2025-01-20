9e30a0ebe2d7923ce8ffb0eea281e7a73d9d52ae
steffen911 <steffen@langfuse.com>
2025-01-20 18:42:51 +0100

    chore: linting

--- web/src/server/api/routers/generations/getAllQueries.ts
+++ web/src/server/api/routers/generations/getAllQueries.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { type z } from "zod";
 import { protectedProjectProcedure } from "@/src/server/api/trpc";
 import { paginationZod } from "@langfuse/shared";
 import { GenerationTableOptions } from "./utils/GenerationTableOptions";
