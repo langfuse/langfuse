a0b2db2055bad6a58bfe8ba2a446303b0a1170a4
steffen911 <steffen@langfuse.com>
2025-01-21 10:36:43 +0100

    chore: lint

--- web/src/server/api/routers/generations/db/getAllGenerationsSqlQuery.ts
+++ web/src/server/api/routers/generations/db/getAllGenerationsSqlQuery.ts
@@ -1,5 +1,5 @@
 import { aggregateScores } from "@/src/features/scores/lib/aggregateScores";
-import { filterAndValidateDbScoreList, type Score } from "@langfuse/shared";
+import { filterAndValidateDbScoreList } from "@langfuse/shared";
 import { type GetAllGenerationsInput } from "../getAllQueries";
 import {
   getObservationsTableWithModelData,
