525492ddf33f648c85bfdf4ae4a91aa77b542f47
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-01-24 14:58:15 +0100

    chore(prompts-api): add commit message to prompts v2 api

--- web/src/__tests__/prompts.v2.servertest.ts
+++ web/src/__tests__/prompts.v2.servertest.ts
@@ -420,6 +420,7 @@ describe("/api/public/v2/prompts API Endpoint", () => {
         prompt: chatMessages,
         type: "chat",
         labels: ["production"],
+        commitMessage: "chore: setup initial prompt",
       });
 
       expect(response.status).toBe(201);
@@ -439,6 +440,7 @@ describe("/api/public/v2/prompts API Endpoint", () => {
       expect(validatedPrompt.labels).toEqual(["production", "latest"]);
       expect(validatedPrompt.createdBy).toBe("API");
       expect(validatedPrompt.config).toEqual({});
+      expect(validatedPrompt.commitMessage).toBe("chore: setup initial prompt");
     });
 
     it("should fail if chat prompt has string prompt", async () => {
