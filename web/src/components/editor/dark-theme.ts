467bab699bf4405ce3d25cb8cea80ac76b43bc61
Marc Klingen <git@marcklingen.com>
2025-01-30 22:49:03 +0100

    push

--- web/src/components/editor/dark-theme.ts
+++ web/src/components/editor/dark-theme.ts
@@ -1,27 +1,20 @@
+import { defaultSettingsBothThemes } from "@/src/components/editor/shared-theme";
 import { tags as t } from "@lezer/highlight";
 import { createTheme, type CreateThemeOptions } from "@uiw/codemirror-themes";
+import { bothThemeStyles } from "@/src/components/editor/shared-theme";
 
 export const defaultSettingsDarkTheme: CreateThemeOptions["settings"] = {
-  background: "#1a1b26",
-  foreground: "#787c99",
-  caret: "#c0caf5",
-  selection: "#515c7e40",
-  selectionMatch: "#16161e",
-  gutterBackground: "#1a1b26",
-  gutterForeground: "#787c99",
-  gutterBorder: "transparent",
-  lineHighlight: "#474b6611",
+  ...defaultSettingsBothThemes,
 };
 
 export const darkThemeStyle: CreateThemeOptions["styles"] = [
+  ...bothThemeStyles,
   { tag: t.keyword, color: "#bb9af7" },
-  { tag: [t.name, t.deleted, t.character, t.macroName], color: "#c0caf5" },
-  { tag: [t.propertyName], color: "#7aa2f7" },
   {
     tag: [t.processingInstruction, t.string, t.inserted, t.special(t.string)],
     color: "#9ece6a",
   },
-  { tag: [t.function(t.variableName), t.labelName], color: "#7aa2f7" },
+  { tag: [t.labelName], color: "#7aa2f7" },
   { tag: [t.color, t.constant(t.name), t.standard(t.name)], color: "#bb9af7" },
   { tag: [t.definition(t.name), t.separator], color: "#c0caf5" },
   { tag: [t.className], color: "#c0caf5" },
@@ -33,17 +26,17 @@ export const darkThemeStyle: CreateThemeOptions["styles"] = [
   { tag: [t.operator, t.operatorKeyword], color: "#bb9af7" },
   { tag: [t.url, t.escape, t.regexp, t.link], color: "#b4f9f8" },
   { tag: [t.meta, t.comment], color: "#444b6a" },
-  { tag: t.strong, fontWeight: "bold" },
-  { tag: t.emphasis, fontStyle: "italic" },
-  { tag: t.link, textDecoration: "underline" },
   { tag: t.heading, fontWeight: "bold", color: "#89ddff" },
-  { tag: [t.atom, t.bool, t.special(t.variableName)], color: "#c0caf5" },
-  { tag: t.invalid, color: "#ff5370" },
+  { tag: [t.atom, t.bool], color: "#c0caf5" },
   { tag: t.strikethrough, textDecoration: "line-through" },
 ];
 
 export const darkThemeInit = (options?: Partial<CreateThemeOptions>) => {
-  const { theme = "dark", settings = {}, styles = [] } = options || {};
+  const {
+    theme = "light", // needs to be light, otherwise gutter border is not rendered
+    settings = {},
+    styles = [],
+  } = options || {};
   return createTheme({
     theme: theme,
     settings: {
--- web/src/components/editor/light-theme.ts
+++ web/src/components/editor/light-theme.ts
@@ -1,21 +1,14 @@
+import { defaultSettingsBothThemes } from "@/src/components/editor/shared-theme";
 import { tags as t } from "@lezer/highlight";
 import { createTheme, type CreateThemeOptions } from "@uiw/codemirror-themes";
+import { bothThemeStyles } from "@/src/components/editor/shared-theme";
 
 export const defaultSettingsLightTheme: CreateThemeOptions["settings"] = {
-  background: "#ffffff",
-  foreground: "#383a42",
-  caret: "#000",
-  selection: "#add6ff",
-  selectionMatch: "#a8ac94",
-  lineHighlight: "#99999926",
-  gutterBackground: "#fff",
-  gutterForeground: "#237893",
-  gutterActiveForeground: "#0b216f",
-  fontFamily:
-    'Menlo, Monaco, Consolas, "Andale Mono", "Ubuntu Mono", "Courier New", monospace',
+  ...defaultSettingsBothThemes,
 };
 
 export const lightThemeStyle: CreateThemeOptions["styles"] = [
+  ...bothThemeStyles,
   {
     tag: [
       t.keyword,
@@ -33,19 +26,6 @@ export const lightThemeStyle: CreateThemeOptions["styles"] = [
     color: "#0000ff",
   },
   { tag: [t.moduleKeyword, t.controlKeyword], color: "#af00db" },
-  {
-    tag: [
-      t.name,
-      t.deleted,
-      t.character,
-      t.macroName,
-      t.propertyName,
-      t.variableName,
-      t.labelName,
-      t.definition(t.name),
-    ],
-    color: "#0070c1",
-  },
   { tag: t.heading, fontWeight: "bold", color: "#0070c1" },
   {
     tag: [
@@ -75,12 +55,9 @@ export const lightThemeStyle: CreateThemeOptions["styles"] = [
     color: "#a31515",
   },
   { tag: [t.angleBracket], color: "#383a42" },
-  { tag: t.strong, fontWeight: "bold" },
-  { tag: t.emphasis, fontStyle: "italic" },
   { tag: t.strikethrough, textDecoration: "line-through" },
   { tag: [t.meta, t.comment], color: "#008000" },
   { tag: t.link, color: "#4078f2", textDecoration: "underline" },
-  { tag: t.invalid, color: "#e45649" },
 ];
 
 export function lightThemeInit(options?: Partial<CreateThemeOptions>) {
