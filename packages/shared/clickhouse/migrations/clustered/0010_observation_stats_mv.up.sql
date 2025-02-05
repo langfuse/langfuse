b6b39604c797c722e842e1c3fb7f53ed3101e79e
steffen911 <steffen@langfuse.com>
2025-02-05 14:11:29 +0100

    chore: change column order

--- packages/shared/clickhouse/migrations/clustered/0010_observation_stats_mv.up.sql
+++ packages/shared/clickhouse/migrations/clustered/0010_observation_stats_mv.up.sql
@@ -1,11 +1,11 @@
 CREATE MATERIALIZED VIEW observation_stats_mv ON CLUSTER default TO observation_stats AS
 SELECT
-    trace_id,
     project_id,
+    trace_id,
     uniqState(id) as count,
     minSimpleState(start_time) as min_start_time,
     maxSimpleState(start_time) as max_start_time,
     maxSimpleState(end_time) as max_end_time,
     groupUniqArrayArraySimpleState([level]) as unique_levels
 FROM observations
-GROUP BY trace_id, project_id;
+GROUP BY project_id, trace_id;
