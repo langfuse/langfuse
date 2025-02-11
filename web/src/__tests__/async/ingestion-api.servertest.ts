268bf9ebfefdd45af962a38510d8206146982f84
steffen911 <steffen@langfuse.com>
2025-02-11 16:17:04 +0100

    chore: lint

--- web/src/__tests__/async/ingestion-api.servertest.ts
+++ web/src/__tests__/async/ingestion-api.servertest.ts
@@ -8,7 +8,6 @@ import {
   getTraceById,
 } from "@langfuse/shared/src/server";
 import { v4 } from "uuid";
-import { prisma } from "@langfuse/shared/src/db";
 
 const projectId = "7a88fb47-b4e2-43b8-a06c-a5ce950dc53a";
 
