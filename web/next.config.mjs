40f78494441c9c7ceb14cbcdb35cff92cfdc17c9
Max Deichmann <m.deichmann@tum.de>
2025-02-12 17:44:00 +0100

    fix: disable sentry (#5505)
    * fix: disable sentry
    
    * chore: remove usages of sentry
    
    * dep: remove sentry dependency
    
    * Revert "dep: remove sentry dependency"
    
    This reverts commit 63fb207fce4fafc17993cc6be9905db3b4e5713c.
    
    * dep: remove sentry dependency
    
    * chore: lint
    
    ---------
    
    Co-authored-by: Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,7 +3,7 @@
  * for Docker builds.
  */
 await import("./src/env.mjs");
-import { withSentryConfig } from "@sentry/nextjs";
+// import { withSentryConfig } from "@sentry/nextjs";
 import { env } from "./src/env.mjs";
 
 /**
@@ -173,44 +173,45 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+// export default withSentryConfig(nextConfig, {
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
+//   org: process.env.SENTRY_ORG,
+//   project: process.env.SENTRY_PROJECT,
 
-  authToken: env.SENTRY_AUTH_TOKEN,
+//   authToken: env.SENTRY_AUTH_TOKEN,
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+//   // Only print logs for uploading source maps in CI
+//   silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+//   // For all available options, see:
+//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+//   // Upload a larger set of source maps for prettier stack traces (increases build time)
+//   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
+//   // Automatically annotate React components to show their full name in breadcrumbs and session replay
+//   reactComponentAnnotation: {
+//     enabled: true,
+//   },
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/api/monitoring-tunnel",
+//   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+//   // This can increase your server load as well as your hosting bill.
+//   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+//   // side errors will fail.
+//   // tunnelRoute: "/api/monitoring-tunnel",
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+//   // Hides source maps from generated client bundles
+//   hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+//   // Automatically tree-shake Sentry logger statements to reduce bundle size
+//   disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false,
-});
+//   // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+//   // See the following for more information:
+//   // https://docs.sentry.io/product/crons/
+//   // https://vercel.com/docs/cron-jobs
+//   automaticVercelMonitors: false,
+// });
+export default nextConfig;
