65b562843211ed2708048f2cf50d15979b899b49
steffen911 <steffen@langfuse.com>
2025-02-18 14:50:53 +0100

    chore: increase ingestion delay to 10s to test possible worker deduplication

--- packages/shared/src/server/ingestion/processEventBatch.ts
+++ packages/shared/src/server/ingestion/processEventBatch.ts
@@ -66,10 +66,10 @@ const getDelay = (delay: number | null) => {
     return env.LANGFUSE_INGESTION_QUEUE_DELAY_MS;
   }
 
-  // Use 5s here to avoid duplicate processing on the worker. If the ingestion delay is set to a lower value,
+  // Use 10s here to avoid duplicate processing on the worker. If the ingestion delay is set to a lower value,
   // we use this instead.
   // Values should be revisited based on a cost/performance trade-off.
-  return Math.min(5000, env.LANGFUSE_INGESTION_QUEUE_DELAY_MS);
+  return Math.min(10000, env.LANGFUSE_INGESTION_QUEUE_DELAY_MS);
 };
 
 /**
