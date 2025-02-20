a152bae7958fa9ad882386097b1e8d59b0b9ae84
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-02-20 09:53:31 +0100

    chore: fix eslint config

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -3,6 +3,6 @@ module.exports = {
   extends: ["@repo/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: [require.resolve("./tsconfig.json")],
   },
 };
