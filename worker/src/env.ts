1c635da53815efacb7e073f1d59e65c3c0979340
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-03-06 08:47:15 +0100

    feat(batch-exports): increase row limit to 1.5M

--- worker/src/env.ts
+++ worker/src/env.ts
@@ -40,7 +40,7 @@ const EnvSchema = z.object({
     .enum(["true", "false"])
     .default("false"),
 
-  BATCH_EXPORT_ROW_LIMIT: z.coerce.number().positive().default(1_000_000),
+  BATCH_EXPORT_ROW_LIMIT: z.coerce.number().positive().default(1_500_000),
   BATCH_EXPORT_DOWNLOAD_LINK_EXPIRATION_HOURS: z.coerce
     .number()
     .positive()
