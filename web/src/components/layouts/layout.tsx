313a1c0d8a885a04974399f850f7f9aee58fd714
David Alexander Pfeiffer <20278364+davidmc971@users.noreply.github.com>
2025-03-12 16:46:50 +0100

    fix: (workaround) evals page overflows when rendering right hand side metadata bar
    This is more of a workaround, we should probably disable the overflow on the main wrapper div in layout.tsx and enable it only where needed - or find out why the overflow inside the metadata sidebar trigggers the scrollbar to pop up - some info on what I found:
    
    - The body and other divs close to the root do not increase their size.
    - The navigation sidebar does increase its vertical height, but limiting that does not resolve the main scrollbar.
    - Limiting specifically the wrapper div in layout.tsx with overflow-hidden does resolve this, but globally disabling it requires most pages to be refactored.

--- web/src/components/layouts/layout.tsx
+++ web/src/components/layouts/layout.tsx
@@ -16,6 +16,8 @@ import { hasOrganizationAccess } from "@/src/features/rbac/utils/checkOrganizati
 import { SidebarInset, SidebarProvider } from "@/src/components/ui/sidebar";
 import { AppSidebar } from "@/src/components/nav/app-sidebar";
 import { CommandMenu } from "@/src/features/command-k-menu/CommandMenu";
+import { useGlobalOverflowSuppressor } from "@/src/features/ui-global-overflow-suppress/global-overflow-suppressor-provider";
+import { cn } from "@/src/utils/tailwind";
 
 const signOutUser = async () => {
   sessionStorage.clear();
@@ -207,6 +209,8 @@ export default function Layout(props: PropsWithChildren) {
 
   const activePathName = navigation.find(({ isActive }) => isActive)?.title;
 
+  const { suppress } = useGlobalOverflowSuppressor();
+
   if (session.status === "loading") return <Spinner message="Loading" />;
 
   // If the user has a token, but does not exist in the database, sign them out
@@ -297,7 +301,7 @@ export default function Layout(props: PropsWithChildren) {
           href={`${env.NEXT_PUBLIC_BASE_PATH ?? ""}/favicon-16x16.png`}
         />
       </Head>
-      <div>
+      <div className={cn({ "overflow-hidden": suppress })}>
         <SidebarProvider>
           <AppSidebar
             navItems={topNavigation}
