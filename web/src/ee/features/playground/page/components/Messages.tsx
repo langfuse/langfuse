de67dbf803e180a648de625c9f223e55c35dc65b
Toby Drane <tobydrane@gmail.com>
2025-03-12 14:48:19 +0000

    Display message for atla model in playground

--- web/src/ee/features/playground/page/components/Messages.tsx
+++ web/src/ee/features/playground/page/components/Messages.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "@/src/components/ui/button";
 import { usePlaygroundContext } from "@/src/ee/features/playground/page/context";
 
@@ -9,6 +10,13 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/src/components/ui/resizable";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/src/components/ui/tooltip";
+import { LLMAdapter } from "@langfuse/shared";
 
 export const Messages: React.FC<MessagesContext> = (props) => {
   return (
@@ -32,17 +40,34 @@ export const Messages: React.FC<MessagesContext> = (props) => {
 };
 
 const SubmitButton = () => {
-  const { handleSubmit, isStreaming } = usePlaygroundContext();
+  const { handleSubmit, isStreaming, modelParams } = usePlaygroundContext();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Button
-      className="flex-0"
-      onClick={() => {
-        handleSubmit().catch((err) => console.error(err));
-      }}
-      loading={isStreaming}
-    >
-      <p>Submit ({"\u2318"} + Enter)</p>
-    </Button>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip
+        open={isOpen && modelParams.adapter.value === LLMAdapter.Atla}
+        onOpenChange={setIsOpen}
+      >
+        <TooltipTrigger>
+          <Button
+            className="flex-0 w-full"
+            disabled={modelParams.adapter.value === LLMAdapter.Atla}
+            onClick={() => {
+              handleSubmit().catch((err) => console.error(err));
+            }}
+            loading={isStreaming}
+          >
+            <p>Submit ({"\u2318"} + Enter)</p>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="m- m-6 w-[24rem]">
+          <p>
+            Atla models are trained to be evaluation models and are not
+            compatiable with chat completions.
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
