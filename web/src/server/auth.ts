9fc22138ff95c82de3c39c60153fe5640aa7a8fa
Andres Carrillo <42779963+andresC98@users.noreply.github.com>
2025-03-13 15:42:00 +0000

    fix(auth): consider case for Gitlab SSO self-hosted URL (#5984)

--- web/src/server/auth.ts
+++ web/src/server/auth.ts
@@ -296,6 +296,12 @@ if (env.AUTH_GITLAB_CLIENT_ID && env.AUTH_GITLAB_CLIENT_SECRET)
       client: {
         token_endpoint_auth_method: env.AUTH_GITLAB_CLIENT_AUTH_METHOD,
       },
+      authorization: {
+        url: `${env.AUTH_GITLAB_URL || 'https://gitlab.com'}/oauth/authorize`,
+        params: { scope: 'read_user' },
+      },
+      token: `${env.AUTH_GITLAB_URL || 'https://gitlab.com'}/oauth/token`,
+      userinfo: `${env.AUTH_GITLAB_URL || 'https://gitlab.com'}/api/v4/user`,
       checks: env.AUTH_GITLAB_CHECKS,
     }),
   );
