d916c4a83428360430a891a164f2a04248af10d4
David Alexander Pfeiffer <20278364+davidmc971@users.noreply.github.com>
2025-03-18 20:13:38 +0100

    fix(ui): For Checkbox component we stop event propagation on CheckboxPrimitive.Root such that our DataTable on row click events do not trigger when selecting a row; should not have unwanted side effects as we do not expect a checkbox to propagate a click

--- web/src/components/ui/checkbox.tsx
+++ web/src/components/ui/checkbox.tsx
@@ -16,6 +16,9 @@ const Checkbox = React.forwardRef<
       "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className,
     )}
+    onClick={(e) => {
+      e.stopPropagation();
+    }}
     {...props}
   >
     <CheckboxPrimitive.Indicator
