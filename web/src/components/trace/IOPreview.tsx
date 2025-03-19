f91768b7d681094c7e1b7f314f532d8d7db648b8
David Alexander Pfeiffer <20278364+davidmc971@users.noreply.github.com>
2025-03-19 10:28:05 +0100

    cleaner falsy check for rendering empty string quotation marks

--- web/src/components/trace/IOPreview.tsx
+++ web/src/components/trace/IOPreview.tsx
@@ -261,7 +261,7 @@ export const OpenAiMessageView: React.FC<{
                   (shouldRenderMarkdown ? (
                     <MarkdownJsonView
                       title={message.name ?? message.role}
-                      content={!message.content ? '""' : message.content}
+                      content={message.content || '""'}
                       className={cn(!!message.json && "rounded-b-none")}
                       customCodeHeaderClassName={cn(
                         message.role === "assistant" && "bg-secondary",
