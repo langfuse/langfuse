7007a46b190900ec809004591f62e7e5133d70fc
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-03-19 12:10:55 +0100

    Revert "Update web/src/features/public-api/components/CreateLLMApiKeyForm.tsx"
    This reverts commit efb2d3b359a85680224876481421d31bfbd44fd3.

--- web/src/features/public-api/components/CreateLLMApiKeyForm.tsx
+++ web/src/features/public-api/components/CreateLLMApiKeyForm.tsx
@@ -105,7 +105,7 @@ export function CreateLLMApiKeyForm({
       case LLMAdapter.Anthropic:
         return customization?.defaultBaseUrlAnthropic ?? "";
       case LLMAdapter.Atla:
-        return customization?.defaultBaseUrlAtla ?? "";
+        return "https://api.atla-ai.com/v1/integrations/langfuse";
       default:
         return "";
     }
