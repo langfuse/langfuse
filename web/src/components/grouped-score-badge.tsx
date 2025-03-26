815dd9a03090de009a3532d8a1705e81d4218a2b
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-03-26 11:19:17 +0100

    chore: refactor

--- web/src/components/grouped-score-badge.tsx
+++ web/src/components/grouped-score-badge.tsx
@@ -21,6 +21,47 @@ const partitionScores = <T extends APIScore | LastUserScore>(
   return { visibleScores, hiddenScores };
 };
 
+const ScoreGroupBadge = <T extends APIScore | LastUserScore>({
+  name,
+  scores,
+}: {
+  name: string;
+  scores: T[];
+}) => {
+  return (
+    <Badge
+      variant="tertiary"
+      key={name}
+      className="flex items-center gap-1 px-2.5 text-xs font-normal"
+    >
+      <div className="w-fit max-w-20 truncate" title={name}>
+        {name}:
+      </div>
+      <div className="flex items-center gap-1 text-nowrap">
+        {scores.map((s, i) => (
+          <span
+            key={i}
+            className="group/score ml-1 flex items-center gap-1 rounded-sm first:ml-0"
+          >
+            {s.stringValue ?? s.value?.toFixed(2) ?? ""}
+            {s.comment && (
+              <HoverCard>
+                <HoverCardTrigger className="inline-block">
+                  <MessageCircleMoreIcon className="mb-[0.0625rem] !size-3" />
+                </HoverCardTrigger>
+                <HoverCardContent className="overflow-hidden whitespace-normal break-normal">
+                  <p>{s.comment}</p>
+                </HoverCardContent>
+              </HoverCard>
+            )}
+            <span className="group-last/score:hidden">,</span>
+          </span>
+        ))}
+      </div>
+    </Badge>
+  );
+};
+
 export const GroupedScoreBadges = <T extends APIScore | LastUserScore>({
   scores,
   maxVisible,
@@ -44,40 +85,9 @@ export const GroupedScoreBadges = <T extends APIScore | LastUserScore>({
 
   return (
     <>
-      {visibleScores.map(([name, scores]) => {
-        return (
-          <Badge
-            variant="tertiary"
-            key={name}
-            className="flex items-center gap-1 px-2.5 text-xs font-normal"
-          >
-            <div className="w-fit max-w-20 truncate" title={name}>
-              {name}:
-            </div>
-            <div className="flex items-center gap-1 text-nowrap">
-              {scores.map((s, i) => (
-                <span
-                  key={i}
-                  className="group/score ml-1 flex items-center gap-1 rounded-sm first:ml-0"
-                >
-                  {s.stringValue ?? s.value?.toFixed(2) ?? ""}
-                  {s.comment && (
-                    <HoverCard>
-                      <HoverCardTrigger className="inline-block">
-                        <MessageCircleMoreIcon className="mb-[0.0625rem] !size-3" />
-                      </HoverCardTrigger>
-                      <HoverCardContent className="overflow-hidden whitespace-normal break-normal">
-                        <p>{s.comment}</p>
-                      </HoverCardContent>
-                    </HoverCard>
-                  )}
-                  <span className="group-last/score:hidden">,</span>
-                </span>
-              ))}
-            </div>
-          </Badge>
-        );
-      })}
+      {visibleScores.map(([name, scores]) => (
+        <ScoreGroupBadge key={name} name={name} scores={scores} />
+      ))}
       {Boolean(hiddenScores.length) && (
         <HoverCard>
           <HoverCardTrigger className="inline-block rounded-sm">
@@ -91,36 +101,7 @@ export const GroupedScoreBadges = <T extends APIScore | LastUserScore>({
           <HoverCardContent className="max-h-[300px] max-w-[200px] overflow-y-auto p-2">
             <div className="flex flex-wrap gap-1">
               {hiddenScores.map(([name, scores]) => (
-                <Badge
-                  variant="tertiary"
-                  key={name}
-                  className="flex items-center gap-1 px-2.5 text-xs font-normal"
-                >
-                  <div className="w-fit max-w-20 truncate" title={name}>
-                    {name}:
-                  </div>
-                  <div className="flex items-center gap-1 text-nowrap">
-                    {scores.map((s, i) => (
-                      <span
-                        key={i}
-                        className="group/score ml-1 flex items-center gap-1 rounded-sm first:ml-0"
-                      >
-                        {s.stringValue ?? s.value?.toFixed(2) ?? ""}
-                        {s.comment && (
-                          <HoverCard>
-                            <HoverCardTrigger className="inline-block">
-                              <MessageCircleMoreIcon className="mb-[0.0625rem] !size-3" />
-                            </HoverCardTrigger>
-                            <HoverCardContent className="overflow-hidden whitespace-normal break-normal">
-                              <p>{s.comment}</p>
-                            </HoverCardContent>
-                          </HoverCard>
-                        )}
-                        <span className="group-last/score:hidden">,</span>
-                      </span>
-                    ))}
-                  </div>
-                </Badge>
+                <ScoreGroupBadge key={name} name={name} scores={scores} />
               ))}
             </div>
           </HoverCardContent>
