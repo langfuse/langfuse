c2983c371e58f4eb8f3e95c97dbccda406f41f51
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-04-04 14:08:20 +0200

    style: revert changing card headings to div element in card.tsx

--- web/src/components/ui/card.tsx
+++ web/src/components/ui/card.tsx
@@ -30,10 +30,10 @@ const CardHeader = React.forwardRef<
 CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <div
+  <h3
     ref={ref}
     className={cn(
       "text-2xl font-semibold leading-none tracking-tight",
