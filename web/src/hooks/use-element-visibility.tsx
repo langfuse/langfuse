40aa239f40771fd596b36e22143e650066bdd551
David Alexander Pfeiffer <20278364+davidmc971@users.noreply.github.com>
2025-04-04 12:18:11 +0200

    explain element visibility hook

--- web/src/hooks/use-element-visibility.tsx
+++ web/src/hooks/use-element-visibility.tsx
@@ -1,3 +1,8 @@
+/**
+ * This hook is used to determine if an element is or was visible in the viewport.
+ * It is useful for determining if a component is visible in the viewport, or if it was visible in the viewport at any point in time.
+ */
+
 import { type RefObject, useEffect, useMemo, useState } from "react";
 
 export function useElementIsVisible<T extends HTMLElement>(ref: RefObject<T>) {
