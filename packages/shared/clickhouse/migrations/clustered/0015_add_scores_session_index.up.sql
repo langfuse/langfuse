a22d698f931cbc381d6aea25395fd78b637cee6f
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-04-09 10:34:19 +0200

    chore: migration adjustment

--- packages/shared/clickhouse/migrations/clustered/0015_add_scores_session_index.up.sql
+++ packages/shared/clickhouse/migrations/clustered/0015_add_scores_session_index.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE scores ON CLUSTER default ADD INDEX IF NOT EXISTS idx_project_session (project_id, session_id) TYPE bloom_filter(0.001) GRANULARITY 1
--- ALTER TABLE scores ON CLUSTER default MATERIALIZE INDEX IF EXISTS idx_project_session;
+ALTER TABLE scores ON CLUSTER default ADD INDEX IF NOT EXISTS idx_project_session (project_id, session_id) TYPE bloom_filter(0.001) GRANULARITY 1;
+ALTER TABLE scores ON CLUSTER default MATERIALIZE INDEX IF EXISTS idx_project_session;
--- packages/shared/clickhouse/migrations/unclustered/0015_add_scores_session_index.up.sql
+++ packages/shared/clickhouse/migrations/unclustered/0015_add_scores_session_index.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE scores ADD INDEX IF NOT EXISTS idx_project_session (project_id, session_id) TYPE bloom_filter(0.001) GRANULARITY 1
--- ALTER TABLE scores MATERIALIZE INDEX IF EXISTS idx_project_session;
+ALTER TABLE scores ADD INDEX IF NOT EXISTS idx_project_session (project_id, session_id) TYPE bloom_filter(0.001) GRANULARITY 1;
+ALTER TABLE scores MATERIALIZE INDEX IF EXISTS idx_project_session;
