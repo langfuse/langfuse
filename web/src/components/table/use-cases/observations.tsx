4758d296e80000cbe2b6f0ffd05e5d02f7d70010
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-04-22 14:26:23 +0200

    chore(observations): show model name in table column

--- web/src/components/table/use-cases/observations.tsx
+++ web/src/components/table/use-cases/observations.tsx
@@ -508,7 +508,7 @@ export default function ObservationsTable({
         if (!model) return null;
 
         return modelId ? (
-          <TableId value={modelId} />
+          <TableId value={model} />
         ) : (
           <UpsertModelFormDrawer
             action="create"
