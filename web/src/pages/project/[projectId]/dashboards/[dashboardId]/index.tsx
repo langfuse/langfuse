e7aa0fbe082ecc84ed060372918e95fa86e99bde
Steffen Schmitz <steffen@langfuse.com>
2025-04-25 11:52:23 +0200

    feat: add support for score name filters on dashboard (#6608)

--- web/src/pages/project/[projectId]/dashboards/[dashboardId]/index.tsx
+++ web/src/pages/project/[projectId]/dashboards/[dashboardId]/index.tsx
@@ -134,6 +134,12 @@ export default function DashboardDetail() {
       internal: "internalValue",
     },
     {
+      name: "Score Name",
+      id: "scoreName",
+      type: "string",
+      internal: "internalValue",
+    },
+    {
       name: "Tags",
       id: "tags",
       type: "arrayOptions",
