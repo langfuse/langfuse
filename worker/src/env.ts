c06af525bf47f01c9115094211a2678d2ab8c6e8
Max Deichmann <m.deichmann@tum.de>
2025-05-01 16:33:59 +0200

    chore: disable dlx retry for oss (#6665)

--- worker/src/env.ts
+++ worker/src/env.ts
@@ -192,7 +192,7 @@ const EnvSchema = z.object({
     .default("true"),
   QUEUE_CONSUMER_DEAD_LETTER_RETRY_QUEUE_IS_ENABLED: z
     .enum(["true", "false"])
-    .default("true"),
+    .default("false"),
 
   // Core data S3 upload - Langfuse Cloud
   LANGFUSE_S3_CORE_DATA_EXPORT_IS_ENABLED: z
