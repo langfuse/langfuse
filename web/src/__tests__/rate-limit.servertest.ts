f0d4cf0ba643567a47b2448316af57efc97c2a4c
Max Deichmann <m.deichmann@tum.de>
2025-05-14 22:41:30 +0200

    fix

--- web/src/__tests__/rate-limit.servertest.ts
+++ web/src/__tests__/rate-limit.servertest.ts
@@ -78,8 +78,8 @@ describe("RateLimitService", () => {
     expect(result?.res).toEqual({
       scope: scope,
       resource: "public-api",
-      points: 20,
-      remainingPoints: 19,
+      points: 30,
+      remainingPoints: 29,
       msBeforeNext: expect.any(Number),
       consumedPoints: 1,
       isFirstInDuration: true,
