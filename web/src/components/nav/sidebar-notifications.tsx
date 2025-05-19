c03fb857e89f9edcb5b6baf4fd005dad2fa1d456
Marc Klingen <git@marcklingen.com>
2025-05-19 15:42:38 +0200

    Update web/src/components/nav/sidebar-notifications.tsx

--- web/src/components/nav/sidebar-notifications.tsx
+++ web/src/components/nav/sidebar-notifications.tsx
@@ -18,7 +18,7 @@ type SidebarNotification = {
   id: string; // Add unique ID for each notification
   title: string;
   description: React.ReactNode;
-  createdAt?: string;
+  createdAt?: string; // optional, used to expire the notication
   link?: string;
   // defaults to "Learn more" if no linkContent and no linkTitle
   linkTitle?: string;
