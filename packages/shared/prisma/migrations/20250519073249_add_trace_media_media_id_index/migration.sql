5e8455afa1973e1bc1f51612d0b9e1a2d4013126
steffen911 <steffen@langfuse.com>
2025-05-19 11:13:59 +0200

    chore: only add index if they do not exist yet

--- packages/shared/prisma/migrations/20250519073249_add_trace_media_media_id_index/migration.sql
+++ packages/shared/prisma/migrations/20250519073249_add_trace_media_media_id_index/migration.sql
@@ -1,2 +1,2 @@
 -- CreateIndex
-CREATE INDEX CONCURRENTLY "trace_media_project_id_media_id_idx" ON "trace_media"("project_id", "media_id");
+CREATE INDEX CONCURRENTLY IF NOT EXISTS "trace_media_project_id_media_id_idx" ON "trace_media"("project_id", "media_id");
--- packages/shared/prisma/migrations/20250519073327_add_observation_media_media_id_index/migration.sql
+++ packages/shared/prisma/migrations/20250519073327_add_observation_media_media_id_index/migration.sql
@@ -1,2 +1,2 @@
 -- CreateIndex
-CREATE INDEX CONCURRENTLY "observation_media_project_id_media_id_idx" ON "observation_media"("project_id", "media_id");
+CREATE INDEX CONCURRENTLY IF NOT EXISTS "observation_media_project_id_media_id_idx" ON "observation_media"("project_id", "media_id");
