5535d54b333d076911017d58db7503684b1deed9
steffen911 <steffen@langfuse.com>
2025-05-26 21:45:58 +0200

    chore: add analytic views in ClickHouse

---
+++ packages/shared/clickhouse/migrations/clustered/0021_analytics_scores.up.sql
@@ -0,0 +1,13 @@
+CREATE VIEW analytics_scores ON CLUSTER default AS
+SELECT
+    project_id,
+    toStartOfHour(timestamp) AS hour,
+    uniq(id) AS countScores,
+    max(source = 'ANNOTATION') AS hasAnnotation,
+    max(source = 'API') AS hasApi,
+    max(source = 'EVAL') AS hasEval
+FROM
+    scores
+GROUP BY
+    project_id,
+    hour;
---
+++ packages/shared/clickhouse/migrations/unclustered/0019_analytics_traces.up.sql
@@ -0,0 +1,13 @@
+CREATE VIEW analytics_traces AS
+SELECT
+    project_id,
+    toStartOfHour(timestamp) AS hour,
+    uniq(id) AS countTraces,
+    max(user_id IS NOT NULL) AS hasUsers,
+    max(session_id IS NOT NULL) AS hasSessions,
+    max(if(environment != 'default', 1, 0)) AS hasEnvironments
+FROM
+    traces
+GROUP BY
+    project_id,
+    hour;
