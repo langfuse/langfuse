f7692b6da2c15cb1007544752fe0e4c387094404
Marc Klingen <git@marcklingen.com>
2025-06-25 18:48:31 +0200

    add icon when sidebar is collapsed

--- web/src/ee/features/billing/components/UsageTracker.tsx
+++ web/src/ee/features/billing/components/UsageTracker.tsx
@@ -13,6 +13,7 @@ import {
 import { ActionButton } from "@/src/components/ActionButton";
 import { cn } from "@/src/utils/tailwind";
 import { AlertTriangle } from "lucide-react";
+import { useSidebar } from "@/src/components/ui/sidebar";
 
 export const UsageTracker = () => {
   const { organization } = useQueryProjectOrOrganization();
@@ -22,6 +23,7 @@ export const UsageTracker = () => {
     organizationId: organization?.id,
     scope: "langfuseCloudBilling:CRUD",
   });
+  const { setOpen } = useSidebar();
 
   const usageQuery = api.cloudBilling.getUsage.useQuery(
     {
@@ -56,33 +58,43 @@ export const UsageTracker = () => {
   const isCritical = percentage > 200;
 
   return (
-    <Card
-      className={cn(
-        "relative max-h-48 overflow-hidden rounded-md bg-opacity-50 shadow-none group-data-[collapsible=icon]:hidden",
-        isCritical && "border-destructive",
+    <>
+      {/* icon when critical and sidebar is collapsed */}
+      {isCritical && (
+        <AlertTriangle
+          className="hidden h-4 w-4 cursor-pointer self-center text-destructive group-data-[collapsible=icon]:inline-block"
+          onClick={() => setOpen(true)}
+        />
       )}
-    >
-      <CardHeader className="p-4 pb-0">
-        <CardTitle className="flex items-center gap-2 text-sm">
-          {isCritical && (
-            <AlertTriangle className="inline-block h-4 w-4 text-destructive" />
-          )}
-          Plan Usage Limit
-        </CardTitle>
-        <CardDescription>
-          {`${usage.toLocaleString()} / ${MAX_EVENTS_FREE_PLAN.toLocaleString()} (${percentage.toFixed(0)}%) ${usageType} in last 30 days. Please upgrade your plan to avoid interruptions.`}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="p-4 pt-2">
-        <ActionButton
-          variant={isCritical ? "default" : "secondary"}
-          size="sm"
-          href={`/organization/${organization?.id}/settings/billing`}
-          hasAccess={hasAccess}
-        >
-          Upgrade plan
-        </ActionButton>
-      </CardContent>
-    </Card>
+      {/* card when sidebar is not collapsed */}
+      <Card
+        className={cn(
+          "relative max-h-48 overflow-hidden rounded-md bg-opacity-50 shadow-none group-data-[collapsible=icon]:hidden",
+          isCritical && "border-destructive",
+        )}
+      >
+        <CardHeader className="p-4 pb-0">
+          <CardTitle className="flex items-center gap-2 text-sm">
+            {isCritical && (
+              <AlertTriangle className="inline-block h-4 w-4 text-destructive" />
+            )}
+            Plan Usage Limit
+          </CardTitle>
+          <CardDescription>
+            {`${usage.toLocaleString()} / ${MAX_EVENTS_FREE_PLAN.toLocaleString()} (${percentage.toFixed(0)}%) ${usageType} in last 30 days. Please upgrade your plan to avoid interruptions.`}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="p-4 pt-2">
+          <ActionButton
+            variant={isCritical ? "default" : "secondary"}
+            size="sm"
+            href={`/organization/${organization?.id}/settings/billing`}
+            hasAccess={hasAccess}
+          >
+            Upgrade plan
+          </ActionButton>
+        </CardContent>
+      </Card>
+    </>
   );
 };
