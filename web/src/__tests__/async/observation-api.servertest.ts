10724265ff4ad5498ce70af5bf4e8c405881932a
Steffen Schmitz <steffen@langfuse.com>
2025-07-04 11:50:35 +0000

    chore: drop console logs

--- web/src/__tests__/async/observation-api.servertest.ts
+++ web/src/__tests__/async/observation-api.servertest.ts
@@ -35,7 +35,6 @@ describe("/api/public/observations API Endpoint", () => {
         "GET",
         "/api/public/observations/" + observationId,
       );
-      console.log(`Result: ${JSON.stringify(getEventRes)}`);
       expect(getEventRes.body).toMatchObject({
         id: observationId,
         traceId: traceId,
