604c944897157b4b69771ff696d7258c7fe3ba6b
Steffen Schmitz <steffen@langfuse.com>
2025-07-07 12:25:15 +0000

    feat(dashboard): allow breakdown by month

--- web/src/features/query/server/queryBuilder.ts
+++ web/src/features/query/server/queryBuilder.ts
@@ -428,7 +428,7 @@ export class QueryBuilder {
       dimensions += `${appliedDimensions
         .map(
           (dimension) =>
-            `any(${dimension.table}.${dimension.sql}) as ${dimension.alias ?? dimension.sql}`,
+            `any(${dimension.sql}) as ${dimension.alias ?? dimension.sql}`,
         )
         .join(",\n")},`;
     }
