584eea619ec1a51b580e4ffa9cdb214a840c1e16
Max Deichmann <m.deichmann@tum.de>
2025-07-16 13:46:17 +0200

    push

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -5,4 +5,29 @@ module.exports = {
   parserOptions: {
     project: true,
   },
+  rules: {
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: [
+          {
+            group: ["react-icons", "react-icons/*"],
+            message:
+              "react-icons import not allowed. Please use lucide-react instead.",
+          },
+        ],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        "src/components/nav/support-menu-dropdown.tsx",
+        "src/pages/auth/sign-in.tsx",
+      ],
+      rules: {
+        "no-restricted-imports": "off",
+      },
+    },
+  ],
 };
