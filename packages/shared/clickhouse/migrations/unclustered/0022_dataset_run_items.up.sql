8ea4dea5860ec0ba7ff599883a06f24673e74eb5
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-07-16 18:55:32 +0200

    fixup(schema): remove indexes, add `output`

--- packages/shared/clickhouse/migrations/unclustered/0022_dataset_run_items.up.sql
+++ packages/shared/clickhouse/migrations/unclustered/0022_dataset_run_items.up.sql
@@ -6,21 +6,24 @@ CREATE TABLE dataset_run_items (
     `dataset_item_id` String,
     `trace_id` String,
     `observation_id` Nullable(String),
+    `dataset_id` String,
 
     -- error field 
     `error` Nullable(String),
+
+    -- denormalized trace fields (mutable, but snapshots are relevant)
+    `output` CODEC(ZSTD(3)), -- json
     
     -- denormalized immutable dataset run fields
-    `dataset_id` String,
     `dataset_run_name` String,
     `dataset_run_description` Nullable(String),
     `dataset_run_metadata` Nullable(String) CODEC(ZSTD(3)), -- json  
     `dataset_run_created_at` DateTime64(3),
 
     -- denormalized dataset item fields (mutable, but snapshots are relevant)
-    `dataset_item_input` Nullable(String) CODEC(ZSTD(3)), -- json
-    `dataset_item_expected_output` Nullable(String) CODEC(ZSTD(3)), -- json
-    `dataset_item_metadata` Nullable(String) CODEC(ZSTD(3)), -- json
+    `dataset_item_input` CODEC(ZSTD(3)), -- json
+    `dataset_item_expected_output` CODEC(ZSTD(3)), -- json
+    `dataset_item_metadata` CODEC(ZSTD(3)), -- json
 
     -- timestamps 
     `created_at` DateTime64(3) DEFAULT now(),
@@ -32,20 +35,9 @@ CREATE TABLE dataset_run_items (
 
     -- TODO: Could consider materialized view to represent mapping of dataset_id -> dataset_run_id 
     -- TODO: Use skip index to cater to multiple dataset_run_items per dataset_item
-    -- TODO: trial and error for query engine on popular queries with other partitions and order by
-    -- TODO: consider including created_at in order by to optimize for pagination
-
-    -- For dataset_run_name filtering (public API)
-    INDEX idx_run_name dataset_run_name TYPE bloom_filter(0.001) GRANULARITY 1,
-
-    -- For trace joins and filtering
-    INDEX idx_trace trace_id TYPE bloom_filter(0.001) GRANULARITY 1,
 
     -- For dataset item lookups
     INDEX idx_dataset_item dataset_item_id TYPE bloom_filter(0.001) GRANULARITY 1,
-
-    -- For direct run ID lookups (experiments)
-    INDEX idx_run_id dataset_run_id TYPE bloom_filter(0.001) GRANULARITY 1
 ) ENGINE = ReplacingMergeTree(event_ts, is_deleted) 
 Partition BY toYYYYMM(created_at)
 ORDER BY (project_id, dataset_id, dataset_run_id, id); 
