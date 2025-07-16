27bd5ac4ad1cb42d0b553b16462aea9242221359
David Halapir <halapir.david@gmail.com>
2025-07-16 14:16:13 +0200

    add comment

--- web/src/features/accounts/server/accounts.router.ts
+++ web/src/features/accounts/server/accounts.router.ts
@@ -8,6 +8,8 @@ import z from "zod";
 import { env } from "@/src/env.mjs";
 import * as crypto from "crypto";
 
+// todo add new protected procedure, only project djb-dev and only from users with Admin rights
+
 export const accountsRouter = createTRPCRouter({
   getUsers: protectedProjectProcedure
     .input(z.object({ projectId: z.string() }))
