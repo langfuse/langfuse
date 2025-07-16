8ce27f9fda4e539267461ff96b7c7c448aea5867
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-07-16 14:36:26 +0200

    fixup: rewrite POST /api/public/dataset-run-items​

--- web/src/pages/api/public/dataset-run-items.ts
+++ web/src/pages/api/public/dataset-run-items.ts
@@ -2,6 +2,7 @@ import { prisma } from "@langfuse/shared/src/db";
 import { withMiddlewares } from "@/src/features/public-api/server/withMiddlewares";
 import { createAuthedProjectAPIRoute } from "@/src/features/public-api/server/createAuthedProjectAPIRoute";
 import {
+  type APIDatasetRunItem,
   GetDatasetRunItemsV1Query,
   GetDatasetRunItemsV1Response,
   PostDatasetRunItemsV1Body,
@@ -10,19 +11,23 @@ import {
 } from "@/src/features/public-api/types/datasets";
 import {
   LangfuseNotFoundError,
-  InvalidRequestError,
   executeWithDatasetRunItemsStrategy,
   DatasetRunItemsOperationType,
 } from "@langfuse/shared";
 import { addDatasetRunItemsToEvalQueue } from "@/src/features/evals/server/addDatasetRunItemsToEvalQueue";
 import {
-  getObservationById,
+  eventTypes,
+  logger,
+  processEventBatch,
+  createOrFetchDatasetRun,
   validateDatasetRunAndFetch,
 } from "@langfuse/shared/src/server";
 import {
   generateDatasetRunItemsForPublicApi,
   getDatasetRunItemsCountForPublicApi,
+  validateCreateDatasetRunItemBodyAndFetch,
 } from "@/src/features/public-api/server/dataset-run-items";
+import { v4 } from "uuid";
 
 export default withMiddlewares({
   POST: createAuthedProjectAPIRoute({
@@ -30,91 +35,46 @@ export default withMiddlewares({
     bodySchema: PostDatasetRunItemsV1Body,
     responseSchema: PostDatasetRunItemsV1Response,
     rateLimitResource: "datasets",
-    fn: async ({ body, auth }) => {
-      const res = await executeWithDatasetRunItemsStrategy({
+    fn: async ({ body, auth, res }) => {
+      return await executeWithDatasetRunItemsStrategy({
         input: body,
         operationType: DatasetRunItemsOperationType.WRITE,
         postgresExecution: async (bodyInput: typeof body) => {
-          const {
-            datasetItemId,
-            observationId,
-            traceId,
-            runName,
-            runDescription,
-            metadata,
-          } = bodyInput;
-
           /**************
            * VALIDATION *
            **************/
 
-          const datasetItem = await prisma.datasetItem.findUnique({
-            where: {
-              id_projectId: {
-                projectId: auth.scope.projectId,
-                id: datasetItemId,
-              },
-              status: "ACTIVE",
-            },
-            include: {
-              dataset: true,
-            },
+          const result = await validateCreateDatasetRunItemBodyAndFetch(
+            bodyInput,
+            auth.scope.projectId,
+          );
+
+          if (!result.success) {
+            throw new LangfuseNotFoundError(result.error);
+          }
+
+          /********************
+           *   RUN CREATION    *
+           ********************/
+
+          const { datasetItem, traceId, observationId } = result;
+
+          const run = await createOrFetchDatasetRun({
+            name: bodyInput.runName,
+            description: bodyInput.runDescription ?? undefined,
+            metadata: bodyInput.metadata ?? undefined,
+            projectId: auth.scope.projectId,
+            datasetId: datasetItem.datasetId,
           });
-
-          if (!datasetItem) {
-            throw new LangfuseNotFoundError(
-              "Dataset item not found or not active",
-            );
-          }
-
-          let finalTraceId = traceId;
-
-          // Backwards compatibility: historically, dataset run items were linked to observations, not traces
-          if (!traceId && observationId) {
-            const observation = await getObservationById({
-              id: observationId,
-              projectId: auth.scope.projectId,
-              fetchWithInputOutput: true,
-            });
-            if (observationId && !observation) {
-              throw new LangfuseNotFoundError("Observation not found");
-            }
-            finalTraceId = observation?.traceId;
-          }
-
-          if (!finalTraceId) {
-            throw new InvalidRequestError("No traceId set");
-          }
 
           /********************
            * RUN ITEM CREATION *
            ********************/
 
-          const run = await prisma.datasetRuns.upsert({
-            where: {
-              datasetId_projectId_name: {
-                datasetId: datasetItem.datasetId,
-                name: runName,
-                projectId: auth.scope.projectId,
-              },
-            },
-            create: {
-              name: runName,
-              description: runDescription ?? undefined,
-              datasetId: datasetItem.datasetId,
-              metadata: metadata ?? undefined,
-              projectId: auth.scope.projectId,
-            },
-            update: {
-              metadata: metadata ?? undefined,
-              description: runDescription ?? undefined,
-            },
-          });
-
           const runItem = await prisma.datasetRunItems.create({
             data: {
-              datasetItemId,
-              traceId: finalTraceId,
+              datasetItemId: datasetItem.id,
+              traceId,
               observationId,
               datasetRunId: run.id,
               projectId: auth.scope.projectId,
@@ -127,8 +87,8 @@ export default withMiddlewares({
 
           await addDatasetRunItemsToEvalQueue({
             projectId: auth.scope.projectId,
-            datasetItemId,
-            traceId: finalTraceId,
+            datasetItemId: datasetItem.id,
+            traceId,
             observationId: observationId ?? undefined,
           });
 
@@ -138,11 +98,95 @@ export default withMiddlewares({
           });
         },
         clickhouseExecution: async (bodyInput: typeof body) => {
-          return {};
+          /**************
+           * VALIDATION *
+           **************/
+
+          const result = await validateCreateDatasetRunItemBodyAndFetch(
+            bodyInput,
+            auth.scope.projectId,
+          );
+
+          if (!result.success) {
+            throw new LangfuseNotFoundError(result.error);
+          }
+
+          const { datasetItem, traceId, observationId } = result;
+
+          /********************
+           *   RUN CREATION    *
+           ********************/
+
+          const run = await createOrFetchDatasetRun({
+            name: bodyInput.runName,
+            description: bodyInput.runDescription ?? undefined,
+            metadata: bodyInput.metadata ?? undefined,
+            projectId: auth.scope.projectId,
+            datasetId: datasetItem.datasetId,
+          });
+
+          /********************
+           * RUN ITEM CREATION *
+           ********************/
+
+          const createdAt = new Date();
+
+          const event = {
+            id: v4(),
+            type: eventTypes.DATASET_RUN_ITEM_CREATE,
+            timestamp: new Date().toISOString(),
+            body: {
+              id: v4(),
+              traceId: traceId,
+              observationId: observationId,
+              error: null, // TODO: allow ingestion of error
+              input: datasetItem.input,
+              expectedOutput: datasetItem.expectedOutput,
+              createdAt: createdAt.toISOString(),
+              datasetId: datasetItem.datasetId,
+              datasetRunId: run.id,
+              datasetItemId: datasetItem.id,
+            },
+          };
+          // note: currently we do not accept user defined ids for dataset run items
+          const ingestionResult = await processEventBatch([event], auth);
+          if (ingestionResult.errors.length > 0) {
+            const error = ingestionResult.errors[0];
+            res
+              .status(error.status)
+              .json({ message: error.error ?? error.message });
+            // TODO: figure out dummy return
+          }
+          if (ingestionResult.successes.length !== 1) {
+            logger.error("Failed to create dataset run item", { result });
+            throw new Error("Failed to create dataset run item");
+          }
+
+          /********************
+           * ASYNC RUN ITEM EVAL *
+           ********************/
+
+          await addDatasetRunItemsToEvalQueue({
+            projectId: auth.scope.projectId,
+            datasetItemId: datasetItem.id,
+            traceId,
+            observationId: observationId ?? undefined,
+          });
+
+          const mockDatasetRunItem: APIDatasetRunItem = {
+            id: event.body.id,
+            datasetRunId: run.id,
+            datasetRunName: run.name,
+            datasetItemId: datasetItem.id,
+            traceId: traceId,
+            observationId: observationId,
+            createdAt: createdAt,
+            updatedAt: createdAt,
+          };
+
+          return mockDatasetRunItem;
         },
       });
-
-      return res;
     },
   }),
   GET: createAuthedProjectAPIRoute({
