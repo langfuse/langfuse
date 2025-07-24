f15246d9dfd3a9c8b8a2212dc8daa9b001a68d98
marliessophie <74332854+marliessophie@users.noreply.github.com>
2025-07-24 18:12:59 +0200

    feat(experiments): support remote experiment trigger  (#8094)
    * fixup(experiments): support trigger for remote experiment run
    
    * chore: remove instructions
    
    * chore: rename webhook > remote experiment server side
    
    * chore: rename webhook > remote experiment client side
    
    * style: design review
    
    * chore: lint
    
    * fix(experiments): change URL validation to use z.url() in RemoteExperimentUpsertForm
    
    * chore: rename
    
    * feat(datasets): add transformation function for datasets and update API responses

--- packages/shared/prisma/generated/types.ts
+++ packages/shared/prisma/generated/types.ts
@@ -359,6 +359,8 @@ export type Dataset = {
   name: string;
   description: string | null;
   metadata: unknown | null;
+  remote_experiment_url: string | null;
+  remote_experiment_payload: unknown | null;
   created_at: Generated<Timestamp>;
   updated_at: Generated<Timestamp>;
 };
