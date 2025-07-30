f7fcb8232182b1be1f48f3b4169cb4ec64a9c0d2
David Halapir <halapir.david@gmail.com>
2025-07-30 17:34:25 +0200

    add comments

--- web/src/features/accounts/AccountsPage.tsx
+++ web/src/features/accounts/AccountsPage.tsx
@@ -5,6 +5,14 @@ import { api } from "@/src/utils/api";
 import { useRouter } from "next/router";
 import { CreateUserDialog } from "@/src/features/accounts/CreateUserDialog";
 
+// fetch all users from supabase
+// show 3 tabs, real, synthethic and snapshots
+// create must vary between real and synthetic
+// snapshot can be created from message view only and requires no input, writes to djb metadata
+// synthethic also writes to djb metadata
+// usernames are auto constructed
+// differ between real and synthetic by the type of djb_metadata, pick if its client side filter or search params with separate routes
+
 export function AccountsPage() {
   const router = useRouter();
 
