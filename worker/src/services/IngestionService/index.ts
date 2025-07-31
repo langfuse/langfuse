1c8ffc607daa36f8ed23f0e8da339f6c13434299
Steffen Schmitz <steffen@langfuse.com>
2025-07-31 11:27:14 +0200

    chore: write single rows into AMTs instead of pre-merged record (#8218)
    * chore: write single rows into AMTs instead of pre-merged record
    
    * chore: adjust assertion
    
    * chore: drop debug log

--- worker/src/services/IngestionService/index.ts
+++ worker/src/services/IngestionService/index.ts
@@ -441,11 +441,15 @@ export class IngestionService {
     this.clickHouseWriter.addToQueue(TableName.Traces, finalTraceRecord);
 
     // Experimental: Also write to traces_mt table if experiment flag is enabled
+    // Here we use the raw events to ensure that we stop relying on the merge logic
     if (
       env.LANGFUSE_EXPERIMENT_INSERT_INTO_AGGREGATING_MERGE_TREES === "true"
     ) {
-      const traceMtRecord = convertTraceToTraceMt(finalTraceRecord);
-      this.clickHouseWriter.addToQueue(TableName.TracesMt, traceMtRecord);
+      traceRecords
+        .map(convertTraceToTraceMt)
+        .forEach((r) =>
+          this.clickHouseWriter.addToQueue(TableName.TracesMt, r),
+        );
     }
 
     // Add trace into trace upsert queue for eval processing
