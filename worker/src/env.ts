738207964169b95ef9c0e75426869081f7a00c91
steffen911 <steffen@langfuse.com>
2025-08-04 16:18:23 +0200

    chore: skip S3 list and legacy trace insert based on env flag

--- worker/src/env.ts
+++ worker/src/env.ts
@@ -241,6 +241,9 @@ const EnvSchema = z.object({
     .positive()
     .default(120_000), // 2 minutes
 
+  LANGFUSE_EXPERIMENT_INSERT_INTO_TRACES_TABLE: z
+    .enum(["true", "false"])
+    .default("true"),
   LANGFUSE_EXPERIMENT_INSERT_INTO_AGGREGATING_MERGE_TREES: z
     .enum(["true", "false"])
     .default("false"),
--- worker/src/queues/ingestionQueue.ts
+++ worker/src/queues/ingestionQueue.ts
@@ -140,8 +140,10 @@ export const ingestionQueueProcessorBuilder = (
       // Check if we should skip S3 list operation
       // The producer sets skipS3List to true if it's an OTel observation
       const shouldSkipS3List =
-        job.data.payload.data.skipS3List && job.data.payload.data.fileKey;
-
+        job.data.payload.data.skipS3List &&
+        job.data.payload.data.fileKey &&
+        // If we do not insert into the traces table, we can skip the list and process single files
+        env.LANGFUSE_EXPERIMENT_INSERT_INTO_TRACES_TABLE === "false";
       const s3Prefix = `${env.LANGFUSE_S3_EVENT_UPLOAD_PREFIX}${job.data.payload.authCheck.scope.projectId}/${clickhouseEntityType}/${job.data.payload.data.eventBodyId}/`;
 
       let totalS3DownloadSizeBytes = 0;
