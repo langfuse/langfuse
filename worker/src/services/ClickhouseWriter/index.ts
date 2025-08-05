4da1229faa6635be34a5da6bda27e0040d0ad4f7
Steffen Schmitz <steffen@langfuse.com>
2025-08-05 16:20:49 +0200

    chore: create background migrations for traces amt tables (#7689)
    * chore: create background migrations for traces amt tables
    
    * chore: add database migrations
    
    * chore: move tests to use AMTs in one config
    
    * chore: shift backfill from {} to map()
    
    * chore: modify ingestion test timeouts
    
    * chore: keep minDate updates tracked
    
    * chore: remove android lib to save swap space
    
    * chore: check disk space
    
    * chore: check mnt usage
    
    * chore: skip swap to check runner performance
    
    * chore: add timestamp lines
    
    * chore: handle deletions
    
    * chore: update test cases
    
    * chore: linting
    
    * chore: allow opt-in creation for background migratoin record
    
    * chore: update comparison query
    
    * chore: concurrency config
    
    * chore: update names
    
    * chore: update names
    
    * chore: update tiemout
    
    * chore: add pre-query logs
    
    * chore: refactor long running query into util function
    
    * chore: refactor query exists checks
    
    * chore: set correct tables for clustered configs
    
    * chore: increase number of retries
    
    * chore: extend logging
    
    * chore: gracefully handle aborted query
    
    * chore: handle errors during execution and raise them
    
    * chore; skip in progress header updates
    
    * Update migrateTracesToTracesAMTs.ts
    
    * chore: use default clickhouse settings for ingestion
    
    * chore: add trace_id index on traces_all_amt
    
    * chore: update sorting behaviour of batchAction test
    
    * chore: drop logger
    
    * chore: update test
    
    * chore: lint and renames
    
    * chore: update test cases
    
    * chore: patch score tests
    
    * chore: add final for the checkTraceExists behaviour
    
    * chore: remove background migration insert
    
    * chore: convert traces_mt to traces_null
    
    * chore: patch tests
    
    * chore: switch from minMap to maxMap
    
    * chore: mark aggregation columns on traces as do not use
    
    * chore: patch new test
    
    * chore: switch to maxMap
    
    * consistent naming

--- worker/src/services/ClickhouseWriter/index.ts
+++ worker/src/services/ClickhouseWriter/index.ts
@@ -9,7 +9,7 @@ import {
   recordIncrement,
   ScoreRecordInsertType,
   TraceRecordInsertType,
-  TraceMtRecordInsertType,
+  TraceNullRecordInsertType,
   DatasetRunItemRecordInsertType,
 } from "@langfuse/shared/src/server";
 
@@ -39,7 +39,7 @@ export class ClickhouseWriter {
 
     this.queue = {
       [TableName.Traces]: [],
-      [TableName.TracesMt]: [],
+      [TableName.TracesNull]: [],
       [TableName.Scores]: [],
       [TableName.Observations]: [],
       [TableName.BlobStorageFileLog]: [],
@@ -106,7 +106,7 @@ export class ClickhouseWriter {
         recordIncrement("langfuse.queue.clickhouse_writer.request");
         await Promise.all([
           this.flush(TableName.Traces, fullQueue),
-          this.flush(TableName.TracesMt, fullQueue),
+          this.flush(TableName.TracesNull, fullQueue),
           this.flush(TableName.Scores, fullQueue),
           this.flush(TableName.Observations, fullQueue),
           this.flush(TableName.BlobStorageFileLog, fullQueue),
@@ -398,7 +398,7 @@ export class ClickhouseWriter {
 
 export enum TableName {
   Traces = "traces", // eslint-disable-line no-unused-vars
-  TracesMt = "traces_mt", // eslint-disable-line no-unused-vars
+  TracesNull = "traces_null", // eslint-disable-line no-unused-vars
   Scores = "scores", // eslint-disable-line no-unused-vars
   Observations = "observations", // eslint-disable-line no-unused-vars
   BlobStorageFileLog = "blob_storage_file_log", // eslint-disable-line no-unused-vars
@@ -411,8 +411,8 @@ type RecordInsertType<T extends TableName> = T extends TableName.Scores
     ? ObservationRecordInsertType
     : T extends TableName.Traces
       ? TraceRecordInsertType
-      : T extends TableName.TracesMt
-        ? TraceMtRecordInsertType
+      : T extends TableName.TracesNull
+        ? TraceNullRecordInsertType
         : T extends TableName.BlobStorageFileLog
           ? BlobStorageFileLogInsertType
           : T extends TableName.DatasetRunItems
