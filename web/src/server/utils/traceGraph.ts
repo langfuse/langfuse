1dd9b2adc3c63e6d66b926e2e70996dc83f08919
Nimar <l.nimar.b@gmail.com>
2025-08-11 17:10:04 +0200

    fix typing

--- web/src/server/utils/traceGraph.ts
+++ web/src/server/utils/traceGraph.ts
@@ -228,7 +228,7 @@ export function processTimingAwareGraph(records: GraphNode[]): GraphNode[] {
       }
     });
 
-    if (bestParent) {
+    if (bestParent && bestParent.name) {
       node.parent_node_id = bestParent.name;
     }
   });
