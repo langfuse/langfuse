5f3486b032e4f987cde4c496298a37f15d71d042
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-08-12 17:12:41 +0200

    feat(annotation): add POST queue API

--- web/src/pages/api/public/annotation-queues/index.ts
+++ web/src/pages/api/public/annotation-queues/index.ts
@@ -2,12 +2,14 @@ import { prisma } from "@langfuse/shared/src/db";
 import { withMiddlewares } from "@/src/features/public-api/server/withMiddlewares";
 import { createAuthedProjectAPIRoute } from "@/src/features/public-api/server/createAuthedProjectAPIRoute";
 import {
+  CreateAnnotationQueueBody,
+  CreateAnnotationQueueResponse,
   GetAnnotationQueuesQuery,
   GetAnnotationQueuesResponse,
 } from "@/src/features/public-api/types/annotation-queues";
+import { InvalidRequestError, MethodNotAllowedError } from "@langfuse/shared";
 
 export default withMiddlewares({
-  // NOTE: Post API requires entitlement check
   GET: createAuthedProjectAPIRoute({
     name: "Get annotation queues",
     querySchema: GetAnnotationQueuesQuery,
@@ -52,6 +54,63 @@ export default withMiddlewares({
           totalPages: Math.ceil(totalItems / query.limit),
         },
       };
+    },
+  }),
+
+  POST: createAuthedProjectAPIRoute({
+    name: "Create annotation queue",
+    bodySchema: CreateAnnotationQueueBody,
+    responseSchema: CreateAnnotationQueueResponse,
+    fn: async ({ body, auth }) => {
+      // entitlement check
+      if (auth.scope.plan === "cloud:hobby") {
+        if (
+          (await prisma.annotationQueue.count({
+            where: {
+              projectId: auth.scope.projectId,
+            },
+          })) >= 1
+        ) {
+          throw new MethodNotAllowedError(
+            "Maximum number of annotation queues reached on Hobby plan.",
+          );
+        }
+      }
+
+      const existingQueue = await prisma.annotationQueue.findFirst({
+        where: {
+          projectId: auth.scope.projectId,
+          name: body.name,
+        },
+      });
+
+      if (existingQueue) {
+        throw new InvalidRequestError("A queue with this name already exists.");
+      }
+
+      // verify the score configs exist
+      const scoreConfigs = await prisma.scoreConfig.findMany({
+        where: {
+          id: { in: body.scoreConfigIds },
+        },
+      });
+
+      if (scoreConfigs.length !== body.scoreConfigIds.length) {
+        throw new InvalidRequestError(
+          "At least one of the score config IDs cannot be found for the given project.",
+        );
+      }
+
+      const queue = await prisma.annotationQueue.create({
+        data: {
+          projectId: auth.scope.projectId,
+          name: body.name,
+          description: body.description,
+          scoreConfigIds: body.scoreConfigIds,
+        },
+      });
+
+      return queue;
     },
   }),
 });
