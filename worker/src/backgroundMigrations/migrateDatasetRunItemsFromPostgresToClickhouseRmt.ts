6fbecbdb2a644d10f49d68285e1ac75c72f6a8b7
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-08-14 11:33:29 +0200

    chore: push

--- worker/src/backgroundMigrations/migrateDatasetRunItemsFromPostgresToClickhouseRmt.ts
+++ worker/src/backgroundMigrations/migrateDatasetRunItemsFromPostgresToClickhouseRmt.ts
@@ -39,7 +39,7 @@ export default class MigrateDatasetRunItemsFromPostgresToClickhouseRmt
       query: "SHOW TABLES",
     });
     const tableNames = (await tables.json()).data as { name: string }[];
-    if (!tableNames.some((r) => r.name === "dataset_run_items_rmt_rmt")) {
+    if (!tableNames.some((r) => r.name === "dataset_run_items_rmt")) {
       // Retry if the table does not exist as this may mean migrations are still pending
       if (attempts > 0) {
         logger.info(
@@ -63,7 +63,7 @@ export default class MigrateDatasetRunItemsFromPostgresToClickhouseRmt
   async run(args: Record<string, unknown>): Promise<void> {
     const start = Date.now();
     logger.info(
-      `Migrating dataset_run_items_rmt from postgres to clickhouse with ${JSON.stringify(args)}`,
+      `Migrating dataset_run_items from postgres to clickhouse with ${JSON.stringify(args)}`,
     );
 
     // @ts-ignore
@@ -126,7 +126,7 @@ export default class MigrateDatasetRunItemsFromPostgresToClickhouseRmt
           -- Dataset ID
           d.id as dataset_id
 
-        FROM dataset_run_items_rmt dri
+        FROM dataset_run_items dri
         JOIN dataset_runs dr ON dri.dataset_run_id = dr.id
         JOIN dataset_items di ON dri.dataset_item_id = di.id  
         JOIN datasets d ON di.dataset_id = d.id
@@ -135,7 +135,7 @@ export default class MigrateDatasetRunItemsFromPostgresToClickhouseRmt
         LIMIT ${batchSize};
       `);
       if (datasetRunItems.length === 0) {
-        logger.info("No more dataset_run_items_rmt to migrate. Exiting...");
+        logger.info("No more dataset_run_items to migrate. Exiting...");
         break;
       }
 
@@ -145,13 +145,13 @@ export default class MigrateDatasetRunItemsFromPostgresToClickhouseRmt
 
       const insertStart = Date.now();
       await clickhouseClient().insert({
-        table: "dataset_run_items_rmt_rmt",
+        table: "dataset_run_items_rmt",
         values: datasetRunItems.map(convertPostgresDatasetRunItemToInsert),
         format: "JSONEachRow",
       });
 
       logger.info(
-        `Inserted ${datasetRunItems.length} dataset_run_items_rmt into Clickhouse in ${Date.now() - insertStart}ms`,
+        `Inserted ${datasetRunItems.length} dataset_run_items into Clickhouse in ${Date.now() - insertStart}ms`,
       );
 
       await prisma.backgroundMigration.update({
@@ -166,7 +166,7 @@ export default class MigrateDatasetRunItemsFromPostgresToClickhouseRmt
       });
 
       if (datasetRunItems.length < batchSize) {
-        logger.info("No more dataset_run_items_rmt to migrate. Exiting...");
+        logger.info("No more dataset_run_items to migrate. Exiting...");
         this.isFinished = true;
       }
 
