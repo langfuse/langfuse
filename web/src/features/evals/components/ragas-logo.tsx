5b60cba2fa5d6208f674a95528c2a6cf00b563d0
Tyler Ashworth <TAshworth@lt.life>
2025-08-19 17:14:27 -0500

    fix: apply prettier formatting to ragas-logo.tsx

--- web/src/features/evals/components/ragas-logo.tsx
+++ web/src/features/evals/components/ragas-logo.tsx
@@ -7,12 +7,7 @@ export const RagasLogoIcon = () => {
   return (
     <div className="flex items-center">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={assetPath}
-        alt="Ragas Logo"
-        width={12}
-        height={12}
-      />
+      <img src={assetPath} alt="Ragas Logo" width={12} height={12} />
     </div>
   );
 };
