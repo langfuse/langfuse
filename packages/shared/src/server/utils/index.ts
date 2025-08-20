88df2b187c93bb5a72aa5d8734d918b54490ef49
Johannes Goslar <jogo@kronberger-spiele.de>
2025-08-20 23:41:25 +0200

    feat(shared): Bundle Size 2 Shared Internal Imports

---
+++ packages/shared/src/server/utils/index.ts
@@ -0,0 +1,4 @@
+export * from "./DatabaseReadStream";
+export * from "./headerUtils";
+export * from "./metadata_conversion";
+export * from "./rendering";
--- packages/shared/src/tableDefinitions/index.ts
+++ packages/shared/src/tableDefinitions/index.ts
@@ -2,3 +2,5 @@ export * from "./sessionsView";
 export * from "./types";
 export * from "./mapDashboards";
 export * from "./promptsTable";
+export * from "./tracesTable";
+export * from "./typeHelpers";
