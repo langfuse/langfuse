503fd37daa7ee654c9e349fe383389982e1bc9ad
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-08-21 19:11:34 +0200

    chore: fix after rebase

--- packages/shared/src/tableDefinitions/index.ts
+++ packages/shared/src/tableDefinitions/index.ts
@@ -2,5 +2,4 @@ export * from "./sessionsView";
 export * from "./types";
 export * from "./mapDashboards";
 export * from "./promptsTable";
-export * from "./mapDatasetRunItemsTable";
 export * from "./datasetItemsTable";
