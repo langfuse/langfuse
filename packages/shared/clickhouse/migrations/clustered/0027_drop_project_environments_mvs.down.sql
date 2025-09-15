d85bded49c5911ab7cacd7236247a19ef444f71f
steffen911 <steffen@langfuse.com>
2025-09-15 17:33:49 +0200

    chore: drop materialized views to fill project_environments

---
+++ packages/shared/clickhouse/migrations/clustered/0027_drop_project_environments_mvs.down.sql
@@ -0,0 +1,25 @@
+-- Recreate materialized views for project_environments table
+
+-- Create materialized view for traces
+CREATE MATERIALIZED VIEW project_environments_traces_mv ON CLUSTER default TO project_environments AS
+SELECT
+    project_id,
+    groupUniqArray(environment) AS environments
+FROM traces
+GROUP BY project_id;
+
+-- Create materialized view for observations
+CREATE MATERIALIZED VIEW project_environments_observations_mv ON CLUSTER default TO project_environments AS
+SELECT
+    project_id,
+    groupUniqArray(environment) AS environments
+FROM observations
+GROUP BY project_id;
+
+-- Create materialized view for scores
+CREATE MATERIALIZED VIEW project_environments_scores_mv ON CLUSTER default TO project_environments AS
+SELECT
+    project_id,
+    groupUniqArray(environment) AS environments
+FROM scores
+GROUP BY project_id;
---
+++ packages/shared/clickhouse/migrations/unclustered/0027_drop_project_environments_mvs.up.sql
@@ -0,0 +1,7 @@
+-- Drop materialized views feeding the project_environments table
+
+DROP VIEW IF EXISTS project_environments_traces_mv;
+
+DROP VIEW IF EXISTS project_environments_observations_mv;
+
+DROP VIEW IF EXISTS project_environments_scores_mv;
