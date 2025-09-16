48905f96f8cce321d668638eeab93d66bdc12cf6
steffen911 <steffen@langfuse.com>
2025-09-16 09:27:39 +0200

    chore: remove migration files

--- packages/shared/clickhouse/migrations/unclustered/0027_drop_project_environments_mvs.up.sql
+++
@@ -1,7 +0,0 @@
--- Drop materialized views feeding the project_environments table
-
-DROP VIEW IF EXISTS project_environments_traces_mv;
-
-DROP VIEW IF EXISTS project_environments_observations_mv;
-
-DROP VIEW IF EXISTS project_environments_scores_mv;
