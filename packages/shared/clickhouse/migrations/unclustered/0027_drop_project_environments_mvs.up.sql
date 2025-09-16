126d33c174c3575d0f512e6445141d94de08cc3b
steffen911 <steffen@langfuse.com>
2025-09-16 10:19:24 +0200

    chore: patch

--- packages/shared/clickhouse/migrations/unclustered/0027_drop_project_environments_mvs.up.sql
+++ packages/shared/clickhouse/migrations/unclustered/0027_drop_project_environments_mvs.up.sql
@@ -1,4 +1,4 @@
 -- Drop materialized views feeding project_environments table
-DROP MATERIALIZED VIEW IF EXISTS project_environments_traces_mv;
-DROP MATERIALIZED VIEW IF EXISTS project_environments_observations_mv;
-DROP MATERIALIZED VIEW IF EXISTS project_environments_scores_mv;
+DROP VIEW IF EXISTS project_environments_traces_mv;
+DROP VIEW IF EXISTS project_environments_observations_mv;
+DROP VIEW IF EXISTS project_environments_scores_mv;
