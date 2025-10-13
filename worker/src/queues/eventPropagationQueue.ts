b460d9905dbfad07ca89a6f5722aa6595eee5ccc
steffen911 <steffen@langfuse.com>
2025-10-13 11:49:16 +0200

    chore: run event propagation in a new trace

--- worker/src/queues/eventPropagationQueue.ts
+++ worker/src/queues/eventPropagationQueue.ts
@@ -1,15 +1,27 @@
 import { Processor } from "bullmq";
-import { logger, QueueJobs } from "@langfuse/shared/src/server";
+import {
+  logger,
+  QueueJobs,
+  instrumentAsync,
+} from "@langfuse/shared/src/server";
 import { handleEventPropagationJob } from "../features/eventPropagation/handleEventPropagationJob";
 
 export const eventPropagationProcessor: Processor = async (job) => {
   if (job.name === QueueJobs.EventPropagationJob) {
-    logger.info("Executing Event Propagation Job");
-    try {
-      return await handleEventPropagationJob(job);
-    } catch (error) {
-      logger.error("Error executing EventPropagationJob", error);
-      throw error;
-    }
+    return await instrumentAsync(
+      {
+        name: "process event-propagation",
+        startNewTrace: true,
+      },
+      async () => {
+        logger.info("Executing Event Propagation Job");
+        try {
+          return await handleEventPropagationJob(job);
+        } catch (error) {
+          logger.error("Error executing EventPropagationJob", error);
+          throw error;
+        }
+      },
+    );
   }
 };
