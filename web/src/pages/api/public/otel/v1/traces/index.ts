29d814a2511cfc315b131325ab19883776ac3b6f
Steffen Schmitz <steffen@langfuse.com>
2025-10-15 18:53:06 +0200

    chore: cleanup

--- web/src/pages/api/public/otel/v1/traces/index.ts
+++ web/src/pages/api/public/otel/v1/traces/index.ts
@@ -103,8 +103,7 @@ export default withMiddlewares({
 
       // At this point, we have the raw OpenTelemetry Span body. We upload the full batch to S3
       // and the OtelIngestionProcessor logic will handle processing in the worker container.
-      await processor.publishToOtelIngestionQueue(resourceSpans);
-      return {};
+      return processor.publishToOtelIngestionQueue(resourceSpans);
     },
   }),
 });
