85669f85318d1136b1a5a0467528aab682e1af16
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-10-20 10:22:45 +0200

    chore: drop context provider from traces component

--- web/src/pages/project/[projectId]/traces/[traceId].tsx
+++ web/src/pages/project/[projectId]/traces/[traceId].tsx
@@ -1,5 +1,4 @@
 import { TracePage } from "@/src/components/trace/TracePage";
-import { ScoreCacheProvider } from "@/src/features/scores/contexts/ScoreCacheContext";
 import { useRouter } from "next/router";
 
 export default function Trace() {
@@ -11,9 +10,5 @@ export default function Trace() {
       ? new Date(decodeURIComponent(router.query.timestamp))
       : undefined;
 
-  return (
-    <ScoreCacheProvider>
-      <TracePage traceId={traceId} timestamp={timestamp} />
-    </ScoreCacheProvider>
-  );
+  return <TracePage traceId={traceId} timestamp={timestamp} />;
 }
