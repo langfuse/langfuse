b4fda8e7be784c5873d38513688a6a8d94850863
Marc Klingen <git@marcklingen.com>
2025-10-25 01:04:10 +0200

    fix dropdown

--- web/src/pages/project/[projectId]/settings/integrations/mixpanel.tsx
+++ web/src/pages/project/[projectId]/settings/integrations/mixpanel.tsx
@@ -184,7 +184,7 @@ const MixpanelIntegrationSettingsForm = ({
           render={({ field }) => (
             <FormItem>
               <FormLabel>Mixpanel Region</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
+              <Select onValueChange={field.onChange} value={field.value}>
                 <FormControl>
                   <SelectTrigger>
                     <SelectValue placeholder="Select a region" />
