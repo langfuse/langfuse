b11e5ea35f8fc5ec0779faa7e6f4ab79da86ee70
Max Deichmann <m.deichmann@tum.de>
2025-11-06 17:03:02 +0100

    chore: revert webhook redirect

--- packages/shared/src/server/repositories/clickhouse.ts
+++ packages/shared/src/server/repositories/clickhouse.ts
@@ -349,6 +349,10 @@ export async function queryClickhouse<T>(opts: {
       // Retry logic for socket hang up and other network errors
       return await backOff(
         async () => {
+          // same logic as for prisma. we want to see queries in development
+          if (env.NODE_ENV === "development") {
+            logger.info(`clickhouse:query ${opts.query}`);
+          }
           const res = await clickhouseClient(
             opts.clickhouseConfigs,
             opts.preferredClickhouseService,
@@ -361,11 +365,6 @@ export async function queryClickhouse<T>(opts: {
               log_comment: JSON.stringify(opts.tags ?? {}),
             },
           });
-
-          // same logic as for prisma. we want to see queries in development
-          if (env.NODE_ENV === "development") {
-            logger.info(`clickhouse:query ${res.query_id} ${opts.query}`);
-          }
 
           span.setAttribute("ch.queryId", res.query_id);
 
