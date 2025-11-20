9cef66a45fe674bbb76f23460e546dc7d321b9fc
froemic <m.froehlich1994@gmail.com>
2025-11-20 15:30:01 +0100

    fix(trace): remove leftover currentNodeRef reference

--- web/src/components/trace/TraceTree.tsx
+++ web/src/components/trace/TraceTree.tsx
@@ -382,7 +382,6 @@ const TraceTreeRow = ({
           className={cn(
             "peer relative flex min-w-0 flex-1 items-start rounded-md py-0.5 pl-1 pr-2 text-left",
           )}
-          ref={currentNodeRef}
         >
           <SpanItem
             node={node}
