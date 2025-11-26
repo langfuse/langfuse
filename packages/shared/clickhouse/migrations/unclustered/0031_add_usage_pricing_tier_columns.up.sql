53ef0ff03e46ea5577c35778179725f6596b2562
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-11-26 18:23:56 +0100

    chore(pricing-migration): remove 'after' statement from migration (#10735)

--- packages/shared/clickhouse/migrations/unclustered/0031_add_usage_pricing_tier_columns.up.sql
+++ packages/shared/clickhouse/migrations/unclustered/0031_add_usage_pricing_tier_columns.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE observations ADD COLUMN usage_pricing_tier_id Nullable(String) AFTER total_cost;
-ALTER TABLE observations ADD COLUMN usage_pricing_tier_name Nullable(String) AFTER usage_pricing_tier_id;
+ALTER TABLE observations ADD COLUMN usage_pricing_tier_id Nullable(String);
+ALTER TABLE observations ADD COLUMN usage_pricing_tier_name Nullable(String);
