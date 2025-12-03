b43f3b98949f70eeaa426b01781531cd18dbf87d
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-03 16:47:56 +0100

    chore: types

--- packages/shared/prisma/generated/types.ts
+++ packages/shared/prisma/generated/types.ts
@@ -429,7 +429,7 @@ export type DatasetItem = {
   dataset_id: string;
   created_at: Generated<Timestamp>;
   updated_at: Generated<Timestamp>;
-  sys_id: string;
+  sys_id: Generated<string>;
   valid_from: Generated<Timestamp>;
   is_deleted: Generated<boolean>;
 };
