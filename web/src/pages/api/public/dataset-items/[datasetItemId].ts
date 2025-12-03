6ffeb6dcd3be41844dc4c2641ee285758b70e0cb
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-03 13:43:45 +0100

    chore: eslint and build

--- web/src/pages/api/public/dataset-items/[datasetItemId].ts
+++ web/src/pages/api/public/dataset-items/[datasetItemId].ts
@@ -51,6 +51,7 @@ export default withMiddlewares({
 
       return transformDbDatasetItemDomainToAPIDatasetItem({
         id: datasetItem.id,
+        sysId: datasetItem.sysId,
         projectId: datasetItem.projectId,
         datasetId: datasetItem.datasetId,
         status: datasetItem.status ?? "ACTIVE",
