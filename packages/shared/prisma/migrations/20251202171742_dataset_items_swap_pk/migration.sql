7ae83fb41a8725043a10130fc78bfd8c2743c22d
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-03 10:38:58 +0100

    chore: push

--- packages/shared/prisma/migrations/20251202171742_dataset_items_swap_pk/migration.sql
+++ packages/shared/prisma/migrations/20251202171742_dataset_items_swap_pk/migration.sql
@@ -7,8 +7,12 @@
 BEGIN;
 
 -- 1. Drop pk constraint that prevents appending
-ALTER TABLE "dataset_items" DROP CONSTRAINT "dataset_items_pkey",
- -- 2. Enforce Physical Identity
+ALTER TABLE "dataset_items" 
+DROP CONSTRAINT "dataset_items_pkey";
+
+-- 2. Add new PK (drop the unique constraint first to avoid duplicate)
+ALTER TABLE "dataset_items"
+DROP CONSTRAINT "dataset_items_sys_id_project_id_key",
 ADD CONSTRAINT "dataset_items_pkey" PRIMARY KEY ("sys_id", "project_id");
 
 COMMIT;
