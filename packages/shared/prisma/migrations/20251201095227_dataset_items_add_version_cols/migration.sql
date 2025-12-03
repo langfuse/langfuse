d7957fc8043e1e1b8b752bc55c076b1092ee00b0
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-03 10:52:18 +0100

    chore: add default and unique constraint for sys_id

--- packages/shared/prisma/migrations/20251201095227_dataset_items_add_version_cols/migration.sql
+++ packages/shared/prisma/migrations/20251201095227_dataset_items_add_version_cols/migration.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 -- Step 1: Add columns
 ALTER TABLE "dataset_items" ADD COLUMN     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "sys_id" TEXT,
+ADD COLUMN     "sys_id" TEXT DEFAULT md5(random()::text || clock_timestamp()::text)::uuid::text,
 ADD COLUMN     "valid_from" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ALTER COLUMN "status" DROP NOT NULL;
 
@@ -14,5 +14,9 @@ WHERE "sys_id" IS NULL;
 -- Step 3: Make column NOT NULL
 ALTER TABLE "dataset_items" 
 ALTER COLUMN "sys_id" SET NOT NULL;
+
+-- Step 4: Add UNIQUE constraint on new PK columns for Phase 1 of PK swap
+ALTER TABLE "dataset_items"
+ADD CONSTRAINT "dataset_items_sys_id_project_id_key" UNIQUE ("sys_id", "project_id");
 
 COMMIT;
