89b72b6a6484cd45b22a80a16a97a0c9df9e08a0
steffen911 <steffen@langfuse.com>
2025-12-04 14:53:19 +0100

    chore: stringify metadata in backfill

--- worker/src/backgroundMigrations/backfillEventsHistoric.ts
+++ worker/src/backgroundMigrations/backfillEventsHistoric.ts
@@ -529,7 +529,7 @@ export default class BackfillEventsHistoric implements IBackgroundMigration {
         o.cost_details,
         coalesce(o.input, '') AS input,
         coalesce(o.output, '') AS output,
-        metadata::JSON AS metadata,
+        toJsonString(o.metadata) AS metadata,
         mapKeys(o.metadata) AS metadata_names,
         mapValues(o.metadata) AS metadata_raw_values,
         multiIf(mapContains(o.metadata, 'resourceAttributes'), 'otel-backfill', 'ingestion-api-backfill') AS source,
