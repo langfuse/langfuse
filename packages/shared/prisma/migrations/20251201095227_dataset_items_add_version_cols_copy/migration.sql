3947bd0a9eae605af43074c79397ed3892a81d33
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-04 18:36:42 +0100

    chore: add migration

---
+++ packages/shared/prisma/migrations/20251201095227_dataset_items_add_version_cols_copy/migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO background_migrations (id, name, script, args)
+VALUES ('d4f5a6b7-c8d9-4e1f-a2b3-c4d5e6f7a8b9', '20251204_1001_backfill_dataset_items_sys_id', 'backfillSysIdForDatasetItems', '{}');
