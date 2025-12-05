931f06a46ed1eae424f1109deb0f068330c15536
Steffen Schmitz <steffen@langfuse.com>
2025-12-05 16:28:43 +0100

    feat: add trace_name column to event table definition (#10947)

--- worker/src/features/eventPropagation/handleEventPropagationJob.ts
+++ worker/src/features/eventPropagation/handleEventPropagationJob.ts
@@ -228,6 +228,7 @@ export const handleEventPropagationJob = async (
           select
             t.id,
             t.project_id,
+            t.name,
             t.user_id,
             t.session_id,
             t.version,
@@ -260,6 +261,7 @@ export const handleEventPropagationJob = async (
           tags,
           public,
           bookmarked,
+          trace_name,
           user_id,
           session_id,
           level,
@@ -311,6 +313,7 @@ export const handleEventPropagationJob = async (
           t.tags as tags,
           t.public as public,
           t.bookmarked AND (obs.parent_observation_id IS NULL OR obs.parent_observation_id = '') AS bookmarked,
+          t.name AS trace_name,
           coalesce(t.user_id, '') AS user_id,
           coalesce(t.session_id, '') AS session_id,
           obs.level,
