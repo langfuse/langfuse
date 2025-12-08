0ef4d75dc155b39c152a16bbf877a1f44aa931ec
steffen911 <steffen@langfuse.com>
2025-12-08 15:01:06 +0100

    chore: flip condition

--- worker/src/backgroundMigrations/backfillEventsHistoric.ts
+++ worker/src/backgroundMigrations/backfillEventsHistoric.ts
@@ -572,15 +572,15 @@ export default class BackfillEventsHistoric implements IBackgroundMigration {
 
     // Apply memory-reducing settings on retries
     const retrySettings =
-      retryCount > 0
+      retryCount > 1
         ? {
-            max_block_size: "4096",
+            max_threads: 1,
+            max_insert_threads: "1",
+            max_block_size: "2048",
           }
-        : retryCount > 1
+        : retryCount > 0
           ? {
-              max_threads: 1,
-              max_insert_threads: "1",
-              max_block_size: "2048",
+              max_block_size: "4096",
             }
           : {};
 
