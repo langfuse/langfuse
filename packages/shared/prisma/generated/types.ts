9f9769393b5a9103c7fe48b69961dbbb7298d4c0
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-11 13:38:38 +0100

    chore: rebase

--- packages/shared/prisma/generated/types.ts
+++ packages/shared/prisma/generated/types.ts
@@ -447,7 +447,7 @@ export type DatasetItem = {
   dataset_id: string;
   created_at: Generated<Timestamp>;
   updated_at: Generated<Timestamp>;
-  sys_id: string | null;
+  sys_id: Generated<string | null>;
   valid_from: Generated<Timestamp>;
   is_deleted: Generated<boolean>;
 };
--- packages/shared/prisma/migrations/20251205133946_dataset_items_create_idx_id_project_id_valid_from/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[id,project_id,valid_from]` on the table `dataset_items` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX CONCURRENTLY "dataset_items_id_project_id_valid_from_key" ON "dataset_items"("id", "project_id", "valid_from");
-
