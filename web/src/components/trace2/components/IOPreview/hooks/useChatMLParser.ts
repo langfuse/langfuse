c24492f13cfb77672c60c1e387bc6734282f9460
Nimar <l.nimar.b@gmail.com>
2025-12-15 18:15:47 +0100

    fix(tracing): show input / output label on trace correctly if not ChatML (#11132)

--- web/src/components/trace2/components/IOPreview/hooks/useChatMLParser.ts
+++ web/src/components/trace2/components/IOPreview/hooks/useChatMLParser.ts
@@ -29,6 +29,7 @@ export interface ChatMLParserResult {
   toolCallCounts: Map<string, number>;
   messageToToolCallNumbers: Map<number, number[]>;
   toolNameToDefinitionNumber: Map<string, number>;
+  inputMessageCount: number;
 }
 
 /**
@@ -156,6 +157,7 @@ export function useChatMLParser(
       toolCallCounts,
       messageToToolCallNumbers,
       toolNameToDefinitionNumber,
+      inputMessageCount,
     };
   }, [parsedInput, parsedOutput, parsedMetadata, observationName]);
 }
