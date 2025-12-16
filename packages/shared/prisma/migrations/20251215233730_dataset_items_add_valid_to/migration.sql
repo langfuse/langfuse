745746fa40ac2e1797944654931d7f94cd799352
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2025-12-16 08:43:33 +0100

    chore: adjust migration

--- packages/shared/prisma/migrations/20251215233730_dataset_items_add_valid_to/migration.sql
+++ packages/shared/prisma/migrations/20251215233730_dataset_items_add_valid_to/migration.sql
@@ -1,5 +1,2 @@
 -- AlterTable
-ALTER TABLE "dataset_items" ADD COLUMN     "valid_to" TIMESTAMP(3);
-
--- RenameIndex
-ALTER INDEX "dataset_item_events_project_id_dataset_id_item_id_created_at_id" RENAME TO "dataset_item_events_project_id_dataset_id_item_id_created_a_idx";
+ALTER TABLE "dataset_items" ADD COLUMN "valid_to" TIMESTAMP(3);
