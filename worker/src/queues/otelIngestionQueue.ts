5f535f8cebc6467c93b5bb54806dc7a82b6b9085
steffen911 <steffen@langfuse.com>
2025-12-17 14:10:02 +0100

    lint

--- worker/src/queues/otelIngestionQueue.ts
+++ worker/src/queues/otelIngestionQueue.ts
@@ -170,7 +170,10 @@ export const otelIngestionQueueProcessor: Processor = async (
       .map((o) => ingestionSchema.safeParse(o))
       .flatMap((o) => {
         if (!o.success) {
-          logger.warn(`Failed to parse otel observation for project ${projectId} in ${fileKey}: ${o.error}`, o.error);
+          logger.warn(
+            `Failed to parse otel observation for project ${projectId} in ${fileKey}: ${o.error}`,
+            o.error,
+          );
           return [];
         }
         return [o.data];
