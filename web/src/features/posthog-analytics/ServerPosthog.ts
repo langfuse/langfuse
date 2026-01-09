9ce8f1cdfe9d8d62fc55420933e3dd11579acb57
Nimar <l.nimar.b@gmail.com>
2026-01-09 15:07:01 +0100

    refactor ph

--- web/src/features/posthog-analytics/ServerPosthog.ts
+++ web/src/features/posthog-analytics/ServerPosthog.ts
@@ -1,18 +1,29 @@
 import { env } from "@/src/env.mjs";
-import { PostHog as OriginalPosthog } from "posthog-node";
+import { PostHog } from "posthog-node";
 
-// Safe as it is intended to be public
-const PUBLIC_POSTHOG_API_KEY =
-  env.NEXT_PUBLIC_POSTHOG_KEY ||
-  "phc_zkMwFajk8ehObUlMth0D7DtPItFnxETi3lmSvyQDrwB";
-const POSTHOG_HOST = env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.posthog.com";
+export class ServerPosthog {
+  private posthog: PostHog | null;
 
-export class ServerPosthog extends OriginalPosthog {
   constructor() {
-    super(PUBLIC_POSTHOG_API_KEY, {
-      host: POSTHOG_HOST,
-    });
+    if (env.NEXT_PUBLIC_POSTHOG_KEY && env.NEXT_PUBLIC_POSTHOG_HOST) {
+      this.posthog = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
+        host: env.NEXT_PUBLIC_POSTHOG_HOST,
+      });
+      if (process.env.NODE_ENV === "development") this.posthog.debug();
+    } else {
+      this.posthog = null;
+    }
+  }
 
-    if (process.env.NODE_ENV === "development") this.debug();
+  capture(...args: Parameters<PostHog["capture"]>) {
+    this.posthog?.capture(...args);
+  }
+
+  async shutdown() {
+    await this.posthog?.shutdown();
+  }
+
+  async flush() {
+    await this.posthog?.flush();
   }
 }
