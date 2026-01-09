699ff126e073c56114fd5297f61ff981c16d4cc6
Nimar <l.nimar.b@gmail.com>
2026-01-09 15:23:34 +0100

    retain default behaviour

--- web/src/features/posthog-analytics/ServerPosthog.ts
+++ web/src/features/posthog-analytics/ServerPosthog.ts
@@ -1,14 +1,24 @@
 import { env } from "@/src/env.mjs";
 import { PostHog } from "posthog-node";
 
+const FALLBACK_POSTHOG_KEY = "phc_zkMwFajk8ehObUlMth0D7DtPItFnxETi3lmSvyQDrwB";
+const FALLBACK_POSTHOG_HOST = "https://eu.posthog.com";
+
 export class ServerPosthog {
   private posthog: PostHog | null;
 
   constructor() {
-    if (env.NEXT_PUBLIC_POSTHOG_KEY && env.NEXT_PUBLIC_POSTHOG_HOST) {
-      this.posthog = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
-        host: env.NEXT_PUBLIC_POSTHOG_HOST,
-      });
+    const telemetryEnabled = env.TELEMETRY_ENABLED !== "false";
+
+    const apiKey =
+      env.NEXT_PUBLIC_POSTHOG_KEY ??
+      (telemetryEnabled ? FALLBACK_POSTHOG_KEY : null);
+    const host =
+      env.NEXT_PUBLIC_POSTHOG_HOST ??
+      (telemetryEnabled ? FALLBACK_POSTHOG_HOST : null);
+
+    if (apiKey && host) {
+      this.posthog = new PostHog(apiKey, { host });
       if (process.env.NODE_ENV === "development") this.posthog.debug();
     } else {
       this.posthog = null;
