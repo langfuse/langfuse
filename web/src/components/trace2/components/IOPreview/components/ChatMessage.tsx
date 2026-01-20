508fb869beb4386414bf1306abc6d8eb5822d8cf
Nimar <l.nimar.b@gmail.com>
2026-01-20 13:56:27 +0100

    fix(trace): dont show empty string on only tool call + thinking strings

--- web/src/components/trace2/components/IOPreview/components/ChatMessage.tsx
+++ web/src/components/trace2/components/IOPreview/components/ChatMessage.tsx
@@ -186,7 +186,7 @@ export function ChatMessage({
         <div style={{ display: shouldRenderMarkdown ? "block" : "none" }}>
           <MarkdownJsonView
             title={title}
-            content={message.content || '""'}
+            content={message.content || ""}
             customCodeHeaderClassName={cn(
               message.role === "assistant" && "bg-secondary",
               message.role === "system" && "bg-primary-foreground",
