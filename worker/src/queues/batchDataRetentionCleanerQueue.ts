574abb1d5fb3b31f0017ac2fe67f02af22b7ceaf
Valery Meleshkin <valeriy@langfuse.com>
2026-01-20 15:49:29 +0100

    chore: validate that we only operate on supported tables

--- worker/src/queues/batchDataRetentionCleanerQueue.ts
+++ worker/src/queues/batchDataRetentionCleanerQueue.ts
@@ -4,12 +4,20 @@ import {
   QueueJobs,
   BatchDataRetentionCleanerJobType,
   BatchDataRetentionTable,
+  BATCH_DATA_RETENTION_TABLES,
 } from "@langfuse/shared/src/server";
 import { BatchDataRetentionCleaner } from "../features/batch-data-retention-cleaner";
 
 export const batchDataRetentionCleanerProcessor: Processor = async (job) => {
   if (job.name === QueueJobs.BatchDataRetentionCleanerJob) {
     const { table } = job.data as BatchDataRetentionCleanerJobType;
+    if (BATCH_DATA_RETENTION_TABLES.indexOf(table) === -1) {
+      logger.error(
+        `Invalid table name for BatchDataRetentionCleaner: ${table}`,
+      );
+      return;
+    }
+
     logger.info(`Executing BatchDataRetentionCleaner for ${table}`);
     try {
       await BatchDataRetentionCleaner.processBatch(
