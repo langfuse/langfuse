98058872dabdf15d4b821986789ecc5abc5118c5
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2026-02-22 12:39:52 +0530

    fix: deleting running evaluator gets stuck

--- web/src/features/evals/components/evaluator-table.tsx
+++ web/src/features/evals/components/evaluator-table.tsx
@@ -438,7 +438,7 @@ export default function EvaluatorTable({ projectId }: { projectId: string }) {
                   aria-label="delete"
                   itemId={id}
                   projectId={projectId}
-                  redirectUrl={`/project/${projectId}/evals`}
+                  isTableAction
                   deleteConfirmation={row.original.scoreName}
                 />
               </DropdownMenuItem>
