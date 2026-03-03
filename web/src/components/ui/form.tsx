bc73a4cae9aaabb1ff02f4a91bc3aaa260dd1e65
Marlies Mayerhofer <74332854+marliessophie@users.noreply.github.com>
2026-03-03 14:42:09 +0100

    Revert "test: trying out commit-stash"
    This reverts commit 2ec5cffbd845127d9eb767ccb2076c15cc9cb2ca.

--- web/src/components/ui/form.tsx
+++ web/src/components/ui/form.tsx
@@ -42,7 +42,6 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext);
   const itemContext = React.useContext(FormItemContext);
-
   const { getFieldState, formState } = useFormContext();
 
   const fieldState = getFieldState(fieldContext.name, formState);
