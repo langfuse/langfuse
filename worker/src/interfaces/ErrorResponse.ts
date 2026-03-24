27bc426046c014781d76de53df9ce0ae70543a0d
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2026-03-24 11:27:57 +0100

    test: should not be merged

--- worker/src/interfaces/ErrorResponse.ts
+++ worker/src/interfaces/ErrorResponse.ts
@@ -3,3 +3,7 @@ import MessageResponse from "./MessageResponse";
 export default interface ErrorResponse extends MessageResponse {
   stack?: string;
 }
+
+export function unUsed() {
+  console.log("Hello");
+}
