863959b61d4b2f35d48ae06f677d28057ec78c32
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-05-07 13:04:43 +0200

    Remove unit form latency label in `score-analytics-utils.ts`

--- web/src/features/dashboard/lib/score-analytics-utils.ts
+++ web/src/features/dashboard/lib/score-analytics-utils.ts
@@ -12,7 +12,7 @@ export const RESOURCE_METRICS = [
     key: "latency",
     value: "Latency",
     objectKey: "avgLatency",
-    label: "Latency (s)",
+    label: "Latency",
     maxFractionDigits: 2,
   },
   {
