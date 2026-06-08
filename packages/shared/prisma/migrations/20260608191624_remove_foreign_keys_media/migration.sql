001c836d0125bd8a434382384e28ae1bea08d89c
Steffen Schmitz <steffenschmitz@hotmail.de>
2026-06-08 21:24:33 +0200

    chore: cleanup

--- packages/shared/prisma/migrations/20260608191624_remove_foreign_keys_media/migration.sql
+++ packages/shared/prisma/migrations/20260608191624_remove_foreign_keys_media/migration.sql
@@ -3,6 +3,3 @@ ALTER TABLE "observation_media" DROP CONSTRAINT "observation_media_project_id_fk
 
 -- DropForeignKey
 ALTER TABLE "trace_media" DROP CONSTRAINT "trace_media_project_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "observation_media" ADD CONSTRAINT "observation_media_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
