66777359b15441822f9791d4fdc6132961624511
Max Deichmann <m.deichmann@tum.de>
2026-06-08 21:27:37 +0200

    Update migration.sql

--- packages/shared/prisma/migrations/20260608191624_remove_foreign_keys_media/migration.sql
+++ packages/shared/prisma/migrations/20260608191624_remove_foreign_keys_media/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE "observation_media" DROP CONSTRAINT "observation_media_project_id_fkey";
+ALTER TABLE "observation_media" DROP CONSTRAINT IF EXISTS "observation_media_project_id_fkey";
 
 -- DropForeignKey
-ALTER TABLE "trace_media" DROP CONSTRAINT "trace_media_project_id_fkey";
+ALTER TABLE "trace_media" DROP CONSTRAINT IF EXISTS "trace_media_project_id_fkey";
