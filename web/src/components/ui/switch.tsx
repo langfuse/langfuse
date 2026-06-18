9b657255179b892e4c18d2342ee9f7f42e354ed4
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-06-18 15:44:57 +0200

    `Switch`: Make color variant explicit

--- web/src/components/ui/switch.tsx
+++ web/src/components/ui/switch.tsx
@@ -7,16 +7,21 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/src/utils/tailwind";
 
 const switchVariants = cva(
-  "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 disabled:cursor-not-allowed data-[state=checked]:bg-primary data-[state=unchecked]:bg-input dark:data-[state=unchecked]:bg-muted-foreground",
+  "peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 disabled:cursor-not-allowed data-[state=unchecked]:bg-input dark:data-[state=unchecked]:bg-muted-foreground",
   {
     variants: {
       size: {
         default: "h-5 w-9",
         sm: "h-4 w-7",
       },
+      color: {
+        default: "data-[state=checked]:bg-primary",
+        green: "data-[state=checked]:bg-dark-green",
+      },
     },
     defaultVariants: {
       size: "default",
+      color: "default",
     },
   },
 );
@@ -43,7 +48,7 @@ interface SwitchProps
       "className"
     >,
     VariantProps<typeof switchVariants> {
-  className?: "data-[state=checked]:bg-dark-green" | "scale-75";
+  className?: "scale-75";
 }
 
 const Switch = React.forwardRef<
