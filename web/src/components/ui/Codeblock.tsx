756f747207d4a7747c3c986320ef7c6130417761
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-07-23 18:37:53 +0200

    Remove unused programming languages map

--- web/src/components/ui/Codeblock.tsx
+++ web/src/components/ui/Codeblock.tsx
@@ -18,37 +18,6 @@ interface Props {
   variant?: keyof typeof headerVariantClasses;
 }
 
-interface languageMap {
-  [key: string]: string;
-}
-
-export const programmingLanguages: languageMap = {
-  javascript: ".js",
-  python: ".py",
-  java: ".java",
-  c: ".c",
-  cpp: ".cpp",
-  "c++": ".cpp",
-  "c#": ".cs",
-  ruby: ".rb",
-  php: ".php",
-  swift: ".swift",
-  "objective-c": ".m",
-  kotlin: ".kt",
-  typescript: ".ts",
-  go: ".go",
-  perl: ".pl",
-  rust: ".rs",
-  scala: ".scala",
-  haskell: ".hs",
-  lua: ".lua",
-  shell: ".sh",
-  sql: ".sql",
-  html: ".html",
-  css: ".css",
-  // add more file extensions here, make sure the key is same as language prop in CodeBlock.tsx component
-};
-
 const CodeBlock: FC<Props> = memo(
   ({ language, value, theme, variant = "default" }) => {
     const [isCopied, setIsCopied] = useState(false);
