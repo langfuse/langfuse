c5d9f38fb822594314c9ef4d102b4f82fa6a0b6a
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-07-24 09:54:43 +0200

    refactor(web): Cleanup `CodeBlock` component (#15381)
    * Freeze CodeBlock className values
    
    * Use default CodeBlock header color
    
    * Add CodeBlock header variant
    
    * Add CodeBlock stories
    
    * Remove unused programming languages map
    
    * Move Codeblock into design system
    
    * Address PR comments

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -10,6 +10,7 @@ import {
 
 const STORY_EXTENSIONS = "@(js|jsx|mjs|ts|tsx)";
 const DESIGN_COMPONENT_STORIES = [
+  "Codeblock/Codeblock",
   "LangfuseIcon/LangfuseIcon",
   "Progress/Progress",
   "Spinner/Spinner",
