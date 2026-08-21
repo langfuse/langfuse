10171dca61a2e173d6db6f0da7709e9ece32fcce
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-08-21 14:01:47 +0200

    Clean up code

--- web/src/features/organizations/utils/organizationNameSchema.ts
+++ web/src/features/organizations/utils/organizationNameSchema.ts
@@ -1,19 +1,16 @@
 import { StringNoHTML } from "@langfuse/shared";
 import * as z from "zod";
 
-const organizationName = StringNoHTML.min(
+export const organizationNameSchema = StringNoHTML.min(
   3,
   "Must have at least 3 characters",
 ).max(60, "Must have at most 60 characters");
 
-export const organizationNameSchema = z.object({
-  name: organizationName,
-});
-
-export const organizationFormSchema = organizationNameSchema.extend({
+export const organizationFormSchema = z.object({
+  name: organizationNameSchema,
   aiFeaturesEnabled: z.boolean(),
 });
 
 export const organizationOptionalNameSchema = z.object({
-  name: organizationName.optional(),
+  name: organizationNameSchema.optional(),
 });
