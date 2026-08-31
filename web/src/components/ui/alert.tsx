508a4744af54615e5d5685e58e3203b414220e5a
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-08-31 17:35:33 +0200

    Require Alert content

--- web/src/components/ui/alert.tsx
+++ web/src/components/ui/alert.tsx
@@ -4,6 +4,8 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/src/utils/tailwind";
 
+type AlertContent = Exclude<React.ReactNode, null | undefined | boolean>;
+
 const alertVariants = cva(
   "relative w-full rounded-lg border p-3 [&>svg~*]:pl-6 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-3 [&>svg]:top-3 [&>svg]:text-foreground",
   {
@@ -39,7 +41,7 @@ type AlertProps = Pick<
   VariantProps<typeof alertVariants>,
   "actionPosition" | "size" | "variant"
 > & {
-  children: React.ReactNode;
+  children: AlertContent;
 };
 type AlertTitleProps = {
   children: React.ReactNode;
