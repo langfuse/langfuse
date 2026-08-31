11b5713ded76416b9b34f50dbbc06e042eb016f1
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-08-31 17:46:26 +0200

    Fix CloudRegionSwitch

--- web/src/pages/auth/sign-up.tsx
+++ web/src/pages/auth/sign-up.tsx
@@ -476,7 +476,7 @@ function SignupPageShell({ children }: { children: React.ReactNode }) {
           </div>
         ) : null}
 
-        <CloudRegionSwitch isSignUpPage />
+        {isLangfuseCloud && <CloudRegionSwitch isSignUpPage />}
 
         <div className="bg-background mt-14 px-6 py-10 shadow-sm sm:mx-auto sm:w-full sm:max-w-[480px] sm:rounded-lg sm:px-10">
           {children}
