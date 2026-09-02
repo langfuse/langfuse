b7afd9abfe5030f9e97d94b71ae2f590321a4e7c
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-09-02 10:57:48 +0200

    Fix ScoreConfigDetails

--- web/src/features/score-configs/components/ScoreConfigDetails.tsx
+++ web/src/features/score-configs/components/ScoreConfigDetails.tsx
@@ -1,11 +1,8 @@
-/* eslint-disable @repo/no-null-render */
 import { isNumericDataType } from "@/src/features/scores/lib/helpers";
 import { isPresent, type ScoreConfigDomain } from "@langfuse/shared";
-import React from "react";
 
 export function ScoreConfigDetails({ config }: { config: ScoreConfigDomain }) {
   const { name, description, minValue, maxValue, dataType } = config;
-  if (!description && !isPresent(minValue) && !isPresent(maxValue)) return null;
   const isNameTruncated = name.length > 20;
 
   return (
