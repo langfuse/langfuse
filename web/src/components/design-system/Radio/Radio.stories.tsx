b2ed6435e263caa7a9836303befe0b355e6c0a3b
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-09-07 15:13:09 +0000

    refactor(web): Move `Radio` to design components (#17141)
    Move Radio to design components

--- web/src/components/design-system/Radio/Radio.stories.tsx
+++ web/src/components/design-system/Radio/Radio.stories.tsx
@@ -1,5 +1,5 @@
 import { fn } from "storybook/test";
-import preview from "../../../.storybook/preview";
+import preview from "../../../../.storybook/preview";
 import { Radio } from "./Radio";
 
 const meta = preview.meta({
