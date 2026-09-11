51d15b276674259e9724c0d410eabfc54a1fe158
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-09-11 14:04:42 +0200

    Fix: filtered-run-pills.tsx

--- web/src/features/datasets/components/DatasetCompareRunsTable.tsx
+++ web/src/features/datasets/components/DatasetCompareRunsTable.tsx
@@ -240,12 +240,14 @@ function DatasetCompareRunsTableInternal(props: {
           </DropdownMenu>
         }
       />
-      <FilteredRunPills
-        projectId={props.projectId}
-        datasetId={props.datasetId}
-        filteredRuns={activeRunFilters}
-        className="px-2 pb-2"
-      />
+      {hasActiveRunFilters && (
+        <FilteredRunPills
+          projectId={props.projectId}
+          datasetId={props.datasetId}
+          filteredRuns={activeRunFilters}
+          className="px-2 pb-2"
+        />
+      )}
       <DataTable
         tableName="datasetCompareRuns"
         columns={columns}
